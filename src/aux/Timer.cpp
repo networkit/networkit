f5fb56f52fdc03901ae52da711272331de446f07
cls <cls@localhost>
2012-11-28 14:36:09 +0100

    TODO: platform-agnostic timers

---
+++ src/aux/Timer.cpp
@@ -0,0 +1,21 @@
+/*
+ * Timer.cpp
+ *
+ *  Created on: 28.11.2012
+ *      Author: cls
+ */
+
+#include "Timer.h"
+
+namespace EnsembleClustering {
+
+Timer::Timer() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Timer::~Timer() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace EnsembleClustering */
