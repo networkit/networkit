954c3d6327b2c7f417a88d7a927f04697ac3687e
cls <cls@localhost>
2012-12-12 14:48:12 +0100

    switching from CppUnit to Google Test

--- src/EnsembleClustering.cpp
+++ src/EnsembleClustering.cpp
@@ -119,38 +119,31 @@ void configureLogging() {
 /**
  * Call this to run all cppunit unit tests.
  */
-int runUnitTestsCppUnit() {
-	// Informiert Test-Listener ueber Testresultate
-	CPPUNIT_NS::TestResult testresult;
+//int runUnitTestsCppUnit() {
+//	// Informiert Test-Listener ueber Testresultate
+//	CPPUNIT_NS::TestResult testresult;
+//
+//	// Listener zum Sammeln der Testergebnisse registrieren
+//	CPPUNIT_NS::TestResultCollector collectedresults;
+//	testresult.addListener(&collectedresults);
+//
+//	// Listener zur Ausgabe der Ergebnisse einzelner Tests
+//	CPPUNIT_NS::BriefTestProgressListener progress;
+//	testresult.addListener(&progress);
+//
+//	// Test-Suite ueber die Registry im Test-Runner einfuegen
+//	CPPUNIT_NS::TestRunner testrunner;
+//	testrunner.addTest(CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest());
+//	testrunner.run(testresult);
+//
+//	// Resultate im Compiler-Format ausgeben
+//	CPPUNIT_NS::CompilerOutputter compileroutputter(&collectedresults, std::cerr);
+//	compileroutputter.write();
+//
+//	// Rueckmeldung, ob Tests erfolgreich waren
+//	return collectedresults.wasSuccessful() ? 0 : 1;
+//}
 
-	// Listener zum Sammeln der Testergebnisse registrieren
-	CPPUNIT_NS::TestResultCollector collectedresults;
-	testresult.addListener(&collectedresults);
-
-	// Listener zur Ausgabe der Ergebnisse einzelner Tests
-	CPPUNIT_NS::BriefTestProgressListener progress;
-	testresult.addListener(&progress);
-
-	// Test-Suite ueber die Registry im Test-Runner einfuegen
-	CPPUNIT_NS::TestRunner testrunner;
-	testrunner.addTest(CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest());
-	testrunner.run(testresult);
-
-	// Resultate im Compiler-Format ausgeben
-	CPPUNIT_NS::CompilerOutputter compileroutputter(&collectedresults, std::cerr);
-	compileroutputter.write();
-
-	// Rueckmeldung, ob Tests erfolgreich waren
-	return collectedresults.wasSuccessful() ? 0 : 1;
-}
-
-/**
- * Call this to run all GoogleTest unit tests
- */
-int runUnitTests(int argc, char **argv) {
-::testing::InitGoogleTest(&argc, argv);
- return RUN_ALL_TESTS();
-}
 
 
 
@@ -188,6 +181,5 @@ int main(int argc, char **argv) {
 	 ::testing::InitGoogleTest(&argc, argv);
 	 return RUN_ALL_TESTS();
 
-
-	return 0;
+	 return 0;
 }
