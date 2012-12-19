79fb334ebec43dbfece5d3f1edfa9828f321bf22
cls <cls@localhost>
2012-12-19 16:02:15 +0100

    LabelPropagation works

--- src/clustering/test/ClusteringTest.h
+++ src/clustering/test/ClusteringTest.h
@@ -60,7 +60,7 @@ TEST_F(ClusteringTest, testModularity) {
 }
 
 
-TEST_F(ClusteringTest, testLabelPropagation) {
+TEST_F(ClusteringTest, testLabelPropagationOnUniformGraph) {
 	GraphGenerator graphGenerator;
 	int n = 100;
 	Graph G = graphGenerator.makeErdosRenyiGraph(n, 0.2);
@@ -75,6 +75,25 @@ TEST_F(ClusteringTest, testLabelPropagation) {
 	DEBUG("modularity produced by LabelPropagation: " << mod);
 	EXPECT_GE(1.0, mod) << "valid modularity values are in [-0.5, 1]";
 	EXPECT_LE(-0.5, mod) << "valid modularity values are in [-0.5, 1]";
+}
+
+
+TEST_F(ClusteringTest, testLabelPropagationOnClusteredGraph) {
+	GraphGenerator graphGenerator;
+	int n = 100;
+	int k = 3; // number of clusters
+	Graph G = graphGenerator.makeClusteredRandomGraph(n, k, 1.0, 0.001);
+
+	LabelPropagation lp;
+	Clustering zeta = lp.run(G);
+
+	Modularity modularity(G);
+	double mod = modularity.getQuality(zeta);
+	DEBUG("modularity produced by LabelPropagation: " << mod);
+
+	EXPECT_TRUE(zeta.isProper(G)) << "the resulting partition should be a proper clustering";
+	EXPECT_EQ(k, zeta.numberOfClusters()) << " " << k << " clusters are easy to detect";
+
 }
 
 
