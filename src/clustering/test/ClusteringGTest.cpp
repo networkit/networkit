8c3bb91ae1b88eb27b9fe4958a3e13d3e826cf6e
cls <cls@localhost>
2013-01-09 17:05:28 +0100

    renamed to ClusteringGTest

---
+++ src/clustering/test/ClusteringGTest.cpp
@@ -0,0 +1,12 @@
+/*
+ * ClusteringTest.cpp
+ *
+ *  Created on: 12.12.2012
+ *      Author: cls
+ */
+
+#include "ClusteringGTest.h"
+
+namespace EnsembleClustering {
+
+} /* namespace EnsembleClustering */
