4ab5cd417a6aeff1e58060058a594097ba548704
cls <christian.staudt@kit.edu>
2013-01-18 14:49:54 +0100

    included command line options parser

--- src/EnsembleClustering.cpp
+++ src/EnsembleClustering.cpp
@@ -8,18 +8,18 @@
 
 #include <iostream>
 #include <utility>
-#include <unordered_map>
+
 
 // log4cxx
 #include "log4cxx/logger.h"
 #include "log4cxx/basicconfigurator.h"
 
-
-
 // GoogleTest
 #include <gtest/gtest.h>
 
+
 // EnsembleClustering
+#include "aux/optionparser.h"
 #include "aux/Log.h"
 #include "aux/Noise.h"
 #include "graph/Graph.h"
@@ -31,16 +31,10 @@
 #include "graph/GraphGenerator.h"
 #include "clustering/base/Modularity.h"
 
-extern "C" {
-#include "stinger.h"
-}
-
 
 
 
 using namespace EnsembleClustering;
-
-
 
 
 /**
@@ -49,21 +43,105 @@ using namespace EnsembleClustering;
 void configureLogging() {
 	// configure logging
 	log4cxx::BasicConfigurator::configure();
-	log4cxx::Logger::getRootLogger()->setLevel(log4cxx::Level::getDebug());
+	log4cxx::Logger::getRootLogger()->setLevel(log4cxx::Level::getDebug());	// TODO: make debug level a command line parameter
+}
+
+
+// *** Option Parser Configuration ***//
+
+class Arg: public OptionParser::Arg {
+
+static OptionParser::ArgStatus Required(const OptionParser::Option& option, bool msg)
+{
+  if (option.arg != 0)
+    return OptionParser::ARG_OK;
+
+  if (msg) {
+	  ERROR("Option '" << option << "' requires an argument\n");
+  }
+  return OptionParser::ARG_ILLEGAL;
+}
+
+};
+
+
+enum  optionIndex { UNKNOWN, HELP, TESTS, GRAPH, ENSEMBLE_SIZE};
+const OptionParser::Descriptor usage[] =
+{
+ {UNKNOWN, 0,"" , ""    ,OptionParser::Arg::None, "USAGE: EnsembleClustering [options]\n\n"
+                                            "Options:" },
+ {HELP,    0,"h" , "help",OptionParser::Arg::None, "  --help  \t Print usage and exit." },
+ {TESTS, 0, "t", "tests", OptionParser::Arg::None, "  --tests \t Run unit tests"},
+ {GRAPH, 0, "g", "graph", OptionParser::Arg::Required, "  --graph \t Run ensemble clusterer on graph"},
+ {ENSEMBLE_SIZE, 0, "", "ensemble-size", OptionParser::Arg::Required, "  --ensemble-size \t number of clusterers in the ensemble"},
+ {UNKNOWN, 0,"" ,  ""   ,OptionParser::Arg::None, "\nExamples:\n"
+                                            " TODO" },
+ {0,0,0,0,0,0}
+};
+
+
+void startEnsembleClusterer(std::string graphPath, int ensembleSize) {
+	assert (ensembleSize > 0);
 }
 
 
 
-
 int main(int argc, char **argv) {
+	/// PARSE OPTIONS
 
-	std::cout << "running EnsembleClustering" << std::endl;
+	argc-=(argc>0); argv+=(argc>0); // skip program name argv[0] if present
+	OptionParser::Stats  stats(usage, argc, argv);
+	OptionParser::Option options[stats.options_max], buffer[stats.buffer_max];
+	OptionParser::Parser parse(usage, argc, argv, options, buffer);
 
+	if (parse.error())
+	 return 1;
+
+	if (options[HELP] || argc == 0) {
+	 OptionParser::printUsage(std::cout, usage);
+	 return 0;
+	}
+
+	for (OptionParser::Option* opt = options[UNKNOWN]; opt; opt = opt->next())
+	 std::cout << "Unknown option: " << opt->name << "\n";
+
+	for (int i = 0; i < parse.nonOptionsCount(); ++i)
+	 std::cout << "Non-option #" << i << ": " << parse.nonOption(i) << "\n";
+
+
+	// CONFIGURE LOGGING
 	configureLogging();
 
-	INFO("=== starting unit tests ===");
+	// RUN UNIT TESTS
 
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+	if (options[TESTS]) {
+
+	   ::testing::InitGoogleTest(&argc, argv);
+	   INFO("=== starting unit tests ===");
+	   return RUN_ALL_TESTS();
+	}
+
+	// RUN PROGRAM ON GRAPH INSTANCE
+
+	int ensembleSize = -1;
+	std::string graphPath = "NONE";
+
+	if (options[GRAPH]) {
+	   DEBUG("called with graph argument: " << options[GRAPH].arg);
+	}
+
+	if (options[ENSEMBLE_SIZE]) {
+	   ensembleSize = std::atoi(options[ENSEMBLE_SIZE].arg);
+	   DEBUG("called with ensemble size: " << ensembleSize);
+	}
+
+	if ((graphPath != "") || (ensembleSize > 0)) {
+	   INFO("starting EnsembleClusterer");
+
+	} else {
+	   ERROR("wrong options");
+	   return 1;
+	}
+
 
 }
