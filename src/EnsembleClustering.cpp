07a76052cca902184e1901dd6e213381e9a8b9e2
cls <cls@localhost>
2013-01-25 16:36:18 +0100

    redesigned command line options

--- src/EnsembleClustering.cpp
+++ src/EnsembleClustering.cpp
@@ -40,9 +40,10 @@
 using namespace EnsembleClustering;
 
 
+
 // graph functions
 
-Graph generateGraph(int64_t n, int64_t k, double pin, double pout, int ensembleSize) {
+Graph generateGraph(int64_t n, int64_t k, double pin, double pout) {
 
 	// prepare generated clustered random graph (planted partition)
 	std::cout << "[BEGIN] making random clustering..." << std::flush;
@@ -83,7 +84,7 @@ Graph readGraph(std::string graphPath) {
 }
 
 // start functions
-
+// FIXME: deprecated
 bool startSoloWithGraph(Graph& G, Clusterer* algo) {
 
 	// RUN BASE CLUSTERER SOLO
@@ -99,7 +100,7 @@ bool startSoloWithGraph(Graph& G, Clusterer* algo) {
 }
 
 
-
+// FIXME: deprecated
 bool startWithGraph(Graph& G, int ensembleSize) {
 
 	EnsembleClusterer ensemble;
@@ -135,6 +136,7 @@ bool startWithGraph(Graph& G, int ensembleSize) {
 		return true;
 }
 
+// FIXME: deprecated
 bool startWithPath(std::string graphPath, int ensembleSize) {
 	assert (ensembleSize > 0);
 	assert (! graphPath.empty());
@@ -159,7 +161,7 @@ bool startWithPath(std::string graphPath, int ensembleSize) {
 	return true;
 }
 
-
+// FIXME: deprecated
 bool startWithGenerated(int64_t n, int64_t k, double pin, double pout, int ensembleSize) {
 
 	// prepare generated clustered random graph (planted partition)
@@ -238,7 +240,7 @@ static OptionParser::ArgStatus Required(const OptionParser::Option& option, bool
 };
 
 
-enum  optionIndex { UNKNOWN, HELP, LOGLEVEL, SEQUENTIAL, TESTS, GRAPH, GENERATE, ENSEMBLE_SIZE, SOLO};
+enum  optionIndex { UNKNOWN, HELP, LOGLEVEL, SEQUENTIAL, TESTS, GRAPH, GENERATE, ENSEMBLE_SIZE, ENSEMBLE, SOLO};
 const OptionParser::Descriptor usage[] =
 {
  {UNKNOWN, 0,"" , ""    ,OptionParser::Arg::None, "USAGE: EnsembleClustering [options]\n\n"
@@ -250,6 +252,7 @@ const OptionParser::Descriptor usage[] =
  {GRAPH, 0, "g", "graph", OptionParser::Arg::Required, "  --graph \t Run ensemble clusterer on graph"},
  {GENERATE, 0, "", "generate", OptionParser::Arg::Required, "  --generate \t Run ensemble clusterer on generated graph with planted partition"},
  {ENSEMBLE_SIZE, 0, "", "ensemble-size", OptionParser::Arg::Required, "  --ensemble-size \t number of clusterers in the ensemble"},
+ {ENSEMBLE, 0, "", "ensemble", OptionParser::Arg::Required, "  --ensemble=<b> \t <b>: number of base clusterers in the ensemble"},	// TODO: provide more options
  {SOLO, 0, "", "solo", OptionParser::Arg::Required, "  --solo=<Algorithm> \t run only a single base algorithm"},
  {UNKNOWN, 0,"" ,  ""   ,OptionParser::Arg::None, "\nExamples:\n"
                                             " TODO" },
@@ -257,6 +260,111 @@ const OptionParser::Descriptor usage[] =
 };
 
 
+// MAIN FUNCTIONS
+
+
+Graph getGraph(OptionParser::Option* options) {
+
+	// generated graph
+	if (options[GENERATE]) {
+		// read n, k, pin, pout
+		std::string genOption = options[GENERATE].arg;
+		std::cout << "\t --generate=" << options[GENERATE].arg << std::endl;
+
+		std::vector<std::string> genArgs = splitAString(genOption, ',');
+		assert (genArgs.size() == 4);
+		int64_t n = std::atoi(genArgs.at(0).c_str());
+		int64_t k = std::atoi(genArgs.at(1).c_str());
+		double pin = std::atof(genArgs.at(2).c_str());
+		double pout = std::atof(genArgs.at(3).c_str());
+
+		Graph G = generateGraph(n, k, pin, pout);
+		return G;
+	} else if (options[GRAPH]) { // graph from file
+		std::string graphPath = options[GRAPH].arg;
+		std::cout << "\t --graph=" << graphPath << std::endl;
+
+		Graph G = readGraph(graphPath);
+		return G;
+	} else {
+		Graph G(0);	// return empty graph
+		G.setName("NONE");
+		return G;
+	}
+}
+
+bool startAlgo(Graph G, OptionParser::Option* options) {
+
+	// if getGraph returns empty graph, abort
+	if (G.isEmpty() && (G.getName() == "NONE")) {
+		std::cout << "[ERROR]Êno graph instance provided." << std::endl;
+		std::cout << "[EXIT]" << std::endl;
+		exit(1);
+	}
+
+
+	if (options[SOLO]) {
+		std::cout << "\t --solo=" << options[SOLO].arg << std::endl;
+		// RUN ONLY SINGLE BASE ALGORITHM
+
+		Clusterer* algo = NULL; // the clusterer
+
+		// get specified base algorithm
+		std::string algoName = options[SOLO].arg;
+		if (algoName == "LabelPropagation") {
+			algo = new LabelPropagation();
+		} else {
+			std::cout << "[ERROR] unknown base algorithm: " << algoName << std::endl;
+			std::cout << "[EXIT]" << std::endl;
+			exit(1);
+		}
+
+		// start solo base algorithm
+		std::cout << "[START] solo base clusterer: " << algoName << std::endl;
+
+		algo->run(G);
+
+		//
+		std::cout << "[DONE]" << std::endl;
+
+	} else if (options[ENSEMBLE]) {
+		// RUN ENSEMBLE
+
+		std::string ensembleOptions = options[GENERATE].arg;
+
+		int ensembleSize = std::atoi(ensembleOptions.c_str()); // TODO: provide more options
+
+		EnsembleClusterer ensemble;
+
+		// CONFIGURE ENSEMBLE CLUSTERER
+
+		// 1. Quality Measure
+		QualityMeasure* qm = new Modularity();
+		ensemble.setQualityMeasure(*qm);
+
+		// 2. Base Clusterers
+		for (int i = 0; i < ensembleSize; i += 1) {
+			Clusterer* base = new LabelPropagation();
+			ensemble.addBaseClusterer(*base);
+		}
+
+		// 3. Final Clusterer
+		Clusterer* final = new LabelPropagation();
+		ensemble.setFinalClusterer(*final);
+
+		// RUN ENSEMBLE CLUSTERER
+		Aux::Timer runtime;
+		runtime.start();
+
+		Clustering result = ensemble.run(G);
+
+		runtime.stop();
+		std::cout << "EnsembleClusterer runtime: " << runtime.elapsed().count() << " ms" << std::endl;
+
+
+	}
+
+}
 
 
 
@@ -309,71 +417,78 @@ int main(int argc, char **argv) {
 	   return RUN_ALL_TESTS();
 	}
 
-	// RUN PROGRAM ON GRAPH INSTANCE
-
-	int ensembleSize = -1;
-	std::string graphPath = "NONE";
-
-	if (options[GRAPH]) {
-		graphPath = options[GRAPH].arg;
-		std::cout << "\t --graph=" << graphPath << std::endl;
-
-		ensembleSize = std::atoi(options[ENSEMBLE_SIZE].arg);
-		std::cout << "\t --ensemble-size=" << ensembleSize << std::endl;
-
-		std::cout << "[START] EnsembleClustering --graph" << std::endl;
-
-		bool done = startWithPath(graphPath, ensembleSize);
-
-		if (done) {
-		   std::cout << "[FINISH] EnsembleClustering --graph" << std::endl;
-		}
-
-	}
-
-	if (options[GENERATE]) {
-		// read n, k, pin, pout
-		std::string genOption = options[GENERATE].arg;
-		std::cout << "\t --generate=" << options[GENERATE].arg << std::endl;
-
-		std::vector<std::string> genArgs = splitAString(genOption, ',');
-		assert (genArgs.size() == 4);
-		int n = std::atoi(genArgs.at(0).c_str());
-		int k = std::atoi(genArgs.at(1).c_str());
-		double pin = std::atof(genArgs.at(2).c_str());
-		double pout = std::atof(genArgs.at(3).c_str());
+	//
 
 
-		ensembleSize = std::atoi(options[ENSEMBLE_SIZE].arg);
-		std::cout << "\t --ensemble-size=" << ensembleSize << std::endl;
-
-		bool done = startWithGenerated(n, k, pin, pout, ensembleSize);
-
-		if (done) {
-		   std::cout << "[FINISH] EnsembleClustering --generate" << std::endl;
-		}
-	}
 
 
-	// RUN ONLY SINGLE BASE ALGORITHM
-	if (options[SOLO]) {
-		Clusterer* algo = NULL;
+	// RUN PROGRAM
 
-		std::string algoName = options[SOLO].arg;
-		if (algoName == "LabelPropagation") {
-			algo = new LabelPropagation();
-		} else {
-			std::cout << "[ERROR] unknown base algorithm: " << algoName << std::endl;
-			std::cout << "[EXIT]" << std::endl;
-			exit(1);
-		}
+	startAlgo(getGraph(options), options);
 
-		if (options[GRAPH]) {
-			graphPath = options[GRAPH].arg;
-			// TODO: start solo with graph
-		}
 
-	}
+//	int ensembleSize = -1;
+//	std::string graphPath = "NONE";
+//
+//	if (options[GRAPH]) {
+//		graphPath = options[GRAPH].arg;
+//		std::cout << "\t --graph=" << graphPath << std::endl;
+//
+//		ensembleSize = std::atoi(options[ENSEMBLE_SIZE].arg);
+//		std::cout << "\t --ensemble-size=" << ensembleSize << std::endl;
+//
+//		std::cout << "[START] EnsembleClustering --graph" << std::endl;
+//
+//		bool done = startWithPath(graphPath, ensembleSize);
+//
+//		if (done) {
+//		   std::cout << "[FINISH] EnsembleClustering --graph" << std::endl;
+//		}
+//	}
+//
+//	if (options[GENERATE]) {
+//		// read n, k, pin, pout
+//		std::string genOption = options[GENERATE].arg;
+//		std::cout << "\t --generate=" << options[GENERATE].arg << std::endl;
+//
+//		std::vector<std::string> genArgs = splitAString(genOption, ',');
+//		assert (genArgs.size() == 4);
+//		int n = std::atoi(genArgs.at(0).c_str());
+//		int k = std::atoi(genArgs.at(1).c_str());
+//		double pin = std::atof(genArgs.at(2).c_str());
+//		double pout = std::atof(genArgs.at(3).c_str());
+//
+//
+//		ensembleSize = std::atoi(options[ENSEMBLE_SIZE].arg);
+//		std::cout << "\t --ensemble-size=" << ensembleSize << std::endl;
+//
+//		bool done = startWithGenerated(n, k, pin, pout, ensembleSize);
+//
+//		if (done) {
+//		   std::cout << "[FINISH] EnsembleClustering --generate" << std::endl;
+//		}
+//	}
+//
+//
+//	// RUN ONLY SINGLE BASE ALGORITHM
+//	if (options[SOLO]) {
+//		Clusterer* algo = NULL;
+//
+//		std::string algoName = options[SOLO].arg;
+//		if (algoName == "LabelPropagation") {
+//			algo = new LabelPropagation();
+//		} else {
+//			std::cout << "[ERROR] unknown base algorithm: " << algoName << std::endl;
+//			std::cout << "[EXIT]" << std::endl;
+//			exit(1);
+//		}
+//
+//		if (options[GRAPH]) {
+//			graphPath = options[GRAPH].arg;
+//			// TODO: start solo with graph
+//		}
+//
+//	}
 
 
 }
