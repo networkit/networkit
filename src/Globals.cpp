0c63956212dec8ad4d13ba8c2144d9fb4301b41c
cls <cls@localhost>
2013-02-13 16:13:08 +0100

    RAND_ORDER is false by default

--- src/Globals.cpp
+++ src/Globals.cpp
@@ -6,5 +6,5 @@
  */
 
 bool PRINT_PROGRESS = true;
-bool RAND_ORDER = true;
+bool RAND_ORDER = false; 	// don't randomize LabelPropagation node order by default, since this seems to be faster and slightly better
 
