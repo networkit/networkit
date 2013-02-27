1d9298d383d1e66fa2d566bf8e3dbfe752498171
cls <cls@localhost>
2013-02-27 12:21:28 +0100

    implemented Luby's independent set algorithm

---
+++ src/independentset/IndependentSetFinder.cpp
@@ -0,0 +1,25 @@
+/*
+ * IndependentSetFinder.cpp
+ *
+ *  Created on: 27.02.2013
+ *      Author: cls
+ */
+
+#include "IndependentSetFinder.h"
+
+namespace EnsembleClustering {
+
+IndependentSetFinder::IndependentSetFinder() {
+	// TODO Auto-generated constructor stub
+
+}
+
+IndependentSetFinder::~IndependentSetFinder() {
+	// TODO Auto-generated destructor stub
+}
+
+std::string IndependentSetFinder::toString() const {
+	return "TODO: implement IndependentSetFinder.toString";
+}
+
+} /* namespace EnsembleClustering */
