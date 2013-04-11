410c46a61709a9a2abdd3b8bd7bfe30c51a9d304
Henning Meyerhenke <meyerhenke@kit.edu>
2013-04-11 11:01:10 +0200

    some cleanup, added PubWebGenerator

---
+++ src/generators/PubWebGenerator.h
@@ -0,0 +1,37 @@
+/*
+ * PubWebGenerator.h
+ *
+ *  Created on: Apr 10, 2013
+ *      Author: Henning
+ */
+
+#ifndef PUBWEBGENERATOR_H_
+#define PUBWEBGENERATOR_H_
+
+#include "StaticGraphGenerator.h"
+#include "../aux/RandomProbability.h"
+#include <cmath>
+
+
+namespace NetworKit {
+
+class PubWebGenerator: public NetworKit::StaticGraphGenerator {
+protected:
+	count n; //!< number of nodes
+	count numDenseAreas; //!< number of areas with more nodes (denser)
+	float neighRad; //!< neighborhood radius
+	count maxNeigh; //!< maximum number of neighbors
+
+	void determineNeighbors(Graph& g);
+	void moveNodeIntoUnitSquare(float& x, float& y);
+	float squaredDistanceInUnitTorus(float x1, float y1, float x2, float y2);
+
+public:
+	PubWebGenerator(count numNodes, count numberOfDenseAreas, float neighborhoodRadius, count maxNumberOfNeighbors);
+	virtual ~PubWebGenerator();
+
+	virtual Graph generate();
+};
+
+} /* namespace NetworKit */
+#endif /* PUBWEBGENERATOR_H_ */
