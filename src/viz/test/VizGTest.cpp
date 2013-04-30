6e76256186532a3c9d9ebfde060e68aed3c3cd71
Henning Meyerhenke <meyerhenke@kit.edu>
2013-04-30 18:29:17 +0200

    force-directed layout runs to the end, but results are still not good, many open issues remain

--- src/viz/test/VizGTest.cpp
+++ src/viz/test/VizGTest.cpp
@@ -25,13 +25,13 @@ TEST_F(VizGTest, testPostscriptWriter) {
 	count n = 60;
 	GraphGenerator graphGen;
 	Graph G = graphGen.makeRandomGraph(n, 0.2);
+	G.initCoordinates();
 
 	// create clustering
 	ClusteringGenerator clusteringGen;
 	Clustering zeta = clusteringGen.makeRandomClustering(G, 3);
 
 	// create coordinates
-	count dimensions = 2;
 	G.forNodes([&](node u) {
 		float x = (float) drand48();
 		float y = (float) drand48();
@@ -43,6 +43,40 @@ TEST_F(VizGTest, testPostscriptWriter) {
 	PostscriptWriter psWriter(G);
 	psWriter.write(zeta, "testGraph.eps");
 }
+
+
+TEST_F(VizGTest, testForceDirectedLayouter) {
+	// create graph
+	count n = 50;
+	GraphGenerator graphGen;
+	Graph G = graphGen.makeRandomGraph(n, 0.2);
+	G.initCoordinates();
+
+	// create coordinates
+	G.forNodes([&](node u) {
+		float x = (float) drand48();
+		float y = (float) drand48();
+		G.setCoordinate(u, 0, x);
+		G.setCoordinate(u, 1, y);
+	});
+
+	// create clustering
+	ClusteringGenerator clusteringGen;
+	Clustering zeta = clusteringGen.makeRandomClustering(G, 3);
+
+	// draw (independent of clustering)
+	std::vector<float> bottomLeft = {0.0, 0.0};
+	std::vector<float> topRight = {1000.0, 1000.0};
+	Point<float> bl(bottomLeft);
+	Point<float> tr(topRight);
+	ForceDirected layouter(bl, tr);
+	layouter.draw(G);
+
+	// write graph to file
+	PostscriptWriter psWriter(G);
+	psWriter.write(zeta, "testGraph.eps");
+}
+
 
 
 
