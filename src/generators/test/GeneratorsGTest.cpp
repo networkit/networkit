a3ed3686435346582533bce4f2b13e63f46d8fcf
cls <cls@localhost>
2013-05-01 17:22:38 +0200

    working on degree distribution and ground truth for BTERGenerator

--- src/generators/test/GeneratorsGTest.cpp
+++ src/generators/test/GeneratorsGTest.cpp
@@ -82,8 +82,25 @@ TEST_F(GeneratorsGTest, testStaticPubWebGenerator) {
 
 
 TEST_F(GeneratorsGTest, testBTERGenerator) {
-	std::vector<count> degreeDistribution { 0, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-	std::vector<double> clusteringCoefficients {0.0, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5 };
+	std::vector<count> degreeDistribution { 0, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+	std::vector<double> clusteringCoefficients {0.0, 0.1, 0.1, 0.1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5 };
+	DEBUG("construct BTERGenerator");
+	BTERGenerator bter(degreeDistribution, clusteringCoefficients, 1.0);
+	std::pair<count, count> nm = bter.desiredGraphSize();
+	DEBUG("call BTERGenerator");
+	Graph G = bter.generate();
+
+	EXPECT_EQ(nm.first, G.numberOfNodes());
+	EXPECT_EQ(nm.second, G.numberOfEdges());
+
+	METISGraphWriter writer;
+	writer.write(G, "output/BTERTest.graph");
+}
+
+
+TEST_F(GeneratorsGTest, testBTERGeneratorWithPowerLawDistribution) {
+	std::vector<double> clusteringCoefficients {0.0, 0.1, 0.1, 0.1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5 };
+	std::vector<count> degreeDistribution = BTERGenerator::generatePowerLawDegreeDistribution(14, 0.5);
 	DEBUG("construct BTERGenerator");
 	BTERGenerator bter(degreeDistribution, clusteringCoefficients, 1.0);
 	std::pair<count, count> nm = bter.desiredGraphSize();
