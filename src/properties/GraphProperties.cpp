ae995d912190886895eae58123290e477230660e
cls <cls@localhost>
2013-06-03 16:26:06 +0200

    stubs for Properties.

---
+++ src/properties/GraphProperties.cpp
@@ -0,0 +1,28 @@
+/*
+ * GraphProperties.cpp
+ *
+ *  Created on: 03.06.2013
+ *      Author: cls
+ */
+
+#include "GraphProperties.h"
+
+namespace NetworKit {
+
+GraphProperties::GraphProperties() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GraphProperties::~GraphProperties() {
+	// TODO Auto-generated destructor stub
+}
+
+std::vector<count> GraphProperties::degreeDistribution(Graph& G) {
+}
+
+std::vector<double> GraphProperties::localClusteringCoefficientPerDegree(
+		Graph& G) {
+}
+
+} /* namespace NetworKit */
