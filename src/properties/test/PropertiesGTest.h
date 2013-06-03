214b52e3ce6364b2557ed1ed1a7be157ecc26248
cls <cls@localhost>
2013-06-03 16:17:47 +0200

    moved ClusteringCoefficient to properties/

---
+++ src/properties/test/PropertiesGTest.h
@@ -0,0 +1,25 @@
+/*
+ * PropertiesGTest.h
+ *
+ *  Created on: 03.06.2013
+ *      Author: cls
+ */
+
+#ifndef PROPERTIESGTEST_H_
+#define PROPERTIESGTEST_H_
+
+#include <gtest/gtest.h>
+
+#include "../ClusteringCoefficient.h"
+#include "../../graph/GraphGenerator.h"
+
+namespace NetworKit {
+
+class PropertiesGTest: public testing::Test {
+public:
+	PropertiesGTest();
+	virtual ~PropertiesGTest();
+};
+
+} /* namespace NetworKit */
+#endif /* PROPERTIESGTEST_H_ */
