eb6e2dc262c5d65b7ed5957c2bee0b14021c32b1
cls <cls@localhost>
2013-06-06 11:42:15 +0200

    unit tests for SCD

---
+++ src/scd/test/SCDGTest.cpp
@@ -0,0 +1,17 @@
+/*
+ * SCDGTest.cpp
+ *
+ *  Created on: 06.06.2013
+ *      Author: cls
+ */
+
+#include "SCDGTest.h"
+
+namespace NetworKit {
+
+
+TEST_F(SCDGTest, testGreedyCommunityExpansion) {
+	// TODO: unit test for GreedyCommunityExpansion
+}
+
+} /* namespace NetworKit */
