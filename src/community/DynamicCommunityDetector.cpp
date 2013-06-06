e5c1ec7fa842a02a7a69d56ba7e30e86c1dc5457
cls <cls@localhost>
2013-06-06 11:43:18 +0200

    renamed DynamicClusterer to DynamicCommunityDetector

---
+++ src/community/DynamicCommunityDetector.cpp
@@ -0,0 +1,20 @@
+/*
+ * DynamicCommunityDetector.cpp
+ *
+ *  Created on: 10.04.2013
+ *      Author: cls
+ */
+
+#include "DynamicCommunityDetector.h"
+
+namespace NetworKit {
+
+DynamicCommunityDetector::DynamicCommunityDetector(Graph& G) {
+	this->G = &G;
+}
+
+DynamicCommunityDetector::~DynamicCommunityDetector() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace NetworKit */
