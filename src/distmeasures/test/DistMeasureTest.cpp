df73fc9d54b0099e32e77aaaf6a23e1dd41f03fe
Henning Meyerhenke <meyerhenke@kit.edu>
2013-06-11 18:36:01 +0200

    AD test seems plausible, mod score for 2-clustering is reasonable

--- src/distmeasures/test/DistMeasureTest.cpp
+++ src/distmeasures/test/DistMeasureTest.cpp
@@ -31,23 +31,12 @@ TEST_F(DistMeasureTest, tryAlgebraicDistances) {
 	AlgebraicDistances ad(g);
 	ad.preprocess(numSystems, numIterations, omega);
 
-#if 0
-	// print pairwise distances for a subset of vertices
-	for (index i = 0; i < 20; ++i) {
-		for (index j = 0; j < i; ++j) {
-			printf("%.4f  ", ad.algdist(i, j, norm));
-		}
-		std::cout << std::endl;
-	}
-#endif
-
-	// write color-coded clustering according to AD
+	// *** produce 2-clustering according to AD ***/
 
 	// determine two vertices with highest distance
 	index best0 = 0;
 	index best1 = 0;
 	double maxDist = ad.algdist(best0, best1, norm);
-
 	for (index i = 0; i < n; ++i) {
 		for (index j = 0; j < i; ++j) {
 			double dist = ad.algdist(i, j , norm);
@@ -69,9 +58,7 @@ TEST_F(DistMeasureTest, tryAlgebraicDistances) {
 	});
 
 	Modularity modScore;
-
 	DEBUG("Finished with assignment to clusters, mod score: " << modScore.getQuality(clustering, g));
-
 
 //	PostscriptWriter psWriter(g);
 //	psWriter.write(clustering, "output/airfoil1-ad-test.eps");
