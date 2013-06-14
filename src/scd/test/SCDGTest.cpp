0abd723e05850cc7132a5b0507f5e3a68f7cf709
Yassine Marrakchi <uidkr@student.kit.edu>
2013-06-14 15:43:43 +0200

    merge

--- src/scd/test/SCDGTest.cpp
+++ src/scd/test/SCDGTest.cpp
@@ -62,52 +62,6 @@ TEST_F(SCDGTest, testConductance) {
 
 }
 
-TEST_F(SCDGTest, testNodeClusterSimilarity) {
-
-	Graph G(5);
-	G.addEdge(0,0);
-	G.addEdge(0,1);
-	G.addEdge(0,2);
-	G.addEdge(0,3);
-	G.addEdge(1,2);
-	G.addEdge(1,4);
-	G.addEdge(2,3);
-	G.addEdge(2,4);
-	G.addEdge(3,4);
-
-	std::unordered_set<node> first, second, third, fourth, fifth;
-	first = {};
-	GreedyCommunityExpansion::Conductance conductance1(G, first);
-	second = {0};
-	GreedyCommunityExpansion::Conductance conductance2(G, second);
-	third = {0,1};
-	GreedyCommunityExpansion::Conductance conductance3(G, third);
-	fourth = {0,1,2};
-	GreedyCommunityExpansion::Conductance conductance4(G, fourth);
-	fifth = {0,1,2,3};
-	GreedyCommunityExpansion::Conductance conductance5(G, fifth);
-
-	double condOne = conductance1.getValue(0);
-	double condTwo = conductance2.getValue(1);
-	double condThree = conductance3.getValue(2);
-	double condFour = conductance4.getValue(3);
-	double condFive = conductance5.getValue(4);
-
-
-	EXPECT_EQ(0.75, 1-condOne) << "1-clustering should have conductance of 0.75";
-
-	EXPECT_GE(0.571429, 1-condTwo) << "2-clustering should have conductance of 4/7";
-	EXPECT_LE(0.571428, 1-condTwo) << "2-clustering should have conductance of 4/7";
-
-	EXPECT_GE(0.666667, condThree) << "3-clustering should have conductance of 2/3";
-	EXPECT_LE(0.666666, condThree) << "3-clustering should have conductance of 2/3";
-
-	EXPECT_EQ(1, condFour) << "4-clustering should have conductance of 1";
-
-	EXPECT_EQ(1, condFive) << "5-clustering should have conductance of 1";
-
-}
-
 
 TEST_F(SCDGTest, tryCommunitySubgraph) {
 	GraphGenerator gen;
@@ -119,15 +73,10 @@ TEST_F(SCDGTest, tryCommunitySubgraph) {
 	std::unordered_set<node> community = GCE.run(G, s);
 	DEBUG("Community detection: DONE");
 
-	DEBUG("Community size: " << community.size());
-
-
 	// get the subgraph of the community
 
 	Graph sub = Subgraph::fromNodes(G,community);
 	DEBUG("Subgraph partitioning: DONE");
-
-	DEBUG("Subgraph number of nodes: " << sub.numberOfNodes());
 
 	// write it to file
 	METISGraphWriter writer;
