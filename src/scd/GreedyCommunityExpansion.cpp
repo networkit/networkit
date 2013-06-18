788d0219abb9fa09033985197a0075f22fe81753
Yassine Marrakchi <uidkr@student.kit.edu>
2013-06-18 02:38:23 +0200

    restructuration of selective community detection

--- src/scd/GreedyCommunityExpansion.cpp
+++ src/scd/GreedyCommunityExpansion.cpp
@@ -28,8 +28,8 @@ std::unordered_set<node> GreedyCommunityExpansion::run(Graph& G, node s) {
 	std::map<node, double> acceptanceValues;
 
 	// TODO: make these selectable later
-	GreedyCommunityExpansion::DummySimilarity acceptability(G, community, shell);
-	GreedyCommunityExpansion::Conductance conductance(G, community);
+	DummySimilarity acceptability(G, community, shell);
+	Conductance conductance(G, community);
 
 	double currentObjectiveValue = conductance.getValue(s);
 
@@ -116,144 +116,6 @@ std::unordered_set<node> GreedyCommunityExpansion::run(Graph& G, node s) {
 	trimm.run(community, G);
 
 	return community;
-}
-
-
-GreedyCommunityExpansion::QualityObjective::QualityObjective(
-	Graph& G, std::unordered_set<node>& community) {
-	this->G = &G;
-	this->community = &community;
-}
-
-GreedyCommunityExpansion::LocalModularityM::LocalModularityM(Graph& G, std::unordered_set<node>& community)
-	: QualityObjective(G, community){
-}
-
-GreedyCommunityExpansion::LocalModularityM::~LocalModularityM() {
-}
-
-double GreedyCommunityExpansion::LocalModularityM::getValue(node v) {
-
-	double inside = 0;
-	double outside = 0;
-	bool modified = false;
-	if (community->find(v) == community->end()) {
-		modified = true;
-	}
-	community->insert(v);
-	for (node u : (*community)) {
-		this->G->forNeighborsOf(u, [&](node x){
-			if (community->find(x) == community->end()){
-				outside ++;
-			} else {
-				if (u == x) {
-					inside++;
-				} else {
-					inside = inside + 0.5;
-				}
-			}
-		});
-	}
-
-	if (modified == true) {
-		community->erase(v);
-	}
-
-	if (outside == 0) {
-		return G->numberOfEdges();
-	}
-	return inside / outside;
-}
-
-GreedyCommunityExpansion::Acceptability::Acceptability(
-	Graph& G, std::unordered_set<node>& community, std::unordered_set<node>& shell) {
-	this->G = &G;
-	this->community = &community;
-	this->shell = &shell;
-}
-
-
-GreedyCommunityExpansion::NodeClusterSimilarity::NodeClusterSimilarity(
-	Graph& G, std::unordered_set<node>& community, std::unordered_set<node>& shell): Acceptability(G, community, shell) {
-}
-
-GreedyCommunityExpansion::NodeClusterSimilarity::~NodeClusterSimilarity() {
-}
-
-double GreedyCommunityExpansion::NodeClusterSimilarity::getValue(node v) {
-
-	double intersection = 0;
-	this->G->forNeighborsOf(v, [&](node u) {
-
-		if (this->community->find(u) != this->community->end()||this->shell->find(u) != this->shell->end()) {
-			intersection++;
-		}
-	});
-
-	if (this->shell->find(v) != this->shell->end()) {
-		intersection++;
-	}
-
-	if (G->hasEdge(v, v)) {
-		return (intersection - 1) / (G->degree(v) + community->size() + shell->size() - intersection  + 1);
-	}
-
-	return intersection / (G->degree(v) + 1 + community->size() + shell->size() - intersection);
-}
-
-GreedyCommunityExpansion::QualityObjective::~QualityObjective() {
-}
-
-GreedyCommunityExpansion::Acceptability::~Acceptability() {
-}
-
-GreedyCommunityExpansion::Conductance::Conductance(Graph& G, std::unordered_set<node>& community)
-	: QualityObjective(G, community){
-}
-
-GreedyCommunityExpansion::Conductance::~Conductance() {
-}
-
-double GreedyCommunityExpansion::Conductance::getValue(node v) {
-	double volume = 0;
-	double boundary = 0;
-	double all = 0;
-	bool modified = false;
-	if (community->find(v) == community->end()) {
-		modified = true;
-	}
-	community->insert(v);
-
-	for (node u : (*community)) {
-		volume = volume + this->G->degree(u);
-		this->G->forNeighborsOf(u, [&](node v){
-			if (community->find(v) == community->end()) {
-				boundary++;
-			}
-		});
-	}
-
-	G->forNodes([&](node v){
-		all = all + G->degree(v);
-	});
-
-	if (modified == true) {
-		community->erase(v);
-	}
-	if (volume == 0 || all-volume == 0)
-		return 0;
-	return 1 - (boundary / std::min(volume, all-volume));
-}
-
-GreedyCommunityExpansion::DummySimilarity::DummySimilarity(
-		Graph& G, std::unordered_set<node>& community, std::unordered_set<node>& shell): Acceptability(G, community, shell) {
-}
-
-GreedyCommunityExpansion::DummySimilarity::~DummySimilarity() {
-}
-
-double GreedyCommunityExpansion::DummySimilarity::getValue(node v) {
-	return 0.5;
 }
 
 std::map<node, std::unordered_set<node> > GreedyCommunityExpansion::seedSetExpansion(
