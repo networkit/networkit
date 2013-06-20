0f76b55fda298e04e1007d47da6c8d92c3bcfcf2
Yassine Marrakchi <uidkr@student.kit.edu>
2013-06-20 14:23:33 +0200

    GCE for a seed set of nodeGCE for a seed set of nodes

--- src/scd/SelectiveSCAN.cpp
+++ src/scd/SelectiveSCAN.cpp
@@ -9,8 +9,7 @@
 
 namespace NetworKit {
 
-SelectiveSCAN::SelectiveSCAN() {
-	// TODO Auto-generated constructor stub
+SelectiveSCAN::SelectiveSCAN(): epsilon(0.7), nhu(3) {
 
 }
 
@@ -18,7 +17,64 @@ SelectiveSCAN::~SelectiveSCAN() {
 	// TODO Auto-generated destructor stub
 }
 
-std::unordered_set<node> SelectiveSCAN::run(Graph& G, node seed) {
+std::unordered_map<node, std::unordered_set<node>> SelectiveSCAN::seedSetExpansion(
+			Graph& G, std::vector<node> set){
+
+	std::unordered_map<node, node> nodesState;
+	std::unordered_map<node, std::unordered_set<node>> communitites;
+	SelectiveSCAN SSCAN;
+
+	for (node u : set) {
+		nodesState.insert(std::pair<node,int>(u, -1));
+	}
+	for (node u : set) {
+		if ((nodesState.find(u))->second == -1  && SSCAN.isCore(u, G).first){
+
+		}
+	}
+	return communitites;
+}
+
+double SelectiveSCAN::nodeNodeSimilarity (node u, node v, Graph& G) {
+
+	int inter = 0;
+	int uni = 0;
+	G.forNeighborsOf(u, [&](node x){
+		if (x != v ) {
+			if (G.hasEdge(x, v)) {
+				inter++;
+				uni++;
+			} else {
+				uni++;
+			}
+		}
+	});
+	G.forNeighborsOf(v, [&](node x){
+		if (x != u ) {
+			if (!G.hasEdge(x, u)) {
+				uni++;
+			}
+		}
+	});
+	return (double) (inter + 2)/ (double) (uni +2);
+}
+
+std::pair<bool,std::vector<node>> SelectiveSCAN::isCore(node u, Graph& G) {
+
+	bool core = false;
+	SelectiveSCAN SSCAN;
+	std::vector<node> similarNeighbors;
+	int count = 0;
+	G.forNeighborsOf(u, [&](node v){
+		if (SSCAN.nodeNodeSimilarity(u, v, G) >= SSCAN.epsilon) {
+			count++;
+			similarNeighbors.push_back(v);
+		}
+	});
+	if (count >= SSCAN.nhu) {
+		core = true;
+	}
+	return std::pair<bool,std::vector<node>>(core, similarNeighbors);
 }
 
 } /* namespace NetworKit */
