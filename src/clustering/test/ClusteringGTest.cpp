d35a3209fbdc163c8446d746a9c4c23a492dcb12
cls <cls@localhost>
2013-06-20 11:51:38 +0200

    using NumericTools for testing modularity

--- src/clustering/test/ClusteringGTest.cpp
+++ src/clustering/test/ClusteringGTest.cpp
@@ -180,7 +180,8 @@ TEST_F(ClusteringGTest, testModularityParallelVsSequential) {
 	INFO("calculating modularity sequentially");
 	double modSeq = modularitySeq.getQuality(zeta, G);
 
-	EXPECT_EQ(modPar, modSeq) << "Modularity values should be equal no matter if calculated in parallel or sequentially";
+	// EXPECT_EQ(modPar, modSeq) << "Modularity values should be equal no matter if calculated in parallel or sequentially";
+	EXPECT_TRUE(NumericTools::equal(modPar, modSeq, 1e-12)) << "Modularity values should be equal within a small error no matter if calculated in parallel or sequentially";
 
 }
 
