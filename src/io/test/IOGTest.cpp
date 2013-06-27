cbbca3b5c2ef28ffe32b7a3368c90b4db460d7b7
Yassine Marrakchi <uidkr@student.kit.edu>
2013-06-27 14:27:31 +0200

    test set up

--- src/io/test/IOGTest.cpp
+++ src/io/test/IOGTest.cpp
@@ -103,25 +103,7 @@ TEST_F(IOGTest, testMETISGraphWriter) {
 	EXPECT_TRUE(exists) << "A file should have been created : " << path;
 
 }
-TEST_F(IOGTest, testMETISGraphReaderAndWriter) {
-	std::string path = "input/dolphins.graph";
 
-	METISGraphReader reader;
-	Graph G = reader.read(path);
-	std::unordered_set<node> cluster = {0,6,18,26,40,53,21,24,25,30,12,31,55,59,4,5,8,56,48,16};
-	for(int i = 0; i < 62; i++){
-
-		G.forNeighborsOf(i, [&](node v){
-			if (cluster.find(i) != cluster.end() || cluster.find(v) != cluster.end()) {
-				G.removeEdge(i,v);
-			}
-		});
-	}
-
-	std::string path1 = "output/dolphins.graph";
-	METISGraphWriter writer;
-	writer.write(G, false, path1);
-}
 
 TEST_F(IOGTest, testMETISGraphWriterWithWeights) {
 	std::string path = "input/jazz2.graph";
@@ -260,7 +242,7 @@ TEST_F(IOGTest, testMETISGraphReaderForNodeExistence2) {
 }
 
 
-TEST_F(IOGTest, tryMETISGraphReaderWithIsolatedNodes) {
+TEST_F(IOGTest, testMETISGraphReaderWithIsolatedNodes) {
 	METISGraphReader reader;
 	Graph G = reader.read("input/example.graph");
 	EXPECT_EQ(4, G.numberOfNodes());
