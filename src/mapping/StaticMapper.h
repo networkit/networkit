449f45a2c7bca719adb05ef4b494206536e88bb0
Henning Meyerhenke <meyerhenke@kit.edu>
2013-07-02 10:46:01 +0200

    interfaces for mapping heuristics

---
+++ src/mapping/StaticMapper.h
@@ -0,0 +1,25 @@
+/*
+ * StaticMapper.h
+ *
+ *  Created on: Jul 2, 2013
+ *      Author: Henning
+ */
+
+#ifndef STATICMAPPER_H_
+#define STATICMAPPER_H_
+
+#include <map>
+#include "../graph/Graph.h"
+
+namespace NetworKit {
+
+class StaticMapper {
+public:
+	StaticMapper();
+	virtual ~StaticMapper();
+
+	virtual std::map<index, index> run(Graph& guest, Graph& host) = 0;
+};
+
+} /* namespace NetworKit */
+#endif /* STATICMAPPER_H_ */
