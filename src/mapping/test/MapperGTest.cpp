e97b6abe876517bd604cae51e8556efed4396573
Henning Meyerhenke <meyerhenke@kit.edu>
2013-07-17 16:48:52 +0200

    mapping test

--- src/mapping/test/MapperGTest.cpp
+++ src/mapping/test/MapperGTest.cpp
@@ -26,7 +26,7 @@ TEST_F(MapperGTest, tryRcmMapping) {
 	// read application graph
 	METISGraphReader graphReader;
 	Graph appGraph = graphReader.read("input/wing.graph");
-	count k = 512;
+	count k = 25;
 
 	// generate or read clustering/partition
 	BalancedLabelPropagation partitioner(1.75);
@@ -35,23 +35,23 @@ TEST_F(MapperGTest, tryRcmMapping) {
 //	Clustering partition = clusteringGenerator.makeContinuousBalancedClustering(appGraph, k);
 
 	// read host (processor) graph
-	Graph host = graphReader.read("input/mapping/grid-8x8x8-dist-arch.graph");
+	Graph host = graphReader.read("input/mapping/grid-5x5-dist-arch.graph");
 
 	// compute communication graph
 	Graph commGraph = partition.communicationGraph(appGraph);
 
 	// evaluate trivial mapping
-	RcmMapperWW mapper;
+	RcmMapper mapper;
 	Mapping mapping = mapper.trivial(commGraph, host);
 	edgeweight cost = mapper.cost(commGraph, host, mapping);
-	INFO("Cost of RCM mapping (airfoil1 512 parts onto 8x8x8 grid): " << cost);
+	INFO("Cost of RCM mapping (airfoil1 " << k << " parts onto 5x5 grid): " << cost);
 
 	// call RCM mapping routine
 	mapping = mapper.run(commGraph, host);
 
 	// check and evaluate mapping
 	cost = mapper.cost(commGraph, host, mapping);
-	INFO("Cost of RCM mapping (airfoil1 512 parts onto 8x8x8 grid): " << cost);
+	INFO("Cost of RCM mapping (airfoil1 " << k << " parts onto 5x5 grid): " << cost);
 }
 
 
