9f340cf88a0c2ababc34c34e15be71629820ae7e
Yassine Marrakchi <uidkr@student.kit.edu>
2013-09-22 23:14:36 +0200

    new algorithm

--- src/io/EdgeListIO.cpp
+++ src/io/EdgeListIO.cpp
@@ -11,7 +11,7 @@ namespace NetworKit {
 
 
 EdgeListIO::EdgeListIO() {
-	separator = '	';
+
 }
 
 EdgeListIO::EdgeListIO(char separator, node firstNode) : separator(), firstNode(firstNode) {
@@ -38,8 +38,9 @@ Graph EdgeListIO::read(std::string path) {
    node maxNode = 0;
 
    while (file.good()) {
+
 	   std::getline(file, line);
-	   std::vector<std::string> split = Aux::StringTools::split(line, separator);
+	   std::vector<std::string> split = Aux::StringTools::split(line, '\t');
 	   std::string prefix = "#";
 	   if (split.size() == 2 && (split[0].compare(0, prefix.length(), prefix) != 0)) {
 		   node u = std::stoi(split[0]);
@@ -62,7 +63,8 @@ Graph EdgeListIO::read(std::string path) {
 
     // split the line into start and end node. since the edges are sorted, the start node has the highest id of all nodes
     while(std::getline(file,line)){
-    	std::vector<std::string> split = Aux::StringTools::split(line, separator);
+
+    	std::vector<std::string> split = Aux::StringTools::split(line, '\t');
     	std::string prefix = "#";
     	std::string splitZero = split[0];
     	bool notHash = (splitZero.compare(0, prefix.length(), prefix) != 0);
