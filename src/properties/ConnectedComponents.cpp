b0a36d501f355c9666cc455567053da506424080
Maximilian Vogel <uocvf@student.kit.edu>
2013-10-16 10:21:18 +0200

    Added code documentation, adapted to code convetions for ConnectedComponents.

--- src/properties/ConnectedComponents.cpp
+++ src/properties/ConnectedComponents.cpp
@@ -2,7 +2,7 @@
  * ConnectedComponents.cpp
  *
  *  Created on: Sep 17, 2013
- *      Author: birderon
+ *      Author: Maximilian Vogel
  */
 
 #include "ConnectedComponents.h"
@@ -11,10 +11,6 @@ namespace NetworKit {
 
 ConnectedComponents::ConnectedComponents() {
 
-}
-
-ConnectedComponents::ConnectedComponents(const Graph& g) {
-	this->run(g);
 }
 
 ConnectedComponents::~ConnectedComponents() {
@@ -29,12 +25,8 @@ void ConnectedComponents::run(const Graph& g) {
 	node startNode = 0;
 	count idOfCC = 0;
 	std::vector<count> resultOfBFS;
-	//resultOfBFS = bfs.run(g,startNode);
-	//for (count i = 0; i < resultOfBFS.size(); i++) {
-	//	std::cout << resultOfBFS.at(i) << std::endl;
-	//}
+
 	do {
-		//std::cout << "run: " << idOfCC << std::endl;
 		resultOfBFS = bfs.run(g,startNode);
 		startNode = infDist;
 		count nodesInCC = 0;
@@ -54,6 +46,7 @@ void ConnectedComponents::run(const Graph& g) {
 
 node ConnectedComponents::findStartNode() {
 	count infDist = std::numeric_limits<count>::max();
+	//TODO: use iterator G.forNodes()?
 	for (count i = 0; i < this->assignedComponent.size(); i++) {
 		if (this->assignedComponent[i] == infDist) {
 			return (node)i;
@@ -65,6 +58,7 @@ node ConnectedComponents::findStartNode() {
 std::vector<node> ConnectedComponents::getComponent(index component) {
 	std::vector<node> nodesOfComponent;
 	count i = 0;
+	//TODO: use iterator G.forNodes()?
 	while (nodesOfComponent.size() < this->componentSizes.at(component)) {
 		if (this->assignedComponent.at(i) == component) {
 			nodesOfComponent.push_back(i);
--- src/properties/ConnectedComponents.h
+++ src/properties/ConnectedComponents.h
@@ -2,7 +2,7 @@
  * ConnectedComponents.h
  *
  *  Created on: Sep 17, 2013
- *      Author: birderon
+ *      Author: Maximilian Vogel
  */
 
 #ifndef CONNECTEDCOMPONENTS_H_
@@ -16,12 +16,38 @@ namespace NetworKit {
 class ConnectedComponents {
 public:
 	ConnectedComponents();
-	ConnectedComponents(const Graph& g);
 	virtual ~ConnectedComponents();
+	/**
+	 * This method determines the connected components for the graph g.
+	 *
+	 * @param[in]	g	graph 
+	 */
 	void run(const Graph& g);
+
+	/**
+	 * This method returns the number of connected components.
+	 */
 	count numberOfComponents();
+
+	/**
+	 * This method returns the size of the given component.
+	 *
+	 * @param[in]	component	the index of questioned component
+	 */
 	count sizeOfComponent(index component);
+
+	/**
+	 * This method returns the the component in which node query is situated.
+	 *
+	 * @param[in]	query	the node whose component is asked for
+	 */
 	count componentOfNode(node query);
+
+	/**
+	 * This method returns a component consisting of a vector of nodes
+	 *
+	 * @param[in]	component	the index of the component that is asked for
+	 */
 	std::vector<node> getComponent(index component);
 
 private:
