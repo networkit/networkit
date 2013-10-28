f2ce18318c63134be7e9a5cddf85394c97b3fca5
Henning Meyerhenke <meyerhenke@kit.edu>
2013-10-28 22:55:07 +0100

    frame of core decomposition

---
+++ src/auxiliary/BucketList.h
@@ -0,0 +1,38 @@
+/*
+ * BucketList.h
+ *
+ *  Created on: Oct 28, 2013
+ *      Author: Henning
+ */
+
+#ifndef BUCKETLIST_H_
+#define BUCKETLIST_H_
+
+#include <vector>
+#include <list>
+
+namespace NetworKit {
+
+class BucketList {
+private:
+	std::vector<std::list<int64_t> > buckets;
+	uint64_t firstList;
+
+public:
+	BucketList();
+	virtual ~BucketList();
+
+	/**
+	 * Insert node entry @a v into list @a value.
+	 */
+	void insert(node v, count listNum);
+
+	/**
+	 * Access first value, move it to next smaller list or
+	 * remove if in list 0.
+	 */
+	void decreaseFirstValue();
+};
+
+} /* namespace NetworKit */
+#endif /* BUCKETLIST_H_ */
