dac810c005a8e9fe69aac3ecebcb8c904b9807ff
cls <christian.staudt@kit.edu>
2013-11-06 16:29:24 +0100

    update DevGuide

---
+++ src/clustering/EdgeCut.cpp
@@ -0,0 +1,31 @@
+/*
+ * EdgeCut.cpp
+ *
+ *  Created on: Jun 20, 2013
+ *      Author: Henning
+ */
+
+#include "EdgeCut.h"
+
+namespace NetworKit {
+
+EdgeCut::EdgeCut() {
+
+}
+
+EdgeCut::~EdgeCut() {
+
+}
+
+// TODO: unit test
+double EdgeCut::getQuality(const Clustering& zeta, const Graph& G) {
+	double cutWeight = 0.0;
+	G.forEdges([&](node u, node v) {
+		if (zeta[u] != zeta[v]) {
+			cutWeight += G.weight(u, v);
+		}
+	});
+	return cutWeight;
+}
+
+} /* namespace NetworKit */
---
+++ src/scd/TGreedyCommunityExpansion.cpp
@@ -0,0 +1,13 @@
+/*
+ * TGreedyCommunityExpansion.cpp
+ *
+ *  Created on: 24.06.2013
+ *      Author: cls, Yassie Marrakchi
+ */
+
+#include "TGreedyCommunityExpansion.h"
+
+namespace NetworKit {
+
+
+} /* namespace NetworKit */
