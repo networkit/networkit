5c10b65352f5df9411ebf4a0b3a7c17393513b37
cls <christian.staudt@kit.edu>
2013-11-06 13:27:53 +0100

    NetworKit development guide

---
+++ src/coarsening/Contracter.h
@@ -0,0 +1,28 @@
+/*
+ * Contracter.h
+ *
+ *  Created on: 30.10.2012
+ *      Author: Christian Staudt (christian.staudt@kit.edu)
+ */
+
+#ifndef CONTRACTER_H_
+#define CONTRACTER_H_
+
+namespace NetworKit {
+
+class Contracter {
+
+public:
+
+	Contracter();
+
+	virtual ~Contracter();
+
+
+};
+
+
+} // namespace
+
+
+#endif /* CONTRACTER_H_ */
