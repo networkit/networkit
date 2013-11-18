4bee480f5ebfde29ae72518b1efb91ee066d52c3
Christian Ocker <christian.ocker@student.kit.edu>
2013-11-18 11:51:00 +0100

    Cleaned up a little bit.

--- src/properties/CoreDecomposition.cpp
+++ src/properties/CoreDecomposition.cpp
@@ -10,25 +10,56 @@
 namespace NetworKit {
 
 CoreDecomposition::CoreDecomposition() {
-
 }
 
 CoreDecomposition::~CoreDecomposition() {
-
 }
 
+struct NodeWithDegree {
+  NodeWithDegree(node _v, count _degree) : v(_v), degree(_degree) {}
+  node v;
+  count degree;
+};
+
 std::vector<count> CoreDecomposition::run(const Graph& G) {
-	std::vector<count> coreness;
+  const count n = G.numberOfNodes();
+	std::vector<count> coreness = std::vector<count>(n, 0);
 
-	G.forNodes([&](node v) {
-		// TODO: fill data structure
-	});
+  //Knotengrad bestimmen
+  std::vector<count> degree = std::vector<count>(n, 0);
+  G.forEdges([&](node u, node v) {
+    degree[u]++;
+    degree[v]++;
+  });
 
-	//index i = 1;
+  // Knoten in entsprechende Liste stecken
+  auto nodesByDegree = new std::list<NodeWithDegree>[n];
+  std::vector<std::list<NodeWithDegree>::iterator> nodePointer;
+  for(node v = 0; v < n; v++) {
+    nodesByDegree[degree[v]].push_front(NodeWithDegree(v, degree[v]));
+    nodePointer.push_back(nodesByDegree[degree[v]].begin());
+  }
+
+	index i = 1;
 	Graph G2 = G;
-	while (G2.numberOfNodes() > 0) {
-		// TODO: main loop
+  while (i - 1 < n) {
+    while(!nodesByDegree[i - 1].empty()) {
+      auto pv = nodesByDegree[i - 1].begin();
+      coreness[pv->v] = i - 1;
+      G2.forEdgesOf(pv->v, [&](node w, node u) {
+        auto pu = nodePointer[u];
+        if(pu->degree > i - 1) {
+          auto &oldList = nodesByDegree[pu->degree];
+          auto &newList = nodesByDegree[pu->degree - 1];
+          newList.splice(newList.begin(), oldList, pu);
+          pu->degree--;
+        }
+      });
+      nodesByDegree[pv->degree].erase(pv);
+    }
+    i++;
 	}
+  delete[] nodesByDegree;
 
 	return coreness;
 }
