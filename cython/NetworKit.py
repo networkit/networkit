3842eb326144681dde7c79fce40a521319a9b1a2
cls <christian.staudt@kit.edu>
2013-11-28 13:29:01 +0100

    NetworKit.batch - generic batch processing script

--- cython/NetworKit.py
+++ cython/NetworKit.py
@@ -10,7 +10,8 @@ __version__ = "2.1"
 
 
 # standard library modules
-
+import csv
+import os
 
 # non standard library modules
 try:
@@ -29,6 +30,8 @@ except ImportError:
 
 # local modules
 import stopwatch
+
+
 
 
 # import from extension module into NetworKit namespace
@@ -113,5 +116,32 @@ def retrieveAttributes(nodes, attributes):
 		attrs[u] = attributes[u]
 	return attrs
 	
-	
+
+# batch processing
+
+
+def batch(graphDir, match, function, outPath, header=None):
+	"""
+	Read graphs from a directory, apply a function and store result in CSV format.
+	:param	graphDir	a directory containing graph files 
+	:param	match		a pattern that must match the filename so the file is treated as a graph
+	:param  function	any function from Graph to list/tuple of values
+	:param	header		CSV file header
+	"""
+	with open(outPath, 'w') as outFile:
+		writer = csv.writer(outFile, delimiter='\t')
+		if header:
+			writer.writerow(header)
+		for root, _, filenames in os.walk(graphDir):
+			for filename in filenames:
+				if match in filename:
+					print("processing {0}".format(filename))
+					graphPath = os.path.join(root, filename)
+					timer = stopwatch.Timer()
+					G = graphio.readGraph(graphPath)
+					timer.stop()
+					row = function(G)
+					row = [filename, timer.elapsed] + list(row)
+					writer.writerow(row)
+
 	
