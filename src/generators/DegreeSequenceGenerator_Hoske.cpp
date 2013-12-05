9919564a510a8bd777106f4b44ffc0a21821fc89
Daniel Hoske <daniel.hoske@student.kit.edu>
2013-12-05 19:31:52 +0100

    Added template for degree sequence graph generator.

---
+++ src/generators/DegreeSequenceGenerator_Hoske.cpp
@@ -0,0 +1,18 @@
+/*
+ * DegreeSequenceGenerator_Hoske.cpp
+ *
+ *  Created on: 05-12-2013 
+ *      Author: dhoske 
+ */
+
+#include "DegreeSequenceGenerator_Hoske.h"
+
+namespace NetworKit {
+
+Graph DegreeSequenceGenerator_Hoske::generate() {
+    Graph G(n);
+    return G;
+}
+
+} /* namespace NetworKit */
+
---
+++ src/generators/DegreeSequenceGenerator_Hoske.h
@@ -0,0 +1,42 @@
+/*
+ * DegreeSequenceGenerator_Hoske.h
+ *
+ *  Created on: 05-12-2013 
+ *      Author: dhoske 
+ */
+
+#ifndef DEGREESEQUENCEGENERATOR_HOSKE_H_
+#define DEGREESEQUENCEGENERATOR_HOSKE_H_
+
+#include "StaticGraphGenerator.h"
+#include <cmath>
+
+namespace NetworKit {
+
+/**
+ * Generates a graph with a given degree sequence. 
+ */
+class DegreeSequenceGenerator_Hoske: public NetworKit::StaticGraphGenerator {
+protected:
+	std::vector<count> deg_seg; /* Desired degree sequence. */
+    count n;                    /* Number of nodes. */
+
+public:
+    /** nullary constructor needed for Python Shell - do not use this to construct instance */
+	DegreeSequenceGenerator_Hoske() {
+    }; 	
+
+    /** Constructor that takes a degree sequence. */
+	DegreeSequenceGenerator_Hoske(const std::vector<count>& degree_sequence) 
+        : deg_seg(degree_sequence), n(count(degree_sequence.size())) {
+    }; 	
+
+    /** Default destrutor. */
+	virtual ~DegreeSequenceGenerator_Hoske() override = default;
+
+    /** Generates a graph with the given degree sequence. */
+	virtual Graph generate() override;
+};
+
+} /* namespace NetworKit */
+#endif /* DEGREESEQUENCEGENERATOR_HOSKE_H_ */
