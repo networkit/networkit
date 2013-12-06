9815165966dfdbf855ba0dee7037b4667c9264ff
maxv <uocvf@student.kit.edu>
2013-12-06 10:16:23 +0100

    implemented missing functions for Cover,Partition; unittests for Partition

--- src/structures/Partition.cpp
+++ src/structures/Partition.cpp
@@ -9,7 +9,7 @@
 
 namespace NetworKit {
 
-Partition::Partition(index z) : z(z), omega(0), data(z+1, none) {
+Partition::Partition(index z) : z(z), omega(0), data(z, none) { // FIXME: data is initialized with z+1, is that correct?
 
 }
 
@@ -34,20 +34,21 @@ void Partition::allToSingletons() {
 	}
 }
 
-void Partition::mergeSubets(index s, index t) {
+void Partition::mergeSubsets(index s, index t) {
 	assert (s <= omega);
 	assert (t <= omega);
 	index m = newSubsetId(); // new id for merged set
 	for (index e = 0; e <= this->z; ++e) {
-		if (data[e] == t || data[e] == t) {
+		if (data[e] == s || data[e] == t) {
 			data[e] = m;
 		}
 	}
 }
 
-bool Partition::isOnePartition(const std::set<index>& elements) {
+bool Partition::isOnePartition(const std::set<index>& elements) { //FIXME what for is elements needed?
 	index one = data[0];	// first subset id should be equal to all others
-	for (index e = 0; e <= this->z; ++e) {
+	// TODO: use iterator forEntries and pair-wise comparison?
+	for (index e = 0; e < this->z; ++e) { // FIXME constructor initializes data with z+1, so <= is necessary. 
 		if (data[e] != one) {
 			return false;
 		}
@@ -55,7 +56,7 @@ bool Partition::isOnePartition(const std::set<index>& elements) {
 	return true;
 }
 
-bool Partition::isSingletonPartition(const std::set<index>& elements) const {
+bool Partition::isSingletonPartition(const std::set<index>& elements) const { //FIXME what for is elements needed?
 	return (numberOfElements() == numberOfSubsets());
 }
 
@@ -89,6 +90,27 @@ index Partition::lowerBound() const {
 
 void Partition::compact() {
 	// TODO: compact partition
+	// PROBLEM: conflict between old and new IDs?
+	std::vector<index> assignedSubsetIDs;
+	std::map<index,index> mapToCompactedIDs; // create map to
+	index i = 1;
+	this->forEntries([&](index e, index s){ // get assigned SubsetIDs and create a map with new IDs
+		if (mapToCompactedIDs.find(s) == mapToCompactedIDs.end()) {
+			//std::find(assignedSubsetIDs.begin(),assignedSubsetIDs.end()) != assignedSubsetIDs.end()
+			//((assignedSubsetIDs.push_back(s);
+			mapToCompactedIDs[s] = i++;
+		}
+	});
+/*	std::map<index,index> mapToCompactedIDs; // create map to
+	for (index i = 0; i < assignedSubsetIDs.size();i++) {
+		mapToCompactedIDs[assignedSubsetIDs[i]] = i;
+	}*/
+	this->forEntries([&](index e, index s){ // replace old SubsetIDs with the new IDs
+		data[e] = mapToCompactedIDs[s];
+	});
+	omega = i-1;
+	// TODO: test implementation
+	
 }
 
 bool Partition::contains(index e) const {
