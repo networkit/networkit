f49551893fee21452d23849b6f40fa8a9499aedc
Daniel Hoske <daniel.hoske@student.kit.edu>
2013-12-06 12:43:38 +0100

    Implemented degree sequence generator.

--- src/generators/DegreeSequenceGenerator_Hoske.cpp
+++ src/generators/DegreeSequenceGenerator_Hoske.cpp
@@ -1,8 +1,8 @@
 /*
  * DegreeSequenceGenerator_Hoske.cpp
  *
- *  Created on: 05-12-2013 
- *      Author: dhoske 
+ *  Created on: 05-12-2013
+ *      Author: dhoske
  */
 
 #include "DegreeSequenceGenerator_Hoske.h"
@@ -10,7 +10,33 @@
 namespace NetworKit {
 
 Graph DegreeSequenceGenerator_Hoske::generate() {
+	using namespace std;
     Graph G(n);
+
+    /* Pairs of (remaining degree, node id). */
+    typedef pair<count, node> DegNode;
+    vector<DegNode> rem_deg(n);
+    for (node v = 0; v < n; ++v) {
+    	rem_deg[v] = {deg_seq[v], v};
+    }
+
+    /* In each loop: fulfil degree requirement for one node. */
+    for (count k = 0; k < n; ++k) {
+    	/* Distribute edges for node with largest remaining degree. */
+    	sort(begin(rem_deg) + k, end(rem_deg), greater<DegNode>());
+    	count& deg = rem_deg[k].first;
+    	node v = rem_deg[k].second;
+    	if (deg > n - k - 1) {
+    		throw invalid_argument("Degree sequence not realizable.");
+    	}
+
+    	deg = 0;
+    	for (count l = k + 1; l <= k + deg; ++l) {
+    		rem_deg[l].first--;
+    		G.addEdge(v, rem_deg[l].second);
+    	}
+    }
+
     return G;
 }
 
--- src/generators/DegreeSequenceGenerator_Hoske.h
+++ src/generators/DegreeSequenceGenerator_Hoske.h
@@ -1,8 +1,8 @@
 /*
  * DegreeSequenceGenerator_Hoske.h
  *
- *  Created on: 05-12-2013 
- *      Author: dhoske 
+ *  Created on: 05-12-2013
+ *      Author: dhoske
  */
 
 #ifndef DEGREESEQUENCEGENERATOR_HOSKE_H_
@@ -14,22 +14,22 @@
 namespace NetworKit {
 
 /**
- * Generates a graph with a given degree sequence. 
+ * Generates a graph with a given degree sequence.
  */
 class DegreeSequenceGenerator_Hoske: public NetworKit::StaticGraphGenerator {
 protected:
-    std::vector<count> deg_seg; /* Desired degree sequence. */
+    std::vector<count> deg_seq; /* Desired degree sequence. */
     count n;                    /* Number of nodes. */
 
 public:
     /** nullary constructor needed for Python Shell - do not use this to construct instance */
     DegreeSequenceGenerator_Hoske() {
-    }; 	
+    };
 
     /** Constructor that takes a degree sequence. */
-    DegreeSequenceGenerator_Hoske(const std::vector<count>& degree_sequence) 
-        : deg_seg(degree_sequence), n(count(degree_sequence.size())) {
-    }; 	
+    DegreeSequenceGenerator_Hoske(const std::vector<count>& degree_sequence)
+        : deg_seq(degree_sequence), n(count(degree_sequence.size())) {
+    };
 
     /** Default destrutor. */
     virtual ~DegreeSequenceGenerator_Hoske() override = default;
