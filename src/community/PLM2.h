8b5dceead65380be03d2f87ccdbfa8dbe8e5b127
maxv <uocvf@student.kit.edu>
2013-12-11 10:32:45 +0100

    updated unit tests for Partition

---
+++ src/community/PLM2.h
@@ -0,0 +1,70 @@
+/*
+ * PLM2.h
+ *
+ * Was unsuccessful in evaluation, requires TBB, therefore excluded from build.
+ *
+ *  Created on: 22.10.2013
+ *      Author: cls
+ */
+
+#if 0
+ 
+#ifndef PLM2_H_
+#define PLM2_H_
+
+#include "Clusterer.h"
+
+
+
+namespace NetworKit {
+
+/**
+ * Lock-free parallel implementation of the Louvain method
+ * for community detection in networks.
+ */
+class PLM2: public NetworKit::Clusterer {
+public:
+
+	/**
+	 * @param[in]	par		parallelization strategy
+	 * @param[in]	gamma	multi-resolution modularity parameter:
+	 * 							1.0 -> standard modularity
+	 * 							0.0 -> one community
+	 * 							2m 	-> singleton communities
+	 *
+	 */
+	PLM2(std::string par="simple", double gamma = 1.0);
+
+	virtual ~PLM2();
+
+	/**
+	 * Perform one optimization pass.
+	 */
+	virtual Clustering pass(const Graph& G);
+
+
+	/**
+	 * Run the algorithm.
+	 */
+	Clustering run(Graph& G) override;
+
+
+	/**
+	 * @return string representation of algorithm and parameters.
+	 */
+	std::string toString() const override;
+
+protected:
+
+	std::string parallelism; //!< switch for the kind of parallelization strategy to use
+	double gamma;	//!< multi-resolution modularity parameter
+	bool anyChange;	//!< indicates whether any change was made to the clustering in the last pass over the nodes
+
+};
+
+} /* namespace NetworKit */
+#endif /* PLM2_H_ */
+
+#endif
+
+
---
+++ src/community/PLMR.h
@@ -0,0 +1,54 @@
+/*
+ * MLPLM.h
+ *
+ *  Created on: 20.11.2013
+ *      Author: cls
+ */
+
+#ifndef PLMR_H_
+#define PLMR_H_
+
+#include "Clusterer.h"
+
+namespace NetworKit {
+
+/**
+ * MultiLevel Parallel LocalMover - a multi-level modularity maximizer.
+ */
+class PLMR: public NetworKit::Clusterer {
+
+public:
+
+	/**
+	 * @param[in]	par		parallelization strategy
+	 * @param[in]	refine	add a second move phase to refine the communities
+	 * @param[in]	gamma	multi-resolution modularity parameter:
+	 * 							1.0 -> standard modularity
+	 * 							0.0 -> one community
+	 * 							2m 	-> singleton communities
+	 *
+	 */
+	PLMR(std::string par="simple",  bool refine=true, double gamma = 1.0);
+
+
+	std::string toString() const override;
+
+	/**
+	 * Detect communities in the given graph.
+	 */
+	Clustering run(Graph& G) override;
+
+	std::pair<Graph, std::vector<node>> coarsen(const Graph& G, const Clustering& zeta);
+
+	Clustering prolong(const Graph& Gcoarse, const Clustering& zetaCoarse, const Graph& Gfine, std::vector<node> nodeToMetaNode);
+
+private:
+
+	std::string parallelism;
+	bool refine;
+	double gamma = 1.0;
+};
+
+} /* namespace NetworKit */
+
+#endif /* MLPLM_H_ */
