123fb07b42b985fa1491e58460424c1850a1e0d8
cls <christian.staudt@kit.edu>
2014-01-02 14:53:38 +0100

    listed FJW as contributor

---
+++ src/clustering/DissimilarityMeasure.cpp
@@ -0,0 +1,21 @@
+/*
+ * DissimilarityMeasure.cpp
+ *
+ *  Created on: 19.01.2013
+ *      Author: Christian Staudt (christian.staudt@kit.edu)
+ */
+
+#include "DissimilarityMeasure.h"
+
+namespace NetworKit {
+
+DissimilarityMeasure::DissimilarityMeasure() {
+	// TODO Auto-generated constructor stub
+
+}
+
+DissimilarityMeasure::~DissimilarityMeasure() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace NetworKit */
