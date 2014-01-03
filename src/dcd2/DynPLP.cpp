4d18822c305633d989ef314d41228d692d75209a
cls <christian.staudt@kit.edu>
2014-01-03 15:28:56 +0100

    started DynPLP

---
+++ src/dcd2/DynPLP.cpp
@@ -0,0 +1,63 @@
+/*
+ * DynPLP.cpp
+ *
+ *  Created on: 03.01.2014
+ *      Author: cls
+ */
+
+#include "DynPLP.h"
+
+namespace NetworKit {
+
+DynPLP::DynPLP(Graph& G, count theta) : DynCommunityDetector(G), updateThreshold(theta) {
+
+}
+
+void DynPLP::process(std::vector<GraphEvent>& stream) {
+	auto isolate = [&](node u) {
+		zeta[u] = zeta.addCluster();
+	};
+
+	for (GraphEvent ev : stream) {
+		switch (ev.type) {
+			case GraphEvent::NODE_ADDITION : {
+				zeta.append(ev.u);
+				zeta[ev.u] = zeta.addCluster();
+				break;
+			}
+			case GraphEvent::NODE_REMOVAL : {
+				zeta[ev.u] = none;
+				break;
+			}
+			case GraphEvent::EDGE_ADDITION : {
+				isolate(ev.u);
+				isolate(ev.v);
+				break;
+			}
+			case GraphEvent::EDGE_REMOVAL : {
+				isolate(ev.u);
+				isolate(ev.v);
+				break;
+			}
+			case GraphEvent::EDGE_WEIGHT_UPDATE : {
+				isolate(ev.u);
+				isolate(ev.v);
+				break;
+			}
+			case GraphEvent::TIME_STEP : {
+				break;
+			}
+			default: {
+				throw std::runtime_error("unknown event type");
+			}
+		}
+	} // end event loop
+}
+
+Clustering DynPLP::retrieve() {
+	typedef cluster label; // a label is the same as a cluster id
+	count nUpdated; // number of nodes which have been updated in last iteration
+
+}
+
+} /* namespace NetworKit */
---
+++ src/dcd2/DynPLP.h
@@ -0,0 +1,34 @@
+/*
+ * DynPLP.h
+ *
+ *  Created on: 03.01.2014
+ *      Author: cls
+ */
+
+#ifndef DYNPLP_H_
+#define DYNPLP_H_
+
+#include "DynCommunityDetector.h"
+
+namespace NetworKit {
+
+/*
+ *
+ */
+class DynPLP: public NetworKit::DynCommunityDetector {
+public:
+	DynPLP(Graph& G, count theta);
+
+	void process(std::vector<GraphEvent>& stream) override;
+
+	Clustering retrieve() override;
+
+private:
+
+	count updateThreshold = 0;
+	count nIterations = 0; //!< number of iterations in last run
+};
+
+} /* namespace NetworKit */
+
+#endif /* DYNPLP_H_ */
