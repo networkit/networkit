56fb6355014341b5eeba4997e0c0453070bb928c
cls <christian.staudt@kit.edu>
2014-01-08 19:51:01 +0100

    added tests for dcd

---
+++ src/dcd2/test/DCD2GTest.cpp
@@ -0,0 +1,48 @@
+/*
+ * DCD2GTest.cpp
+ *
+ *  Created on: 07.01.2014
+ *      Author: cls
+ */
+
+#include "DCD2GTest.h"
+
+#include "../DynPLP.h"
+#include "../../dynamics/DGSStreamParser.h"
+#include "../../dynamics/GraphUpdater.h"
+
+namespace NetworKit {
+
+
+TEST_F(DCD2GTest, testGraphUpdater) {
+	DGSStreamParser parser("input/example2.dgs", true, 1);
+	DEBUG("getting event stream");
+	std::vector<GraphEvent> stream = parser.getStream();
+	Graph G;
+	GraphUpdater gu(G);
+	DEBUG("updating graph");
+	gu.update(stream);
+	EXPECT_EQ(3, G.numberOfNodes());
+
+}
+
+
+TEST_F(DCD2GTest, testDynPLP) {
+	DGSStreamParser parser("input/example2.dgs");
+	std::vector<GraphEvent> stream = parser.getStream();
+	Graph G;
+	DynPLP dynPLP(G, 0);
+
+	GraphUpdater gu(G);
+	gu.update(stream);
+
+	dynPLP.process(stream);
+
+
+
+}
+
+
+
+
+} /* namespace NetworKit */
