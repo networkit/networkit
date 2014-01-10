0d216543b04cb1e7f898288661c052f33d99dcf6
Florian Weber <florian.weber@sfz-bw.de>
2014-01-10 20:30:39 +0100

    remove thread-safety from the Random-functions for gcc < 4.8
    This is done because these versions do not yet support thread_local which is needed for this

--- src/auxiliary/Random.cpp
+++ src/auxiliary/Random.cpp
@@ -10,6 +10,16 @@
 
 #include "Random.h"
 
+// If GCC does not support thread local, we are sad and don't use it:
+#ifdef __GNUC__
+#	if (__GNUC__ > 4) || (__GNUC__ == 4 && __GNUC_MINOR >= 8)
+#		define AUX_THREAD_LOCAL thread_local
+#	else
+#		define AUX_THREAD_LOCAL
+#	endif
+#else // we don't know our plattform, so don't support it:
+#	define AUX_THREAD_LOCAL 
+#endif
 
 namespace Aux {
 namespace Random {
@@ -23,13 +33,13 @@ std::mt19937_64& getURNG();
 }
 
 uint64_t getSeed() {
-	thread_local static std::random_device urng{};
+	AUX_THREAD_LOCAL static std::random_device urng{};
 	std::uniform_int_distribution<uint64_t> dist{};
 	return dist(urng);
 }
 
 uint64_t integer() {
-	thread_local static std::uniform_int_distribution<uint64_t> dist{};
+	AUX_THREAD_LOCAL static std::uniform_int_distribution<uint64_t> dist{};
 	return dist(getURNG());
 }
 uint64_t integer(uint64_t upperBound) {
@@ -42,7 +52,7 @@ uint64_t integer(uint64_t lowerBound, uint64_t upperBound) {
 }
 
 double real() {
-	thread_local static std::uniform_real_distribution<double> dist{};
+	AUX_THREAD_LOCAL static std::uniform_real_distribution<double> dist{};
 	return dist(getURNG());
 }
 double real(double upperBound) {
@@ -55,13 +65,13 @@ double real(double lowerBound, double upperBound) {
 }
 
 double probability() {
-	thread_local static std::uniform_real_distribution<double> dist{0.0, std::nexttoward(1.0, 2.0)};
+	AUX_THREAD_LOCAL static std::uniform_real_distribution<double> dist{0.0, std::nexttoward(1.0, 2.0)};
 	return dist(getURNG());
 }
 
 namespace {
 std::mt19937_64& getURNG() {
-	thread_local static std::mt19937_64 generator{getSeed()};
+	AUX_THREAD_LOCAL static std::mt19937_64 generator{getSeed()};
 	return generator;
 }
 } // anonymous namespace
