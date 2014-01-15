ad6859563347411051180e2599c291a75b8eae4f
Henning Meyerhenke <meyerhenke@kit.edu>
2014-01-15 16:38:05 +0100

    dynamic PubWeb is growing...

---
+++ src/generators/DynamicPathGenerator.h
@@ -0,0 +1,28 @@
+/*
+ * DynamicPathGenerator.h
+ *
+ *  Created on: 14.01.2014
+ *      Author: cls
+ */
+
+#ifndef DYNAMICPATHGENERATOR_H_
+#define DYNAMICPATHGENERATOR_H_
+
+#include "DynamicGraphGenerator.h"
+#include <map>
+
+namespace NetworKit {
+
+/**
+ * Example dynamic graph generator: Generates a dynamically growing path.
+ */
+class DynamicPathGenerator: public NetworKit::DynamicGraphGenerator {
+public:
+	DynamicPathGenerator();
+
+	std::vector<GraphEvent> generate(count nSteps) override;
+};
+
+} /* namespace NetworKit */
+
+#endif /* DYNAMICPATHGENERATOR_H_ */
