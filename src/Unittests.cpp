608caf2664c5c4f05bd4d134838cdffa48551d00
cls <christian.staudt@kit.edu>
2014-01-15 12:40:35 +0100

    debug DGSStreamParser

---
+++ src/Unittests.cpp
@@ -0,0 +1,148 @@
+//============================================================================
+// Name        : CommunityDetection.cpp
+// Author      : Christian Staudt (christian.staudt@kit.edu),
+//		 Henning Meyerhenke (henning.meyerhenke@kit.edu)
+// Version     :
+// Copyright   : � 2012, Christian Staudt, Henning Meyerhenke
+// Description : Combining parallel clustering and ensemble learning
+//============================================================================
+
+// includes
+#include <iostream>
+#include <utility>
+#include <cfenv>	// floating point exceptions
+#include <stdexcept>
+
+// GoogleTest
+#ifndef NOGTEST
+#include <gtest/gtest.h>
+#endif
+
+// OpenMP
+#include <omp.h>
+
+// necessary for some reasons?
+#include "Globals.h"
+#include "ext/optionparser.h"
+#include "auxiliary/Log.h"
+#include "graph/Graph.h"
+
+
+using namespace NetworKit;
+
+
+
+
+/**
+ *  Set the number of threads available to the program.
+ */
+void setNumberOfThreads(int nThreads) {
+#ifdef _OPENMP
+		omp_set_num_threads(nThreads);
+#else
+		WARN("Thread option ignored since OpenMP is deactivated.");
+#endif
+}
+
+// *** Option Parser Configuration ***//
+
+class Arg: public OptionParser::Arg {
+
+static OptionParser::ArgStatus Required(const OptionParser::Option& option, bool msg)
+{
+  if (option.arg != 0)
+    return OptionParser::ARG_OK;
+
+  if (msg) {
+	  std::cout << "Option '" << option << "' requires an argument" << std::endl;
+  }
+  return OptionParser::ARG_ILLEGAL;
+}
+
+};
+
+// TODO: clean up obsolete parameters
+enum  optionIndex { UNKNOWN, HELP, LOGLEVEL, THREADS, TESTS };
+const OptionParser::Descriptor usage[] =
+{
+ {UNKNOWN, 0,"" , ""    ,OptionParser::Arg::None, ""
+                                            "Options:" },
+ {HELP,    0,"h" , "help",OptionParser::Arg::None, "  --help  \t Print usage and exit." },
+ {LOGLEVEL,    0, "" , "loglevel", OptionParser::Arg::Required, "  --loglevel=<LEVEL>  \t set the log level" },
+ {THREADS,    0, "" , "threads", OptionParser::Arg::Required, "  --threads=<NUM>  \t set the maximum number of threads" },
+ {TESTS, 0, "t", "tests", OptionParser::Arg::None, "  --tests \t Run unit tests"},
+ {UNKNOWN, 0,"" ,  ""   ,OptionParser::Arg::None, "\nExamples:\n"
+                                            " TODO" },
+ {0,0,0,0,0,0}
+};
+
+
+int main(int argc, char **argv) {
+	std::cout << "*** NetworKit Unit Tests *** " << std::endl;
+
+	// ENABLE FLOATING POINT EXCEPTIONS (needs GNU extension, apparently only available on Linux)
+#ifdef _GNU_SOURCE
+	// feenableexcept(FE_ALL_EXCEPT);
+#endif
+
+
+	/// PARSE OPTIONS
+	argc-=(argc>0); argv+=(argc>0); // skip program name argv[0] if present
+
+	OptionParser::Stats  stats(usage, argc, argv);
+	OptionParser::Option options[stats.options_max], buffer[stats.buffer_max];
+	OptionParser::Parser parse(usage, argc, argv, options, buffer);
+
+	if (parse.error())
+	 return 1;
+
+	if (options[HELP]) {
+	 OptionParser::printUsage(std::cout, usage);
+	 return 0;
+	}
+
+	for (OptionParser::Option* opt = options[UNKNOWN]; opt; opt = opt->next())
+	 std::cout << "Unknown option: " << opt->name << "\n";
+
+	for (int i = 0; i < parse.nonOptionsCount(); ++i)
+	 std::cout << "Non-option #" << i << ": " << parse.nonOption(i) << "\n";
+
+
+
+
+	// CONFIGURE LOGGING
+
+
+#ifndef NOLOGGING
+#ifndef NOLOG4CXX
+	if (options[LOGLEVEL]) {
+		Aux::configureLogging(options[LOGLEVEL].arg);
+	} else {
+		Aux::configureLogging();	// with default level
+	}
+	std::cout << Aux::currentLogLevel() << std::endl;
+#endif
+#endif
+
+
+	// CONFIGURE PARALLELISM
+
+#ifdef _OPENMP
+	omp_set_nested(1); // enable nested parallelism
+#endif
+
+	if (options[THREADS]) {
+		// set number of threads
+		int nThreads = std::atoi(options[THREADS].arg);
+		setNumberOfThreads(nThreads);
+	}
+
+
+#ifndef NOGTEST
+	   ::testing::InitGoogleTest(&argc, argv);
+	   INFO("=== starting unit tests ===");
+	   return RUN_ALL_TESTS();
+#else
+	   throw std::runtime_error("unit tests are excluded from build by the NOGTEST preprocessor directive");
+#endif
+}
---
+++ src/dcd2/DynamicCommunityDetection.cpp
@@ -0,0 +1,176 @@
+/*
+ * DynamicCommunityDetection.cpp
+ *
+ *  Created on: 10.01.2014
+ *      Author: cls
+ */
+
+#include "DynamicCommunityDetection.h"
+
+#include "DynPLP.h"
+#include "DynPLM.h"
+#include "StaticAdapter.h"
+#include "../dynamics/DGSStreamParser.h"
+#include "../dynamics/GraphUpdater.h"
+#include "../clustering/Modularity.h"
+#include "../auxiliary/Timer.h"
+#include "../community/PLP.h"
+#include "../community/PLM2.h"
+#include "../clustering/SampledRandMeasure.h"
+
+
+namespace NetworKit {
+
+DynamicCommunityDetection::DynamicCommunityDetection(std::string inputPath, std::string algoName, std::string updateStrategy, count interval,
+	std::vector<std::string> recordSettings) :
+	 inputPath(inputPath), algoName(algoName), updateStrategy(updateStrategy), interval(interval), recordSettings(recordSettings) {
+}
+
+void DynamicCommunityDetection::run() {
+
+	// check if property with the given name should be recorded
+	auto record = [&](std::string key) {
+		return std::find(recordSettings.begin(), recordSettings.end(), key) != recordSettings.end();
+	};
+
+	DEBUG("reading full event stream from file");
+	DGSStreamParser parser(inputPath, true, 1);
+	std::vector<GraphEvent> stream = parser.getStream();
+	DEBUG("setting up graph");
+	Graph G;
+	GraphUpdater gu(G);
+
+	DEBUG("setting up algorithms");
+	DynCommunityDetector* algo = NULL;
+	if (algoName == "DynPLP") {
+		algo = new DynPLP();
+	} else if (algoName == "DynPLM") {
+		algo = new DynPLM(updateStrategy);
+	} else if (algoName == "PLP") {
+		algo = new StaticAdapter(new PLP());
+	} else if (algoName == "PLM") {
+		algo = new StaticAdapter(new PLM2());
+	}
+
+	DEBUG("attaching graph");
+	algo->attachGraph(G);
+
+	Aux::Timer timer;
+
+
+	// slicing stream by timesteps
+	auto i = stream.begin();
+	auto j = stream.begin();
+
+	count run = 0;
+
+	// advance the iterators forward to the next <interval> time steps
+	auto advance = [&](){
+		count steps = 0;
+		i = j;
+		for (; (steps < interval) && (j < stream.end()); ++j) {
+			if (j->type == GraphEvent::TIME_STEP) {
+				steps++;
+			}
+		}
+	};
+
+	DEBUG("advancing dynamic graph");
+	while (j != stream.end()) {
+		run++; // next run
+		advance(); // advance iterators
+		std::vector<GraphEvent> slice(i, j);
+		DEBUG("updating graph");
+		gu.update(slice);
+		DEBUG("number of nodes: " << G.numberOfNodes());
+
+
+		timer.start();
+		//
+		algo->update(slice);
+		//
+		timer.stop();
+		updateTime.push_back(timer.elapsedMilliseconds());
+
+		timer.start();
+		//
+		Clustering zeta = algo->detect();
+		//
+		timer.stop();
+		detectTime.push_back(timer.elapsedMilliseconds());
+
+		DEBUG("upper community id bound: " << zeta.upperBound());
+		// DEBUG("zeta at time " << G.time() << ": " << Aux::vectorToString(zeta.getVector()));
+		// 
+		
+		// record time step
+		step.push_back(G.time());
+
+		if (record("quality")) {
+			DEBUG("recording quality");
+			Modularity mod;
+			quality.push_back(mod.getQuality(zeta, G));
+			INFO("modularity at time " << G.time() << ": " << quality.back());
+		}
+
+		if (record("continuity")) {
+
+			if (run >= 2) {
+				SampledRandMeasure sampledRand(1000);
+				double cont = sampledRand.getDissimilarity(G, zeta, previous);
+				continuity.push_back(cont);
+			} else {
+				continuity.push_back(0.0); // to make timelines the same length
+			}
+
+		}
+
+		if (record("communityCount")) {
+			DEBUG("recording community count");
+			communityCount.push_back(zeta.numberOfClusters());
+		}
+
+		if (record("communitySizes")) {
+			DEBUG("recording community sizes");
+			std::vector<count> sizes = zeta.clusterSizes();
+
+			// double avgSize = std::accumulate(sizes.begin(), sizes.end(), 0) / (double) sizes.size();
+			communitySizes.push_back(sizes);
+		}
+
+
+		previous = zeta; // save last solution for next run
+
+	}
+
+
+	// record graph size
+	size = gu.getSizeTimeline();
+
+}
+
+std::vector<double> DynamicCommunityDetection::getTimeline(std::string key) {
+	if (key == "updateTime") {
+		return updateTime;
+	} else if (key == "detectTime") {
+		return detectTime;
+	} else if (key == "quality") {
+		return quality;
+	} else if (key == "continuity") {
+		return continuity;
+	} else if (key == "communityCount") {
+		return communityCount;
+	} else if (key == "step") {
+		return step;
+	} else {
+		throw std::runtime_error("unknown timeline key");
+		return {};
+	}
+}
+
+
+std::vector<std::pair<count, count> > DynamicCommunityDetection::getGraphSizeTimeline() {
+	return size;
+}
+
+} /* namespace NetworKit */
