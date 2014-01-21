a73f46a8c4f1428542313ee3a3af0fd664fb90fc
Henning Meyerhenke <meyerhenke@kit.edu>
2014-01-21 17:53:35 +0100

    Erdos-Renyi G(n,p) generator, comments

--- src/generators/ErdosRenyiGenerator.h
+++ src/generators/ErdosRenyiGenerator.h
@@ -22,6 +22,14 @@ protected:
 	double p;
 
 public:
+	/**
+	 * Creates random graphs in the G(n,p) model.
+	 * The generation follows Vladimir Batagelj and Ulrik Brandes: "Efficient
+	 * generation of large random networks", Phys Rev E 71, 036113 (2005).
+	 *
+	 * @param nNodes Number of nodes n in the graph.
+	 * @param prob Probability of existence for each edge p.
+	 */
 	ErdosRenyiGenerator(count nNodes, double prob);
 	virtual ~ErdosRenyiGenerator();
 
