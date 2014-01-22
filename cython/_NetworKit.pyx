ff2bdddbb0833257e609e246ef693066d2cb2aeb
cls <christian.staudt@kit.edu>
2014-01-22 10:56:02 +0100

    pythonized Erd?sRenyiGenerator and added to UserGuide

--- cython/_NetworKit.pyx
+++ cython/_NetworKit.pyx
@@ -379,6 +379,31 @@ cdef class PubWebGenerator:
 		return Graph(0).setThis(self._this.generate())
 
 
+cdef extern from "../src/generators/ErdosRenyiGenerator.h":
+	cdef cppclass _ErdosRenyiGenerator "NetworKit::ErdosRenyiGenerator":
+		_ErdosRenyiGenerator(count nNodes, double prob) except +
+		_Graph generate() except +
+
+cdef class ErdosRenyiGenerator:
+	"""
+	  Creates random graphs in the G(n,p) model.
+	  The generation follows Vladimir Batagelj and Ulrik Brandes: "Efficient
+	  generation of large random networks", Phys Rev E 71, 036113 (2005).
+	 
+	 Parameters:
+	  - nNodes Number of nodes n in the graph.
+	  - prob Probability of existence for each edge p.
+	"""
+
+	cdef _ErdosRenyiGenerator* _this
+
+	def __cinit__(self, nNodes, prob):
+		self._this = new _ErdosRenyiGenerator(nNodes, prob)
+
+	def generate(self):
+		return Graph(0).setThis(self._this.generate())
+
+
 
 
 # Module: graphio
