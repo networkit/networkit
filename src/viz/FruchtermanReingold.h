e85da6dd92a24a6a8e7cd3a3508c15699ddb61e7
Henning Meyerhenke <meyerhenke@kit.edu>
2014-01-23 14:01:33 +0100

    restructuring and extending drawing features

--- src/viz/FruchtermanReingold.h
+++ src/viz/FruchtermanReingold.h
@@ -10,28 +10,35 @@
 
 #include "Layouter.h"
 #include "Point.h"
+#include "PostscriptWriter.h"
+
 #include <vector>
 #include <cmath>
+
 
 namespace NetworKit {
 
 /**
- * TODO: class documentation
+ * Fruchterman-Reingold graph drawing algorithm. We mostly follow
+ * the description in Stephen G. Kobourov: Spring Embedders and Force
+ * Directed Graph Drawing Algorithms.
  */
 class FruchtermanReingold: public NetworKit::Layouter {
 private:
-	const count MAX_ITER = 200;
+	const count MAX_ITER = 250;
 
 public:
 
 	/**
-	 * Constructor
+	 * Constructor. DO NOT use for creating objects, only needed for
+	 * Python shell.
 	 */
-	FruchtermanReingold(); // nullary constructor needed for Python shell
+	FruchtermanReingold() {}
 
 	/**
-	 * @param bottomLeft Coordinate of point in bottom/left corner
-	 * @param topRight Coordinate of point in top/right corner
+	 * Constructor.
+	 * @param[in] bottomLeft Coordinate of point in bottom/left corner
+	 * @param[in] topRight Coordinate of point in top/right corner
 	 */
 	FruchtermanReingold(Point<float> bottomLeft, Point<float> topRight);
 
