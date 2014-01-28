5548ef2a0f7f594426c158580580082b4a586c8d
cls <christian.staudt@kit.edu>
2014-01-28 16:56:07 +0100

    DynamicPubWebGenerator.generate can now be called multiple times

---
+++ src/coarsening/PartitionCoarsening.h
@@ -0,0 +1,31 @@
+/*
+ * PartitionCoarsening.h
+ *
+ *  Created on: 28.01.2014
+ *      Author: cls
+ */
+
+#ifndef PARTITIONCOARSENING_H_
+#define PARTITIONCOARSENING_H_
+
+#include "GraphCoarsening.h"
+#include "../clustering/Clustering.h"
+
+namespace NetworKit {
+
+/*
+ *
+ */
+class PartitionCoarsening: public NetworKit::GraphCoarsening {
+
+public:
+
+	virtual std::pair<Graph, std::vector<node> > run(const Graph& G, const Clustering& zeta);
+
+
+
+};
+
+} /* namespace NetworKit */
+
+#endif /* PARTITIONCOARSENING_H_ */
