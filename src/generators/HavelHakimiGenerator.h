cfe8946405d2da5695be81ed1c3f81684e12820b
Henning Meyerhenke <meyerhenke@kit.edu>
2014-01-31 16:49:50 +0100

    Havel-Hakimi works in linear time

--- src/generators/HavelHakimiGenerator.h
+++ src/generators/HavelHakimiGenerator.h
@@ -9,6 +9,7 @@
 #define HAVELHAKIMIGENERATOR_H_
 
 #include <vector>
+#include <stack>
 
 #include "../graph/Graph.h"
 #include "../auxiliary/ShellList.h"
@@ -16,6 +17,12 @@
 
 namespace NetworKit {
 
+/**
+ * 	Havel-Hakimi algorithm for generating a graph according to a given degree sequence.
+ * 	The sequence, if it is realizable, is reconstructed exactly.
+ *  The resulting graph usually has a high clustering coefficient.
+ *  Construction runs in linear time O(m).
+ */
 class HavelHakimiGenerator: public NetworKit::StaticGraphGenerator  {
 protected:
 	std::vector<count> seq;
