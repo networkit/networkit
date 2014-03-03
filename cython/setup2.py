d1f41eb170bef990c1237d59b31549186646c332
cls <christian.staudt@kit.edu>
2014-03-03 11:46:07 +0100

    G.getName

---
+++ cython/setup2.py
@@ -0,0 +1,120 @@
+from setuptools import setup
+from setuptools import Extension
+#from distutils.extension import Extension
+from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+
+import os
+import shutil
+
+from subprocess import Popen
+import shlex
+import sys
+
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument("-j", "--jobs", dest="jobs", help="specify number of jobs")
+parser.add_argument("-o", "--optimize", dest="optimize", help="specify build type: O=optimize, D=debug, P=profiling")
+#parser.add_option("-i", "--inplace", dest="dest", help="states whether extensions shall be build inplace or in a subfolder")
+(options,args) = parser.parse_known_args()
+if options.jobs != None:
+	jobs = options.jobs
+else:
+	jobs = "1" # TODO how to get the number of available threads? 
+if options.optimize != None:
+	optimize = options.optimize
+else:
+	optimize = "O"
+
+# make sure sys.argv is correct for setuptools
+args.reverse()
+args.append(__file__)
+args.reverse() # this is not a very nice way to do this for sure
+sys.argv = args
+for e in sys.argv:
+	print(e)
+
+
+def build_NetworKit():
+	os.chdir("../")
+	comp_cmd = "scons --optimize={0} --target=Core -j{1}".format(optimize,jobs)
+	#print("initializing NetworKit compilation with: {0}".format(comp_cmd))
+	comp_proc = Popen(shlex.split(comp_cmd))
+	comp_proc.wait()
+	os.chdir("./cython")
+	try:
+		os.remove("_NetworKit.cpp")
+	except:
+		print("_NetworKit.cpp already deleted")
+
+def additional_clean():
+	os.chdir("../")
+	clean_cmd = "scons --optimize={0} --target=Core -c".format(optimize)
+	clean_proc = Popen(shlex.split(clean_cmd))
+	clean_proc.wait()
+	os.chdir("./cython")
+	#os.rmdir("./build")
+	try:
+		os.remove("_NetworKit.cpp")
+	except:
+		print("_NetworKit.cpp already deleted")
+
+
+
+if ("build_ext" in sys.argv):
+	build_NetworKit()
+elif (("develop" in sys.argv) and ("--uninstall" not in sys.argv)):
+	try:
+		os.mkdir("NetworKit")
+	except:
+		foo = 0
+	build_NetworKit()
+elif "clean" in sys.argv:
+	additional_clean()
+	
+# try-catch block when shutil.which is not available
+try:
+	if (shutil.which("g++-4.8") is not None):
+		os.environ["CC"] = "g++-4.8"
+		os.environ["CXX"] = "g++-4.8"
+
+	elif (shutil.which("g++-4.7") is not None):
+		os.environ["CC"] = "g++-4.7"
+		os.environ["CXX"] = "g++-4.7"
+
+	else:
+		pass
+except:
+	os.environ["CC"] = "g++"
+	os.environ["CXX"] = "g++"
+
+print("Using compilers: {0} and {1}".format(os.environ["CC"], os.environ["CXX"]))
+
+src = ["_NetworKit.pyx"]	# list of source files
+			
+print("source files: {0}".format(src))
+
+modules = [Extension("_NetworKit",
+					src,
+					language = "c++",
+					extra_compile_args=["-fopenmp", "-std=c++11", "-O3", "-DNOGTEST"],
+					extra_link_args=["-fopenmp", "-std=c++11"],
+					libraries=["NetworKit-Core-{0}".format(optimize)],
+					library_dirs=["../"])]
+
+for e in modules:
+	e.cython_directives = {"embedsignature" : True}
+
+setup(name="NetworKit",
+	author="Christian L. Staudt",
+	author_email = "christian.staudt@kit.edu",
+	description = "NetworKit is a toolbox for high-performance network analysis",
+	license = "MIT",
+	keywords = "graph algorithm network analysis social network",
+	version="2.1",
+	cmdclass={"build_ext": build_ext},
+	ext_modules=modules,
+	py_modules = ["NetworKit.py"])
+
+print("[Done] setup.py")
