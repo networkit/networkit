9e54eb13e420e0c7db71579a847b1f748e317d3e
Henning Meyerhenke <meyerhenke@kit.edu>
2014-03-10 16:32:08 +0100

    harmonized clustering coefficient definition

---
+++ src/cpp/auxiliary/StringTools.cpp
@@ -0,0 +1,21 @@
+/*
+ * StringTools.cpp
+ *
+ *  Created on: 11.02.2013
+ *      Author: Christian Staudt (christian.staudt@kit.edu)
+ */
+
+#include "StringTools.h"
+
+namespace Aux {
+
+StringTools::StringTools() {
+	// TODO Auto-generated constructor stub
+
+}
+
+StringTools::~StringTools() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace NetworKit */
