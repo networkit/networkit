cf39481c77f3f68e44aeb134bd489ecc62b0e8c5
cls <christian.staudt@kit.edu>
2014-03-11 15:05:06 +0100

    bugfix nxadapter

---
+++ src/cpp/community/CommunityGraph.h
@@ -0,0 +1,48 @@
+/*
+ * CommunityGraph.h
+ *
+ *  Created on: 22.08.2013
+ *      Author: cls
+ */
+
+#ifndef COMMUNITYGRAPH_H_
+#define COMMUNITYGRAPH_H_
+
+#include "../structures/Partition.h"
+
+namespace NetworKit {
+
+class CommunityGraph {
+public:
+
+	CommunityGraph();
+
+	virtual ~CommunityGraph();
+
+	/**
+	 * Create a graph coarsened according to communities. Edge weights are the weights of
+	 * inter-community cuts.
+	 */
+	virtual void run(const Graph& G, const Partition& zeta);
+
+	virtual Graph getGraph();
+
+	/** 
+	 * Maps community id to id of node in the community graph.
+	 */
+	virtual std::map<index, node> getCommunityToNodeMap();
+
+	/** 
+	 * Maps node id in the community graph to community id.
+	 */
+	virtual std::map<node, index> getNodeToCommunityMap(); 
+
+
+private:
+	Graph Gcom;		//!< the current community graph
+	std::vector<node> communityToSuperNode;		//!< community id -> node id in community graph
+
+};
+
+} /* namespace NetworKit */
+#endif /* COMMUNITYGRAPH_H_ */
---
+++ src/cpp/generators/test/GeneratorsGTest.h
@@ -0,0 +1,48 @@
+/*
+ * GeneratorsGTest.h
+ *
+ *  Created on: 09.04.2013
+ *      Author: cls
+ */
+
+#ifndef NOGTEST
+
+#ifndef GENERATORSGTEST_H_
+#define GENERATORSGTEST_H_
+
+#include <gtest/gtest.h>
+#include <cmath>
+
+#include "../DynamicGraphSource.h"
+#include "../DynamicBarabasiAlbertGenerator.h"
+#include "../PubWebGenerator.h"
+#include "../DynamicPubWebGenerator.h"
+#include "../ErdosRenyiGenerator.h"
+#include "../ChungLuGenerator.h"
+#include "../HavelHakimiGenerator.h"
+#include "../../viz/PostscriptWriter.h"
+#include "../../community/ClusteringGenerator.h"
+#include "../../community/PLP.h"
+#include "../../community/PLM.h"
+#include "../../io/METISGraphWriter.h"
+#include "../../io/DotGraphWriter.h"
+#include "../BarabasiAlbertGenerator.h"
+#include "../../io/GraphIO.h"
+#include "../../io/METISGraphReader.h"
+#include "../../properties/GraphProperties.h"
+#include "../../community/Modularity.h"
+#include "../../dynamics/GraphUpdater.h"
+#include "../../auxiliary/MissingMath.h"
+
+namespace NetworKit {
+
+class GeneratorsGTest: public testing::Test {
+public:
+	GeneratorsGTest();
+	virtual ~GeneratorsGTest();
+};
+
+} /* namespace NetworKit */
+#endif /* GENERATORSGTEST_H_ */
+
+#endif /*NOGTEST */
