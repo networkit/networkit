2f483347bc6bbad9a32efbf98796d640afcdbce0
maxv <uocvf@student.kit.edu>
2014-03-13 00:18:59 +0100

    graphio convenient, documented in UserGuide; merged Subgraph into Userguide

---
+++ src/cpp/auxiliary/ShellList.cpp
@@ -0,0 +1,55 @@
+/*
+ * ShellList.cpp
+ *
+ *  Created on: Nov 4, 2013
+ *      Author: Lukas Barth, David Weiß
+ */
+
+#include <algorithm> // std::for_each, loop iterating over containers and executing a given function for each element
+#include "ShellList.h"
+
+namespace Aux {
+
+ShellList::ShellList(const std::vector<count>& sequence): seq(sequence)
+{
+	assert(! seq.empty());
+	count n = seq.size();
+	count k = (* std::max_element(seq.begin(), seq.end())) + 1;
+	shells.resize(k);
+	nodeHandle.resize(n);
+	nodeShell.resize(n);
+
+	DEBUG("allocation in shell list finished");
+
+	for (index i = 0; i < seq.size(); ++i) {
+ 		count shell = seq[i];
+ 		this->shells[shell].push_front(i);
+ 		this->nodeHandle[i] = this->shells[shell].begin();
+ 		this->nodeShell[i] = shell;
+	}
+}
+
+void 
+ShellList::decreaseShell(const node v) 
+{
+	count shell = this->nodeShell[v];
+	assert(shell > 0);
+  
+	this->shells[shell].erase(this->nodeHandle[v]);
+  
+	--this->nodeShell[v];
+	--shell;
+  
+	this->shells[shell].push_back(v);
+	this->nodeHandle[v] = std::prev(this->shells[shell].end());
+}
+
+void 
+ShellList::forEachNodeInShell(const count shell, std::function<void (node)> func)
+{
+	assert(shell >= 0);
+	assert(shell < this->shells.size());
+	std::for_each(this->shells[shell].begin(), this->shells[shell].end(), func);
+}
+
+}//namespace Aux
---
+++ src/cpp/io/GraphWriter.h
@@ -0,0 +1,28 @@
+/*
+ * GraphWriter.h
+ *
+ *  Created on: 30.01.2013
+ *      Author: Christian Staudt (christian.staudt@kit.edu)
+ */
+
+#ifndef GRAPHWRITER_H_
+#define GRAPHWRITER_H_
+
+#include "../graph/Graph.h"
+
+#include <fstream>
+
+namespace NetworKit {
+
+class GraphWriter {
+public:
+
+	GraphWriter();
+
+	virtual ~GraphWriter();
+
+	virtual void write(Graph& G, std::string path) = 0;
+};
+
+} /* namespace NetworKit */
+#endif /* GRAPHWRITER_H_ */
