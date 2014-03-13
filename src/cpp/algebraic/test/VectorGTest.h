097e8c8420268e7f8f3b0c9e4f087f639a6a7e4d
mwegner <mwegner@localhost>
2014-03-13 10:51:11 +0100

    Vector class and tests

---
+++ src/cpp/algebraic/test/VectorGTest.h
@@ -0,0 +1,25 @@
+/*
+ * VectorGTest.h
+ *
+ *  Created on: 13.03.2014
+ *      Author: Michael Wegner (michael.wegner@student.kit.edu)
+ */
+
+#ifndef NOGTEST
+
+#ifndef VECTORGTEST_H_
+#define VECTORGTEST_H_
+
+#include <gtest/gtest.h>
+#include "../Vector.h"
+#include "../../auxiliary/Log.h"
+
+class VectorGTest : public testing::Test {
+public:
+	VectorGTest();
+	virtual ~VectorGTest();
+};
+
+#endif /* VECTORGTEST_H_ */
+
+#endif
