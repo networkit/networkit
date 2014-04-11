ec0b45af6854c390d41a8768d076d4714f785b0d
Florian Weber <florian.weber@sfz-bw.de>
2014-04-11 12:57:11 +0200

    some improvements to the random-element-pickers

--- src/cpp/auxiliary/Random.h
+++ src/cpp/auxiliary/Random.h
@@ -8,8 +8,9 @@
  *      Author: FJW
  */
 
-#include <cstdint>
 #include <cassert>
+#include <cstddef>
+#include <cstdint>
 #include <random>
 #include <stdexcept>
 
@@ -57,32 +58,44 @@ double real(double lowerBound, double upperBound);
  */
 double probability();
 
-/** 
+/**
+ * @returns a size_t in the range [0, max - 1] that can for example be used to
+ * access a random element of a container.
+ */
+std::size_t index(std::size_t max);
+
+/**
  * @returns an integer distributed binomially
  * @param n 	number of trials
  * @param p 	success probability
  */
 //uint64_t binomial(double n, double p);
 
-std::mt19937_64& getURNG();
 
-/** @returns a uniform random choice from an indexable container of elements 
-*/
-template<typename Container> typename Container::const_reference choice(const Container& container) {
-    std::uniform_int_distribution<std::size_t> dist{0, container.size() - 1};
-    return container[dist(getURNG())];
+/**
+ * @returns a uniform random choice from an indexable container of elements.
+ */
+template<typename Container>
+typename Container::const_reference choice(const Container& container) {
+	return container[index(container.size())];
 }
 
 
-/** @returns a weighted random choice from a vector of elements with given weights */
-template <typename Element> Element weightedChoice(const std::vector<std::pair<Element, double> >& weightedElements) {
+/**
+ * @returns a weighted random choice from a vector of elements with given weights.
+ */
+template <typename Element>
+const Element& weightedChoice(const std::vector<std::pair<Element, double>>& weightedElements) {
 	double total = 0.0;
 	for (const auto& entry : weightedElements) {
+		assert(entry.second >= 0.0 && "This algorithm only works with non-negative weights");
 		total += entry.second;
 	}
 	double r = Aux::Random::real(total);
 	for (const auto& entry : weightedElements) {
-		if (r < entry.second) return entry.first;
+		if (r < entry.second) {
+			return entry.first;
+		}
 		r -= entry.second;
 	}
 	throw std::runtime_error("should never get here"); // should never get here
