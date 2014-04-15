b7a8a4fe776796bc8f98a385fa7cfdb71cbc1b9e
Henning Meyerhenke <meyerhenke@kit.edu>
2014-04-15 10:12:32 +0200

    interface change

---
+++ src/cpp/graph/Dijkstra.cpp
@@ -0,0 +1,78 @@
+/*
+ * Dijkstra.cpp
+ *
+ *  Created on: Jul 23, 2013
+ *      Author: Henning, Christian Staudt
+ */
+
+#include "Dijkstra.h"
+
+namespace NetworKit {
+
+Dijkstra::Dijkstra(const Graph& G, node source) : G(G), source(source) {
+
+}
+
+Dijkstra::~Dijkstra() {
+
+}
+
+void Dijkstra::run() {
+
+	// init distances
+	edgeweight infDist = std::numeric_limits<edgeweight>::max();
+	distances.clear();
+	distances.resize(G.upperNodeIdBound(), infDist);
+	previous.clear();
+	previous.resize(G.upperNodeIdBound(), none); 
+	distances[source] = 0;
+
+	// priority queue with distance-node pairs
+	Aux::PrioQueue<edgeweight, node> pq(distances);
+
+
+	auto relax([&](node u, node v, edgeweight w) {
+		if (distances[v] > distances[u] + w) {
+			distances[v] = distances[u] + w;
+			previous[v] = u; // new predecessor on shortest path
+			pq.decreaseKey(distances[v], v);
+		}
+	});
+
+	while (pq.size() > 0) {
+//		DEBUG("pq size: ", pq.size());
+
+		node current = pq.extractMin().second;
+//		DEBUG("pq size: ", pq.size());
+//		TRACE("current node in Dijkstra: " , current);
+
+		G.forWeightedEdgesOf(current, relax);
+	}
+
+}
+
+
+std::vector<edgeweight> Dijkstra::getDistances() const {
+	return distances;
+}
+
+
+std::vector<node> Dijkstra::getPath(node t, bool forward) const {
+	std::vector<node> path;
+	if (previous[t] == none) { // t is not reachable from source
+		WARN("there is no path from ", source, " to ", t);
+		return path;
+	}
+	node v = t;
+	while (v != source) {
+		path.push_back(v);
+		v = previous[t];
+	}
+
+	if (forward) {
+		std::reverse(path.begin(), path.end());
+	}
+	return path;
+}
+
+} /* namespace NetworKit */
---
+++ src/cpp/io/GraphReader.h
@@ -0,0 +1,42 @@
+/*
+ * GraphReader.h
+ *
+ *  Created on: 17.01.2013
+ *      Author: Christian Staudt (christian.staudt@kit.edu)
+ */
+
+#ifndef GRAPHREADER_H_
+#define GRAPHREADER_H_
+
+#include "../graph/Graph.h"
+#include "../auxiliary/StringTools.h"
+
+
+namespace NetworKit {
+
+/**
+ * Abstract base class for graph readers.
+ */
+class GraphReader {
+
+public:
+
+	GraphReader();
+
+	virtual ~GraphReader();
+
+	/**
+	 * Given the path of an input file, read the graph contained.
+	 *
+	 * @param[in]	path	input file path
+	 */
+	virtual Graph read(std::string path) = 0;
+
+	/** only to be used by cython - this eliminates an unnecessary copy */
+	Graph* _read(std::string& path) {
+		return new Graph{std::move(read(path))};
+	};
+};
+
+} /* namespace NetworKit */
+#endif /* GRAPHREADER_H_ */
