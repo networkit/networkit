09678c396dbf9e353b984b00dacbf56c091b8113
cls <christian.staudt@kit.edu>
2014-04-15 12:19:49 +0200

    info output in ApproxBetweenness

--- src/cpp/centrality/ApproxBetweenness.cpp
+++ src/cpp/centrality/ApproxBetweenness.cpp
@@ -27,12 +27,13 @@ void ApproxBetweenness::run() {
 
 	double c = 1; // TODO: what is c?
 
+	INFO("estimating vertex diameter");
 	count vd = Diameter::estimatedVertexDiameter(G);
 	double r = (c / (epsilon * epsilon)) * (floor(log(vd - 2))) + log(1 / delta);
 
 	// double r = (c / (epsilon * epsilon)) * (3 + log(1 / delta));
 
-	INFO("trying ", r, " samples");
+	INFO("taking ", r, " path samples");
 	for (count i = 1; i <= r; ++i) {
 		DEBUG("sample ", i);
 		// if (i >= 1000) throw std::runtime_error("too many iterations");
@@ -44,21 +45,19 @@ void ApproxBetweenness::run() {
 			v = Sampling::randomNode(G);
 		} while (v == u);
 		Dijkstra dijkstra(G, u);
+		DEBUG("running Dijkstra for node ", u);
 		dijkstra.run();
 		if (dijkstra.numberOfPaths(v) > 0) { // at least one path between {u, v} exists
 			// random path sampling and estimation update
 			node s = v;
 			node t = v;
 			while (t != u)  {
-
-				TRACE("u, v, s, t: ", u, " ", v, "  ", s, " ", t);
 				// sample z in P_u(t) with probability sigma_uz / sigma_us
 				std::vector<std::pair<node, double> > choices;
 
 				for (node z : dijkstra.getPredecessors(t)) {
 					choices.emplace_back(z, dijkstra.numberOfPaths(z) / (double) dijkstra.numberOfPaths(s)); 	// sigma_uz / sigma_us
 				}
-				DEBUG("choices and weights: ", choices);
 				node z = Aux::Random::weightedChoice(choices);
 				if (z != u) {
 					scoreData[z] = scoreData[z] + 1 / (double) r;
