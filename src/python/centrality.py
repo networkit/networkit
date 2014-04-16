68b7f8a511e1aad38f70b6401f8bb9d46b5e7877
maxv <uocvf@student.kit.edu>
2014-04-16 14:23:02 +0200

    pythonized METIS for double area-weighted connectivity

--- src/python/centrality.py
+++ src/python/centrality.py
@@ -1,4 +1,4 @@
-from _NetworKit import Betweenness, PageRank, EigenvectorCentrality, DegreeCentrality
+from _NetworKit import Betweenness, PageRank, EigenvectorCentrality, DegreeCentrality, AreaWeightedCentrality
 
 
 def ranking(G, algorithm=Betweenness, normalized=False):
