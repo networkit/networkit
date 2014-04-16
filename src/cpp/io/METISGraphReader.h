e548313c515c7eea50e1cbd54b9ce318d6eac11e
maxv <uocvf@student.kit.edu>
2014-04-16 18:13:31 +0200

    fixed User Guide notebook

---
+++ src/cpp/io/METISGraphReader.h
@@ -0,0 +1,33 @@
+/*
+ * METISGraphReader.h
+ *
+ *  Created on: 17.01.2013
+ *      Author: Christian Staudt (christian.staudt@kit.edu)
+ */
+
+#ifndef METISGRAPHREADER_H_
+#define METISGRAPHREADER_H_
+
+#include "GraphReader.h"
+
+#include "METISParser.h"
+#include "../auxiliary/StringTools.h"
+
+namespace NetworKit {
+
+class METISGraphReader: public NetworKit::GraphReader {
+
+public:
+
+	METISGraphReader();
+
+	virtual ~METISGraphReader();
+
+	virtual Graph read(std::string path);
+
+	virtual Graph* readToHeap(std::string path);
+
+};
+
+} /* namespace NetworKit */
+#endif /* METISGRAPHREADER_H_ */
