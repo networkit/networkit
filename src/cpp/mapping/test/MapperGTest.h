2201e0a269a188e1599f07446e29fd2a18428401
cls <christian.staudt@kit.edu>
2014-05-05 18:16:28 +0200

    TODO Betweenness

---
+++ src/cpp/mapping/test/MapperGTest.h
@@ -0,0 +1,41 @@
+/*
+ * MapperGTest.h
+ *
+ *  Created on: Jul 15, 2013
+ *      Author: Henning
+ */
+
+#ifndef NOGTEST
+
+#ifndef MAPPERGTEST_H_
+#define MAPPERGTEST_H_
+
+#include <gtest/gtest.h>
+
+#include "../StaticMapper.h"
+#include "../RecBisMapper.h"
+#include "../RcmMapper.h"
+#include "../RcmMapperWW.h"
+#include "../GreedyMapper.h"
+#include "../../partitioning/BalancedLabelPropagation.h"
+#include "../../graph/Graph.h"
+#include "../../graph/GraphGenerator.h"
+#include "../../io/DibapGraphReader.h"
+#include "../../io/METISGraphReader.h"
+#include "../../io/METISGraphWriter.h"
+#include "../../community/ClusteringGenerator.h"
+
+
+namespace NetworKit {
+
+class MapperGTest: public testing::Test {
+public:
+	MapperGTest();
+	virtual ~MapperGTest();
+};
+
+}
+
+#endif /* MAPPERGTEST_H_ */
+
+#endif
