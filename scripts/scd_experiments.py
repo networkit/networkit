cb056d62a2aaf2a3e62b80a17a900dfc70c646a9
cls <christian.staudt@kit.edu>
2014-05-13 14:00:48 +0200

    scd_experiments

--- scripts/scd_experiments.py
+++ scripts/scd_experiments.py
@@ -3,6 +3,9 @@ from selectivecommunity import *
 
 import logging
 import numpy
+import os
+import pandas
+import math
 
 def jaccard(A, B):
 	i = len(A.intersection(B))
@@ -10,15 +13,15 @@ def jaccard(A, B):
 	return i / u
 
 
-def loadLFR(n, mu, minc=50, maxc=250, k=15, maxk=100, t1=2.5, t2=1.5):
+def loadLFR(dir, n, mu, minc=50, maxc=250, k=15, maxk=100, t1=2.5, t2=1.5):
 	mu = round(mu, 2)
 	fpattern = "{0}_n={1}_mu={2}_minc={3}_maxc_{4}_k={5}_maxk={6}_t1={7}_t2={8}.dat"
 	communityFileName = fpattern.format("community", n, mu, minc, maxc, k, maxk, t1, t2)
 	logging.info("loading {0}".format(communityFileName))
-	truth = community.readCommunities(communityFileName, formats.edgelist_tab_zero)
+	truth = community.readCommunities(os.path.join(dir, communityFileName), formats.edgelist_tab_zero)
 	graphFileName = fpattern.format("network", n, mu, minc, maxc, k, maxk, t1, t2)
 	logging.info("loading {0}".format(graphFileName))
-	G = readGraph(graphFileName, format=formats.edgelist_tab_one)
+	G = readGraph(os.path.join(dir, graphFileName), format=formats.edgelist_tab_one)
 	return (G, truth)
 
 
@@ -38,39 +41,48 @@ def LFRAccuracy(SCDAlgo, G, seeds, truth):
 	return (communities, accuracy)
 
 
+print("creating value ranges")
+alphaRange = numpy.around(numpy.arange(0.01, 0.26, 0.01), 2)
+epsilonRange = numpy.around(numpy.arange(1e-6, 10 * 1e-6, 1e-6), 6)
+
+# available range of LFR parameters
+muRange = numpy.around(numpy.arange(0.1, 1.0, 0.05), 2)
+
+#nRange = [1000, 10000, 100000, 1000000]
+nRange = [1000, 10000]
+
+print("done value ranges")
 
 
 
-def parameterStudyPageRankNibble():
+def parameterStudyPageRankNibble(LFRDir, nSeeds=100):
+
 	logging.info("starting parameter study")
 	# range for alpha
-	alphaRange = numpy.arange(0.01, 0.26, 0.01)
-	epsilonRange = numpy.arange(0.01, 0.11, 0.01)
 
-	# available range of LFR parameters
-	muRange = numpy.arange(0.1, 1.0, 0.05)
-	#nRange = [1000, 10000, 100000, 1000000]
-	nRange = [1000]
-
-	# number of seeds
-	nSeeds = 100
-
+	meanAccuracies = {}
 	for n in nRange:
-		n = str(n)
-		print(n)
 		for mu in muRange:
+			print(mu)
 			# load graph and ground truth
-			(G, truth) = loadLFR(n, mu)
+			(G, truth) = loadLFR(LFRDir, n, mu)
 			seeds = [G.randomNode() for i in range(nSeeds)]	# reuse the same seeds
+
+			# make data tables
+			meanAccuracy = numpy.zeros((len(alphaRange), len(epsilonRange)))
 
 			for alpha in alphaRange:
 				for epsilon in epsilonRange:
-						alpha, epsilon = round(alpha, 2), round(epsilon, 2)
-						pageRankNibble = PageRankNibble(G, alpha, epsilon)
-						(communities, accuracy) = LFRAccuracy(pageRankNibble, G, seeds, truth)
-						# aggregate accuracy over seeds
-						minAccuracy, maxAccuracy = numpy.min([a for a in accuracy.values()]), numpy.max([a for a in accuracy.values()])
-						meanAccuracy = numpy.mean([a for a in accuracy.values()])
-						print("alpha={0}, epsilon={1} -> minAccuray={2}, maxAccuracy={3}, meanAccuracy={4}".format(alpha, epsilon, minAccuracy, maxAccuracy, meanAccuracy))
+					pageRankNibble = PageRankNibble(G, alpha, epsilon)
+					(communities, accuracy) = LFRAccuracy(pageRankNibble, G, seeds, truth)
+					# aggregate accuracy over seeds
+					a, e = numpy.where(alphaRange == alpha), numpy.where(epsilonRange == epsilon)
+					meanAccuracy[a,e] = numpy.mean([a for a in accuracy.values()])
+					# print("alpha={0}, epsilon={1} -> maxAccuracy={3}, meanAccuracy={4}".format(alpha, epsilon, maxAccuracy, meanAccuracy))
 
-						
+					# make data frames
+					# meanAccuracyDf = pandas.DataFrame(meanAccuracy, index=pandas.MultiIndex.from_product([alphaRange, epsilonRange], names=['alpha', 'epsilon']))
+					# print(meanAccuracyDf)
+			meanAccuracies[(n, mu)] = meanAccuracy
+
+	return meanAccuracies
