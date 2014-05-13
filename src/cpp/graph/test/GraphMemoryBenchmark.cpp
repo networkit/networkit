9727849757b48d5a6c9a4e3d550edd993608abe7
Marvin Ritter <marvin.ritter@gmail.com>
2014-05-13 14:25:31 +0200

    added a big graph to the benchmark, save in-2004.graph from 10th DIMACS Implementation Challenge in the directory above NetworKit

--- src/cpp/graph/test/GraphMemoryBenchmark.cpp
+++ src/cpp/graph/test/GraphMemoryBenchmark.cpp
@@ -20,6 +20,8 @@ TEST_F(GraphMemoryBenchmark, shrinkToFitForErdosRenyi) {
 	GraphGenerator gen;
 	Aux::Timer timerGen, timerShrink;
 	
+
+
 	std::vector<count> graphSizes = {1000, 5000, 10000};
 	for (auto& n : graphSizes) {
 		timerGen.start();
@@ -32,14 +34,16 @@ TEST_F(GraphMemoryBenchmark, shrinkToFitForErdosRenyi) {
 		timerShrink.stop();
 		count after = G.getMemoryUsage();
 
-		printf("G(n = %lu, m = %lu), generation took %lu ms, memory used before shrink: %lu, memory used after shrink: %lu, relative saving: %f, shrinking took %lu us\n",
+		bool useMS = timerShrink.elapsedMicroseconds() > 5000;
+		printf("G(n = %lu, m = %lu), generation took %lu ms, memory used before shrink: %lu KB, memory used after shrink: %lu KB, relative saving: %f, shrinking took %lu %s\n",
 			G.numberOfNodes(),
 			G.numberOfEdges(),
 			timerGen.elapsedMilliseconds(),
-			before,
-			after,
+			before  / 1024,
+			after / 1024,
 			1.0 - (double) after / before,
-			timerShrink.elapsedMicroseconds());
+			useMS ? timerShrink.elapsedMilliseconds() : timerShrink.elapsedMicroseconds(),
+			useMS ? "ms" : "us");
 	}
 }
 
@@ -47,24 +51,30 @@ TEST_F(GraphMemoryBenchmark, shrinkToFitForGraphReader) {
 	Aux::Timer timerRead, timerShrink;
 	METISGraphReader reader;
 
-	timerRead.start();
-	Graph G = reader.read("input/caidaRouterLevel.graph");
-	timerRead.stop();
+	std::vector<std::string> graphFiles = {"input/astro-ph.graph", "input/caidaRouterLevel.graph", "../in-2004.graph"};
+	for (auto& graphFile : graphFiles) {
+		timerRead.start();
+		Graph G = reader.read(graphFile);
+		timerRead.stop();
 
-	count before = G.getMemoryUsage();
-	timerShrink.start();
-	G.shrinkToFit();
-	timerShrink.stop();
-	count after = G.getMemoryUsage();
+		count before = G.getMemoryUsage();
+		timerShrink.start();
+		G.shrinkToFit();
+		timerShrink.stop();
+		count after = G.getMemoryUsage();
 
-	printf("G(n = %lu, m = %lu), reading took %lu ms, memory used before shrink: %lu, memory used after shrink: %lu, relative saving: %f, shrinking took %lu us\n",
-		G.numberOfNodes(),
-		G.numberOfEdges(),
-		timerRead.elapsedMilliseconds(),
-		before,
-		after,
-		1.0 - (double) after / before,
-		timerShrink.elapsedMicroseconds());
+		bool useMS = timerShrink.elapsedMicroseconds() > 5000;
+		printf("G(n = %lu, m = %lu), reading %s took %lu ms, memory used before shrink: %lu KB, memory used after shrink: %lu KB, relative saving: %f, shrinking took %lu %s\n",
+			G.numberOfNodes(),
+			G.numberOfEdges(),
+			graphFile.c_str(),
+			timerRead.elapsedMilliseconds(),
+			before / 1024,
+			after / 1024,
+			1.0 - (double) after / before,
+			useMS ? timerShrink.elapsedMilliseconds() : timerShrink.elapsedMicroseconds(),
+			useMS ? "ms" : "us");
+	}
 }
 
 
