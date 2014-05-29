f1db1394c0decf5aaf060b8e078caf331e9815c3
Florian Weber <florian.weber@sfz-bw.de>
2014-05-29 02:57:45 +0200

    replace asserts that check whether a file is open with calls to Aux::enforceOpened

---
+++ src/cpp/mapping/test/MapperGTest.h
@@ -0,0 +1,28 @@
+/*
+ * MapperGTest.h
+ *
+ *  Created on: Jul 15, 2013
+ *      Author: Henning
+ */
+
+#ifndef NOGTEST
+
+#ifndef MAPPERGTEST_H_
+#define MAPPERGTEST_H_
+
+#include <gtest/gtest.h>
+
+
+namespace NetworKit {
+
+class MapperGTest: public testing::Test {
+public:
+	MapperGTest();
+	virtual ~MapperGTest();
+};
+
+}
+
+#endif /* MAPPERGTEST_H_ */
+
+#endif
