2ebe354b069b2fb27b14d9458642dcd3420b894f
Marvin Ritter <marvin.ritter@gmail.com>
2014-05-29 13:46:51 +0200

    found better sized graphs for community detection benchmarks

--- src/cpp/community/test/CommunityDetectionBenchmark.cpp
+++ src/cpp/community/test/CommunityDetectionBenchmark.cpp
@@ -7,6 +7,8 @@
 
 #ifndef NOGTEST
 
+#include <map>
+
 #include "CommunityDetectionBenchmark.h"
 #include "../PLP.h"
 #include "../PLM.h"
@@ -22,97 +24,69 @@ void CommunityDetectionBenchmark::SetUp() {
 
 }
 
-TEST_F(CommunityDetectionBenchmark, timePLP) {
+TEST_F(CommunityDetectionBenchmark, timeClusteringAlgos) {
 	Aux::Timer timer;
-
-	std::string graph = "~/graphs/in-2004.graph";
-	Graph G = this->metisReader.read(graph);
-
-	std::string name = "PLP";
-	CommunityDetectionAlgorithm* algo = new PLP;
-
-	timer.start();
-	Partition zeta = algo->run(G);
-	timer.stop();
-
 	Modularity mod;
-	auto communitySizes = zeta.subsetSizes();
-	delete algo;
 
-	printf("%s on %s: %.1f s\n\t# communities: %lu\n\tmodularity: %f\n",
-		name.c_str(), graph.c_str(),
-		timer.elapsedMilliseconds() / 1000.0,
-		zeta.numberOfSubsets(),
-		mod.getQuality(zeta, G));
-}
-
-TEST_F(CommunityDetectionBenchmark, timePLM) {
-
-	std::string graph = "~/graphs/in-2004.graph";
-	Graph G = this->metisReader.read(graph);
-
-	std::string name = "PLM";
-	CommunityDetectionAlgorithm* algo = new PLM;
-
+	// std::string graph = "../graphs/uk-2002.graph";
+	std::string graph = "../graphs/uk-2007-05.graph";
+	printf("Reading graph file %s ...\n", graph.c_str());
 	timer.start();
-	Partition zeta = algo->run(G);
+	const Graph G = this->metisReader.read(graph);
 	timer.stop();
+	printf("Reading graph took %.1f s\n", timer.elapsedMilliseconds() / 1000.0);
 
-	Modularity mod;
-	auto communitySizes = zeta.subsetSizes();
-	delete algo;
+	std::map<std::string, CommunityDetectionAlgorithm*> algos = {
+		std::make_pair("Parallel Label Propagation", (CommunityDetectionAlgorithm*) new PLP),
+		std::make_pair("Parallel Louvain", (CommunityDetectionAlgorithm*) new PLM)
+	};
 
-	printf("%s on %s: %.1f s\n\t# communities: %lu\n\tmodularity: %f\n",
-		name.c_str(), graph.c_str(),
-		timer.elapsedMilliseconds() / 1000.0,
-		zeta.numberOfSubsets(),
-		mod.getQuality(zeta, G));
+	for (auto it = algos.begin(); it != algos.end(); it++) {
+		Graph Gcopy = G;
+
+		printf("Timing %s ...\n", it->first.c_str());
+		timer.start();
+		Partition zeta = it->second->run(Gcopy);
+		timer.stop();
+
+		auto communitySizes = zeta.subsetSizes();
+
+		printf("%s on %s: %.1f s\n\t# communities: %lu\n\tmodularity: %f\n",
+			it->first.c_str(), graph.c_str(),
+			timer.elapsedMilliseconds() / 1000.0,
+			zeta.numberOfSubsets(),
+			mod.getQuality(zeta, G));
+		
+		delete it->second;
+	}
 }
 
-TEST_F(CommunityDetectionBenchmark, timeBetweennessCentrality) {
+TEST_F(CommunityDetectionBenchmark, timeCentralities) {
 	Aux::Timer timer;
 
+	std::string graph = "../graphs/cond-mat-2005.graph";
+	const Graph G = this->metisReader.read(graph);
 
-	std::string graph = "~/graphs/in-2004.graph";
-	Graph G = this->metisReader.read(graph);
+	Graph G1 = this->metisReader.read("../graphs/cond-mat.graph");
+	Graph G2 = this->metisReader.read("../graphs/uk-2002.graph");
+	std::map<std::string, Centrality*> cens = {
+		std::make_pair("Betweenness Centrality on cond-mat", (Centrality*) new Betweenness(G1)),
+		std::make_pair("Page Rank Centrality on uk-2002", (Centrality*) new PageRank(G2, 1e-6))
+	};
 
-	Centrality* cen = new Betweenness(G, 1e-6);
-	std::string name = "Betweenness Centrality";
+	for (auto it = cens.begin(); it != cens.end(); it++) {
+		timer.start();
+		it->second->run();
+		timer.stop();
+		auto ranking = it->second->ranking();
 
-	timer.start();
-	cen->run();
-	timer.stop();
-	auto ranking = cen->ranking();
-	delete cen;
-
-	printf("%s on %s: %.1f s\n\tranking: [(%lu: %f), (%lu: %f), ...]\n",
-		name.c_str(), graph.c_str(),
-		timer.elapsedMilliseconds() / 1000.0,
-		ranking[0].first, ranking[0].second,
-		ranking[1].first, ranking[1].second); 
-}
-
-TEST_F(CommunityDetectionBenchmark, timePageRankCentrality) {
-	Aux::Timer timer;
-
-	std::string graph = "~/graphs/in-2004.graph";
-	Graph G = this->metisReader.read(graph);
-
-	Centrality* cen = new PageRank(G, 1e-6);
-	std::string name = "Page Rank Centrality";
-
-	timer.start();
-	cen->run();
-	timer.stop();
-	auto ranking = cen->ranking();
-	delete cen;
-
-	printf("%s on %s: %.1f s\n\tranking: [(%lu: %f), (%lu: %f), ...]\n",
-		name.c_str(), graph.c_str(),
-		timer.elapsedMilliseconds() / 1000.0,
-		ranking[0].first, ranking[0].second,
-		ranking[1].first, ranking[1].second); 
-}
+		printf("%s: %.1f s\n\tranking: [(%lu: %f), (%lu: %f), ...]\n",
+			it->first.c_str(),
+			timer.elapsedMilliseconds() / 1000.0,
+			ranking[0].first, ranking[0].second,
+			ranking[1].first, ranking[1].second); 
+	}
+}	
 
 } /* namespace NetworKit */
 
