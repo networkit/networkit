4edd243e5fd99663b7762ff4969b01aefe2344d6
Marvin Ritter <marvin.ritter@gmail.com>
2014-05-29 14:16:40 +0200

    merged benchmarks

--- src/cpp/community/test/CommunityDetectionBenchmark.cpp
+++ src/cpp/community/test/CommunityDetectionBenchmark.cpp
@@ -7,12 +7,14 @@
 
 #ifndef NOGTEST
 
+#include <map>
+
 #include "CommunityDetectionBenchmark.h"
 #include "../PLP.h"
 #include "../PLM.h"
 #include "../Modularity.h"
 #include "../../centrality/Betweenness.h"
-#include "../../centrality/ApproxBetweenness.h"
+#include "../../centrality/PageRank.h"
 #include "../../auxiliary/Timer.h"
 #include "../../structures/Partition.h"
 
@@ -22,60 +24,69 @@ void CommunityDetectionBenchmark::SetUp() {
 
 }
 
-TEST_F(CommunityDetectionBenchmark, timePLP) {
+TEST_F(CommunityDetectionBenchmark, timeClusteringAlgos) {
 	Aux::Timer timer;
 	Modularity mod;
-	PLP plp;
 
-	// Graph G = this->metisReader.read("/mnt/windows/Users/Marvin/Desktop/uk-2002.graph");
-	Graph G = this->metisReader.read("../in-2004.graph");
-	
+	// std::string graph = "../graphs/uk-2002.graph";
+	std::string graph = "../graphs/uk-2007-05.graph";
+	printf("Reading graph file %s ...\n", graph.c_str());
 	timer.start();
-	Partition zeta = plp.run(G);
+	const Graph G = this->metisReader.read(graph);
 	timer.stop();
+	printf("Reading graph took %.1f s\n", timer.elapsedMilliseconds() / 1000.0);
 
-	// communitySizes = zeta.subsetSizes();
-	printf("PLP: %.2f s\n\t# communities: %lu\n\tmodularity: %f\n",
-		timer.elapsedMilliseconds() / 1000.0,
-		zeta.numberOfSubsets(),
-		mod.getQuality(zeta, G));
+	std::map<std::string, CommunityDetectionAlgorithm*> algos = {
+		std::make_pair("Parallel Label Propagation", (CommunityDetectionAlgorithm*) new PLP),
+		std::make_pair("Parallel Louvain", (CommunityDetectionAlgorithm*) new PLM)
+	};
+
+	for (auto it = algos.begin(); it != algos.end(); it++) {
+		Graph Gcopy = G;
+
+		printf("Timing %s ...\n", it->first.c_str());
+		timer.start();
+		Partition zeta = it->second->run(Gcopy);
+		timer.stop();
+
+		auto communitySizes = zeta.subsetSizes();
+
+		printf("%s on %s: %.1f s\n\t# communities: %lu\n\tmodularity: %f\n",
+			it->first.c_str(), graph.c_str(),
+			timer.elapsedMilliseconds() / 1000.0,
+			zeta.numberOfSubsets(),
+			mod.getQuality(zeta, G));
+		
+		delete it->second;
+	}
 }
 
-TEST_F(CommunityDetectionBenchmark, timePLM) {
+TEST_F(CommunityDetectionBenchmark, timeCentralities) {
 	Aux::Timer timer;
-	Modularity mod;
-	PLM plm;
 
-	// Graph G = this->metisReader.read("/mnt/windows/Users/Marvin/Desktop/uk-2002.graph");
-	Graph G = this->metisReader.read("../in-2004.graph");
+	std::string graph = "../graphs/cond-mat-2005.graph";
+	const Graph G = this->metisReader.read(graph);
 
-	timer.start();
-	Partition zeta = plm.run(G);
-	timer.stop();
+	Graph G1 = this->metisReader.read("../graphs/cond-mat.graph");
+	Graph G2 = this->metisReader.read("../graphs/uk-2002.graph");
+	std::map<std::string, Centrality*> cens = {
+		std::make_pair("Betweenness Centrality on cond-mat", (Centrality*) new Betweenness(G1)),
+		std::make_pair("Page Rank Centrality on uk-2002", (Centrality*) new PageRank(G2, 1e-6))
+	};
 
-	// communitySizes = zeta.subsetSizes();
-	printf("PLM: %.2f s\n\t# communities: %lu\n\tmodularity: %f\n",
-		timer.elapsedMilliseconds() / 1000.0,
-		zeta.numberOfSubsets(),
-		mod.getQuality(zeta, G));
-}
+	for (auto it = cens.begin(); it != cens.end(); it++) {
+		timer.start();
+		it->second->run();
+		timer.stop();
+		auto ranking = it->second->ranking();
 
-TEST_F(CommunityDetectionBenchmark, timeApproxBetweennessCentrality) {
-	Aux::Timer timer;
-
-	Graph G = this->metisReader.read("../cnr-2000.graph");
-
-	timer.start();
-	ApproxBetweenness bc(G, 0.1);
-	bc.run();
-	timer.stop();
-
-	printf("Approximated Betweenness Centrality: %.2f s\n\tscore: [%f, %f, %f, ...]\n",
-		timer.elapsedMilliseconds() / 1000.0,
-		bc.score(0),
-		bc.score(1),
-		bc.score(2));
-}
+		printf("%s: %.1f s\n\tranking: [(%lu: %f), (%lu: %f), ...]\n",
+			it->first.c_str(),
+			timer.elapsedMilliseconds() / 1000.0,
+			ranking[0].first, ranking[0].second,
+			ranking[1].first, ranking[1].second); 
+	}
+}	
 
 } /* namespace NetworKit */
 
