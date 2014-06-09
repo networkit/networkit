5e360ac0254edde2ce522658bd4a22fcb19fe12d
Gerd Lindner <gerd.lindner@student.kit.edu>
2014-06-09 09:04:26 +0200

    Calculation of Best prefix jaccard coefficient added.

--- src/cpp/backbones/SimmelianBackbone.h
+++ src/cpp/backbones/SimmelianBackbone.h
@@ -25,26 +25,42 @@ struct RankedEdge {
 	count simmelianness; 	//The number of triangles the edge is embedded in.
 	count rank; 			//The rank within the ranked neighborhood.
 
-	RankedEdge (node ego, node alter, count s) : ego(ego), alter(alter), simmelianness(s), rank(0) {
+	RankedEdge(node ego, node alter, count s) :
+			ego(ego), alter(alter), simmelianness(s), rank(0) {
 	}
 
-	RankedEdge (node ego, node alter, count s, count r) : ego(ego), alter(alter), simmelianness(s), rank(r) {
+	RankedEdge(node ego, node alter, count s, count r) :
+			ego(ego), alter(alter), simmelianness(s), rank(r) {
 	}
 
 	bool operator<(const RankedEdge& other) const {
-		return (simmelianness > other.simmelianness) || (simmelianness == other.simmelianness && alter < other.alter);
+		return (simmelianness > other.simmelianness)
+				|| (simmelianness == other.simmelianness && alter < other.alter);
 	}
 
 	bool operator>(const RankedEdge& other) const {
-		return (simmelianness < other.simmelianness) || (simmelianness == other.simmelianness && alter > other.alter);
+		return (simmelianness < other.simmelianness)
+				|| (simmelianness == other.simmelianness && alter > other.alter);
 	}
 
 	bool operator==(const RankedEdge& other) const {
-		return ego == other.ego && simmelianness == other.simmelianness && alter == other.alter && rank == other.rank;
+		return ego == other.ego && simmelianness == other.simmelianness
+				&& alter == other.alter && rank == other.rank;
 	}
 };
 
 typedef std::vector<RankedEdge> RankedNeighbors;
+
+/**
+ * Represents the result of the comparison of two ranked neighborhood lists, namely
+ * the overlap of the top-k neighbors and the maximum jaccard index.
+ */
+struct Redundancy {
+	count overlap;
+	double jaccard;
+
+	Redundancy(count o, double j) : overlap(o), jaccard(j) { }
+};
 
 /** 
  * Calculates the simmelian backbone for a given input graph.
@@ -66,14 +82,15 @@ private:
 	FRIEND_TEST(SimmelianBackboneGTest, testRankedNeighborhoodSkippedRanks);
 	FRIEND_TEST(SimmelianBackboneGTest, testOverlapFiltering);
 	std::vector<RankedNeighbors> getRankedNeighborhood(const Graph& g, edgeCountMap& triangles);
-	count getOverlap(const RankedNeighbors& first, const RankedNeighbors& second, const count& maxRank);
+	Redundancy getOverlap(const RankedNeighbors& first, const RankedNeighbors& second, const count& maxRank);
 	void matchNeighbors(std::vector<RankedEdge>::const_iterator& egoIt,
-						const RankedNeighbors& egoNeighbors,
-						std::set<node>& egoNeighborsUnmatched,
-						std::set<node>& alterNeighborsUnmatched,
-						const count& rank,
-						count& overlap);
+			const RankedNeighbors& egoNeighbors,
+			std::set<node>& egoNeighborsUnmatched,
+			std::set<node>& alterNeighborsUnmatched,
+			const count& rank,
+			count& overlap);
 };
 
-} /* namespace NetworKit */
+}
+/* namespace NetworKit */
 #endif /* SIMMELIANBACKBONE_H_ */
