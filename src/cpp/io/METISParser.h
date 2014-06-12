cdbe3a012604f806bcc22239f5e29f2b3fd94a71
cls <christian.staudt@kit.edu>
2014-06-12 11:15:59 +0200

    fixed bug in nxadapter

---
+++ src/cpp/io/METISParser.h
@@ -0,0 +1,65 @@
+/*
+ * METISParser.h
+ *
+ *  Created on: 27.11.2012
+ *      Author: Christian Staudt (christian.staudt@kit.edu)
+ */
+
+#ifndef METISPARSER_H_
+#define METISPARSER_H_
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <utility>
+#include <stdexcept>
+#include <tuple>
+
+#include "../auxiliary/Log.h"
+
+
+namespace NetworKit {
+
+typedef uint64_t index;
+typedef index node;
+typedef uint64_t count;
+
+/**
+ * Parser for the METIS file format.
+ */
+class METISParser {
+
+protected:
+
+	std::ifstream graphFile;
+
+
+public:
+
+	METISParser(std::string path);
+
+	virtual ~METISParser();
+
+
+	/**
+	 * Get the METIS graph file header
+	 */
+	std::tuple<count, count, index> getHeader();
+
+	/**
+	 * Test if graph file has a next line.
+	 */
+	bool hasNext();
+
+	/**
+	 * Get adjacencies from the next line in the METIS graph file.
+	 */
+	std::vector<node> getNext();
+
+	std::vector<std::pair<node,double>> getNextWithWeights();
+
+};
+} /* namespace NetworKit */
+#endif /* METISPARSER_H_ */
