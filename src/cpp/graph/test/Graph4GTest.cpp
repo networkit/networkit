5f484518c724f43d9c95af544449222a1757822e
Marvin Ritter <marvin.ritter@gmail.com>
2014-06-13 17:20:37 +0200

    added test case for Graph.randomEdge()

--- src/cpp/graph/test/Graph4GTest.cpp
+++ src/cpp/graph/test/Graph4GTest.cpp
@@ -625,7 +625,31 @@ TEST_P(Graph4GTest, testHasEdge) {
 }
 
 TEST_P(Graph4GTest, testRandomEdge) {
+	// we only test the uniform version
 
+	count n = 4;
+	count m = 5;
+	count samples = 50000;
+	double maxAbsoluteError = 0.005;
+
+	Graph G = createParameterizedGraph(n);
+	G.addEdge(0, 1); // 0 * 1 = 0
+	G.addEdge(1, 2); // 1 * 2 = 2
+	G.addEdge(3, 2); // 3 * 2 = 1 (mod 5)
+	G.addEdge(2, 2); // 2 * 2 = 4
+	G.addEdge(3, 1); // 3 * 1 = 3
+	ASSERT_EQ(m, G.numberOfEdges());
+
+	std::vector<count> drawCounts(m, 0);
+	for (count i = 0; i < samples; i++) {
+		auto e = G.randomEdge(true);
+		count id = (e.first * e.second) % 5;
+		drawCounts[id]++;
+	}
+	for (node id = 0; id < m; id++) {
+		double p = drawCounts[id] / (double) samples;
+		ASSERT_NEAR(1.0 / m, p, maxAbsoluteError);
+	}
 }
 
 
