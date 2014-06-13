cc21c15b5017a1d2935d4f32ca484f44363a3975
cls <christian.staudt@kit.edu>
2014-06-13 15:13:27 +0200

    added ApproxBetweenness2

---
+++ src/cpp/centrality/ApproxBetweenness2.cpp
@@ -0,0 +1,68 @@
+/*
+* ApproxBetweenness2.cpp
+*
+*  Created on: 13.06.2014
+*      Author: Christian Staudt, Elisabetta Bergamini
+*/
+
+
+#include "ApproxBetweenness2.h"
+#include "../graph/BFS.h"
+#include "../graph/Dijkstra.h"
+#include "../graph/SSSP.h"
+
+namespace NetworKit {
+
+ApproxBetweenness2::ApproxBetweenness2(const Graph& G, count nSamples, bool normalized) : Centrality(G, normalized), nSamples(nSamples) {
+}
+
+void ApproxBetweenness2::run() {
+	scoreData = std::vector<double>(G.upperNodeIdBound(), 0.0);
+
+	std::vector<node> sampledNodes = G.nodes();
+
+	// // sample nodes
+	// for (count i = 0; i <= nSamples; ++i) {
+	// 	sampledNodes.push_back(G.randomNode());
+	// }
+
+	for (node s : sampledNodes) {
+		// run single-source shortest path algorithm
+		SSSP* sssp;
+		if (G.isWeighted()) {
+			sssp = new Dijkstra(G, s);
+		} else {
+			sssp = new BFS(G, s);
+		}
+
+		sssp->run();
+
+		// create stack of nodes in non-decreasing order of distance
+		std::vector<node> stack = G.nodes();
+		std::sort(stack.begin(), stack.end(), [&](node u, node v){
+			return (sssp->distance(u) > sssp->distance(v));
+		});
+
+		// compute dependencies and add the contributions to the centrality score
+		std::vector<double> dependency(G.upperNodeIdBound(), 0.0);
+		for (node t : stack) {
+			for (node p : sssp->getPredecessors(t)) {
+				// TODO: make weighting factor configurable
+				// (sssp->distance(p) / sssp->distance(t))
+				dependency[p] += (sssp->numberOfPaths(p) / sssp->numberOfPaths(t)) * (1 + dependency[t]);
+			}
+
+			scoreData[t] += dependency[t];
+		}
+
+	} // end for sampled nodes
+
+	// extrapolate
+	G.forNodes([&](node v) {
+		scoreData[v] = scoreData[v] * (G.numberOfNodes() / nSamples);
+	});
+
+}
+
+
+} /* namespace NetworKit */
