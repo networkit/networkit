436fcea4b957107daf75b8145641d36dd55be720
Marc Nemes <uxcqb@student.kit.edu>
2014-06-18 10:43:14 +0200

    moved EffectiveDiameter GTest to Properties GTest

--- src/cpp/properties/test/PropertiesGTest.cpp
+++ src/cpp/properties/test/PropertiesGTest.cpp
@@ -421,6 +421,22 @@ TEST_F(PropertiesGTest, testDegreeAssortativity) {
 //	assortativity = GraphProperties::degreeAssortativity(cnr);
 }
 
+TEST_F(PropertiesGTest, testEffectiveDiameter) {
+	using namespace std;
+	vector<pair<string, int>> testInstances= {
+		pair<string, int>("celegans_metabolic", 6),
+		pair<string, int>("jazz", 5),
+		pair<string, int>("karate", 5),
+		pair<string, int>("lesmis", 5)
+	};
+
+	for (auto testInstance : testInstances) {
+		METISGraphReader reader;
+		Graph G = reader.read("input/" + testInstance.first + ".graph");
+		int diameter = EffectiveDiameter::effectiveDiameter(G);
+		EXPECT_EQ(diameter, testInstance.second);
+	}
+}
 
 } /* namespace NetworKit */
 
