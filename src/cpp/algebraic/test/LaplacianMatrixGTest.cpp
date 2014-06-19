6f531da9f2792411bb6ead3fb3e51362e50fea39
Daniel Hoske <daniel.hoske@gmail.com>
2014-06-19 22:29:28 +0200

    Added conjugate gradient method.

---
+++ src/cpp/algebraic/test/LaplacianMatrixGTest.cpp
@@ -0,0 +1,77 @@
+/*
+ * LaplacianMatrixGTest.cpp
+ *
+ *  Created on: 25.03.2014
+ *      Author: Michael Wegner (michael.wegner@student.kit.edu)
+ */
+
+#include "LaplacianMatrixGTest.h"
+
+namespace NetworKit {
+
+LaplacianMatrixGTest::LaplacianMatrixGTest() {
+}
+
+LaplacianMatrixGTest::~LaplacianMatrixGTest() {
+}
+
+TEST(LaplacianMatrixGTest, trySmallLaplacianMatrix) {
+	NetworKit::Graph graph(6);
+	graph.addEdge(0, 0); // self-loop
+	graph.addEdge(0, 1);
+	graph.addEdge(0, 4);
+	graph.addEdge(1, 4);
+	graph.addEdge(1, 2);
+	graph.addEdge(2, 3);
+	graph.addEdge(3, 4);
+	graph.addEdge(3, 5);
+
+	LaplacianMatrix laplacianMatrix(graph);
+	ASSERT_EQ(graph.numberOfNodes(), laplacianMatrix.numberOfRows());
+	ASSERT_EQ(graph.numberOfNodes(), laplacianMatrix.numberOfRows());
+
+	EXPECT_EQ(2, laplacianMatrix(0,0));
+	EXPECT_EQ(-1, laplacianMatrix(0,1));
+	EXPECT_EQ(0, laplacianMatrix(0,2));
+	EXPECT_EQ(0, laplacianMatrix(0,3));
+	EXPECT_EQ(-1, laplacianMatrix(0,4));
+	EXPECT_EQ(0, laplacianMatrix(0,5));
+	EXPECT_EQ(3, laplacianMatrix(1,1));
+	EXPECT_EQ(-1, laplacianMatrix(1,2));
+	EXPECT_EQ(0, laplacianMatrix(1,3));
+	EXPECT_EQ(-1, laplacianMatrix(1,4));
+	EXPECT_EQ(0, laplacianMatrix(1,5));
+	EXPECT_EQ(2, laplacianMatrix(2,2));
+	EXPECT_EQ(-1, laplacianMatrix(2,3));
+	EXPECT_EQ(0, laplacianMatrix(2,4));
+	EXPECT_EQ(0, laplacianMatrix(2,5));
+	EXPECT_EQ(3, laplacianMatrix(3,3));
+	EXPECT_EQ(-1, laplacianMatrix(3,4));
+	EXPECT_EQ(-1, laplacianMatrix(3,5));
+	EXPECT_EQ(3, laplacianMatrix(4,4));
+	EXPECT_EQ(0, laplacianMatrix(4,5));
+	EXPECT_EQ(1, laplacianMatrix(5,5));
+}
+
+TEST(LaplacianMatrixGTest, tryLaplacianMatrixOfLesmisGraph) {
+	// read lesmis graph
+	NetworKit::METISGraphReader graphReader;
+	NetworKit::Graph graph = graphReader.read("input/lesmis.graph");
+
+	// create LaplacianMatrix
+	LaplacianMatrix mat(graph);
+
+	mat.forElementsInRowOrder([&](const index row, const index column, const double value){
+		if (row == column) {
+			EXPECT_EQ(graph.weightedDegree(row) - graph.weight(row, row), value);
+		} else {
+			EXPECT_EQ(-graph.weight(row, column), value);
+		}
+	});
+
+}
+
+
+
+} /* namespace NetworKit */
+
---
+++ src/cpp/community/SampledGraphStructuralRandMeasure.h
@@ -0,0 +1,43 @@
+/*
+ * SampledRandMeasure.h
+ *
+ *  Created on: 01.07.2013
+ *      Author: cls
+ */
+
+#ifndef SAMPLEDGRAPHSTRUCTURALRANDMEASURE_H_
+#define SAMPLEDGRAPHTRUCTURALRANDMEASURE_H_
+
+#include "DissimilarityMeasure.h"
+
+namespace NetworKit {
+
+/**
+ * The graph-structural Rand measure assigns a similarity value in [0,1]
+ * to two partitions of a graph, by considering connected pairs of nodes.
+ * This implementation approximates the index by sampling.
+ */
+class SampledGraphStructuralRandMeasure: public NetworKit::DissimilarityMeasure {
+
+public:
+
+	/**
+	 * Constructs the SampledGraphStructuralRandMeasure. A maximum of @a maxSamples samples are drawn.
+	 *
+	 * @param maxSamples The amount of samples to draw.
+	 */
+	SampledGraphStructuralRandMeasure(count maxSamples);
+
+	/** Default destructor */
+	virtual ~SampledGraphStructuralRandMeasure();
+
+	virtual double getDissimilarity(Graph& G, Partition& first, Partition& second);
+
+protected:
+
+	count maxSamples;
+
+};
+
+} /* namespace NetworKit */
+#endif /* SAMPLEDRANDMEASURE_H_ */
