a3e4d24983ee1a3afe19942889273daa583db83d
Gerd Lindner <gerd.lindner@student.kit.edu>
2014-06-21 08:44:00 +0200

    Multiscale backbone: normalization implemented

--- src/cpp/backbones/MultiscaleBackbone.cpp
+++ src/cpp/backbones/MultiscaleBackbone.cpp
@@ -15,14 +15,28 @@ MultiscaleBackbone::MultiscaleBackbone(const Graph& g, double a) :
 Graph MultiscaleBackbone::calculate() {
 	Graph backboneGraph = cloneGraphWithoutEdges(graph);
 
+	//The following vector is used for the _local_ normalization of edgeweights.
+	//We use a global vector for performance reasons.
+	std::vector<edgeweight> normalizedWeights(graph.upperNodeIdBound());
+
 	graph.forNodes([&](node u) {
 		int k = graph.degree(u);
 
+		//Normalize edgeweights
+		edgeweight maxEdgeweight = 0.0;
 		graph.forNeighborsOf(u, [&](node v) {
-			edgeweight p = graph.weight(u, v);
+			maxEdgeweight = std::max(maxEdgeweight, graph.weight(u, v));
+		});
+		graph.forNeighborsOf(u, [&](node v) {
+			normalizedWeights[v] = graph.weight(u, v) / maxEdgeweight;
+		});
+
+		//Filter edges by probability calculation
+		graph.forNeighborsOf(u, [&](node v) {
+			edgeweight p = normalizedWeights[v];
 			double a = 1 - (1 - pow(p - 1, k - 1));
 			if (a < alpha)
-				backboneGraph.setWeight(u, v, p);
+				backboneGraph.setWeight(u, v, graph.weight(u, v));
 		});
 	});
 
--- src/cpp/backbones/test/MultiscaleBackboneGTest.cpp
+++ src/cpp/backbones/test/MultiscaleBackboneGTest.cpp
@@ -16,12 +16,14 @@ namespace NetworKit {
 TEST_F(MultiscaleBackboneGTest, testSimple) {
 	Graph g(6, true, false);
 
-	g.setWeight(0, 1, 0.39);
-	g.setWeight(0, 2, 0.01);
-	g.setWeight(0, 3, 0.5);
-	g.setWeight(0, 4, 0.1);
-	g.setWeight(4, 5, 1);
-	g.setWeight(3, 5, 0.1);
+	g.setWeight(0, 1, 10.0);
+	g.setWeight(0, 2, 1.0);
+	g.setWeight(0, 3, 5.0);
+	g.setWeight(0, 4, 1.0);
+	g.setWeight(4, 5, 1.0);
+	g.setWeight(3, 5, 2.0);
+
+
 
 }
 
