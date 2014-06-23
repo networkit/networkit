919186947089d9e6646e5fd3dd17df7c5ec5e633
Marc Nemes <uxcqb@student.kit.edu>
2014-06-24 00:56:33 +0200

    adapted NetworKit style

--- src/cpp/properties/EffectiveDiameter.cpp
+++ src/cpp/properties/EffectiveDiameter.cpp
@@ -1,66 +1,74 @@
+/*
+ * EffectiveDiameter.cpp
+ *
+ *  Created on: 16.06.2014
+ *      Author: Marc Nemes
+ */
+
 #include "EffectiveDiameter.h"
+#include "ConnectedComponents.h"
+
+#include <math.h>
+#include <iterator>
 
 namespace NetworKit {
-	/*
-	void EffectiveDiameter::foobar() {
-		using namespace std;
 
-		std::cout << "test";
+count EffectiveDiameter::effectiveDiameter(const Graph& G) {
+	// Check whether the graph is connected
+	ConnectedComponents cc(G);
+	cc.run();
+	if (cc.numberOfComponents() > 1) {
+		throw std::runtime_error("Graph not connected - diameter is infinite");
 	}
-	*/
 
-	int EffectiveDiameter::effectiveDiameter(const Graph& G) {
-		//TODO: check whether graph is connected
+	// list of nodes that are connected to all other nodes
+	std::set<node> finished;
+	// diameter[node][distance][connected_nodes]
+	std::vector<std::vector<std::set<node> > > diameter;
+	// initialize all nodes
+	G.forNodes([&](node v){
+		std::set<node> connectedNodes;
+		std::vector<std::set<node> > inner;
+		// at the beginning, nodes are only connected to themselves
+		connectedNodes.insert(v);
+		// connect all nodes with themselves at the distance 0
+		inner.push_back(connectedNodes);
+		diameter.push_back(inner);
+	});
 
-		// list of nodes that are connected to all other nodes
-		std::set<int> finished;
-		// diameter[node][distance][connected_nodes]
-		std::vector<std::vector<std::set<int> > > diameter;
-		// initialize all nodes
+	// current diameter
+	count d = 1;
+	// number of nodes that are connected to all other nodes
+	count i = 0;
+	// number of nodes that need to be connected with all other nodes
+	count threshold = (uint64_t) (ceil(0.9 * G.numberOfNodes()) + 0.5);
+	// as long as we need to connect more nodes
+	while (i < threshold) {
 		G.forNodes([&](node v){
-			std::set<int> list;
-			std::vector<std::set<int> > inner;
-			// at the beginning, nodes are only connected to themselves
-			list.insert(v);
-			// connect all nodes with themselves at the distance 0
-			inner.push_back(list);
-			diameter.push_back(inner);
-
-		});
-
-		// current diameter
-		int d = 1;
-		// number of nodes that are connected to all other nodes
-		uint64_t i = 0;
-		// number of nodes that need to be connected with all other nodes
-		uint64_t threshold = (uint64_t) (ceil(0.9 * G.numberOfNodes()) + 0.5);
-		// as long as we need to connect more nodes
-		while (i < threshold) {
-			G.forNodes([&](node v){
-				// only consider nodes that are not already connected to every other node
-				if (finished.find(v) == finished.end()) {
-					// the node is connected to all nodes from the previous iteration
-					std::set<int> list = diameter[v][d-1];
-					diameter[v].push_back(list);
-					// and to all previous connected nodes of all neighbors
-					G.forNeighborsOf(v, [&](node u) {
-						//diameter[v][d] = diameter[u][d-1];
-						std::set<int>::iterator it;
-						for (it = diameter[u][d-1].begin(); it != diameter[u][d-1].end(); ++it)	{
-						    // add the current neighbor of u to the neighborhood of v
-						    diameter[v][d].insert(*it);
-						}
-					});
-					// do no longer consider this node once it's connected to all nodes
-					if (diameter[v][d].size() == G.numberOfNodes()) {
-						finished.insert(v);
-						i++;
+			// only consider nodes that are not already connected to every other node
+			if (finished.find(v) == finished.end()) {
+				// the node is connected to all nodes from the previous iteration
+				std::set<node> connectedNodes = diameter[v][d-1];
+				diameter[v].push_back(connectedNodes);
+				// and to all previous connected nodes of all neighbors
+				G.forNeighborsOf(v, [&](node u) {
+					for (auto it : diameter[u][d-1]) {
+						// add the current neighbor of u to the neighborhood of v
+						diameter[v][d].insert(it);
 					}
+				});
+				// do no longer consider this node once it's connected to all nodes
+				if (diameter[v][d].size() == G.numberOfNodes()) {
+					finished.insert(v);
+					i++;
 				}
-			});
-			d++;
-		}
-		// return the found effective diameter
-		return d-1;
+			}
+		});
+		d++;
+		std::cout << d << std::endl;
 	}
+	// return the found effective diameter
+	return d-1;
+}
+
 }
--- src/cpp/properties/EffectiveDiameter.h
+++ src/cpp/properties/EffectiveDiameter.h
@@ -1,19 +1,14 @@
 /*
- * Eccentricity.h
+ * EffectiveDiameter.h
  *
- *  Created on: 19.02.2014
- *      Author: cls
+ *  Created on: 16.06.2014
+ *      Author: Marc Nemes
  */
 
 #ifndef EFFECTIVEDIAMETER_H_
 #define EFFECTIVEDIAMETER_H_
 
 #include "../graph/Graph.h"
-
-#include <vector>
-#include <set>
-#include <math.h>
-#include <iterator>
 
 namespace NetworKit {
 
@@ -22,8 +17,11 @@ class EffectiveDiameter {
 public:
 	/**
 	 * determines the effective diameter of a given graph
+	 * the effective diameter is the smallest distance so that 90% percent of the nodes are connected to all other nodes within that distance
+	 * @param G the given graph
+	 * @return the effective diameter of the graph
 	 */
-	static int effectiveDiameter(const Graph& G);
+	static count effectiveDiameter(const Graph& G);
 
 };
 
