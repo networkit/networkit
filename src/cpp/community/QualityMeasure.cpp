5dd7a030ef055466990d966de5f95e34785e8a04
Florian Weber <florian.weber@sfz-bw.de>
2014-06-23 18:45:18 +0200

    add unittest for overflow-checks of number-parser

---
+++ src/cpp/community/QualityMeasure.cpp
@@ -0,0 +1,17 @@
+/*
+ * QualityMeasure.cpp
+ *
+ *  Created on: 10.12.2012
+ *      Author: Christian Staudt (christian.staudt@kit.edu)
+ */
+
+#include "QualityMeasure.h"
+
+namespace NetworKit {
+
+QualityMeasure::~QualityMeasure() {
+}
+
+
+} /* namespace NetworKit */
+
---
+++ src/cpp/viz/test/VizGTest.cpp
@@ -0,0 +1,207 @@
+/*
+ * PostscriptWriterGTest.cpp
+ *
+ *  Created on: Apr 10, 2013
+ *      Author: Henning
+ */
+
+#ifndef NOGTEST
+
+#include "VizGTest.h"
+#include <vector>
+
+#include "../PostscriptWriter.h"
+#include "../FruchtermanReingold.h"
+#include "../MaxentStress.h"
+#include "../MultilevelLayouter.h"
+#include "../../graph/Graph.h"
+#include "../../graph/GraphGenerator.h"
+#include "../../community/ClusteringGenerator.h"
+#include "../../io/PartitionWriter.h"
+#include "../../io/METISGraphReader.h"
+#include "../../io/DibapGraphReader.h"
+#include "../../generators/PubWebGenerator.h"
+
+
+
+namespace NetworKit {
+
+VizGTest::VizGTest() {
+
+}
+
+VizGTest::~VizGTest() {
+
+}
+
+
+TEST_F(VizGTest, testPostscriptWriterOnRandomGraph) {
+	// create graph
+	count n = 60;
+	count numClusters = 3;
+	double pin = 0.35;
+	double pout = 0.05;
+
+	GraphGenerator graphGen;
+	Graph G = graphGen.makeClusteredRandomGraph(n, numClusters, pin, pout);
+	G.initCoordinates();
+
+	// create coordinates
+	G.forNodes([&](node u) {
+		Point<float> p(drand48(), drand48());
+		G.setCoordinate(u, p);
+	});
+
+	// write graph to file
+	PostscriptWriter psWriter;
+	psWriter.write(G, "output/testGraph.eps");
+}
+
+TEST_F(VizGTest, testPostscriptWriterOnRealGraph) {
+	// read graph and coordinates from binary file
+	DibapGraphReader reader;
+	Graph G = reader.read("input/airfoil1.gi");
+
+	// write graph to file
+	PostscriptWriter psWriter;
+	psWriter.write(G, "output/airfoil1.eps");
+}
+
+
+static float edgeDistanceSum(Graph& G) {
+	float dist = 0.0f;
+
+	G.forEdges([&](node u, node v) {
+		Point<float> p = G.getCoordinate(u) - G.getCoordinate(v);
+		dist += p.length();
+	});
+
+	return dist;
+}
+
+TEST_F(VizGTest, testFRLayouter) {
+ 	// create graph
+ 	count n = 80;
+ 	count numClusters = 3;
+ 	double pin = 0.175;
+ 	double pout = 0.005;
+
+ 	GraphGenerator graphGen;
+ 	Graph G = graphGen.makeClusteredRandomGraph(n, numClusters, pin, pout);
+ 	G.initCoordinates();
+ 	INFO("Number of edges: ", G.numberOfEdges());
+
+ 	// draw (independent of clustering) and write again
+ 	Point<float> bl(0.0, 0.0);
+ 	Point<float> tr(1.0, 1.0);
+
+ 	FruchtermanReingold fdLayouter(bl, tr);
+ 	fdLayouter.draw(G);
+ 	PostscriptWriter psWriter(true);
+ 	psWriter.write(G, "output/testForceGraph.eps");
+
+ 	// test edge distances
+ 	float dist = edgeDistanceSum(G);
+ 	float avg = dist / (float) G.numberOfEdges();
+ 	INFO("avg edge length: ", avg);
+ 	EXPECT_LE(avg, 0.25);
+}
+
+ TEST_F(VizGTest, tryMaxentLayouter) {
+  	// create graph
+  	count n = 80;
+  	count numClusters = 3;
+  	double pin = 0.175;
+  	double pout = 0.005;
+
+  	GraphGenerator graphGen;
+  	Graph G = graphGen.makeClusteredRandomGraph(n, numClusters, pin, pout);
+  	G.initCoordinates();
+  	INFO("Number of edges: ", G.numberOfEdges());
+
+  	// draw (independent of clustering) and write again
+  	Point<float> bl(0.0, 0.0);
+  	Point<float> tr(1.0, 1.0);
+
+  	MaxentStress msLayouter(bl, tr);
+ 	msLayouter.draw(G);
+  	PostscriptWriter psWriter(true);
+  	psWriter.write(G, "output/testMaxentGraph.eps");
+
+ 	// test edge distances
+ 	float dist = edgeDistanceSum(G);
+ 	float avg = dist / (float) G.numberOfEdges();
+ 	DEBUG("avg edge length: ", avg);
+ 	EXPECT_LE(avg, 0.25);
+}
+
+ TEST_F(VizGTest, tryMultilevelLayouter) {
+  	// create graph
+  	count n = 300;
+  	count numClusters = 4;
+  	double pin = 0.1;
+  	double pout = 0.005;
+
+  	GraphGenerator graphGen;
+  	Graph G = graphGen.makeClusteredRandomGraph(n, numClusters, pin, pout);
+  	G.initCoordinates();
+  	INFO("Number of edges: ", G.numberOfEdges());
+
+  	METISGraphWriter gWriter;
+  	gWriter.write(G, "output/testMultilevelGraph.graph");
+
+  	// draw (independent of clustering) and write again
+  	Point<float> bl(0.0, 0.0);
+  	Point<float> tr(1.0, 1.0);
+
+  	MultilevelLayouter mlLayouter(bl, tr);
+  	mlLayouter.draw(G);
+  	PostscriptWriter psWriter4(true);
+  	psWriter4.write(G, "output/testMultilevelGraph.eps");
+
+  	// test edge distances
+ 	float dist = edgeDistanceSum(G);
+ 	float avg = dist / (float) G.numberOfEdges();
+ 	DEBUG("avg edge length: ", avg);
+ 	EXPECT_LE(avg, 0.25);
+}
+
+ TEST_F(VizGTest, tryGraphDrawing) {
+ 	// create graph
+	METISGraphReader reader;
+	Graph G = reader.read("input/lesmis.graph");
+
+ 	// draw (independent of clustering) and write again
+ 	Point<float> bl(0.0, 0.0);
+ 	Point<float> tr(1.0, 1.0);
+
+ 	FruchtermanReingold fdLayouter(bl, tr);
+ 	fdLayouter.draw(G);
+ 	PostscriptWriter psWriter2(true);
+ 	psWriter2.write(G, "output/testLesmisFR.eps");
+
+ 	// test edge distances
+ 	float dist = edgeDistanceSum(G);
+ 	float avg = dist / (float) G.numberOfEdges();
+ 	INFO("avg edge length: ", avg);
+ 	EXPECT_LE(avg, 0.25);
+
+ 	MultilevelLayouter mlLayouter(bl, tr);
+  	mlLayouter.draw(G);
+  	PostscriptWriter psWriter4(true);
+  	psWriter4.write(G, "output/testLesmisMl.eps");
+
+ 	// test edge distances
+ 	dist = edgeDistanceSum(G);
+ 	avg = dist / (float) G.numberOfEdges();
+ 	INFO("avg edge length: ", avg);
+ 	EXPECT_LE(avg, 0.25);
+}
+
+
+} /* namespace NetworKit */
+
+
+#endif /*NOGTEST */
+
+
