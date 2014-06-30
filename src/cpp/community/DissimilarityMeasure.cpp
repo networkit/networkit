7ccda83713dbdadd9bb99dc1d3d8db3c11ff6b66
cls <christian.staudt@kit.edu>
2014-06-30 19:30:06 +0200

    Added tag 3.2 for changeset 10a94e43b862

---
+++ src/cpp/community/DissimilarityMeasure.cpp
@@ -0,0 +1,15 @@
+/*
+ * DissimilarityMeasure.cpp
+ *
+ *  Created on: 19.01.2013
+ *      Author: Christian Staudt (christian.staudt@kit.edu)
+ */
+
+#include "DissimilarityMeasure.h"
+
+namespace NetworKit {
+
+DissimilarityMeasure::~DissimilarityMeasure() {
+}
+
+} /* namespace NetworKit */
