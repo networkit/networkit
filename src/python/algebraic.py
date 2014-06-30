bb02f410b4348aa141946b6a9f08ac50b957034d
mwegner <mwegner@localhost>
2014-06-30 17:17:34 +0200

    added comments

--- src/python/algebraic.py
+++ src/python/algebraic.py
@@ -1,7 +1,18 @@
 import scipy.sparse
 
 def adjacencyMatrix(G):
-	""" Return the adjacency matrix of an undirected graph."""
+	""" Get the adjacency matrix of the undirected graph `G`.
+
+	Parameters
+	----------
+	G : Graph
+		The graph.
+
+	Returns
+	-------
+	:py:class:`scipy.sparse.csr_matrix`	
+		The adjacency matrix of the graph.
+	"""
 	n = G.numberOfNodes()
 	A = scipy.sparse.lil_matrix((n,n))
 	for (u, v) in G.edges():
@@ -11,7 +22,21 @@ def adjacencyMatrix(G):
 	return A
 
 def laplacianMatrix(G):
-	""" Return the laplacian matrix of an undirected graph"""
+	""" Get the laplacian matrix of the undirected graph `G`.
+
+	Parameters
+	----------
+	G : Graph
+		The graph.
+
+	Returns
+    -------
+    lap : ndarray
+        The N x N laplacian matrix of graph.
+    diag : ndarray
+        The length-N diagonal of the laplacian matrix.
+        diag is returned only if return_diag is True.	
+	"""
 	A = adjacencyMatrix(G)
 	return scipy.sparse.csgraph.laplacian(A)
 
