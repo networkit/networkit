360451406c63ff4902bc4261a2a154ce70ecc29e
Gerd Lindner <gerd.lindner@student.kit.edu>
2014-07-03 13:06:58 +0200

    merge

--- Doc/docs2pdf.sh
+++ Doc/docs2pdf.sh
@@ -2,7 +2,6 @@
 # get pandoc at http://code.google.com/p/pandoc/downloads/list
 
 OPTIONS="-V geometry:margin=2.5cm"
-pandoc ${OPTIONS} Profile.mdown -o Profile.pdf
-pandoc ${OPTIONS} Readme.mdown -o Readme.pdf
+pandoc ${OPTIONS} ../Readme.mdown -o Readme.pdf
 pandoc ${OPTIONS} UserGuide.mdown -o UserGuide.pdf
 pandoc ${OPTIONS} DevGuide.mdown -o DevGuide.pdf
