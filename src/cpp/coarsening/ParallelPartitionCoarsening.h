0f19d639b1f00deb227bfafd297e8b9323e30630
cls <christian.staudt@kit.edu>
2014-07-14 15:54:57 +0200

    added ParallelPartitionCoarsening

---
+++ src/cpp/coarsening/ParallelPartitionCoarsening.h
@@ -0,0 +1,55 @@
+/*
+ * ParallelPartitionCoarsening.h
+ *
+ *  Created on: 03.07.2014
+ *      Author: cls
+ */
+
+#ifndef PARALLELPARTITIONCOARSENING_H_
+#define PARALLELPARTITIONCOARSENING_H_
+
+#include "../Globals.h"
+#include "GraphCoarsening.h"
+#include "../structures/Partition.h"
+
+namespace NetworKit {
+
+/**
+ * @ingroup coarsening
+ */
+class ParallelPartitionCoarsening: public NetworKit::GraphCoarsening {
+
+public:
+
+	virtual std::pair<Graph, std::vector<node> > run(const Graph& G, const Partition& zeta);
+
+
+private:
+
+	class ThreadSafeAdjacency {
+
+	public:
+
+		ThreadSafeAdjacency(count z, count nThreads) : nThreads(nThreads), z(z), adja(z), weight(z) {
+			for (count i = 0; i < z; ++i) {
+				adja[i].resize(nThreads);
+				weight[i].resize(nThreads);
+			}
+		};
+
+		void add(index thread, node u, node v, edgeweight ew = defaultEdgeWeight) {
+			adja[u                    ]
+		}
+
+	private:
+		count nThreads;
+		count z;
+		std::vector< std::vector<std::vector<node> > > adja;
+		std::vector< std::vector<std::vector<edgeweight> > > weight;
+	};
+
+};
+
+} /* namespace NetworKit */
+
+#endif /* PARALLELPARTITIONCOARSENING_H_ */
