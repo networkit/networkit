22843f73ae6538a0b6e52e0dd4c52a048c1b6ca1
Gerd Lindner <gerd.lindner@student.kit.edu>
2014-07-16 17:34:03 +0200

    Adapted simmel testcase to overlap calculation with reciprocity

--- src/cpp/backbones/test/SimmelianBackboneGTest.cpp
+++ src/cpp/backbones/test/SimmelianBackboneGTest.cpp
@@ -50,65 +50,10 @@ TEST_F(SimmelianBackboneGTest, testOverlapCounting) {
 	r = simmel.getOverlap(0, 1, neighbors, 4);
 	EXPECT_EQ(3, r.overlap) << "wrong overlap";
 	EXPECT_DOUBLE_EQ((1.0/1.0), r.jaccard) << "wrong jaccard index";
-
-	//Build up a ranked neighborhood graph. Notation: Alter(Rank)
-	//Node 1: 1(1),2(1),3(1),5(2),6(2),7(3),9(4)
-	//Node 2: 1(1),2(2),4(2),6(3),7(3),9(4),10(5),11(6)
-/*
-	RankedNeighbors neighbors1;
-	RankedNeighbors neighbors2;
-	neighbors1.push_back(RankedEdge(1,1,0,1));
-	neighbors1.push_back(RankedEdge(1,2,0,1));
-	neighbors1.push_back(RankedEdge(1,3,0,1));
-	neighbors1.push_back(RankedEdge(1,5,0,2));
-	neighbors1.push_back(RankedEdge(1,6,0,2));
-	neighbors1.push_back(RankedEdge(1,7,0,3));
-	neighbors1.push_back(RankedEdge(1,9,0,4));
-
-	neighbors2.push_back(RankedEdge(2,1,0,1));
-	neighbors2.push_back(RankedEdge(2,2,0,2));
-	neighbors2.push_back(RankedEdge(2,4,0,2));
-	neighbors2.push_back(RankedEdge(2,6,0,3));
-	neighbors2.push_back(RankedEdge(2,7,0,3));
-	neighbors2.push_back(RankedEdge(2,9,0,4));
-	neighbors2.push_back(RankedEdge(2,10,0,5));
-	neighbors2.push_back(RankedEdge(2,11,0,6));
-
-	std::vector<RankedNeighbors> neighbors(3);
-	neighbors[1] = neighbors1;
-	neighbors[2] = neighbors2;
-
-	SimmelianBackbone simmel (0.5);
-	Redundancy r (0, 0.0);
-
-	r =  simmel.getOverlap(1, 2, neighbors, 1);
-	EXPECT_EQ(1, r.overlap) << "wrong overlap";
-	EXPECT_DOUBLE_EQ((1.0/3.0), r.jaccard) << "wrong jaccard index";
-
-	r = simmel.getOverlap(2, 1, neighbors, 1);
-	EXPECT_EQ(1, r.overlap) << "wrong overlap";
-	EXPECT_DOUBLE_EQ((1.0/3.0), r.jaccard) << "wrong jaccard index";
-
-	//Maximum prefix jaccard index for k=4 (5/8).
-	r = simmel.getOverlap(1, 2, neighbors, 10);
-	EXPECT_EQ(5, r.overlap) << "wrong overlap";
-	EXPECT_DOUBLE_EQ((5.0/8.0), r.jaccard) << "wrong jaccard index";
-
-	r = simmel.getOverlap(2, 1, neighbors, 10);
-	EXPECT_EQ(5, r.overlap) << "wrong overlap";
-	EXPECT_DOUBLE_EQ((5.0/8.0), r.jaccard) << "wrong jaccard index";
-
-	r = simmel.getOverlap(1, 2, neighbors, 2);
-	EXPECT_EQ(2, r.overlap) << "wrong overlap";
-	EXPECT_DOUBLE_EQ((2.0/6.0), r.jaccard) << "wrong jaccard index";
-
-	r = simmel.getOverlap(2, 1, neighbors, 2);
-	EXPECT_EQ(2, r.overlap) << "wrong overlap";
-	EXPECT_DOUBLE_EQ((2.0/6.0), r.jaccard) << "wrong jaccard index";*/
 }
 
 TEST_F(SimmelianBackboneGTest, testRankedNeighborhood) {
-	/*Graph g(10);
+	Graph g(10);
 
 	g.addEdge(4,5);
 	g.addEdge(4,6);
@@ -141,11 +86,11 @@ TEST_F(SimmelianBackboneGTest, testRankedNeighborhood) {
 	EXPECT_EQ(3, neighborhood[8].size());
 	EXPECT_EQ(RankedEdge(8, 4, 2, 1), neighborhood[8][0]);
 	EXPECT_EQ(RankedEdge(8, 7, 1, 2), neighborhood[8][1]);
-	EXPECT_EQ(RankedEdge(8, 9, 1, 2), neighborhood[8][2]);*/
+	EXPECT_EQ(RankedEdge(8, 9, 1, 2), neighborhood[8][2]);
 }
 
 TEST_F(SimmelianBackboneGTest, testRankedNeighborhoodSkippedRanks) {
-	/*Graph g(7);
+	Graph g(7);
 
 	g.addEdge(0,1);
 	g.addEdge(0,2);
@@ -182,11 +127,11 @@ TEST_F(SimmelianBackboneGTest, testRankedNeighborhoodSkippedRanks) {
 	EXPECT_EQ(RankedEdge(4, 0, 3, 1), neighborhood[4][0]);
 	EXPECT_EQ(RankedEdge(4, 3, 1, 2), neighborhood[4][1]);
 	EXPECT_EQ(RankedEdge(4, 5, 1, 2), neighborhood[4][2]);
-	EXPECT_EQ(RankedEdge(4, 6, 1, 2), neighborhood[4][3]);*/
+	EXPECT_EQ(RankedEdge(4, 6, 1, 2), neighborhood[4][3]);
 }
 
 TEST_F(SimmelianBackboneGTest, testOverlapFiltering) {
-	/*Graph g(10);
+	Graph g(10);
 
 	g.addEdge(0,1);
 	g.addEdge(1,2);
@@ -217,7 +162,6 @@ TEST_F(SimmelianBackboneGTest, testOverlapFiltering) {
 	g.addEdge(5,1);
 
 	SimmelianBackbone simmel(2, 1);
-	//TODO: ranks are "skipped" in reference implementation. Why?
 	Graph b = simmel.calculate(g);
 
 	EXPECT_EQ(20, b.numberOfEdges());
@@ -230,11 +174,11 @@ TEST_F(SimmelianBackboneGTest, testOverlapFiltering) {
 	EXPECT_TRUE(b.hasEdge(1,2));
 	EXPECT_TRUE(b.hasEdge(2,3));
 	EXPECT_TRUE(b.hasEdge(3,4));
-	EXPECT_TRUE(b.hasEdge(4,0));*/
+	EXPECT_TRUE(b.hasEdge(4,0));
 }
 
 TEST_F(SimmelianBackboneGTest, testBackboneTrivial) {
-	/*Graph g(5);
+	Graph g(5);
 
 	g.addEdge(0,1);
 	g.addEdge(0,2);
@@ -245,7 +189,7 @@ TEST_F(SimmelianBackboneGTest, testBackboneTrivial) {
 	Graph b = simmel.calculate(g);
 
 	EXPECT_EQ(3, b.numberOfEdges()) << "wrong edge count in backbone";
-	EXPECT_EQ(5, b.numberOfNodes()) << "wrong node count in backbone";*/
+	EXPECT_EQ(5, b.numberOfNodes()) << "wrong node count in backbone";
 }
 
 
