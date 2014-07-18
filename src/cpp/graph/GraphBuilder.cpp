a6ca574d01fef4b531d654ee184496e44c2d9a6a
Marvin Ritter <marvin.ritter@gmail.com>
2014-07-19 00:03:22 +0200

    improved GraphBuilder, added parallel version for toGraph

---
+++ src/cpp/graph/GraphBuilder.cpp
@@ -0,0 +1,249 @@
+/*
+ * GraphBuilder.cpp
+ *
+ *  Created on: 15.07.2014
+ *      Author: Marvin Ritter (marvin.ritter@gmail.com)
+ */
+
+#include <stdexcept>
+#include <omp.h>
+
+#include "GraphBuilder.h"
+
+namespace NetworKit {
+
+GraphBuilder::GraphBuilder(count n, bool weighted, bool directed) :
+	n(n),
+	weighted(weighted),
+	directed(directed),
+	halfEdges(n),
+	halfEdgeWeights(weighted ? n : 0)
+{
+}
+
+index GraphBuilder::indexHalfEdgeArray(node u, node v) const {
+	for (index i = 0; i < halfEdges[u].size(); i++) {
+		node x = halfEdges[u][i];
+		if (x == v) {
+			return i;
+		}
+	}
+	return none;
+}
+
+node GraphBuilder::addNode() {
+	halfEdges.push_back(std::vector<node>{});
+	if (weighted) {
+		halfEdgeWeights.push_back(std::vector<edgeweight>{});
+	}
+	return n++;
+}
+
+void GraphBuilder::addEdge(node u, node v, edgeweight ew) {
+	halfEdges[u].push_back(v);
+	if (weighted) {
+		halfEdgeWeights[u].push_back(ew);
+	}
+}
+
+void GraphBuilder::increaseWeight(node u, node v, edgeweight ew) {
+	if (!weighted) {
+		throw std::runtime_error("Cannot increase edge weight in unweighted graph.");
+	}
+
+	index vi = indexHalfEdgeArray(u, v);
+
+	if (directed) {
+		if (vi != none) {
+			halfEdgeWeights[u][vi] += ew;
+		} else {
+			addEdge(u, v, ew);
+		}
+	} else {
+		if (vi != none) {
+			halfEdgeWeights[u][vi] += ew;
+		} else {
+			index ui = indexHalfEdgeArray(v, u);
+			if (ui != none) {
+				halfEdgeWeights[v][ui] += ew;
+			} else {
+				addEdge(u, v, ew);
+			}
+		}
+	}
+}
+
+Graph GraphBuilder::toGraphParallel() {
+	Graph G(n, weighted, directed);
+
+	int maxThreads = omp_get_max_threads();
+
+	using adjacencylists = std::vector< std::vector<node> >;
+	using weightlists = std::vector< std::vector<edgeweight> >;
+
+	std::vector<adjacencylists> inEdgesPerThread(maxThreads, adjacencylists(n));
+	std::vector<weightlists> inWeightsPerThread(weighted ? maxThreads : 0, weightlists(n));
+
+	G.parallelForNodes([&](node v) {
+		int tid = omp_get_thread_num();
+		for (node u : halfEdges[v]) {
+			inEdgesPerThread[tid][u].push_back(v);
+		}
+		if (weighted) {
+			for (index i = 0; i < halfEdges[v].size(); i++) {
+				node u = halfEdges[v][i];
+				edgeweight ew = halfEdgeWeights[v][i];
+				inWeightsPerThread[tid][u].push_back(ew);
+			}
+		}
+	});
+
+	G.parallelForNodes([&](node v) {
+		count inDeg = 0;
+		count outDeg = halfEdges[v].size();
+		for (int tid = 0; tid < maxThreads; tid++) {
+			inDeg += inEdgesPerThread[tid][v].size();
+		}
+
+		if (directed) {
+			G.inEdges[v].reserve(inDeg);
+			G.outEdges[v].reserve(outDeg);
+			if (weighted) {
+				G.inEdgeWeights[v].reserve(inDeg);
+				G.outEdgeWeights[v].reserve(outDeg);
+			}
+		} else {
+			G.outEdges[v].reserve(outDeg + inDeg);
+			if (weighted) {
+				G.outEdgeWeights[v].reserve(outDeg + inDeg);
+			}
+		}
+
+		std::copy(halfEdges[v].begin(), halfEdges[v].end(), G.outEdges[v].end());
+		halfEdges[v].clear();
+		if (weighted) {
+			std::copy(halfEdgeWeights[v].begin(), halfEdgeWeights[v].end(), G.outEdgeWeights[v].end());
+			halfEdgeWeights[v].clear();
+		}
+
+		if (directed) {
+			G.inDeg[v] = inDeg;
+			G.outDeg[v] = outDeg;
+			for (int tid = 0; tid < maxThreads; tid++) {
+				std::copy(inEdgesPerThread[tid][v].begin(), inEdgesPerThread[tid][v].end(), G.inEdges[v].end());
+				inEdgesPerThread[tid][v].clear();
+			}
+			if (weighted) {
+				for (int tid = 0; tid < maxThreads; tid++) {
+					std::copy(inWeightsPerThread[tid][v].begin(), inWeightsPerThread[tid][v].end(), G.inEdgeWeights[v].end());
+					inWeightsPerThread[tid][v].clear();
+				}	
+			}
+		} else {
+			G.outDeg[v] = inDeg + outDeg;
+			for (int tid = 0; tid < maxThreads; tid++) {
+				std::copy(inEdgesPerThread[tid][v].begin(), inEdgesPerThread[tid][v].end(), G.outEdges[v].end());
+				inEdgesPerThread[tid][v].clear();
+			}
+			if (weighted) {
+				for (int tid = 0; tid < maxThreads; tid++) {
+					std::copy(inWeightsPerThread[tid][v].begin(), inWeightsPerThread[tid][v].end(), G.outEdgeWeights[v].end());
+					inWeightsPerThread[tid][v].clear();
+				}	
+			}
+		}
+	});
+
+	// calculate correct m
+	G.m = 0;
+	G.forNodes([&](node v) {
+		G.m += G.degree(v);
+	});
+	if (!directed) {
+		G.m /= 2;
+	}
+
+	// bring the builder into an empty, but valid state
+	n = 0;
+	halfEdges.clear();
+	halfEdgeWeights.clear();
+
+	return G;
+}
+
+Graph GraphBuilder::toGraphSequential() {
+	Graph G(n, weighted, directed);
+
+	if (directed) {
+		throw std::runtime_error("toGraphSequential does not yet support directed graphs");
+	}
+
+	if (weighted) {
+		throw std::runtime_error("toGraphSequential does not yet support weighted graphs");
+	}
+
+	std::vector<count> missingEdgesCounts(n, 0);
+
+	G.forNodes([&](node v) {
+		G.outDeg[v] = halfEdges[v].size();
+		G.outEdges[v] = halfEdges[v];
+		halfEdges[v].clear();
+	});
+
+	G.forNodes([&](node v) {
+		// increase count of incoming edges for neighbors
+		for (node u : G.outEdges[v]) {
+			missingEdgesCounts[u]++;
+		}
+	});
+
+	// add second half of the edge
+	if (directed) {
+		// directed: outEdges is complete, fill inEdges
+		// missingEdgesCounts are our inDegrees
+		G.inDeg = missingEdgesCounts;
+
+		G.forNodes([&](node v) {
+			G.inEdges[v].reserve(G.inDeg[v]);
+		});
+		G.forNodes([&](node v) {
+			for (node u : G.outEdges[v]) {
+				// edge v -> u
+				G.inEdges[u].push_back(v);
+			}
+		});
+	} else {
+		// undirected: so far each edge is just saved at one node
+		// add it to the other node as well
+		G.forNodes([&](node v) {
+			G.outEdges[v].reserve(G.outDeg[v] + missingEdgesCounts[v]);
+		});
+		G.forNodes([&](node v) {
+			for (index i = 0; i < G.outDeg[v]; i++) {
+				node u = G.outEdges[v][i];
+				G.outEdges[u].push_back(v);
+			}
+		});
+		G.forNodes([&](node v) {
+			G.outDeg[v] += missingEdgesCounts[v];
+		});
+	}
+
+	// calculate correct m	
+	G.m = 0;
+	G.forNodes([&](node v) {
+		G.m += G.degree(v);
+	});
+	if (!directed) {
+		G.m /= 2;
+	}
+
+	// bring the builder into an empty, but valid state
+	n = 0;
+	halfEdges.clear();
+	halfEdgeWeights.clear();
+
+	return G;
+}
+
+} /* namespace NetworKit */
