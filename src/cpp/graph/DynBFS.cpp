580f97a013cfa732997054b077b399da747314e9
ebergamini <ebergamini@localhost>
2014-07-18 14:45:01 +0200

    implemented dynamic bfs

--- src/cpp/graph/DynBFS.cpp
+++ src/cpp/graph/DynBFS.cpp
@@ -19,11 +19,51 @@ void DynBFS::update(const std::vector<GraphEvent>& batch) {
 	std::vector<std::queue<node> > queues(maxLevels);
 
 	// insert nodes from the batch whose distance has changed (affected nodes) into the queues
+	for (GraphEvent edge : batch) {
+		if (edge.type!=GraphEvent::EDGE_ADDITION || edge.w!=1.0)
+			throw std::runtime_error("wrong update");
+		if (distances[edge.u] >= distances[edge.v]+1) {
+			queues[distances[edge.v]+1].push(u);
+		} else if (distances[edge.v] >= distances[edge.u]+1) {
+			queues[distances[edge.u]+1].push(v);
+		}
+	}
 
 	// extract nodes from the queues and scan incident edges
-
+	std::queue<node> visited(G.upperNodeIdBound());
+	int m = 1;
+	while (m < maxLevels) {
+		while (!queues[m].empty()) {
+			node w = queues[m].pop();
+			if (color[w] == BLACK) {
+				continue;
+			}
+			visited.push(w);
+			color[w] = BLACK;
+			distances[w] = m;
+			previous[w].clear();
+			npaths[w] = 0;
+			G.forNeighborsOf(w, [&](node z) {
+				//z is a predecessor for w
+				if (distances[w] == distances[z]+1) {
+					previous[w].push_back(z);
+					npaths[w] += npaths[z];
+				}
+				//w is a predecessor for z
+				else if (color[z] == WHITE && distances[z] >= distances[w]+1 ) {
+					color[z] = GRAY;
+					queues[m].push(z);
+				}
+			}
+		}
+		m = m+1;
+	}
 
 	// reset colors
+	while(!visited.empty()) {
+		node w = visited.pop();
+		color[w] = WHITE;
+	}
 }
 
 } /* namespace NetworKit */
