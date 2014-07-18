3ecd7abf7ae0cef1af99b27fa774c5a65aa4adc0
Michael Hamann <michael.hamann@kit.edu>
2014-07-18 15:48:16 +0200

    Connected components: inline accessors, cache number of components
    Previously requesting the number of components was in O(n), now it's
    O(1).

--- src/cpp/properties/ConnectedComponents.cpp
+++ src/cpp/properties/ConnectedComponents.cpp
@@ -13,7 +13,7 @@
 
 namespace NetworKit {
 
-ConnectedComponents::ConnectedComponents(const Graph& G) : G(G) {
+ConnectedComponents::ConnectedComponents(const Graph& G) : G(G), numComponents(0) {
 }
 
 void ConnectedComponents::run() {
@@ -21,6 +21,7 @@ void ConnectedComponents::run() {
 
 	DEBUG("initializing labels");
 	component = Partition(G.upperNodeIdBound(), none);
+	numComponents = 0;
 
 	// perform breadth-first searches
 	G.forNodes([&](node u) {
@@ -31,6 +32,7 @@ void ConnectedComponents::run() {
 			G.BFSfrom(u, [&](node v) {
 				component[v] = c;
 			});
+			++numComponents;
 		}
 	});
 
@@ -41,14 +43,6 @@ Partition ConnectedComponents::getPartition() {
 	return this->component;
 }
 
-count ConnectedComponents::numberOfComponents() {
-	return this->component.numberOfSubsets();
-}
-
-count ConnectedComponents::componentOfNode(node u) {
-	assert (component[u] != none);
-	return component[u];
-}
 
 std::map<index, count> ConnectedComponents::getComponentSizes() {
 	return this->component.subsetSizeMap();
