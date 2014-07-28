ef04660298e2b21a30f5fd2d959dd8e04dbda280
Michael Hamann <michael.hamann@kit.edu>
2014-07-28 15:30:02 +0200

    Added function traits for template magic

---
+++ src/cpp/auxiliary/FunctionTraits.h
@@ -0,0 +1,37 @@
+#ifndef FUNCTIONTRAITS_H
+#define FUNCTIONTRAITS_H
+
+#include <type_traits>
+#include <tuple>
+
+namespace Aux {
+
+// Code by kennytm (auraHT Ltd.) 2011. See
+// http://stackoverflow.com/questions/7943525/is-it-possible-to-figure-out-the-parameter-type-and-return-type-of-a-lambda
+template <typename T>
+struct FunctionTraits
+	: public FunctionTraits<decltype(&T::operator())>
+{};
+// For generic types, directly use the result of the signature of its 'operator()'
+
+template <typename ClassType, typename ReturnType, typename... Args>
+struct FunctionTraits<ReturnType(ClassType::*)(Args...) const> {
+// we specialize for pointers to member function
+	constexpr static std::size_t arity = sizeof...(Args);
+	// arity is the number of arguments.
+
+	typedef ReturnType result_type;
+
+	template <std::size_t i>
+	struct arg
+	{
+		typedef typename std::tuple_element<i, std::tuple<Args...>>::type type;
+		// the i-th argument is equivalent to the i-th tuple element of a tuple
+		// composed of those arguments.
+	};
+};
+
+
+} // namespace Aux
+
+#endif // FUNCTIONTRAITS_H
