b340593d8115a0f91a406e231744206b1b40723f
maxv <uocvf@student.kit.edu>
2014-07-30 09:46:03 +0200

    added runUntil()-method to BFS and Dijkstra

--- src/cpp/graph/BFS.cpp
+++ src/cpp/graph/BFS.cpp
@@ -66,4 +66,60 @@ void BFS::run() {
 	}
 }
 
+void BFS::runUntil(node t) {
+	edgeweight infDist = std::numeric_limits<edgeweight>::max();
+	count z = G.upperNodeIdBound();
+	distances.clear();
+	distances.resize(z, infDist);
+
+	if (storePaths) {
+		previous.clear();
+		previous.resize(z);
+		npaths.clear();
+		npaths.resize(z, 0);
+		npaths[source] = 1;
+	}
+
+	if (storeStack) {
+		std::stack<node> empty;
+		std::swap(stack, empty);
+	}
+
+	std::queue<node> q;
+	q.push(source);
+	distances[source] = 0;
+
+	while (! q.empty()) {
+		node u = q.front();
+		q.pop();
+
+		if (storeStack) {
+			stack.push(u);
+		}
+
+		if (u == t) {
+			break;
+		}
+		// TRACE("current node in BFS: " , u);
+//		TRACE(distances);
+
+		// insert untouched neighbors into queue
+		G.forNeighborsOf(u, [&](node v) {
+			// TRACE("scanning neighbor ", v);
+
+			if (distances[v] == infDist) {
+				q.push(v);
+				distances[v] = distances[u] + 1;
+				if (storePaths) {
+					previous[v] = {u};
+					npaths[v] = npaths[u];
+				}
+			} else if (storePaths && (distances[v] == distances[u] + 1)) {
+				previous[v].push_back(u); 	// additional predecessor
+				npaths[v] += npaths[u]; 	// all the shortest paths to u are also shortest paths to v now
+			}
+		});
+	}
+}
+
 } /* namespace NetworKit */
