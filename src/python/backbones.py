4e42393a22bcbbdf95880b081e0e5e2908c157cb
Gerd Lindner <gerd.lindner@student.kit.edu>
2014-08-04 16:32:31 +0200

    Missing imports in backbones.py

--- src/python/backbones.py
+++ src/python/backbones.py
@@ -1,6 +1,6 @@
-from _NetworKit import SimmelianBackboneParametric, SimmelianBackboneNonParametric, MultiscaleBackbone, LocalSimilarityBackbone, SimmelianMultiscaleBackbone, ChibaNishizekiTriangleCounter, EdgeAttribute, GlobalThresholdFilter, SimmelianJaccardAttributizer
+from _NetworKit import SimmelianBackboneParametric, SimmelianBackboneNonParametric, MultiscaleBackbone, LocalSimilarityBackbone, SimmelianMultiscaleBackbone, ChibaNishizekiTriangleCounter, EdgeAttribute, GlobalThresholdFilter, SimmelianJaccardAttributizer, LocalSimilarityAttributizer, MultiscaleAttributizer, SimmelianOverlapAttributizer
 
-def writeCsv(file, G, B):	
+def writeCsv(file, G, B):
 	f = open(file, "w+")
 	f.write("Source,Target,Type,{0}\n".format("Backbone"))
 	for edge in G.edges():
