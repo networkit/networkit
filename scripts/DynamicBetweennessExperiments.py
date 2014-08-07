3bdb32a766bc413a8f3b93ff419338bc6f9f7f13
cls <christian.staudt@kit.edu>
2014-08-07 14:32:32 +0200

    removed batch size argument

--- scripts/DynamicBetweennessExperiments.py
+++ scripts/DynamicBetweennessExperiments.py
@@ -2,7 +2,7 @@ from NetworKit import *
 from dynamic import *
 
 
-def removeAndAddEdges(G, nEdges, batchSize=1, tabu=None):
+def removeAndAddEdges(G, nEdges, tabu=None):
 	if nEdges > G.numberOfEdges():
 		raise Error("G does not have enough edges")
 
@@ -20,18 +20,21 @@ def removeAndAddEdges(G, nEdges, batchSize=1, tabu=None):
 	addStream = []
 	i = 0
 	for (u, v) in removed:
-		if (i % batchSize) == 0:
-			addStream.append(GraphEvent(GraphEvent.TIME_STEP, 0, 0, 0))
 		addStream.append(GraphEvent(GraphEvent.EDGE_ADDITION, u, v, 1.0))
 		i += 1
 
 	return (removeStream, addStream)
 
 
+def applyStreams(G, removeStream, addStream):
+	updater = dynamic.GraphUpdater(G)
+	updater.udpate(removeStream)
+	updater.update(addStream)
+
 
 def test():
 	G = readGraph("../input/PGPgiantcompo.graph")
 	T = graph.SpanningForest(G).generate()
 
-	(removeStream, addStream) = removeAndAddEdges(G, 100, batchSize=5, tabu=T)
+	(removeStream, addStream) = removeAndAddEdges(G, 100, tabu=T)
 	return (removeStream, addStream)
