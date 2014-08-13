a77c96f3aa344e6dc88c53d87217f1ee97d7866a
cls <christian.staudt@kit.edu>
2014-08-13 11:57:49 +0200

    test for StochasticBlockmodel generator

--- src/cpp/generators/StochasticBlockmodel.h
+++ src/cpp/generators/StochasticBlockmodel.h
@@ -20,16 +20,16 @@ public:
 	* Construct a undirected regular ring lattice.
 	*
 	* @param nNodes 		number of nodes in target graph
-	* @param nBlocks		number of blocks (=k)
+	* @param n		number of blocks (=k)
 	* @param membership		maps node ids to block ids (consecutive, 0 <= i < nBlocks)
 	* @param affinity		matrix of size k x k with edge probabilities betweeen the blocks
 	*/
-	StochasticBlockmodel(count nNodes, count nBlocks, const std::vector<index>& membership, const std::vector<std::vector<double> >& affinity);
+	StochasticBlockmodel(count n, count nBlocks, const std::vector<index>& membership, const std::vector<std::vector<double> >& affinity);
 
 	virtual Graph generate();
 
 protected:
-		count nNodes;
+		count n;
 		count nBlocks;
 		std::vector<index> membership;
 		std::vector<std::vector<double> > affinity;
