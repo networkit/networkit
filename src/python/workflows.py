ee4c6b4bfa51945d28fb5848355117836741ad7e
Marvin Ritter <marvin.ritter@gmail.com>
2014-08-20 10:16:06 +0100

    merge with Dev

---
+++ src/python/workflows.py
@@ -0,0 +1,31 @@
+""" This module provides convenient workflows constructed from NetworKit functions."""
+
+
+from NetworKit import *
+
+import operator
+import logging
+
+
+def extractLargestComponent(G):
+	"""
+	Extract the subgraph of the largest connected component.
+
+	Parameters
+	----------
+	G : Graph
+		Input graph.
+	Returns
+	-------
+	Graph
+		Subgraph of largest component, preserving node ids of orignal graph.
+	"""
+
+	cc = properties.ConnectedComponents(G)
+	cc.run()
+	cSizes = cc.getComponentSizes()
+	(largestCompo, size) = max(cSizes.items(), key=operator.itemgetter(1))
+	logging.info("extracting component {0} containing {1} nodes".format(largestCompo, size))
+	compoNodes = [v for v in G.nodes() if cc.componentOfNode(v) is largestCompo]
+	C = graph.Subgraph().fromNodes(G, compoNodes)
+	return C
