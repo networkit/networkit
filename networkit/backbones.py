340cbb521525e6ecab7b3a0295a1980edb4fd08d
Michael Hamann <michael.hamann@kit.edu>
2014-08-27 18:01:08 +0200

    Renamed LocalLogAttributizer to LocalFilterAttributizer

--- networkit/backbones.py
+++ networkit/backbones.py
@@ -3,7 +3,7 @@
 __author__ = "Gerd Lindner"
 
 from _NetworKit import ChibaNishizekiTriangleCounter, SimmelianJaccardAttributizer, GlobalThresholdFilter, LocalSimilarityAttributizer, MultiscaleAttributizer, SimmelianOverlapAttributizer, RandomAttributizer, LocalDegreeAttributizer, ForestFireAttributizer, \
-	AttributeAsWeight, LinearizeAttribute, JaccardSimilarityAttributizer, LocalLogAttributizer
+	AttributeAsWeight, LinearizeAttribute, JaccardSimilarityAttributizer, LocalFilterAttributizer
 
 #SimmelianBackboneParametric, SimmelianBackboneNonParametric, MultiscaleBackbone, LocalSimilarityBackbone, SimmelianMultiscaleBackbone, RandomBackbone
 
--- networkit/cpp/backbones/LocalLogAttributizer.cpp
+++
@@ -1,6 +0,0 @@
-/*
- *
- */
-
-#include "LocalLogAttributizer.h"
-
