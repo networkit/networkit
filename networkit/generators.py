46effc412dcfeec76e5dee53d3439492a29c42b5
Moritz von Looz <moritz.looz-corswarem@kit.edu>
2014-09-01 11:35:14 +0200

    merge Dev -> hyperbolic_generator

---
+++ networkit/generators.py
@@ -0,0 +1,2 @@
+# extension imports
+from _NetworKit import BarabasiAlbertGenerator, PubWebGenerator, ErdosRenyiGenerator, ClusteredRandomGraphGenerator, DorogovtsevMendesGenerator, DynamicPubWebGenerator, DynamicPathGenerator, ChungLuGenerator, HavelHakimiGenerator, HyperbolicGenerator, DynamicHyperbolicGenerator, DynamicDorogovtsevMendesGenerator, RmatGenerator, DynamicForestFireGenerator, RegularRingLatticeGenerator, WattsStrogatzGenerator
