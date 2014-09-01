666efe6ac4a20b51ede554fb148834bfe80fae12
Elisabetta Bergamini <elisabetta.bergamini@kit.edu>
2014-09-01 15:42:39 +0200

    merged Dev -> dynbc

---
+++ networkit/cpp/dynamics/GraphEventSource.cpp
@@ -0,0 +1,15 @@
+/*
+ * GraphEventSource.cpp
+ *
+ *  Created on: 02.04.2013
+ *      Author: cls
+ */
+
+#include "GraphEventSource.h"
+
+namespace NetworKit {
+
+GraphEventSource::~GraphEventSource() {
+}
+
+} /* namespace NetworKit */
