fe21c2bbe2e0736e49ae0a6465ea0f1bf8f29290
Michael Hamann <michael.hamann@kit.edu>
2014-09-18 19:08:18 +0200

    Updated EdmondsKarp algorithm to new NetworKit features
    The algorithm seems to work now but I haven't run any bigger test cases.

--- networkit/cpp/flow/EdmondsKarp.cpp
+++ networkit/cpp/flow/EdmondsKarp.cpp
@@ -9,25 +9,9 @@
 
 namespace NetworKit {
 
-index EdmondsKarp::getEdgeIdx(const Graph &graph, node u, node v) const {
-	index idx = 0;
-	bool found = false;
-	graph.forNeighborsOf(u, [&](node neighbor){
-		if (neighbor == v) {
-			found = true;
-		}
-
-		if (!found) {
-			idx++;
-		}
-	});
-
-	return idx;
-}
-
-edgeweight EdmondsKarp::BFS(const Graph &graph, std::vector<std::vector<edgeweight>> &flow, node source, node sink, std::vector<node> &pred) const {
-	pred = std::vector<node>(graph.numberOfNodes(), none);
-	std::vector<edgeweight> gain(graph.numberOfNodes(), 0);
+edgeweight EdmondsKarp::BFS(const Graph &graph, std::vector<edgeweight> &flow, node source, node sink, std::vector<node> &pred) const {
+	pred = std::vector<node>(graph.upperNodeIdBound(), none);
+	std::vector<edgeweight> gain(graph.upperNodeIdBound(), 0);
 
 	std::queue<node> Q;
 	Q.push(source);
@@ -37,11 +21,10 @@ edgeweight EdmondsKarp::BFS(const Graph &graph, std::vector<std::vector<edgeweig
 		node u = Q.front(); Q.pop();
 
 		bool sinkReached = false;
-		graph.forNeighborsOf(u, [&](node v){
-			index edgeIdx = getEdgeIdx(graph, u, v);
-			if (flow[u][edgeIdx] < graph.weight(u,v) && pred[v] == none) { // only add those neighbors with rest capacity and which were not discovered yet
+		graph.forNeighborsOf(u, [&](node _u, node v, edgeweight weight, edgeid eid){
+			if (flow[eid] < weight && pred[v] == none) { // only add those neighbors with rest capacity and which were not discovered yet
 				pred[v] = u;
-				gain[v] = std::min(gain[u], graph.weight(u,v) - flow[u][edgeIdx]);
+				gain[v] = std::min(gain[u], weight - flow[eid]);
 
 				if (v != sink && !sinkReached) {
 					Q.push(v);
@@ -59,11 +42,10 @@ edgeweight EdmondsKarp::BFS(const Graph &graph, std::vector<std::vector<edgeweig
 	return 0.0;
 }
 
-edgeweight EdmondsKarp::solveMaxFlow(const Graph &graph, const node source, const node sink, std::vector<std::vector<edgeweight>> &flow) const {
-	flow = std::vector<std::vector<edgeweight>>(graph.numberOfNodes());
-	graph.forNodes([&](node u){
-		flow[u] = std::vector<edgeweight>(graph.degree(u), 0.0);
-	});
+edgeweight EdmondsKarp::solveMaxFlow(const Graph &graph, const node source, const node sink, std::vector<edgeweight> &flow) const {
+	if (!graph.hasEdgeIds()) { throw std::runtime_error("edges have not been indexed - call indexEdges first"); }
+	flow.clear();
+	flow.resize(graph.upperEdgeIdBound(), 0.0);
 
 	edgeweight maxFlow = 0;
 	while (true) {
@@ -75,8 +57,7 @@ edgeweight EdmondsKarp::solveMaxFlow(const Graph &graph, const node source, cons
 		node v = sink;
 		while (v != source) {
 			node u = pred[v];
-			flow[u][getEdgeIdx(graph, u, v)] += gain;
-			flow[v][getEdgeIdx(graph, v, u)] += gain; // undirected!
+			flow[graph.edgeId(u, v)] += gain;
 			v = u;
 		}
 	}
@@ -84,9 +65,9 @@ edgeweight EdmondsKarp::solveMaxFlow(const Graph &graph, const node source, cons
 	return maxFlow;
 }
 
-void EdmondsKarp::computeSourceSet(const Graph &graph, const node source, const node sink, const std::vector<std::vector<edgeweight>> &flow, std::vector<node> &sourceSet) const {
+void EdmondsKarp::computeSourceSet(const Graph &graph, const node source, const node sink, const std::vector<edgeweight> &flow, std::vector<node> &sourceSet) const {
 	// perform bfs from source
-	std::vector<bool> visited(graph.numberOfNodes(), false);
+	std::vector<bool> visited(graph.upperNodeIdBound(), false);
 	sourceSet.clear();
 	std::queue<node> Q;
 	Q.push(source);
@@ -95,8 +76,8 @@ void EdmondsKarp::computeSourceSet(const Graph &graph, const node source, const 
 		node u = Q.front(); Q.pop();
 		sourceSet.push_back(u);
 
-		graph.forNeighborsOf(u, [&](node v) {
-			if (!visited[v] && flow[u][getEdgeIdx(graph, u, v)] < graph.weight(u,v)) {
+		graph.forNeighborsOf(u, [&](node _u, node v, edgeweight weight, edgeid eid) {
+			if (!visited[v] && flow[eid] < weight) {
 				Q.push(v);
 				visited[v] = true;
 			}
@@ -105,39 +86,26 @@ void EdmondsKarp::computeSourceSet(const Graph &graph, const node source, const 
 }
 
 edgeweight EdmondsKarp::run(const Graph &graph, const node source, const node sink) const {
-	std::vector<std::vector<edgeweight>> flow;
+	std::vector<edgeweight> flow;
 	return solveMaxFlow(graph, source, sink, flow);
 }
 
 edgeweight EdmondsKarp::run(const Graph &graph, const node source, const node sink, std::vector<node> &sourceSet) const {
-	std::vector<std::vector<edgeweight>> flow;
+	std::vector<edgeweight> flow;
 	edgeweight maxFlow = solveMaxFlow(graph, source, sink, flow);
 	computeSourceSet(graph, source, sink, flow, sourceSet);
 
 	return maxFlow;
 }
 
-edgeweight EdmondsKarp::run(Graph &graph, const node source, const node sink, int &attribute_id) const {
-	std::vector<std::vector<edgeweight>> flow;
+edgeweight EdmondsKarp::run(const NetworKit::Graph &graph, const node source, const node sink, std::vector< NetworKit::edgeweight > &flow) const {
 	edgeweight maxFlow = solveMaxFlow(graph, source, sink, flow);
-
-	// FIXME: update to new edge attribute system
-	// attribute_id = graph.addEdgeAttribute_double(0.0);
-	graph.forEdges([&](node u, node v) {
-		// FIXME: graph.setAttribute_double(u, v, attribute_id, flow[u][getEdgeIdx(graph, u, v)]);
-	});
 
 	return maxFlow;
 }
 
-edgeweight EdmondsKarp::run(Graph &graph, node source, node sink, std::vector<node> &sourceSet, int &attribute_id) const {
-	std::vector<std::vector<edgeweight>> flow;
+edgeweight EdmondsKarp::run(const NetworKit::Graph &graph, node source, node sink, std::vector< NetworKit::node > &sourceSet, std::vector< NetworKit::edgeweight > &flow) const {
 	edgeweight maxFlow = solveMaxFlow(graph, source, sink, flow);
-
-	// FIXME: attribute_id = graph.addEdgeAttribute_double(0.0);
-	graph.forEdges([&](node u, node v) {
-		// FIXME: graph.setAttribute_double(u, v, attribute_id, flow[u][getEdgeIdx(graph, u, v)]);
-	});
 
 	computeSourceSet(graph, source, sink, flow, sourceSet);
 
