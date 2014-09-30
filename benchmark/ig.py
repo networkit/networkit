4c67828423732e175e2d843694c50c16a6524927
Alehins <Alehins@localhost>
2014-09-30 02:05:02 +0100

    Added relevant methods to the igraph benchmark file

--- benchmark/ig.py
+++ benchmark/ig.py
@@ -2,7 +2,7 @@ try:
 	import igraph
 except ImportError:
 	error("igraph not available")
-	
+
 
 class Algo:
 	""" runner for an algorithm"""
@@ -10,7 +10,9 @@ class Algo:
 		raise Exception("Not implemented")
 
 	def loadGraph(key, basePath):
-		raise NotImplementedError("TODO")
+		G = igraph.read(basePath, format="edgelist")  # Check if the format is actually edgelist
+		return G
+
 
 class bConnectedComponents(Algo):
 	name = "ConnectedComponents"
@@ -18,102 +20,105 @@ class bConnectedComponents(Algo):
 	def run(self, G):
 		c = igraph.Graph.components(G)
 
+
 # - k-core decomposition (properties.CoreDecomposition)
 
 class bCoreDecomposition(Algo):
-name = "CoreDecomposition"
+	name = "CoreDecomposition"
 
-def run(self, G):
-	igraph.Graph.coreness(G)
+	def run(self, G):
+		igraph.Graph.coreness(G)
+
 
 # - degree distribution power-law estimation (properties.powerLawExponent)
 
 class bPowerLaw(Algo):
-name = "PowerLaw"
+	name = "PowerLaw"
 
-def run(self, G):
-	raise NotImplementedError("TODO")
+	def run(self, G):
+		raise NotImplementedError("TODO")  # Can't find relevant method
+
 
 # - degree assortativity (properties.degreeAssortativity)
 
 class bDegreeAssortativity(Algo):
-name = "DegreeAssortativity"
+	name = "DegreeAssortativity"
 
-def run(self, G):
-	raise NotImplementedError("TODO")
+	def run(self, G):
+		igraph.Graph.assortativity_degree(G)
 
 
 # - BFS & Dijkstra (graph.BFS, graph.Dijkstra)
-class bBFS(Algo):
-name = "BFS"
 
-def run(self, G):
-	raise NotImplementedError("TODO")
+class bBFS(Algo):
+	name = "BFS"
+
+	def run(self, G):
+		igraph.Graph.shortest_paths(G)
 
 
 # - community detection (community.PLM, community.PLP)
-
 class bCommunityDetection(Algo):
-name = "CommunityDetection"
+	name = "CommunityDetection"
 
-def run(self, G):
-	raise NotImplementedError("TODO")
+	def run(self, G):
+		raise NotImplementedError("TODO")  # Need to check which method corresponds to PLM/PLP
 
 # - diameter, exact (properties.Diameter.exactDiameter) and estimate (properties.Diameter.estimatedDiameterRange)
 
-class bDiameter(Algo):
-name = "Diameter"
 
-def run(self, G):
-	raise NotImplementedError("TODO")
+class bDiameter(Algo):
+	name = "Diameter"
+
+	def run(self, G):
+		igraph.Graph.diameter(G)
 
 
 class bDiameterEstimate(Algo):
-name = "Diameter"
+	name = "Diameter"
 
-def run(self, G):
-	raise NotImplementedError("TODO")
+	def run(self, G):
+		raise NotImplementedError("TODO")  # Can't find relevant method
+
 
 # - clustering coefficients (average local), exact (properties.ClusteringCoefficient.avgLocal) and approximated (properties.ClusteringCoefficient.approxAvgLocal)
-
 class bClusteringCoefficient(Algo):
-name = "ClusteringCoefficient"
+	name = "ClusteringCoefficient"
 
-def run(self, G):
-	raise NotImplementedError("TODO")
+	def run(self, G):
+		igraph.Graph.transitivity_undirected(G)
+
 
 class bApproxClusteringCoefficient(Algo):
-name = "ApproxClusteringCoefficient"
+	name = "ApproxClusteringCoefficient"
 
-def run(self, G):
-	# TODO: specify number of trials
-	raise NotImplementedError("TODO")
-
-
+	def run(self, G):
+		# TODO: specify number of trials
+		raise NotImplementedError("TODO")  # Can't find relevant method
 
 # - centrality
 
+
 # 	- PageRank (centrality.PageRank, centrality.SciPyPageRank)
-
 class bPageRank(Algo):
-name = "PageRank"
+	name = "PageRank"
 
-def run(self, G):
-	raise NotImplementedError("TODO")
+	def run(self, G):
+		pr = igraph.Graph.pagerank(G)
 
 # 	- Eigenvector centrality (centrality.EigenvectorCentrality, centrality.SciPyEVZ)
 
 
 # 	- betweenness,  exact (centrality.Betweenness) and approximated (centrality.ApproxBetweenness, centrality.ApproxBetweenness2)
-
 class bBetweenness(Algo):
-name = "Betweenness"
+	name = "Betweenness"
 
-def run(self, G):
-	raise NotImplementedError("TODO")
+	def run(self, G):
+		b = igraph.Graph.betweenness(G)
+
 
 class bApproxBetweenness(Algo):
-name = "ApproxBetweenness"
+	name = "ApproxBetweenness"
 
-def run(self, G):
-	raise NotImplementedError("TODO")
+	def run(self, G):
+		raise NotImplementedError("TODO")  # Can't find relevant method
