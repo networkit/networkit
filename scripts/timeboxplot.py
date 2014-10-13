289d5e3ad1ad627264f408c2b9e96cc46c845b9e
Moritz von Looz <moritz.looz-corswarem@kit.edu>
2014-10-13 14:42:20 +0200

    fixed bug in timeboxplot changed parameter studies script to vary stretch instead of factor

--- scripts/timeboxplot.py
+++ scripts/timeboxplot.py
@@ -10,23 +10,35 @@ stamp = sys.argv[1]
 with open(str(stamp)+'-mag.json', 'r') as j:
 	maglist = json.load(j)
 
-with open(str(stamp)+'-table-scatter.dat', 'w') as scatter:
-	for mag in maglist:
-		# load data
-		n = 10**mag
-		if not os.path.isfile(str(stamp)+'-cost-'+str(n)+'.json'):
-			print ("File ", str(stamp),'-cost-',str(n),'.json', 'does not exist. aborting.')
-			break
-		with open(str(stamp)+'-cost-'+str(n)+'.json', 'r') as j:
-			cost = json.load(j)
-		with open(str(stamp)+'-edges-'+str(n)+'.json', 'r') as j:
-			edges = json.load(j)
-		print("Reformating", str(stamp),",",str(n),".")
-		with open(str(stamp)+'-cost-'+str(n)+'.json', 'r') as j:
-			cost = json.load(j)
-		with open(str(stamp)+'-edges-'+str(n)+'.json', 'r') as j:
-			edges = json.load(j)
+classlist = ['a','b','c','d','e']
+magindex = 0
+offset = 0
 
-		for i in range(0,len(edges)):
-			scatter.write(str(edges[i]) + '\t' + str(cost[i]) + '\n')
+with open(str(stamp)+'-table-eps-avg.dat', 'w') as eps:
+	with open(str(stamp)+'-table-scatter.dat', 'w') as scatter:
+		scatter.write('x\ty\tlabel\n')
+		eps.write('x\ty\tlabel\n')
+		for mag in maglist:
+			# load data
+			n = 10**mag
+			if not os.path.isfile(str(stamp)+'-cost-'+str(n)+'.json'):
+				print ("File ", str(stamp),'-cost-',str(n),'.json', 'does not exist. aborting.')
+				break
+			with open(str(stamp)+'-cost-'+str(n)+'.json', 'r') as j:
+				cost = json.load(j)
+			with open(str(stamp)+'-edges-'+str(n)+'.json', 'r') as j:
+				edges = json.load(j)
+			print("Reformating", str(stamp),",",str(n),".")
+			with open(str(stamp)+'-cost-'+str(n)+'.json', 'r') as j:
+				cost = json.load(j)
+			with open(str(stamp)+'-edges-'+str(n)+'.json', 'r') as j:
+				edges = json.load(j)
+
+			for i in range(offset,len(edges)):
+				scatter.write(str(edges[i]) + '\t' + str(cost[i]) + '\t' + classlist[magindex] + '\n')
+				eps.write(str(edges[i]/n) + '\t' + str(edges[i]/cost[i]) + '\t' + classlist[magindex] + '\n')
+			offset = len(edges) #workaround for a bug in the parameter studies code: lists are not emptied when going to higher magnitude
+			scatter.write('\n')
+			eps.write('\n')
+			magindex = magindex + 1
 
