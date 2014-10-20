dbe844fab586df5377b9f28874bbe3963cfeeec0
cls <christian.staudt@kit.edu>
2014-10-20 11:44:26 +0200

    dynamic.graphFromStream can now create directed graphs

--- networkit/dynamic.py
+++ networkit/dynamic.py
@@ -2,15 +2,19 @@
 from _NetworKit import Graph, GraphEvent, DGSStreamParser, GraphUpdater
 
 
-def graphFromStream(stream):
+def graphFromStream(stream, weighted, directed):
 	""" Convenience function for creating a new graph from a stream of graph events
 
 	Parameters
 	----------
 	stream : list of GraphEvent
 		event stream
+	weighted : produce a weighted or unweighted graph
+		boolean
+	directed : produce a directed or undirected graph
+		boolean
 	"""
-	G = Graph()
+	G = Graph(0, weighted, directed)
 	gu = GraphUpdater(G)
 	gu.update(stream)
 	return G
