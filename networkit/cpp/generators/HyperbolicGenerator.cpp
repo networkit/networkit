df6118c2aa90419246d42370e6db2cd2b1c66a9c
Moritz von Looz <moritz.looz-corswarem@kit.edu>
2014-10-22 18:39:52 +0200

    added documentation to hyperbolic generator cleaned up hyperbolic space

--- networkit/cpp/generators/HyperbolicGenerator.cpp
+++ networkit/cpp/generators/HyperbolicGenerator.cpp
@@ -3,6 +3,10 @@
  *
  *  Created on: 20.05.2014
  *      Author: Moritz v. Looz (moritz.looz-corswarem@kit.edu)
+ * This generator is a simplified version of the model presented in
+ * "Hyperbolic geometry of complex networks. Physical Review E, 82:036106, Sep 2010." by
+ *   Dmitri Krioukov, Fragkiskos Papadopoulos, Maksim Kitsak, Amin Vahdat, and Marian Boguna
+ *
  */
 
 #include <cstdlib>
@@ -26,14 +30,18 @@ HyperbolicGenerator::HyperbolicGenerator() {
 	factor = 1;
 	nodeCount = 10000;
 }
-
+/**
+ * Construct a generator for n nodes and the parameters, t, alpha and s.
+ */
 HyperbolicGenerator::HyperbolicGenerator(count n, double distanceFactor, double alpha, double stretchradius) {
 	nodeCount = n;
 	stretch = stretchradius;
 	factor = distanceFactor;
 	this->alpha = alpha;
 }
-
+/**
+ * Construct a generator for n nodes and m edges
+ */
 HyperbolicGenerator::HyperbolicGenerator(count n, count m) {
 	nodeCount = n;
 	double R = acosh((double)n/(2*M_PI)+1);
@@ -44,8 +52,8 @@ HyperbolicGenerator::HyperbolicGenerator(count n, count m) {
 }
 
 HyperbolicGenerator::~HyperbolicGenerator() {
-	// TODO Auto-generated destructor stub
 }
+
 
 Graph HyperbolicGenerator::generate() {
 	return generate(nodeCount, factor, alpha, stretch);
@@ -56,6 +64,7 @@ Graph HyperbolicGenerator::generate(count n, double distanceFactor, double alpha
 	vector<double> angles(n);
 	vector<double> radii(n);
 	double r = HyperbolicSpace::hyperbolicRadiusToEuclidean(R);
+	//sample points randomly
 	HyperbolicSpace::fillPoints(&angles, &radii, stretchradius, alpha);
 	INFO("Generated Points");
 	return generate(&angles, &radii, r, R*distanceFactor);
@@ -84,6 +93,8 @@ Graph HyperbolicGenerator::generate(vector<double> * angles, vector<double> * ra
 	index n = angles->size();
 	assert(radii->size() == n);
 	Quadtree<index> quad(R);
+
+	//initialize a graph builder for n nodes and an undirected, unweighted graph with direct swap
 	GraphBuilder result(n, false, false, true);
 	for (index i = 0; i < n; i++) {
 		assert(radii->at(i) < R);
@@ -96,20 +107,21 @@ Graph HyperbolicGenerator::generate(vector<double> * angles, vector<double> * ra
 	Aux::ProgressMeter progress(n, 1000);
 	#pragma omp parallel for schedule(dynamic, 1000)
 	for (index i = 0; i < n; i++) {
-			vector<index> near = quad.getCloseElements(HyperbolicSpace::polarToCartesian(angles->at(i), radii->at(i)), thresholdDistance);
-			for (index j : near) {
-				if (i != j) {
-						result.addEdge(i,j);
-				}
-			}
-
-			if (i % 1000 == 0) {
-				#pragma omp critical (progress)
-				{
-					progress.signal(i);
-				}
+		//get neighbours for node i
+		vector<index> near = quad.getCloseElements(HyperbolicSpace::polarToCartesian(angles->at(i), radii->at(i)), thresholdDistance);
+		for (index j : near) {
+			if (i != j) {
+				//we add half-edges from both directions at the same time. Due to the symmetry of distances, the correct edges will be formed in parallel without a need for deduplication
+				result.addEdge(i,j);
 			}
 		}
+			if (i % 1000 == 0) {
+			#pragma omp critical (progress)
+			{
+				progress.signal(i);
+			}
+		}
+	}
 
 	timer.stop();
 	INFO("Generated Graph, took ", timer.elapsedMilliseconds(), " milliseconds.");
--- networkit/cpp/generators/HyperbolicGenerator.h
+++ networkit/cpp/generators/HyperbolicGenerator.h
@@ -22,13 +22,54 @@ typedef index node; // node indices are 0-based
 class HyperbolicGenerator: public NetworKit::StaticGraphGenerator {
 public:
 	HyperbolicGenerator();
+	/**
+	 * @param[in] n Number of nodes
+	 * @param[in] factor Size of neighborhood radius. If factor=1, radius = R
+	 * @param[in] alpha Dispersion parameter, default=1
+	 * @param[in] stretchradius Stretching the hyperbolic disk results in thinner graphs, default=1
+	 */
 	HyperbolicGenerator(count n, double factor = 1, double alpha = 1, double stretchradius = 1);
+
+	/**
+	 * @param[in] n Number of nodes
+	 * @param[in] m Number of edges
+	 */
 	HyperbolicGenerator(count n, count m);
+
 	virtual ~HyperbolicGenerator();
+
+	/**
+	 * @param[in] n Number of nodes
+	 * @param[in] stretch Parameter s
+	 * @return Theoretical number of edges expected from a graph with alpha=1 and factor=1
+	 */
 	static double expectedNumberOfEdges(count n, double stretch);
+
+	/**
+	 * @param[in] angles Pointer to angles of node positions
+	 * @param[in] radii Pointer to radii of node positions
+	 * @param[in] R radius of hyperbolic disk to place nodes in
+	 * @param[in] thresholdDistance Edges are added for nodes closer to each other than this threshold
+	 * @return Graph to be generated according to parameters
+	 */
 	static Graph generate(vector<double> * angles, vector<double> * radii, double R, double thresholdDistance);
+
+	/**
+	 * Convenience function to convert polar coordinates into Cartesian coordinates
+	 */
 	static std::map<index, Point<float> > getCoordinates(vector<double> &angles, vector<double> &radii);
+
+	/**
+	 * @param[in] n Number of nodes
+	 * @param[in] factor Size of neighborhood radius. If factor=1, radius = R
+	 * @param[in] alpha Dispersion parameter, default=1
+	 * @param[in] stretchradius Stretching the hyperbolic disk results in thinner graphs, default=1
+	 * @return Graph to be generated according to parameters
+	 */
 	Graph generate(count n, double distanceFactor=1, double alpha=1, double stretchradius = 1);
+	/**
+	 * @return Graph to be generated according to parameters specified in constructor.
+	 */
 	Graph generate();
 
 private:
