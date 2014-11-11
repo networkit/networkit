c22b2fdfad60749290c84c579ad1e14b93addf50
cls <christian.staudt@kit.edu>
2014-11-11 16:39:45 +0100

    extended plot module

--- networkit/plot.py
+++ networkit/plot.py
@@ -2,16 +2,17 @@ from networkit import *
 import matplotlib.pyplot as plt
 import numpy as np
 import operator
+import pandas
 
 try:
 	import seaborn
 	seaborn.set_style("whitegrid")
 except ImportError as importError:
-	print("WARNING: module 'seaborn' is not installed, but recommended")
+	print("WARNING: module 'seaborn' is not installed, plotting functionality will be limited")
 	print(importError)
 
 
-def degreeDistribution(G):
+def degreeDistribution(G, **kwargs):
 	"""Plots the degree distribution of the given network."""
 	dd = properties.degreeDistribution(G)
 	plt.yscale("symlog")
@@ -22,16 +23,19 @@ def degreeDistribution(G):
 
 # TODO: hop plot
 
-# TODO: core decomposition sequence
+def coreDecompositionSequence(G, **kwargs):
+	""" Plots the core decomposition sequence of G, i.e. the size of the k-shell for the core number k"""
+	shells = properties.CoreDecomposition(G).run().shells()
+	data = pandas.DataFrame({"k": range(len(shells)), "n_k": [len(shell) for shell in shells]})
+	plt.xlabel("core number")
+	plt.ylabel("number of nodes")
+	plt.plot(data["k"], data["n_k"], **kwargs)
 
-# TODO: clustering coefficient per degree
 
-def clusteringPerDegree(G):
+def clusteringPerDegree(G, **kwargs):
+	""" Plots the local clustering coefficient for nodes with specific degree"""
 	degs = properties.degreeSequence(G)
 	cc = properties.ClusteringCoefficient.exactLocal(G)
-	pairs = sorted(zip(degs, cc), key=operator.itemgetter(0))
-	x, y = zip(*pairs)
-	plt.xlabel("degree")
-	plt.ylabel("clustering coefficient")
-	plt.scatter(x, y)
-	seaborn.jointplot(np.array(x), np.array(y), kind="reg")
+	data = pandas.DataFrame({"deg": degs, "cc" : cc})
+	data = data.groupby("deg", as_index=False).mean()
+	jointplot = seaborn.jointplot("deg", "cc", data, kind="reg", ylim=(0, 1), **kwargs)
