bde2e9e55f1d026890b675e94287adb26ff95e61
mwegner <mwegner@localhost>
2014-11-14 08:20:12 +0100

    some first tests

---
+++ networkit/cpp/numerics/test/MultigridSolverGTest.h
@@ -0,0 +1,40 @@
+/*
+ * MultigridSolverGTest.h
+ *
+ *  Created on: 03.11.2014
+ *      Author: Michael
+ */
+
+#ifndef NOGTEST
+
+#ifndef MULTIGRIDSOLVERGTEST_H_
+#define MULTIGRIDSOLVERGTEST_H_
+
+#include "gtest/gtest.h"
+
+#include "../../algebraic/Matrix.h"
+#include "../../algebraic/Vector.h"
+#include "../../io/METISGraphReader.h"
+#include "../../properties/ConnectedComponents.h"
+#include "../../structures/Partition.h"
+
+#include "../MultigridSolver.h"
+#include "../MatchingHierarchyBuilder.h"
+#include "../GaussSeidelRelaxation.h"
+
+using namespace std;
+
+namespace NetworKit {
+
+class MultigridSolverGTest : public testing::Test {
+protected:
+	const vector<string> GRAPH_INSTANCES = {"power.graph", "arxiv-qfin-author.dgs"};
+
+	Vector randZeroSum(const Graph &graph, size_t seed);
+};
+
+} /* namespace NetworKit */
+
+#endif /* MULTIGRIDSOLVERGTEST_H_ */
+
+#endif
