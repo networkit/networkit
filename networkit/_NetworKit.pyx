c5c5797f2c0b94408c26720c3dc4f38590e7f2cf
Michael Hamann <michael.hamann@kit.edu>
2014-11-18 18:12:19 +0100

    Added configuration model graph generator

--- networkit/_NetworKit.pyx
+++ networkit/_NetworKit.pyx
@@ -1426,6 +1426,64 @@ cdef class HavelHakimiGenerator:
 		"""
 		return Graph(0).setThis(self._this.generate())
 
+cdef extern from "cpp/generators/ConfigurationModelGenerator.h":
+	cdef cppclass _ConfigurationModelGenerator "NetworKit::ConfigurationModelGenerator":
+		_ConfigurationModelGenerator(vector[count] degreeSequence, bool ignoreIfRealizable) except +
+		_Graph generate() except +
+		bool isRealizable() except +
+		bool getRealizable() except +
+
+cdef class ConfigurationModelGenerator:
+	"""
+	Configuration model graph generator for generating a random simple graph with exactly the given degree sequence.
+
+	This implementation is based on the paper
+	"Random generation of large connected simple graphs with prescribed degree distribution" by Fabien Viger and Matthieu Latapy,
+	available at http://www-rp.lip6.fr/~latapy/FV/generation.html, however without preserving connectivity (this could later be added as
+	optional feature).
+
+	The Havel-Hakami generator is used for the initial graph generation, then the Markov-Chain Monte-Carlo algorithm as described and
+	implemented by Fabien Viger and Matthieu Latapy but without the steps for ensuring connectivity is executed. This should lead to a
+	graph that is drawn uniformly at random from all graphs with the given degree sequence.
+
+	Note that at most 10 times the number of edges edge swaps are performed (same number as in the abovementioned implementation) and
+	in order to limit the running time, at most 200 times as many attempts to perform an edge swap are made (as certain degree distributions
+	do not allow edge swaps at all).
+
+	Parameters
+	----------
+	degreeSequence : vector[count]
+		The degree sequence that shall be generated
+	ignoreIfRealizable : bool, optional
+		If true, generate the graph even if the degree sequence is not realizable. Some nodes may get lower degrees than requested in the sequence.
+	"""
+	cdef _ConfigurationModelGenerator *_this
+
+	def __cinit__(self, vector[count] degreeSequence, bool ignoreIfRealizable = False):
+		self._this = new _ConfigurationModelGenerator(degreeSequence, ignoreIfRealizable)
+
+	def __dealloc__(self):
+		del self._this
+
+	def isRealizable(self):
+		return self._this.isRealizable()
+
+	def getRealizable(self):
+		return self._this.getRealizable()
+
+	def generate(self):
+		"""
+		Generate a graph according to the configuration model.
+
+		Issues a INFO log message if the wanted number of edge swaps cannot be performed because of the limit of attempts (see in the description of the class for details).
+
+		Returns
+		-------
+		Graph
+			The generated graph.
+		"""
+		return Graph().setThis(self._this.generate())
+
 
 cdef extern from "cpp/generators/RmatGenerator.h":
 	cdef cppclass _RmatGenerator "NetworKit::RmatGenerator":
