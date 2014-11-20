237f71f5af50e010e9b2a7a8b2ff0edf55344398
Gerd Lindner <lindner.gerd@gmail.com>
2014-11-20 10:49:04 +0100

    Moved GeometricMean and Linearization attributizers to edgeproperties

---
+++ networkit/cpp/edgeproperties/GeometricMeanAttributizer.cpp
@@ -0,0 +1,40 @@
+/*
+ * GeometricMeanAttributizer.h
+ *
+ *  Created on: 20.11.2014
+ *      Author: Michael Hamann
+ */
+
+#include <cmath>
+#include "GeometricMeanAttributizer.h"
+#include "../auxiliary/Log.h"
+
+namespace NetworKit {
+
+GeometricMeanAttributizer::GeometricMeanAttributizer(const Graph& graph, const std::vector<double>& attribute): graph(graph), attribute(attribute) {
+}
+
+std::vector< double > GeometricMeanAttributizer::getAttribute() {
+	std::vector<double> result(graph.upperEdgeIdBound());
+	
+	std::vector<double> nodeSum(graph.upperNodeIdBound());
+	
+	graph.parallelForNodes([&](node u) {
+		graph.forEdgesOf(u, [&](node _u, node v, edgeid eid) {
+			nodeSum[u] += attribute[eid];
+		});
+	});
+	
+	graph.parallelForEdges([&](node u, node v, edgeid eid) {
+		if (attribute[eid] > 0) {
+			result[eid] = attribute[eid] * 1.0 / std::sqrt(nodeSum[u] * nodeSum[v]);
+			if (std::isnan(result[eid])) {
+				ERROR("Attribute ", attribute[eid], " couldn't be normalized with sum ", nodeSum[u], " and sum ", nodeSum[v]);
+			}
+		}
+	});
+	
+	return result;
+}
+
+} /* namespace NetworKit */
