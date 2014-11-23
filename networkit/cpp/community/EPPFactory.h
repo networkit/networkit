db61e2becd00b694a0e0578c4793b6ee7dafb06d
maxv <maximilian.vogel@student.kit.edu>
2014-11-23 11:29:25 +0100

    added documentation for EPPFactory

--- networkit/cpp/community/EPPFactory.h
+++ networkit/cpp/community/EPPFactory.h
@@ -1,5 +1,5 @@
 /*
- * EnsembleFactory.cpp
+ * EPPFactory.h
  *
  *  Created on: 12.11.2013
  *      Author: Christian Staudt (christian.staudt@kit.edu)
@@ -11,7 +11,22 @@
 #include "EPP.h"
 namespace NetworKit {
 namespace EPPFactory {
+	/**
+	 * Creates an instance of the ensemble preprocessing.
+	 * @param	G	The graph on which the ensemble is supposed to run.
+	 * @param	ensembleSize	The amount of baseAlgorithms to preprocess the communities.
+	 * @param	baseAlgorithm	Algorithm ("PLP","PLM") to preprocess the communities. ensembleSize instances will be created.
+	 * @param	finalAlgorithm	Algorithm ("PLP" "PLM[R]") to finish the ensemble.
+	 */
 	EPP make(const Graph& G, count ensembleSize, std::string baseAlgorithm, std::string finalAlgorithm);
+
+	/**
+	 * Creates an instance of the ensemble preprocessing and returns a pointer to it (for Cython).
+	 * @param	G	The graph on which the ensemble is supposed to run.
+	 * @param	ensembleSize	The amount of baseAlgorithms to preprocess the communities.
+	 * @param	baseAlgorithm	Algorithm ("PLP","PLM") to preprocess the communities. ensembleSize instances will be created.
+	 * @param	finalAlgorithm	Algorithm ("PLP" "PLM[R]") to finish the ensemble.
+	 */
 	EPP* makePtr(const Graph& G, count ensembleSize, std::string baseAlgorithm, std::string finalAlgorithm);
 }
 }
