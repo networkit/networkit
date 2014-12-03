3683c20d0eed826365decf7730df2d8c6983011a
Gerd Lindner <lindner.gerd@gmail.com>
2014-12-03 08:06:50 +0100

    Improved sparsification evaluation and plots

--- scripts/BackboneEvaluation/consoleWriter.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class ConsoleWriter:
-
-	def receiveResult(self, taskResult):
-		for row in taskResult.data:
-			print(row)
---
+++ scripts/SparsificationEvaluation/evaluationSparsifiers.py
@@ -0,0 +1,161 @@
+from types import *
+from networkit import *
+
+class S_SimmelianBackboneNonParametric:
+    
+    def getShortName(self):
+        return "Simmelian NonParametric"
+
+    def getAlgorithm(self):
+        return sparsification.SimmelianBackboneNonParametric()
+
+    def requiresWeight(self):
+        return False
+    
+    def parameterizationType(self):
+        return "Default"
+
+# -----------------------------------------------------------
+
+class OriginalAlgorithm(sparsification.Sparsifier):
+    
+    def getAttribute(self, G):
+        return None
+
+    def _getSparsifiedGraph(self, G, parameter, attribute):
+        return G
+
+    def _getParameterizationAlgorithm(self):
+        return sparsification.SimpleParameterization()
+    
+class S_Original:
+    
+    def getShortName(self):
+        return "Original"
+
+    def requiresWeight(self):
+        return False
+    
+    def getAlgorithm(self):
+        return OriginalAlgorithm()
+    
+    def parameterizationType(self):
+        return "None"
+
+# -----------------------------------------------------------
+
+class S_SimmelianMultiscale:
+    
+    def getShortName(self):
+        return "Simmelian Multiscale"
+
+    def getAlgorithm(self):
+        return sparsification.SimmelianMultiscaleBackbone()
+
+    def requiresWeight(self):
+        return False
+    
+    def parameterizationType(self):
+        return "Default"
+# -----------------------------------------------------------
+
+class S_SimmelianBackboneParametric:
+
+    def getShortName(self):
+        return "Simmelian Parametric"
+    
+    def getAlgorithm(self):
+        return sparsification.SimmelianBackboneParametric()
+
+    def requiresWeight(self):
+        return False
+    
+    def parameterizationType(self):
+        return "Default"
+
+# -----------------------------------------------------------
+
+class S_LocalSimilarity:
+
+    def getShortName(self):
+        return "Local Similarity"
+    
+    def getAlgorithm(self):
+        return sparsification.LocalSimilarityBackbone()
+
+    def requiresWeight(self):
+        return False
+    
+    def parameterizationType(self):
+        return "Default"
+
+# -----------------------------------------------------------
+
+class S_Multiscale:
+    
+    def getShortName(self):
+        return "Multiscale"
+
+    def getAlgorithm(self):
+        return sparsification.MultiscaleBackbone()
+    
+    def requiresWeight(self):
+        return True
+    
+    def parameterizationType(self):
+        return "Default"
+
+# -----------------------------------------------------------
+
+class S_Random:
+    def __init__(self, tag):
+        self._tag = tag
+
+    def getShortName(self):
+        return ("Random " + self._tag).strip()
+
+    def getAlgorithm(self):
+        return sparsification.RandomBackbone()
+
+    def requiresWeight(self):
+        return False
+    
+    def parameterizationType(self):
+        return "Default"
+
+# -----------------------------------------------------------
+
+class S_ForestFire:
+
+    def __init__(self, tag, pf, tber):
+        self.tag = tag
+        self.pf = pf
+        self.tber = tber
+
+    def getShortName(self):
+        return ("ForestFire " + self.tag).strip()
+
+    def getAlgorithm(self):
+        return sparsification.ForestFireBackbone(self.pf, self.tber)
+    
+    def requiresWeight(self):
+        return False
+    
+    def parameterizationType(self):
+        return "Default"
+
+# -----------------------------------------------------------
+
+class S_LocalDegree:
+
+    def getShortName(self):
+        return "Local Degree"
+
+    def getAlgorithm(self):
+        return sparsification.LocalDegreeBackbone()
+    
+    def requiresWeight(self):
+        return False
+    
+    def parameterizationType(self):
+        return "Default"
