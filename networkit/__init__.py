c491573d637399ffd45b74c990b81244d016b354
cls <christian.staudt@kit.edu>
2014-12-04 18:03:45 +0100

    bumped version to 3.5

--- networkit/__init__.py
+++ networkit/__init__.py
@@ -6,7 +6,7 @@ usage examples can be found on http://nbviewer.ipython.org/urls/networkit.iti.ki
 __author__ = "Christian Staudt"
 __copyright__ = "Copyright (c) 2014 Christan Staudt"
 __license__ = "MIT"
-__version__ = "3.4"
+__version__ = "3.5"
 
 
 # standard library modules
