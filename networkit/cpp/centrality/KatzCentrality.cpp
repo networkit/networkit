1aaf1a974047c4581bccd46a6692b5aaf9e0043d
Henning Meyerhenke <meyerhenke@kit.edu>
2015-01-09 17:48:23 +0100

    fix for numerical centrality measures on directed graphs

---
+++ networkit/cpp/centrality/KatzCentrality.cpp
@@ -0,0 +1,72 @@
+/*
+ * KatzCentrality.cpp
+ *
+ *  Created on: 09.01.2015
+ *      Author: Henning
+ */
+
+#include "KatzCentrality.h"
+#include "../auxiliary/NumericTools.h"
+
+namespace NetworKit {
+
+KatzCentrality::KatzCentrality(const Graph& G, double tol):
+		Centrality(G, true), tol(tol)
+{
+
+}
+
+void KatzCentrality::run() {
+	count z = G.upperNodeIdBound();
+	std::vector<double> values(z, 1.0);
+	scoreData = values;
+	double length = 0.0;
+	double oldLength = 0.0;
+
+	// Katz centrality constants
+	double alpha = 1e-3;
+	double beta = 1.0;
+
+	auto converged([&](double val, double other) {
+		// compute residual
+		return (Aux::NumericTools::equal(val, other, tol));
+	});
+
+	do {
+		oldLength = length;
+
+		// iterate matrix-vector product
+		G.parallelForNodes([&](node u) {
+			values[u] = 0.0;
+			G.forNeighborsOf(u, [&](node v) {
+				values[u] += G.weight(u, v) * scoreData[v];
+			});
+			values[u] *= alpha;
+			values[u] += beta;
+		});
+
+		// normalize values
+		length = 0.0;
+		length = G.parallelSumForNodes([&](node u) {
+			return (values[u] * values[u]);
+		});
+		length = sqrt(length);
+		G.parallelForNodes([&](node u) {
+			values[u] /= length;
+		});
+
+//		TRACE("length: ", length);
+//		TRACE(values);
+
+		scoreData = values;
+	} while (! converged(length, oldLength));
+
+	// check sign and correct if necessary
+	if (scoreData[0] < 0) {
+		G.parallelForNodes([&](node u) {
+			scoreData[u] = fabs(scoreData[u]);
+		});
+	}
+}
+
+} /* namespace NetworKit */
---
+++ networkit/cpp/centrality/KatzCentrality.h
@@ -0,0 +1,36 @@
+/*
+ * KatzCentrality.h
+ *
+ *  Created on: 09.01.2015
+ *      Author: Henning
+ */
+
+#ifndef KATZCENTRALITY_H_
+#define KATZCENTRALITY_H_
+
+#include "Centrality.h"
+
+namespace NetworKit {
+
+/**
+ * @ingroup centrality
+ * Computes the Katz centrality of the graph.
+ */
+class KatzCentrality: public Centrality {
+protected:
+	double tol; // error tolerance
+
+public:
+	/**
+	 * Constructs the EigenvectorCentrality class for the given Graph @a G. @a tol defines the tolerance for convergence.
+	 *
+	 * @param[in] G The graph.
+	 * @param[in] tol The tolerance for convergence.
+	 */
+	KatzCentrality(const Graph& G, double tol = 1e-9);
+
+	virtual void run();
+};
+
+} /* namespace NetworKit */
+#endif /* KATZCENTRALITY_H_ */
