Fixed calculation of KS statistics (now using sequences instead of distributions)

scripts/SparsificationEvaluation/evaluationProperties.py
@@ -209,36 +209,28 @@ class P_KolmogorowSmirnow:
 	def getName(self):
 		return "KolmogorowSmirnow"
 
-	def getCCDistPerDegree(self, inputGraph):
-		localCCs = properties.ClusteringCoefficient.exactLocal(inputGraph)
-		maxDegree = max([inputGraph.degree(n) for n in inputGraph.nodes()])
-		ccPerDegree = list(map(lambda d : np.average([localCCs[n] for n in inputGraph.nodes() if inputGraph.degree(n) == d]), range(0, maxDegree + 1)))
-		#We map nan to 0. is this the way to go?? (currently, above line will produce many warnings because we try to calculate the average values of empty lists...)
-		ccPerDegree = list(map(lambda avg : 0.0 if math.isnan(avg) else avg, ccPerDegree))
-		return ccPerDegree
-
-	def getWCCSizesDistribution(self, inputGraph):
+	def getWCCSizes(self, inputGraph):
 		wccs = properties.ConnectedComponents(inputGraph)
 		wccs.run()
-		componentSizesList = list(map(lambda tuple_ID_Size: tuple_ID_Size[1], list(wccs.getComponentSizes().items())))
-		componentSizesDist = list(map(lambda s : len([c for c in componentSizesList if c == s]), range(0, max(componentSizesList) + 1)))
-		return componentSizesDist
+		componentSizes = list(map(lambda tuple_ID_Size: tuple_ID_Size[1], list(wccs.getComponentSizes().items())))
+		#componentSizesDist = list(map(lambda s : len([c for c in componentSizesList if c == s]), range(0, max(componentSizesList) + 1)))
+		return componentSizes
 
 	def getValues(self, graph, sparsifiedGraph):
 		#Degree Distribution
-		ddGraph = properties.degreeDistribution(graph)
-		ddSparsifiedGraph = properties.degreeDistribution(sparsifiedGraph)
-		ks_dd, p_dd = stats.ks_2samp(ddGraph, ddSparsifiedGraph)
+		sampleGraph = properties.degreeSequence(graph)
+		sampleSparsifiedGraph = properties.degreeSequence(sparsifiedGraph)
+		ks_dd, p_dd = stats.ks_2samp(sampleGraph, sampleSparsifiedGraph)
 
-		#Distribution of clustering coefficients (per degree)
-		ddGraph = self.getCCDistPerDegree(graph)
-		ddSparsifiedGraph = self.getCCDistPerDegree(sparsifiedGraph)
-		ks_cc_perDegree, p_cc_perDegree = stats.ks_2samp(ddGraph, ddSparsifiedGraph)
+		#Distribution of clustering coefficients (TODO: per degree?)
+		sampleGraph = properties.ClusteringCoefficient.exactLocal(graph)#self.getCCDistPerDegree(graph)
+		sampleSparsifiedGraph = properties.ClusteringCoefficient.exactLocal(sparsifiedGraph)#self.getCCDistPerDegree(sparsifiedGraph)
+		ks_cc_perDegree, p_cc_perDegree = stats.ks_2samp(sampleGraph, sampleSparsifiedGraph)
 
 		#Distribution of sizes of weakly connected components
-		ddGraph = self.getWCCSizesDistribution(graph)
-		ddSparsifiedGraph = self.getWCCSizesDistribution(sparsifiedGraph)
-		ks_wccSizes, p_wccSizes = stats.ks_2samp(ddGraph, ddSparsifiedGraph)
+		sampleGraph = self.getWCCSizes(graph)
+		sampleSparsifiedGraph = self.getWCCSizes(sparsifiedGraph)
+		ks_wccSizes, p_wccSizes = stats.ks_2samp(sampleGraph, sampleSparsifiedGraph)
 
 		return {'ks_dd':ks_dd, 'p_dd':p_dd, 'ks_cc_perDegree':ks_cc_perDegree, 'p_cc_perDegree':p_cc_perDegree, 'ks_wccSizes':ks_wccSizes, 'p_wccSizes':p_wccSizes}
 