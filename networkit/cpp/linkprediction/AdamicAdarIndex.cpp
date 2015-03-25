bcaf3792d3b787431bc42f2b574f9ddb5378d2bc
Kolja Esders <kolja.esders@student.kit.edu>
2015-03-25 12:32:46 +0100

    Added Adamic/Adar index.
    * Edited notebook accordingly
    * Exposed to python

---
+++ networkit/cpp/linkprediction/AdamicAdarIndex.cpp
@@ -0,0 +1,31 @@
+/*
+ * AdamicAdarIndex.cpp
+ *
+ *  Created on: 25.03.2015
+ *      Author: Kolja Esders (kolja.esders@student.kit.edu)
+ */
+
+#include "AdamicAdarIndex.h"
+
+#include <cmath>
+
+namespace NetworKit {
+
+AdamicAdarIndex::AdamicAdarIndex() : commonNeighborsIndex() {
+}
+
+AdamicAdarIndex::AdamicAdarIndex(const Graph& G) : LinkPredictor(G), commonNeighborsIndex() {
+}
+
+double AdamicAdarIndex::runImpl(node u, node v) {
+  commonNeighborsIndex.setGraph(*G);
+  std::vector<node> commonNeighbors = commonNeighborsIndex.getCommonNeighbors(u, v);
+  double sum = 0;
+  for (node w : commonNeighbors) {
+    sum += 1.0 / std::log(G->degree(w));
+  }
+  return sum;
+}
+
+
+} // namespace NetworKit
