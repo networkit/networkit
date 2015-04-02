9dff1bd130ddf20f1cf3009b812966257d882368
Kolja Esders <kolja.esders@student.kit.edu>
2015-04-02 02:29:08 +0200

    Changed behaviour on tie during ROC/PR construction
    * If the new calculated x-value is equal to the previous one the old
      y-value will be discarded in favor of the new one.

--- networkit/cpp/linkprediction/ROCMetric.cpp
+++ networkit/cpp/linkprediction/ROCMetric.cpp
@@ -13,15 +13,14 @@ std::pair<std::vector<double>, std::vector<double>> ROCMetric::generatePoints() 
   std::pair<std::vector<double>, std::vector<double>> points;
   points.first.reserve(thresholds.size());
   points.second.reserve(thresholds.size());
-
-  INFO("falsePositives.size() = ", falsePositives.size());
-
   for (index i = 0; i < thresholds.size(); ++i) {
     double falsePositiveRatio = 1.0 * falsePositives.at(i) / numNegatives;
-    if (points.first.size() == 0 || points.first.back() < falsePositiveRatio) {
+    if (!points.first.empty() && points.first.back() == falsePositiveRatio) {
+      points.second.pop_back();
+    } else {
       points.first.push_back(falsePositiveRatio);
-      points.second.push_back(1.0 * truePositives.at(i) / numPositives);
     }
+    points.second.push_back(1.0 * truePositives.at(i) / numPositives);
   }
   return points;
 }
