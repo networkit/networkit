68665896d04ba50a3833e53c2a7ccd3f15f89ace
Kolja Esders <kolja.esders@student.kit.edu>
2015-04-03 02:45:30 +0200

    Documented Jaccard index thoroughly

--- networkit/cpp/linkprediction/JaccardIndex.h
+++ networkit/cpp/linkprediction/JaccardIndex.h
@@ -16,17 +16,18 @@ namespace NetworKit {
 /**
  * @ingroup linkprediction
  *
- *
+ * Implementation of the Jaccard index which normalizes the Common Neighbors Index
+ * by dividing through the number of nodes in the neighboorhood-union.
  */
 class JaccardIndex : public LinkPredictor {
 private:
-  CommonNeighborsIndex commonNeighborsIndex;
+  CommonNeighborsIndex commonNeighborsIndex; //!< Used to get the number of common neighbors of u and v
 
   /**
-   *
+   * Returns the Jaccard index for the given node-pair (@a u, @a v).
    * @param u First node
    * @param v Second node
-   * @return 
+   * @return the Jaccard index for the given node-pair (@a u, @a v)
    */
   double runImpl(node u, node v) override;
 
@@ -39,6 +40,12 @@ public:
    */
   explicit JaccardIndex(const Graph& G);
 
+  /**
+   * Returns the union of the neighboorhoods of @a u and @a v.
+   * @param u First node
+   * @param v Second node
+   * @return a vector containing all the nodes in the neighboorhood-union of u and v
+   */
   std::vector<node> getNeighborsUnion(node u, node v) const;
 };
 
