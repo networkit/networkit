d1e8426fd14cc551c4f2841ba0c6ab47fbfe4a2c
Kolja Esders <kolja.esders@student.kit.edu>
2015-04-06 14:52:04 +0200

    TrainingGraphSampler is now a namespace instead of a class

--- networkit/cpp/linkprediction/TrainingGraphSampler.h
+++ networkit/cpp/linkprediction/TrainingGraphSampler.h
@@ -19,28 +19,28 @@ namespace NetworKit {
  *
  * Samples a given amount of links from a given graph in order to generate a training graph.
  */
-class TrainingGraphSampler {
-public:
-  /**
-   * Returns a graph that contains @a trainPercentage percent of links form the given graph @a G.
-   * The links are randomly selected from G until the given percentage is reached.
-   * @param G The graph to construct the training graph from
-   * @param trainPercentage Percentage of links regarding the number of links in the
-   * given graph that should be in the returned graph
-   * @return a graph that contains the given percentage of links from @a G
-   */
-  static Graph byPercentage(const Graph& G, double trainPercentage);
+namespace TrainingGraphSampler {
 
-  /**
-   * Returns a graph that contains @a numTrainEdges links from the given graph @a G.
-   * The links are randomly selected from G until the given count is reached.
-   * @param G The graph to construct the training graph from
-   * @param numTrainLinks Number of links the returned graph should consist of
-   * @return a graph that contains the given number of links from @a G
-   */
-  static Graph byCount(const Graph& G, count numTrainLinks);
+/**
+ * Returns a graph that contains @a trainPercentage percent of links form the given graph @a G.
+ * The links are randomly selected from G until the given percentage is reached.
+ * @param G The graph to construct the training graph from
+ * @param trainPercentage Percentage of links regarding the number of links in the
+ * given graph that should be in the returned graph
+ * @return a graph that contains the given percentage of links from @a G
+ */
+Graph byPercentage(const Graph& G, double trainPercentage);
 
-};
+/**
+ * Returns a graph that contains @a numTrainEdges links from the given graph @a G.
+ * The links are randomly selected from G until the given count is reached.
+ * @param G The graph to construct the training graph from
+ * @param numTrainLinks Number of links the returned graph should consist of
+ * @return a graph that contains the given number of links from @a G
+ */
+Graph byCount(const Graph& G, count numTrainLinks);
+
+} // namespace TrainingGraphSampler
 
 } // namespace NetworKit
 
