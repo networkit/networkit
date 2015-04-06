a6c6104031e6cb08df3e0466bfb028734392f5e3
Kolja Esders <kolja.esders@student.kit.edu>
2015-04-06 02:04:34 +0200

    Documented ROCMetric

--- networkit/cpp/linkprediction/ROCMetric.h
+++ networkit/cpp/linkprediction/ROCMetric.h
@@ -16,11 +16,16 @@ namespace NetworKit {
 /**
  * @ingroup linkprediction
  *
- * Provides data points for the receiver operating characteristic of
- * a given set of predictions for graph edges.
+ * Provides points that define the Receiver Operating Characteristic curve for a given set of predictions.
  */
 class ROCMetric : public EvaluationMetric {
 private:
+  /**
+   * Generate the points of the Receiver Operating Characteristic curve regarding the previously set predictions.
+   * Note that in the case of multiple y-values mapping to the same x-value the highest (=latest) y-value gets picked.
+   * @return a pair of vectors where the first vector contains the false positive rates and the second vector the
+   * corresponding true positive rates
+   */
   std::pair<std::vector<double>, std::vector<double>> generatePoints() override;
 
 public:
