a2428ae32f345effa674c038d70721f42048ffac
Arie Slobbe <nn9081@partner.kit.edu>
2015-06-08 09:57:50 +0200

    python test files

--- networkit/test/test_community.py
+++ networkit/test/test_community.py
@@ -7,7 +7,7 @@ from networkit import *
 class Test_CommunityDetection(unittest.TestCase):
 
 	def setUp(self):
-		self.G = readGraph("input/PGPgiantcompo.graph",Format.METIS)
+		self.G = readGraph("PGPgiantcompo.graph",Format.METIS)
 
 	def test_PLM(self):
 		comms = community.detectCommunities(self.G, community.PLM(self.G))
@@ -17,7 +17,7 @@ class Test_CommunityDetection(unittest.TestCase):
 			self.assertTrue(comms.contains(v))
 
 	def test_CutClustering(self):
-		jazz = readGraph("input/jazz.graph",Format.METIS)
+		jazz = readGraph("jazz.graph",Format.METIS)
 
 		cc = community.CutClustering(jazz,0.5076142131979697)
 
