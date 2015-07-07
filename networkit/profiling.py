4fb949288d62cfbf37d6428b9e683fd54bd4cdc2
aristoteles <pinguin1410@gmx.de>
2015-07-07 20:12:28 +0200

    worker (multiprocessing) class added

--- networkit/profiling.py
+++ networkit/profiling.py
@@ -1,13 +1,15 @@
 #
 # file: profiling.py
+# author: Mark Erb
 #
 
 from networkit import *
 
+import multiprocessing
 from IPython.core.display import *
 from urllib.parse import quote
-from abc import ABCMeta, abstractmethod
 import io
+import sys
 
 import numpy as np
 import matplotlib.mlab as mlab
@@ -17,9 +19,9 @@ import pandas as pd
 			
 			
 try:
-	__IPYTHON__	
+	__IPYTHON__
 except:
-	raise NameError("module has been loaded outside of \"IPython\"")
+	raise ImportError("module has been loaded outside of \"IPython\"")
 
 	
 def readfile(postfix):
@@ -74,26 +76,81 @@ display_html(
 		</script>
 	""")
 )
+
+
+class Worker(multiprocessing.Process):
+	def __init__(self, tasks, results):
+		multiprocessing.Process.__init__(self)
+		self.__tasks = tasks
+		self.__results = results
+		
+	def run(self):
+		while True:
+			task = self.__tasks.get()
+			if task is None:
+				self.__tasks.task_done()
+				break
+			result = task.run()
+			self.__tasks.task_done()
+			self.__results.put(result)
+		
 	
+class Plot_Task(object):
+	def __init__(self, name, index, data):
+		self.__name = name
+		self.__index = index
+		self.__data = data
+	
+	
+	def run(self):
+		plt.ioff()	
+		xLabel = "x-Axis"
+		yLabel = "y-Axis"
+		fig = plt.figure()
+		n, bins, patches = plt.hist(self.__data, 50, normed=1, facecolor='green', alpha=0.75)
+		plt.xlabel(xLabel)
+		plt.ylabel(yLabel)
+		plt.grid(True)
+		fig.tight_layout()
+		imgdata = io.StringIO()
+		fig.savefig(imgdata, format='svg')
+		plt.close(fig)
+		plaintext = imgdata.getvalue()
+		plaintext = " ".join(plaintext[plaintext.find("<svg "):].split())
+		encoded = quote(plaintext, safe='');
+		return (self.__name, self.__index, encoded)
+		
 	
 class Profiling:
 	__TOKEN = object();		
 	__pageCount = 0
 	__verbose = False
-	__verbose_tab = "    "
-	__parallel = True
+	__parallel = multiprocessing.cpu_count() * 2
 		
 		
 	def __init__(self, G, token):
 		if token is not self.__TOKEN:
 			raise ValueError("call create(G) to create an instance")
 		self.__G = G
-		self.__centralities = self.__computeNodeCentralities(G)
-			
-			
+		self.__measures = {}
+		
+	
 	@classmethod
 	def create(cls, G):
-		return cls(G, cls.__TOKEN)
+		result = cls(G, cls.__TOKEN)
+		
+		for parameter in [ 
+			(centrality.DegreeCentrality, 			(G, )),
+			(centrality.CoreDecomposition, 			(G, )),
+			(centrality.LocalClusteringCoefficient, (G, )),
+			(centrality.PageRank, 					(G, )),
+			(centrality.KPathCentrality,			(G, )),
+			(centrality.KatzCentrality,				(G, )),
+			(centrality.ApproxBetweenness2,			(G, max(42, G.numberOfNodes() / 1000), False))
+		]: result.__addMeasure(parameter)
+		
+		result.__loadMeasures()
+		return result;
 	
 
 	@classmethod
@@ -107,7 +164,9 @@ class Profiling:
 	
 	
 	@classmethod
-	def setParallel(cls, parallel)
+	def setParallel(cls, parallel):
+		if (parallel < 1):
+			raise ValueError("parallel < 1");
 		cls.__parallel = parallel
 	
 
@@ -118,106 +177,131 @@ class Profiling:
 
 	def show(self):
 		pageIndex = self.__pageCount
+		tasks = multiprocessing.JoinableQueue()
+		results = multiprocessing.Queue()
+		
+		if self.__verbose:
+			timerAll = stopwatch.Timer()
+		
+		workers = [Worker(tasks, results) for i in range(self.__parallel)]
+		for w in workers:
+			w.deamon = True
+			w.start()
 			
-		plt.ioff()
-	
-		hist = Histogram()
+		numberOfTasks = 0
+		for key in self.__measures:
+			tasks.put(Plot_Task(key, 0, self.__measures[key]["data"]))
+			numberOfTasks += 1
+		for i in range(self.__parallel):
+			tasks.put(None)
+		tasks.join()
+		
+		while(numberOfTasks):
+			(name, index, image) = results.get()
+			self.__measures[name]["image"] = image
+			numberOfTasks -= 1
 		
 		centralities = ""
-		for key in self.__centralities:
-			data = hist.plotSet(self.__centralities[key], "x-Axis", "y-Axis")
-			centralities += '<div class="Plot" title="' + key + '" data-image="' + data + '" />'
-					
+		for key in self.__measures:
+			image = self.__measures[key]["image"]
+			centralities = centralities + "<div class=\"Plot\" title=\"" + key + "\" data-image=\"data:image/svg+xml;utf8," + image + "\" />"
+			
 		result = readfile("profile.html")
 		result = result.format(**locals());
 		display_html(HTML(result))
-			
-		self.__pageCount = self.__pageCount + 1
 		
-		
-	def __computeNodeCentralities(self, G):
-		n = G.numberOfNodes()
-		
-		nodeCentralityAlgos = [	(centrality.DegreeCentrality, 			(G, )),
-								(centrality.CoreDecomposition, 			(G, )),
-								(centrality.LocalClusteringCoefficient, (G, )),
-								(centrality.PageRank, 					(G, )),
-								(centrality.KPathCentrality,			(G, )),
-								(centrality.KatzCentrality,				(G, )),
-								(centrality.ApproxBetweenness2,			(G, max(42, n / 1000), False)) ]
-								
-		result = {}
 		if self.__verbose:
-			print("Centralities:")
-		for (algoClass, params) in nodeCentralityAlgos:
-			algoName = algoClass.__name__
-			if self.__verbose:
-				print(self.__verbose_tab + algoName + ": ", end="", flush=True)
-				t = stopwatch.Timer()
-			algo = algoClass(*params)
-			algo.run()
-			result[algoName] = algo.scores()
-			if self.__verbose:
-				print("{:.2F} s".format(t.elapsed))
-		return result;
+			print("\ntotal time: {:.2F} s".format(timerAll.elapsed))
+	
+		self.__pageCount = self.__pageCount + 1
 	
 	
-class Plot:
-	__metaclass__ = ABCMeta
+	def __addMeasure(self, args):
+		(measureClass, parameters) = args
+		measureName = measureClass.__name__
+		measure = {}
+		measure["index"] = len(self.__measures)
+		measure["class"] = measureClass
+		measure["parameters"] = parameters
+		self.__measures[measureName] = measure
+	
+	
+	def __loadMeasures(self):
+		if self.__verbose:
+			timerAll = stopwatch.Timer()
+		
+		for name in self.__measures:
+			measure = self.__measures[name]
+			instance = measure["class"](*measure["parameters"])
+			if self.__verbose:
+				print(name + ": ", end="", flush=True)
+			timerInstance = stopwatch.Timer()
+			instance.run()
+			elapsed = timerInstance.elapsed
+			if self.__verbose:
+				print("{:.2F} s".format(elapsed))
+			measure["data"] = instance.scores()
+			measure["time"] = elapsed
+		if self.__verbose:
+			print("\ntotal time: {:.2F} s".format(timerAll.elapsed))	
 			
-	def init(self, xScale, yScale):
-		plt.clf();
-		fig, ax = plt.subplots()
-		if xScale:
-			ax.set_xscale('log')
-		if yScale:
-			ax.set_yscale('log')
+	
+# class Plot:
+	# __metaclass__ = ABCMeta
+			
+	# def init(self, xScale, yScale):
+		# plt.clf();
+		# fig, ax = plt.subplots()
+		# if xScale:
+			# ax.set_xscale('log')
+		# if yScale:
+			# ax.set_yscale('log')
 
 
-	def createImageURI(self):
-		fig = plt.gcf()
-		fig.tight_layout()
-		imgdata = io.StringIO()
-		fig.savefig(imgdata, format='svg')
-		plaintext = imgdata.getvalue()
-		plaintext = " ".join(plaintext[plaintext.find("<svg "):].split())
-		encoded = quote(plaintext, safe='');
-		encoded = "data:image/svg+xml;utf8," + encoded; 
-		return encoded;
+	# def createImageURI(self):
+		# fig = plt.gcf()
+		# fig.tight_layout()
+		# imgdata = io.StringIO()
+		# fig.savefig(imgdata, format='svg')
+		# plaintext = imgdata.getvalue()
+		# plaintext = " ".join(plaintext[plaintext.find("<svg "):].split())
+		# encoded = quote(plaintext, safe='');
+		# encoded = "data:image/svg+xml;utf8," + encoded; 
+		# return encoded;
 		
 			
-	def plotSet(self, data, xLabel, yLabel):
-		result = ""
-		for xScale in range(0, 2):
-			for yScale in range(0, 2):
-				result += self.plot(data, xLabel, yLabel, xScale, yScale)
-				if (not(xScale and yScale)):
-					result += "|"
-		return result;
+	# def plotSet(self, data, xLabel, yLabel):
+		# result = ""
+		# for xScale in range(0, 2):
+			# for yScale in range(0, 2):
+				# result += self.plot(data, xLabel, yLabel, xScale, yScale)
+				# if (not(xScale and yScale)):
+					# result += "|"
+		# return result;
 			
 		
-	@abstractmethod 
-	def plot(self, data, xLabel, yLabel, xScale, yScale): pass
+	# @abstractmethod 
+	# def plot(self, data, xLabel, yLabel, xScale, yScale): pass
 			
 			
-class Histogram(Plot):
-	def plot(self, data, xLabel, yLabel, xScale, yScale):
-		self.init(xScale, yScale)
-		n, bins, patches = plt.hist(data, 50, normed=1, facecolor='green', alpha=0.75)
-		plt.xlabel(xLabel)
-		plt.ylabel(yLabel)
-		plt.grid(True)
-		return self.createImageURI()
+# class Histogram(Plot):
+	# def plot(self, data, xLabel, yLabel, xScale, yScale):
+		# self.init(xScale, yScale)
+		# n, bins, patches = plt.hist(data, 50, normed=1, facecolor='green', alpha=0.75)
+		# plt.xlabel(xLabel)
+		# plt.ylabel(yLabel)
+		# plt.grid(True)
+		# return self.createImageURI()
 				
 				
-class HistogramSeaborn(Plot):
-	def plot(self, data, xLabel, yLabel, xScale, yScale):
-		self.init(xScale, yScale)
-		sns.distplot(data);
-		plt.xlabel(xLabel)
-		plt.ylabel(yLabel)
-		plt.grid(True)
-		return self.createImageURI()
+# class HistogramSeaborn(Plot):
+	# def plot(self, data, xLabel, yLabel, xScale, yScale):
+		# self.init(xScale, yScale)
+		# sns.distplot(data);
+		# plt.xlabel(xLabel)
+		# plt.ylabel(yLabel)
+		# plt.grid(True)
+		# return self.createImageURI()
 	
 # def asImage(plotFunction, plotArgs=[], plotKwargs={}, size=(8,6)):
 	# """
