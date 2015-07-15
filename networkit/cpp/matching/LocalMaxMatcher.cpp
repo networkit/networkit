228f11f647c6b6df1f1cdbe3cf56cb02ecf00c5b
Moritz von Looz <moritz.looz-corswarem@kit.edu>
2015-07-15 11:28:43 +0200

    bugfix in APSPGTest

---
+++ networkit/cpp/matching/LocalMaxMatcher.cpp
@@ -0,0 +1,86 @@
+/*
+ * ParallelMatcher.cpp
+ *
+ *  Created on: 05.12.2012
+ *      Author: Christian Staudt (christian.staudt@kit.edu)
+ */
+
+#include "LocalMaxMatcher.h"
+
+namespace NetworKit {
+
+LocalMaxMatcher::LocalMaxMatcher(Graph& graph): Matcher(graph)
+{
+
+}
+
+// TODO: update to new edge attribute system
+
+
+Matching LocalMaxMatcher::run() {
+	int64_t z = G.upperNodeIdBound();
+	count E = G.numberOfEdges();
+	Matching M(z);
+
+	// put edges into array of triples
+	struct MyEdge {
+		node s; // source
+		node t; // target
+		edgeweight w; // weight
+	};
+
+	std::vector<MyEdge> edges(E);
+	index e = 0;
+	G.forEdges([&](node u, node v, edgeweight w) {
+		edges[e].s = u;
+		edges[e].t = v;
+		edges[e].w = w + Aux::Random::real(1e-6);
+		++e;
+	});
+
+	// candidates records mating candidates
+	std::vector<MyEdge> candidates(z);
+	G.parallelForNodes([&](node u) {
+		candidates[u].w = (edgeweight) 0;
+		candidates[u].t = u; // itself as mating partner => unmatched
+	});
+
+	while (E > 0) {
+		// for each edge find out if it is locally maximum
+		for (auto edge: edges) {
+			if (edge.w > candidates[edge.s].w && edge.w > candidates[edge.t].w) {
+				candidates[edge.s].t = edge.t;
+				candidates[edge.s].w = edge.w;
+				candidates[edge.t].t = edge.s;
+				candidates[edge.t].w = edge.w;
+			}
+		}
+
+		// check if candidates agree to match; if so, then match them
+		for (auto edge: edges) {
+			node u = edge.s;
+			node v = edge.t;
+			if (candidates[u].t == v && candidates[v].t == u) {
+				// both nodes agree
+				M.match(u, v);
+			}
+		}
+
+		// create remaining "graph" by selecting remaining edges (as triples)
+		// adjust candidates
+		std::vector<MyEdge> newEdges;
+		for (auto edge: edges) {
+			if (! M.isMatched(edge.s) && ! M.isMatched(edge.t)) {
+				newEdges.push_back(edge);
+				candidates[edge.s].w = (edgeweight) 0;
+				candidates[edge.t].w = (edgeweight) 0;
+			}
+		}
+		edges = newEdges;
+		E = edges.size();
+	}
+
+	return M;
+}
+
+} /* namespace NetworKit */
---
+++ networkit/cpp/properties/ConnectedComponents.h
@@ -0,0 +1,85 @@
+/*
+ * ConnectedComponents.cpp
+ *
+ *  Created on: Dec 16, 2013
+ *      Author: cls
+ */
+
+#ifndef CONNECTEDCOMPONENTS_H_
+#define CONNECTEDCOMPONENTS_H_
+
+#include "../graph/Graph.h"
+#include "../graph/BFS.h"
+#include "../structures/Partition.h"
+#include "../base/Algorithm.h"
+
+namespace NetworKit {
+
+/**
+ * @ingroup properties
+ * Determines the connected components of an undirected graph.
+ */
+class ConnectedComponents : public Algorithm {
+public:
+	/**
+	 * Create ConnectedComponents class for Graph @a G.
+	 *
+	 * @param G The graph.
+	 */
+	ConnectedComponents(const Graph& G);
+
+	/**
+	 * This method determines the connected components for the graph given in the constructor.
+	 */
+	void run();
+
+	/**
+	 * Get the number of connected components.
+	 *
+	 * @return The number of connected components.
+	 */
+	count numberOfComponents();
+
+	/**
+	 * Get the the component in which node @a u is situated.
+	 *
+	 * @param[in]	u	The node whose component is asked for.
+	 */
+	count componentOfNode(node u);
+
+
+	/**
+	 * Get a Partition that represents the components.
+	 *
+	 * @return A partition representing the found components.
+	 */
+	Partition getPartition();
+
+    /**
+     *Return the map from component to size
+     */
+    std::map<index, count> getComponentSizes();
+
+
+private:
+	const Graph& G;
+	Partition component;
+	count numComponents;
+	bool hasRun;
+};
+
+inline count ConnectedComponents::componentOfNode(node u) {
+	assert (component[u] != none);
+	if (!hasRun) throw std::runtime_error("run method has not been called");
+	return component[u];
+}
+
+inline count ConnectedComponents::numberOfComponents() {
+	if (!hasRun) throw std::runtime_error("run method has not been called");
+	return this->numComponents;
+}
+
+}
+
+
+#endif /* CONNECTEDCOMPONENTS_H_ */
