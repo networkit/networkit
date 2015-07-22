40a435225df9633e27536b190a045a2d2c3aa939
Moritz von Looz <moritz.looz-corswarem@kit.edu>
2015-07-22 21:29:23 +0200

    10 test cases for the prizes of one!

--- networkit/cpp/io/test/IOBenchmark.cpp
+++ networkit/cpp/io/test/IOBenchmark.cpp
@@ -55,66 +55,70 @@ TEST_F(IOBenchmark, benchRasterReader) {
 		INFO("[DONE] reading raster data set " , runtime.elapsedTag());
 		EXPECT_EQ(xcoords.size(), ycoords.size());
 
-		//transform into polar coordinates
-		runtime.start();
-		vector<double> angles(xcoords.size());
-		vector<double> radii(xcoords.size());
-		vector<index> content(xcoords.size());
-		double maxR = 0;
-		for (index i = 0; i < xcoords.size(); i++) {
-			HyperbolicSpace::cartesianToPolar(Point2D<double>(xcoords[i], ycoords[i]), angles[i], radii[i]);
-			maxR = std::max(maxR, radii[i]);
-			content[i] = i;
-		}
-		runtime.stop();
+		count numRuns = 10;
 
-		INFO("Converted coordinates", runtime.elapsedTag());
-		//define query function
-		double T = 0.01;
-		double thresholdDistance = maxR/1000;
-		double beta = 1/T;
-		auto edgeProb = [](double distance) -> double {return exp(-distance*200);};
-		//auto edgeProb = [beta, thresholdDistance](double distance) -> double {return 1 / (exp(beta*(distance-thresholdDistance)/2)+1);};
-
-		// perform range queries
-		runtime.start();
-		QuadtreePolarEuclid<index> tree(angles, radii, content);
-		runtime.stop();
-		INFO("Filled quadtree", runtime.elapsedTag());
-
-		uint64_t numQueries = 1000;
-		long treeTotalNeighbours = 0;
-
-		runtime.start();
-		for (uint64_t q = 0; q < numQueries; ++q) {
-			vector<index> result;
-			index comparison = Aux::Random::integer(xcoords.size());
-			Point2D<double> query(xcoords[comparison], ycoords[comparison]);
-			tree.getElementsProbabilistically(query, edgeProb, result);
-			treeTotalNeighbours += result.size();
-		}
-		runtime.stop();
-		INFO("Completed ", numQueries, " quadtree queries with on average ", treeTotalNeighbours / numQueries, " neighbours", runtime.elapsedTag());
-
-		long naiveTotalNeighbours = 0;
-		runtime.start();
-		for (uint64_t q = 0; q < numQueries; ++q) {
-			vector<index> result;
-			index comparison = Aux::Random::integer(xcoords.size());
-			double x = xcoords[comparison];
-			double y = ycoords[comparison];
+		for (index run = 0; run < numRuns; run++) {
+			//transform into polar coordinates
+			runtime.start();
+			vector<double> angles(xcoords.size());
+			vector<double> radii(xcoords.size());
+			vector<index> content(xcoords.size());
+			double maxR = 0;
 			for (index i = 0; i < xcoords.size(); i++) {
-				double xdiff = xcoords[i] - x;
-				double ydiff = ycoords[i] - y;
-				double prob = edgeProb(pow(xdiff*xdiff+ydiff*ydiff , 0.5));
-				double random = Aux::Random::real();
-				if (random < prob) result.push_back(i);
+				HyperbolicSpace::cartesianToPolar(Point2D<double>(xcoords[i], ycoords[i]), angles[i], radii[i]);
+				maxR = std::max(maxR, radii[i]);
+				content[i] = i;
 			}
-			naiveTotalNeighbours += result.size();
+			runtime.stop();
+
+			INFO("Converted coordinates", runtime.elapsedTag());
+			//define query function
+			double T = 0.01;
+			double thresholdDistance = maxR/1000;
+			double beta = 1/T;
+			auto edgeProb = [](double distance) -> double {return exp(-(distance*200+5));};
+			//auto edgeProb = [beta, thresholdDistance](double distance) -> double {return 1 / (exp(beta*(distance-thresholdDistance)/2)+1);};
+
+			// perform range queries
+			runtime.start();
+			QuadtreePolarEuclid<index> tree(angles, radii, content);
+			runtime.stop();
+			INFO("Filled quadtree", runtime.elapsedTag());
+
+			uint64_t numQueries = 1000;
+			long treeTotalNeighbours = 0;
+
+			runtime.start();
+			for (uint64_t q = 0; q < numQueries; ++q) {
+				vector<index> result;
+				index comparison = Aux::Random::integer(xcoords.size());
+				Point2D<double> query(xcoords[comparison], ycoords[comparison]);
+				tree.getElementsProbabilistically(query, edgeProb, result);
+				treeTotalNeighbours += result.size();
+			}
+			runtime.stop();
+			INFO("Completed ", numQueries, " quadtree queries with on average ", treeTotalNeighbours / numQueries, " neighbours", runtime.elapsedTag());
+
+			long naiveTotalNeighbours = 0;
+			runtime.start();
+			for (uint64_t q = 0; q < numQueries; ++q) {
+				vector<index> result;
+				index comparison = Aux::Random::integer(xcoords.size());
+				double x = xcoords[comparison];
+				double y = ycoords[comparison];
+				for (index i = 0; i < xcoords.size(); i++) {
+					double xdiff = xcoords[i] - x;
+					double ydiff = ycoords[i] - y;
+					double prob = edgeProb(pow(xdiff*xdiff+ydiff*ydiff , 0.5));
+					double random = Aux::Random::real();
+					if (random < prob) result.push_back(i);
+				}
+				naiveTotalNeighbours += result.size();
+			}
+			runtime.stop();
+			INFO("Completed ", numQueries, " naive queries with on average ", naiveTotalNeighbours / numQueries, " neighbours", runtime.elapsedTag());
+			EXPECT_NEAR(treeTotalNeighbours, naiveTotalNeighbours, treeTotalNeighbours / 10);
 		}
-		runtime.stop();
-		INFO("Completed ", numQueries, " naive queries with on average ", naiveTotalNeighbours / numQueries, " neighbours", runtime.elapsedTag());
-		EXPECT_NEAR(treeTotalNeighbours, naiveTotalNeighbours, treeTotalNeighbours / 20);
 	}
 }
 
