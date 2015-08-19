440b6b37e341f124dc84cb607c2eff9cd946728e
Gerd Lindner <gerd.lindner@student.kit.edu>
2015-08-19 13:04:31 +0200

    TriangleCounter renamed

--- networkit/sparsification.py
+++ networkit/sparsification.py
@@ -3,7 +3,7 @@
 __author__ = "Gerd Lindner"
 
 from _NetworKit import ChibaNishizekiTriangleEdgeScore, GlobalThresholdFilter, LocalSimilarityScore, MultiscaleScore, SimmelianOverlapScore, RandomEdgeScore, LocalDegreeScore, ForestFireScore, \
-	EdgeAttributeAsWeight, EdgeAttributeLinearizer, LocalFilterScore, AdamicAdarDistance, ChanceCorrectedTriangleScore, NodeNormalizedTriangleScore, TriangleCounter, RandomNodeEdgeScore, ChungLuScore, ChibaNishizekiQuadrangleEdgeScore, GeometricMeanScore, \
+	EdgeAttributeAsWeight, EdgeAttributeLinearizer, LocalFilterScore, AdamicAdarDistance, ChanceCorrectedTriangleScore, NodeNormalizedTriangleScore, TriangleEdgeScore, RandomNodeEdgeScore, ChungLuScore, ChibaNishizekiQuadrangleEdgeScore, GeometricMeanScore, \
 	EdgeAttributeNormalizer, EdgeAttributeBlender, PrefixJaccardCoefficient, SCANStructuralSimilarityScore
 
 # local imports
@@ -528,9 +528,9 @@ class TriangleSparsifier(Sparsifier):
 
 	def scores(self, G):
 		""" Returns the triangle counts of the input graph. """
-		attributizer_t = TriangleCounter(G)
-		a_t = attributizer_t.scores()
-		return a_t
+		triangleScore = TriangleEdgeScore(G)
+		triangleScore.run()
+		return triangleScore.scores()
 
 	def _getSparsifiedGraph(self, G, parameter, attribute):
 		gf = GlobalThresholdFilter(G, attribute, parameter, True)
