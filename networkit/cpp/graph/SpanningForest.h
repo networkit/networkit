fcbe08aec32b2f6ac2c6064b58c074a5aca18c26
Henning Meyerhenke <meyerhenke@kit.edu>
2015-09-06 18:18:41 +0200

    random spanning forest

---
+++ networkit/cpp/graph/SpanningForest.h
@@ -0,0 +1,37 @@
+/*
+ * SpanningForest.h
+ *
+ *  Created on: 06.09.2015
+ *      Author: Henning
+ */
+
+#ifndef SPANNINGFOREST_H_
+#define SPANNINGFOREST_H_
+
+#include "Graph.h"
+
+namespace NetworKit {
+
+/**
+ * Base class for spanning forest/tree algorithms.
+ */
+class SpanningForest {
+protected:
+	const Graph& G;
+	Graph forest;
+
+public:
+	SpanningForest(const Graph& G);
+	virtual ~SpanningForest() = default;
+
+	virtual void run() = 0;
+
+	/**
+	 * @return Forest computed by run method.
+	 * Note: So far no explicit check if run method has been invoked before.
+	 */
+	Graph getForest();
+};
+
+} /* namespace NetworKit */
+#endif /* SPANNINGFOREST_H_ */
