82cbdb20c57b6421657966e5e5d6dca7c4612532
cls <christian.staudt@kit.edu>
2015-09-22 09:00:23 +0200

    reverted changes in graph.py

--- networkit/graph.py
+++ networkit/graph.py
@@ -2,19 +2,3 @@
 from _NetworKit import Graph, BFS, Dijkstra, Subgraph, DynBFS, DynDijkstra, SpanningForest, GraphTools
 
 from enum import Enum
-
-def findCycle(G):
-	Color = Enum("Color", "white gray black")
-	color = {}
-	for u in G.nodes():
-		color[u] = Color.white
-	S = list()
-	s = G.randomNode()
-	S.append(s)
-	while len(S) > 0:
-		u = S.pop()
-		print(u)
-		if color[u] is Color.white:
-			color[u] = Color.gray
-			for v in G.neighbors(u):
-				S.append(v)
