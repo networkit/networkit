673e6c3bfc512758b22666b62ecd7e545c9e460c
mwegner <mwegner@localhost>
2015-10-23 11:59:43 +0200

    fixed bug in SnapConverter

--- networkit/cpp/numerics/test/SnapConverter.cpp
+++ networkit/cpp/numerics/test/SnapConverter.cpp
@@ -6,7 +6,7 @@
  */
 
 #include "SnapConverter.h"
-#include "../../io/SNAPGraphReader.h"
+#include "../../io/EdgeListReader.h"
 #include "../../io/METISGraphWriter.h"
 
 #include <string>
@@ -20,12 +20,14 @@ TEST(SnapConverter, convert) {
 	vector<string> filePaths = {"Brightkite_edges.snap", "CA-AstroPh.snap", "CA-CondMat.snap", "CA-GrQc.snap", "CA-HepPh.snap", "CA-HepTh.snap",
 								"Email-Enron.snap", "Gowalla_edges.snap", "as-skitter.snap", "com-amazon.ungraph.snap", "com-orkut.ungraph.snap",
 								"com-youtube.ungraph.snap", "roadNet-CA.snap", "roadNet-PA.snap", "roadNet-TX.snap"};
-	SNAPGraphReader snapReader;
+
 	METISGraphWriter metisWriter;
 	for (string filePath : filePaths) {
+		EdgeListReader snapReader('\t',0,"#",false);
 		filePath = "/Users/Michael/Downloads/SNAP/" + filePath;
 		INFO("Reading ", filePath);
 		Graph G = snapReader.read(filePath);
+		INFO("n = ", G.numberOfNodes(), " m = ", G.numberOfEdges());
 		string newFilePath = filePath.substr(0, filePath.find_last_of("."));
 		metisWriter.write(G, newFilePath + ".graph");
 	}
