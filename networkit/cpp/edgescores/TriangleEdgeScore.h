acc8b63b65241ffaa2136b753cac1d3c7cc9b548
Michael Hamann <michael.hamann@kit.edu>
2015-11-02 11:30:41 +0100

    Improved version of TriangleEdgeScore, deprecate ChibaNishizekiTriangleEdgeScore
    The new version of TriangleEdgeScore does not contain that many
    optimizations that avoid work (in theory) but therefore also needs less
    checks which seems to be faster in practice. Furthermore no locks or
    atomics are needed anymore. The performance seems to be much better
    than before and in particular it is now similar to
    ChibaNishizekiTriangleEdgeScore even when parallelism is disabled.
    Therfore TriangleEdgeScore should be used instead of
    ChibaNishizekiTriangleEdgeScore.

--- networkit/cpp/edgescores/TriangleEdgeScore.h
+++ networkit/cpp/edgescores/TriangleEdgeScore.h
@@ -13,7 +13,17 @@
 namespace NetworKit {
 
 /**
- * An implementation of the triangle counting algorithm by Ortmann et al.
+ * A parallel triangle counting implementation based on ideas in [0].
+ *
+ * This implementation avoids less work but needs therefore also less checks and
+ * is (apart from a fast initialization) parallel without any locks. In experiments
+ * this implementation seems to be fast both in non-parallel as well as in parallel
+ * settings. With only one thread its performance is similar to the sequential
+ * ChibaNishizekiTriangleEdgeScore in NetworKit.
+ *
+ * [0] Triangle Listing Algorithms: Back from the Diversion
+ * Mark Ortmann and Ulrik Brandes                                                                          *
+ * 2014 Proceedings of the Sixteenth Workshop on Algorithm Engineering and Experiments (ALENEX). 2014, 1-8
  */
 class TriangleEdgeScore : public EdgeScore<count> {
 
