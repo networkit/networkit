72cbe24f7aa288120bdeca35074522e683f0baa2
cls <christian.staudt@kit.edu>
2015-11-02 11:41:50 -0500

    refactored unit tests

--- networkit/cpp/matching/test/MatcherGTest.cpp
+++ networkit/cpp/matching/test/MatcherGTest.cpp
@@ -37,16 +37,17 @@ TEST_F(MatcherGTest, testLocalMaxMatching) {
 	count numExpEdges = n / 2;
 	bool isProper = M.isProper(G);
 	EXPECT_TRUE(isProper);
-	EXPECT_EQ(M.size(), numExpEdges);
+	EXPECT_EQ(M.size(G), numExpEdges);
 
 #if !defined _WIN32 && !defined _WIN64 && !defined WIN32 && !defined WIN64
 	DibapGraphReader reader;
 	Graph airfoil1 = reader.read("input/airfoil1.gi");
 	LocalMaxMatcher lmm(airfoil1);
-	M = lmm.run();
+	lmm.run();
+	M = lmm.getMatching();
 	isProper = M.isProper(airfoil1);
 	EXPECT_TRUE(isProper);
-	DEBUG("LocalMax on airfoil1 produces matching of size: " , M.size());
+	DEBUG("LocalMax on airfoil1 produces matching of size: " , M.size(G));
 #endif
 }
 
@@ -73,7 +74,7 @@ TEST_F(MatcherGTest, testPgaMatching) {
 	count numExpEdges = n / 2;
 	bool isProper = M.isProper(G);
 	EXPECT_TRUE(isProper);
-	EXPECT_EQ(M.size(), numExpEdges);
+	EXPECT_EQ(M.size(G), numExpEdges);
 	DEBUG("Finished PGA matching on 50-clique");
 
 
@@ -81,10 +82,11 @@ TEST_F(MatcherGTest, testPgaMatching) {
 	DibapGraphReader reader;
 	Graph airfoil1 = reader.read("input/airfoil1.gi");
 	PathGrowingMatcher pga2(airfoil1);
-	M = pga2.run();
+	pga2.run();
+	M = pga2.getMatching();
 	isProper = M.isProper(airfoil1);
 	EXPECT_TRUE(isProper);
-	DEBUG("PGA on airfoil1 produces matching of size: " , M.size());
+	DEBUG("PGA on airfoil1 produces matching of size: " , M.size(G));
 #endif
 }
 
