d118e6bc3a32df01454f5a6f1a77accb0aacf10a
maxv <maximilian.vogel@student.kit.edu>
2016-02-26 08:55:54 +0100

    fixed n0 initialization for BarabasiAlbert generator; work in progress: implementation of batagelj's method

--- networkit/cpp/generators/BarabasiAlbertGenerator.h
+++ networkit/cpp/generators/BarabasiAlbertGenerator.h
@@ -23,13 +23,29 @@ private:
 	count k; //!< Attachments made per node
 	count nMax; //!< The maximal number of nodes attached
 	count n0; //!< The number of initial connected nodes
+	bool batagelj; //!< Specifies whether to use batagelj's method or the original one
 
 	Graph initializeGraph();
+
+	/**
+	 * Implementation of ALG 5 of https://kops.uni-konstanz.de/bitstream/handle/123456789/5799/random.pdf?sequence=1
+	 * Running time is O(n+m)
+	 * @return The generated graph
+	 */
+	Graph generateBatagelj();
 
 public:
 	BarabasiAlbertGenerator();
 
-	BarabasiAlbertGenerator(count k, count nMax, count n0 = 0);
+	/**
+	 *
+	 *
+	 * @param k Number of attachments per node
+	 * @param nMax Maximum number of nodes in the graph
+	 * @param n0 Number of connected nodes to begin with
+	 * @param batagelj Specifies whether to use batagelj's method or the original one; default: false
+	 */
+	BarabasiAlbertGenerator(count k, count nMax, count n0 = 0, bool batagelj=false);
 
 	Graph generate() override;
 };
