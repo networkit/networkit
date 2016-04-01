4a6035bc3945b876f24c77e7b28937812a8b9e7d
cls <christian.staudt@kit.edu>
2016-04-01 13:24:31 +0200

    fixed stats.gini

--- networkit/_NetworKit.pyx
+++ networkit/_NetworKit.pyx
@@ -8510,14 +8510,14 @@ def sort2(sample):
 
 def gini(values):
 	"""
-	Computes the Gini coefficient for the distribution given as a list of values. 
+	Computes the Gini coefficient for the distribution given as a list of values.
 	"""
-	sorted_list = sorted(list_of_values)
+	sorted_list = sorted(values)
 	height, area = 0, 0
 	for value in sorted_list:
 		height += value
 		area += height - value / 2.
-	fair_area = height * len(list_of_values) / 2
+	fair_area = height * len(values) / 2
 	return (fair_area - area) / fair_area
 
 
