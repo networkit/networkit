95a3ef87ec9efe1cacc136146b6bc0acfec1f502
mwegner <mwegner@localhost>
2016-04-12 15:53:15 +0200

    updated test, removed old dependencies

--- networkit/cpp/numerics/test/LAMGGTest.h
+++ networkit/cpp/numerics/test/LAMGGTest.h
@@ -23,20 +23,10 @@ namespace NetworKit {
 
 class LAMGGTest : public testing::Test {
 protected:
-	const vector<string> GRAPH_INSTANCES = {"walshaw/wing.graph"};
-
-	const std::vector<count> grid2DSizes = {4, 8, 16, 32, 64, 128, 256, 512, 1024};
-	const std::vector<count> grid3DSizes = {2, 4, 8, 16, 32, 64, 128};
-	const std::vector<count> barabasiSizes = {25, 100, 500, 1000, 5000, 10000, 50000};
+	const vector<string> GRAPH_INSTANCES = {"input/jazz.graph", "input/power.graph", "input/wing.graph"};
 
 	Vector randZeroSum(const Graph &graph, size_t seed) const;
 	Vector randVector(count dimension, double lower, double upper) const;
-	Vector capacitanceProblem(const Graph &graph) const;
-
-	void genGrid2D(count n) const;
-	void genGrid3D(count n) const;
-	void genBarabasi(count n, count attachment = 4) const;
-
 };
 
 } /* namespace NetworKit */
