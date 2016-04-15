43228bc021d2773c720c5b922ddfa9c6b51c2d99
cls <christian.staudt@kit.edu>
2016-04-15 13:16:51 +0200

    BTER scaling fixed

--- networkit/generators.py
+++ networkit/generators.py
@@ -195,8 +195,7 @@ class BTERReplicator:
 	addpath('{1}')
 	[ccd,gcc] = ccperdeg(G);
 	nd = accumarray(nonzeros(sum(G,2)),1);
-	nd = repmat(nd, {2}, 1);
-	ccd = repmat(ccd, {2}, 1);
+	nd = nd * {2};
 	[E1,E2] = bter(nd,ccd);
 	G_bter = bter_edges2graph(E1,E2);
 	save('bter_output', 'G_bter')
