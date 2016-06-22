777355db01c35f0f940d4f6940097afc64b263e3
Elisabetta Bergamini <elisabetta.bergamini@kit.edu>
2016-06-22 11:38:41 +0200

    added centrality measures complexities to documentation

--- networkit/cpp/centrality/DegreeCentrality.h
+++ networkit/cpp/centrality/DegreeCentrality.h
@@ -20,8 +20,9 @@ namespace NetworKit {
 class DegreeCentrality: public NetworKit::Centrality {
 public:
 	/**
-	 * Constructs the DegreeCentrality class for the given Graph @a G. If the betweenness scores should be normalized,
-	 * then set @a normalized to <code>true</code>.
+	 * Constructs the DegreeCentrality class for the given Graph @a G. If the centrality scores should be normalized,
+	 * then set @a normalized to <code>true</code>. The run() method runs in O(n) time, where n is the number of
+	 * nodes in the graph.
 	 *
 	 * @param G The graph.
 	 * @param normalized Set this parameter to <code>true</code> if scores should be normalized in the interval [0,1].
