cc4ac0e0eb52f14cbf4ecc07cd63671bfcae480f
Kolja Esders <kolja.esders@student.kit.edu>
2017-01-04 17:21:00 +0100

    Interface to determine which reader can be used

--- networkit/cpp/io/GraphReader.h
+++ networkit/cpp/io/GraphReader.h
@@ -28,6 +28,23 @@ public:
 	 * @param[in]	path	input file path
 	 */
 	virtual Graph read(const std::string& path) = 0;
+
+        /**
+         * Returns whether, based on the given header lines, this reader is able to read the graph file
+         * where the header lines where taken from.
+         *
+         * @param[in] headerLines First lines of a graph file that suffice to uniquely identify the
+         *                        graph format being used.
+         *                        Example: In the case of an edge list, this should encompass
+         *                        1) the line that indicates the number of nodes and edges
+         *                        --> Indicates an edge list format in the first place
+         *                        2) the first line with with an actual edge definition
+         *                        --> Indicates the seperator being used and the lowest node id (usually 0 or 1) 
+         *
+         * @return whether, based on the given header lines, this reader is able to read the graph file
+         */
+        virtual bool accepts(const std::vector<std::string&> headerLines) = 0;
+
 };
 
 } /* namespace NetworKit */
