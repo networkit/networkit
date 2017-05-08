bee828726f3805f9f5c3e1d023ac2d92e5647559
Michael Hamann <michael.hamann@kit.edu>
2017-05-08 11:28:34 +0200

    MaximalCliques: add interface documentation, nicer Python interface

--- networkit/_NetworKit.pyx
+++ networkit/_NetworKit.pyx
@@ -6782,17 +6782,47 @@ cdef extern from "cpp/clique/MaximalCliques.h":
 		vector[vector[node]] getCliques() except +
 
 cdef class MaximalCliques(Algorithm):
+	"""
+	Algorithm for listing all maximal cliques.
+
+	The implementation is based on the "hybrid" algorithm described in
+
+	Eppstein, D., & Strash, D. (2011).
+	Listing All Maximal Cliques in Large Sparse Real-World Graphs.
+	In P. M. Pardalos & S. Rebennack (Eds.),
+	Experimental Algorithms (pp. 364–375). Springer Berlin Heidelberg.
+	Retrieved from http://link.springer.com/chapter/10.1007/978-3-642-20662-7_31
+
+	The running time of this algorithm should be in O(d^2 * n * 3^{d/3})
+	where f is the degeneracy of the graph, i.e., the maximum core number.
+	The running time in practive depends on the structure of the graph. In
+	particular for complex networks it is usually quite fast, even graphs with
+	millions of edges can usually be processed in less than a minute.
+
+	Parameters
+	----------
+	G : Graph
+		The graph to list the cliques for
+	maximumOnly : bool
+		A value of True denotes that only one maximum clique is desired. This enables
+		further optimizations of the algorithm to skip smaller cliques more
+		efficiently. This parameter is only considered when no callback is given.
+	callback : callable
+		If a callable Python object is given, it will be called once for each
+		maximal clique. Then no cliques will be stored. The callback must accept
+		one parameter which is a list of nodes.
+	"""
 	cdef NodeVectorCallbackWrapper* _callback;
 	cdef Graph _G
 	cdef object _py_callback
 
-	def __cinit__(self, Graph G not None, maxOnlyOrCallback):
+	def __cinit__(self, Graph G not None, bool maximumOnly = False, object callback = None):
 		self._G = G
 
-		if callable(maxOnlyOrCallback):
+		if callable(callback):
 			# Make sure the callback is not de-allocated!
-			self._py_callback = maxOnlyOrCallback
-			self._callback = new NodeVectorCallbackWrapper(maxOnlyOrCallback)
+			self._py_callback = callback
+			self._callback = new NodeVectorCallbackWrapper(callback)
 			try:
 				self._this = new _MaximalCliques(self._G._this, dereference(self._callback))
 			finally:
@@ -6800,7 +6830,7 @@ cdef class MaximalCliques(Algorithm):
 				self._callback = NULL
 		else:
 			self._callback = NULL
-			self._this = new _MaximalCliques(self._G._this, <bool?>(maxOnlyOrCallback));
+			self._this = new _MaximalCliques(self._G._this, maximumOnly);
 
 	def __dealloc__(self):
 		if not self._callback == NULL:
@@ -6808,6 +6838,17 @@ cdef class MaximalCliques(Algorithm):
 			self._callback = NULL
 
 	def getCliques(self):
+		"""
+		Return all found cliques unless a callback was given.
+
+		This method will throw if a callback was given and thus the cliques were not stored.
+		If only the maximum clique was stored, it will return exactly one clique unless the graph
+		is empty.
+
+		Returns
+		-------
+		A list of cliques, each being represented as a list of nodes.
+		"""
 		return (<_MaximalCliques*>(self._this)).getCliques()
 
 # Module: linkprediction
