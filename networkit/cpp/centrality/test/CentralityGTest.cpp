c5a4285139c02bc5fde40df6926bb3bd000300c7
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2018-03-19 11:21:42 +0100

    Centrality test: Test Katz top-k vs. exact algo

--- networkit/cpp/centrality/test/CentralityGTest.cpp
+++ networkit/cpp/centrality/test/CentralityGTest.cpp
@@ -186,6 +186,101 @@ TEST_F(CentralityGTest, testKatzCentralityDirected) {
 	EXPECT_EQ(kc_ranking[0].first, 699);
 }
 
+TEST_F(CentralityGTest, testKatzTopk) {
+	METISGraphReader reader;
+	Graph G = reader.read("input/caidaRouterLevel.graph");
+
+	// Compute max. degree to ensure that we use the same alpha for both algos.
+	count maxDegree = 0;
+	G.forNodes([&](node u) {
+	if (G.degree(u) > maxDegree)
+		maxDegree = G.degree(u);
+	});
+
+	KatzCentrality exactAlgo(G, 1.0 / (maxDegree + 1), 1.0);
+	DynamicKatz topAlgo(G, 100);
+	exactAlgo.run();
+	topAlgo.run();
+
+	// We cannot compare the ranking as the algorithms might return different rankings
+	// for nodes that have equal/nearly equal scores.
+	// Instead, epsilon-compare the exact scores of the i-th node and the expected i-th node.
+	auto exactRanking = exactAlgo.ranking();
+	auto topRanking = topAlgo.ranking();
+	for(count i = 0; i < std::min(G.numberOfNodes(), count{100}); i++)
+		EXPECT_LT(fabs(exactAlgo.score(topRanking[i].first) - exactRanking[i].second), 0.000001);
+}
+
+TEST_F(CentralityGTest, testKatzDynamicAddition) {
+	METISGraphReader reader;
+	Graph G = reader.read("input/caidaRouterLevel.graph");
+	DynamicKatz kc(G, 100);
+	DEBUG("start kc run");
+	kc.run();
+	DEBUG("finish kc");
+	node u, v;
+	do {
+		u = G.randomNode();
+		v = G.randomNode();
+	} while (G.hasEdge(u, v));
+	GraphEvent e(GraphEvent::EDGE_ADDITION, u, v, 1.0);
+	G.addEdge(u, v);
+	kc.update(e);
+	DynamicKatz kc2(G, 100);
+	kc2.run();
+	const edgeweight tol = 1e-9;
+	for (count i = 0; i <= kc.levelReached; i ++) {
+		INFO("i = ", i);
+		G.forNodes([&](node u){
+		//	if (kc.nPaths[i][u] != kc2.nPaths[i][u]) {
+		//		INFO("i = ", i, ", node ", u, ", dyn kc paths: ", kc.nPaths[i][u], ", stat paths: ", kc2.nPaths[i][u]);
+		//	}
+			EXPECT_EQ(kc.nPaths[i][u], kc2.nPaths[i][u]);
+		});
+	}
+	G.forNodes([&](node u){
+		EXPECT_NEAR(kc.score(u), kc2.score(u), tol);
+	});
+
+	INFO("Level reached: ", kc.levelReached, ", ", kc2.levelReached);
+}
+
+TEST_F(CentralityGTest, testKatzDynamicDeletion) {
+	// SNAPGraphReader reader;
+	// Graph G = reader.read("input/wiki-Vote.txt"); // TODO: replace by smaller graph
+
+	METISGraphReader reader;
+	Graph G = reader.read("input/caidaRouterLevel.graph");
+	DynamicKatz kc(G, 100);
+	DEBUG("start kc run");
+	kc.run();
+	DEBUG("finish kc");
+	std::pair<node, node> p = G.randomEdge();
+	node u = p.first;
+	node v = p.second;
+	INFO("Deleting edge ", u, ", ", v);
+	GraphEvent e(GraphEvent::EDGE_REMOVAL, u, v, 1.0);
+	G.removeEdge(u, v);
+	kc.update(e);
+	DynamicKatz kc2(G, 100);
+	kc2.run();
+	const edgeweight tol = 1e-9;
+	for (count i = 0; i <= kc.levelReached; i ++) {
+		INFO("i = ", i);
+		G.forNodes([&](node u){
+			if (kc.nPaths[i][u] != kc2.nPaths[i][u]) {
+				INFO("i = ", i, ", node ", u, ", dyn kc paths: ", kc.nPaths[i][u], ", stat paths: ", kc2.nPaths[i][u]);
+			}
+			EXPECT_EQ(kc.nPaths[i][u], kc2.nPaths[i][u]);
+		});
+	}
+	G.forNodes([&](node u){
+		EXPECT_NEAR(kc.score(u), kc2.score(u), tol);
+	});
+
+	INFO("Level reached: ", kc.levelReached, ", ", kc2.levelReached);
+}
+
 // TODO: replace by smaller graph
 TEST_F(CentralityGTest, testPageRankDirected) {
 	SNAPGraphReader reader;
@@ -200,79 +295,6 @@ TEST_F(CentralityGTest, testPageRankDirected) {
 	const double tol = 1e-3;
 	EXPECT_EQ(pr_ranking[0].first, 699);
 	EXPECT_NEAR(pr_ranking[0].second, 0.00432, tol);
-}
-
-TEST_F(CentralityGTest, testKatzTopk) {
-  // SNAPGraphReader reader;
-  // Graph G = reader.read("input/wiki-Vote.txt"); // TODO: replace by smaller graph
-
-  METISGraphReader reader;
-  Graph G = reader.read("input/caidaRouterLevel.graph");
-  DynamicKatz kc(G, 100);
-  DEBUG("start kc run");
-  kc.run();
-  DEBUG("finish kc");
-  node u, v;
-  do {
-    u = G.randomNode();
-    v = G.randomNode();
-  } while (G.hasEdge(u, v));
-  GraphEvent e(GraphEvent::EDGE_ADDITION, u, v, 1.0);
-  G.addEdge(u, v);
-  kc.update(e);
-  DynamicKatz kc2(G, 100);
-  kc2.run();
-  const edgeweight tol = 1e-9;
-  for (count i = 0; i <= kc.levelReached; i ++) {
-    INFO("i = ", i);
-    G.forNodes([&](node u){
-      // if (kc.nPaths[i][u] != kc2.nPaths[i][u]) {
-      //   INFO("i = ", i, ", node ", u, ", dyn kc paths: ", kc.nPaths[i][u], ", stat paths: ", kc2.nPaths[i][u]);
-      // }
-      EXPECT_EQ(kc.nPaths[i][u], kc2.nPaths[i][u]);
-    });
-  }
-  G.forNodes([&](node u){
-    EXPECT_NEAR(kc.score(u), kc2.score(u), tol);
-  });
-
-  INFO("Level reached: ", kc.levelReached, ", ", kc2.levelReached);
-}
-
-TEST_F(CentralityGTest, testKatzTopkDeletion) {
-  // SNAPGraphReader reader;
-  // Graph G = reader.read("input/wiki-Vote.txt"); // TODO: replace by smaller graph
-
-  METISGraphReader reader;
-  Graph G = reader.read("input/caidaRouterLevel.graph");
-  DynamicKatz kc(G, 100);
-  DEBUG("start kc run");
-  kc.run();
-  DEBUG("finish kc");
-  std::pair<node, node> p = G.randomEdge();
-  node u = p.first;
-  node v = p.second;
-  INFO("Deleting edge ", u, ", ", v);
-  GraphEvent e(GraphEvent::EDGE_REMOVAL, u, v, 1.0);
-  G.removeEdge(u, v);
-  kc.update(e);
-  DynamicKatz kc2(G, 100);
-  kc2.run();
-  const edgeweight tol = 1e-9;
-  for (count i = 0; i <= kc.levelReached; i ++) {
-    INFO("i = ", i);
-    G.forNodes([&](node u){
-      if (kc.nPaths[i][u] != kc2.nPaths[i][u]) {
-        INFO("i = ", i, ", node ", u, ", dyn kc paths: ", kc.nPaths[i][u], ", stat paths: ", kc2.nPaths[i][u]);
-      }
-      EXPECT_EQ(kc.nPaths[i][u], kc2.nPaths[i][u]);
-    });
-  }
-  G.forNodes([&](node u){
-    EXPECT_NEAR(kc.score(u), kc2.score(u), tol);
-  });
-
-  INFO("Level reached: ", kc.levelReached, ", ", kc2.levelReached);
 }
 
 TEST_F(CentralityGTest, testEigenvectorCentrality) {
