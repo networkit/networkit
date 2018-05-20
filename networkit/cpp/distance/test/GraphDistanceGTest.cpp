0e189aa57a8173559fc0addb59bf9d9de0402f5e
Roman Bange <roman.bange@posteo.de>
2018-05-20 21:07:55 +0200

    removed unused graph files move grid-5x5-dist-arch.graph file because mapping folder is deleted refactor test class where grid-5x5-dist-arch.graph is used

--- networkit/cpp/distance/test/GraphDistanceGTest.cpp
+++ networkit/cpp/distance/test/GraphDistanceGTest.cpp
@@ -23,7 +23,7 @@ GraphDistanceGTest::~GraphDistanceGTest() {
 // TODO: fix graph
 TEST_F(GraphDistanceGTest, testGraphWeightedDistance) {
 	METISGraphReader reader;
-	Graph g = reader.read("input/mapping/grid-5x5-dist-arch.graph");
+	Graph g = reader.read("input/grid-5x5-dist-arch.graph");
 
 	GraphDistance gd;
 
@@ -44,7 +44,7 @@ TEST_F(GraphDistanceGTest, testGraphWeightedDistance) {
 
 TEST_F(GraphDistanceGTest, testGraphUnweightedDistance) {
 	METISGraphReader reader;
-	Graph g = reader.read("input/mapping/grid-5x5-dist-arch.graph");
+	Graph g = reader.read("input/grid-5x5-dist-arch.graph");
 
 	GraphDistance gd;
 
