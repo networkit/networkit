0fd238888e571c844b6b9f32158941d64320f730
MarvinPogoda <30391525+MarvinPogoda@users.noreply.github.com>
2018-06-14 19:45:34 +0200

    Optimized MahmoodyGB-parallelization

--- networkit/cpp/centrality/MahmoodyGroupBetweenness.cpp
+++ networkit/cpp/centrality/MahmoodyGroupBetweenness.cpp
@@ -20,83 +20,104 @@ MahmoodyGroupBetweenness::MahmoodyGroupBetweenness(const Graph& G,count groupSiz
 {
 }
 
-
 void MahmoodyGroupBetweenness::run()
 {	
-	
-	
 	//Create data structures for the hypergraph.
-	std::vector<int64_t> v(G.numberOfNodes());
-	std::vector<std::vector<node>> adjacencyList(G.numberOfNodes());
-	std::vector<int> hyperEdges;	
+	std::vector<int64_t> bucketInitializer(G.numberOfNodes());
+	std::vector<std::vector<node>> incidencyList(G.numberOfNodes());
+	std::vector<int> hyperEdges;
+	int samples = groupSize * log(G.numberOfNodes()) / (epsilon*epsilon); 
+	std::vector<std::vector<int>> hyperEdgesPerSample(samples);	
+	Aux::BucketPQ nodeDegrees(bucketInitializer,-samples,1);
 
-	int samples = 2 * log(pow(G.numberOfNodes(),3)) / epsilon; 
-	Aux::BucketPQ nodeDegrees(v,-samples,1);
-	omp_lock_t lock;
-	omp_init_lock(&lock);
 	#pragma omp parallel for
 	for(int l = 0;l < samples;l++)   
 	{
 		node s= G.randomNode();
 		node t;
 		do{
-			t= G.randomNode();
-			
+			t= G.randomNode();	
 		}while(s == t);
 
 		BFS bfs(G,s,true,true,t);
 		bfs.run();
 		std::set<std::vector<node>> shortestPaths = bfs.getPaths(t);
 
-		if(shortestPaths.size() == 0){		//If the selected nodes are in different connected components, the hyperedge is an empty set.
-		continue;				//Chooseing nodes in different connected components wont affect the algorithm.
-		}					//(See Mahmoody "Scalable Betweenness Centrality Maximization via Sampling",page 4,Lemma 3,2016)
-			
+		//If the selected nodes are in different connected components, the hyperedge is an empty set.
+		//Chooseing nodes in different connected components wont affect the algorithm.
+		//(See Mahmoody "Scalable Betweenness Centrality Maximization via Sampling",page 4,Lemma 3,2016)	
+		if(shortestPaths.size() == 0){		
+		continue;				
+		}					
+
 		//Uniformly select a shortest path
 		std::set<std::vector<node>>::const_iterator iterator(shortestPaths.begin());
 		advance(iterator,Aux::Random::integer(shortestPaths.size() - 1));
 		std::vector<node> newHyperEdge = *iterator;
-		omp_set_lock(&lock);
-		
-		//Insert length of hyperedge and save the position for the ajdacency list.
-		hyperEdges.push_back(newHyperEdge.size());
-		node hyperEdgeStart= hyperEdges.size() - 1;
-		
-		for(node const& n:newHyperEdge){
-			//Updates the degree of nodes in the hypergraph
-			nodeDegrees.changeKey(nodeDegrees.getKey(n) - 1,n);
-			
-			//Insert new Hyperedge into the hyperedge-list
-			hyperEdges.push_back(n);
 
-			//Update AdjancecyList
-			adjacencyList[n].push_back(hyperEdgeStart);
+		//Insert the HyperEdge into hyperEdgePerSample
+		for(auto n:newHyperEdge){
+			hyperEdgesPerSample[l].push_back(n);				
 		}
-		omp_unset_lock(&lock);
 	}
-	
-    	//Extract nodes with highest degress.
-	for(count j = 0;j < groupSize;j++){
-	std::pair<int,node> elem = nodeDegrees.extractMin();
-	node n = elem.second;
-	
-	//Update degrees
-	for(auto hyperEdge:adjacencyList[n]){
+
+		//Transfer edges from hyperEdgesPerSample to hyperEdges and prepare building nodeDegrees.
+		std::vector<int> tempDegrees(G.numberOfNodes());
+		for(auto edge:hyperEdgesPerSample){
+		node hyperEdgeStart= hyperEdges.size() ;
+		hyperEdges.push_back(edge.size());
+			for(auto const& n:edge){
+				//Safe degrees temporary to minimize the use of the changeKey-option
+				tempDegrees[n]-=1;		
+				//Insert new Hyperedge into the hyperedge-list
+				hyperEdges.push_back(n);
+				//Update incidencecyList
+				incidencyList[n].push_back(hyperEdgeStart);
+			}
+		}
+		//Build nodeDegrees
+		for(node i = 0; i<G.numberOfNodes();i++)
+		nodeDegrees.changeKey(tempDegrees[i],i);
+			
+    	//Extract nodes with highest degrees.
+	std::vector<int> degreeDecrease(G.numberOfNodes());
+	node v;
+	int degree;
+	for(count j = 0;j < groupSize;j++){ 	
+		//Lazy-Queue-Update tries to reduce the number of updates,that need to be done to get the next maximum degree. 
+		//We only need to update the max-degree-node as best case. The worst case is updateing every node.
+		std::pair<int,node> elem= nodeDegrees.extractMin(); 		
+		v = elem.second;						
+		degree= elem.first;
+		//degreeDecreased[v]=0 -> v has max degree					
+		while(degreeDecrease[v]>0){
+			//Update.
+			degree += degreeDecrease[v];
+			degreeDecrease[v]=0;
+			//Check if v still has max degree						
+			nodeDegrees.changeKey(degree,v);
+			elem = nodeDegrees.extractMin();
+			v= elem.second;
+			degree= elem.first;
+		}
+
+	//Upgrade temporal degreeDecrease to minimize number of accesses of BucketPQ
+	for(auto hyperEdge:incidencyList[v]){
 		count start= hyperEdge+1;
 		count end = start+hyperEdges[hyperEdge];
 		for(count i=start;i < end;i++){
-			if(hyperEdges[i] != n){
-				node decreasedNode = hyperEdges[i];
-				nodeDegrees.changeKey(nodeDegrees.getKey(decreasedNode)+1,decreasedNode);
+			if(hyperEdges[i] != v){
+				degreeDecrease[hyperEdges[i]]+=1;
 			}
 		}
-		hyperEdges[start - 1] = 0;
+		//"Deleting" Hyperedge
+		hyperEdges[start - 1] = 0;		
 	}
-	maxGroup.push_back(n);
+	maxGroup.push_back(v);
 	}
-	
 	hasRun=true;
 }
 	
 }/* namespace NetworKit */
+
 
