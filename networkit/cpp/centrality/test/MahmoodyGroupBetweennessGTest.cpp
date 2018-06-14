ed969a85b8abab36f080483332003b930feb08df
MarvinPogoda <30391525+MarvinPogoda@users.noreply.github.com>
2018-06-14 22:48:45 +0200

    Gtest Changed2

--- networkit/cpp/centrality/test/MahmoodyGroupBetweennessGTest.cpp
+++ networkit/cpp/centrality/test/MahmoodyGroupBetweennessGTest.cpp
@@ -55,27 +55,36 @@ TEST_F(MahmoodyGroupBetweennessGTest, testMahmoodyGroupBetweennessSmallGraph1) {
 TEST_F(MahmoodyGroupBetweennessGTest, testMahmoodyGroupBetweennessSmallGraph2) {
 
 	Aux::Random::setSeed(2,false);
-	Graph g(8, false,false);
+	Graph g(9, false,false);
+	g.addEdge(0,2);
+	g.addEdge(0,3);
 	g.addEdge(0,1);
 	g.addEdge(1,2);
-	g.addEdge(0,3);
-	g.addEdge(0,4);
+	g.addEdge(1,3);
 	g.addEdge(1,5);
+	g.addEdge(1,4);
+	g.addEdge(2,3);
+	g.addEdge(2,6);
 	g.addEdge(2,5);
-	g.addEdge(5,7);
 	g.addEdge(3,4);
-	g.addEdge(5,6);
+	g.addEdge(3,6);
+	g.addEdge(4,8);
 	g.addEdge(4,5);
-	g.addEdge(4,6);
+	g.addEdge(5,7);
+	g.addEdge(5,6);
+	g.addEdge(6,7);
+	g.addEdge(7,8);
 
-	MahmoodyGroupBetweenness gb(g,5,0.1);
+
+
+
+	MahmoodyGroupBetweenness gb(g,3,0.1);
 	gb.run();
 
 	EXPECT_EQ(gb.groupMaxBetweenness()[0],5);
 	EXPECT_EQ(gb.groupMaxBetweenness()[1],0);
 	EXPECT_EQ(gb.groupMaxBetweenness()[2],4);
-	EXPECT_EQ(gb.groupMaxBetweenness()[3],6);
-	EXPECT_EQ(gb.groupMaxBetweenness()[4],3);
+
 	
 
 
@@ -91,17 +100,21 @@ TEST_F(MahmoodyGroupBetweennessGTest, testMahmoodyGroupBetweennessSmallGraph3) {
 
 	Aux::Random::setSeed(13,false);
 	Graph g(10, false,false);
-	g.addEdge(2,4);
+	g.addEdge(0,1);
+	g.addEdge(0,2);
 	g.addEdge(1,2);
 	g.addEdge(1,8);
-	g.addEdge(0,9);
-	g.addEdge(7,8);
-	g.addEdge(4,9);
-	g.addEdge(1,5);
+	g.addEdge(1,3);
+	g.addEdge(2,6);
+	g.addEdge(2,5);
 	g.addEdge(3,4);
-	g.addEdge(1,2);
+	g.addEdge(3,8);
 	g.addEdge(4,7);
-	g.addEdge(6,7);
+	g.addEdge(4,5);
+	g.addEdge(5,7);
+	g.addEdge(5,6);
+	g.addEdge(6,8);
+
 
 	MahmoodyGroupBetweenness gb(g,3,0.2);
 	gb.run();
@@ -118,37 +131,7 @@ TEST_F(MahmoodyGroupBetweennessGTest, testMahmoodyGroupBetweennessSmallGraph3) {
 
 }
 
-TEST_F(MahmoodyGroupBetweennessGTest, testMahmoodyGroupBetweennessSmallGraph4) {
 
-	Aux::Random::setSeed(2,false);
-	Graph g(8, false,false);
-	g.addEdge(0,1);
-	g.addEdge(1,2);
-	g.addEdge(0,3);
-	g.addEdge(2,4);
-	g.addEdge(1,5);
-	g.addEdge(4,3);
-	g.addEdge(5,7);
-	g.addEdge(3,4);
-	g.addEdge(1,6);
-	g.addEdge(4,5);
-	g.addEdge(4,6);
-
-	MahmoodyGroupBetweenness gb(g,5,0.1);
-	gb.run();
-
-	EXPECT_EQ(gb.groupMaxBetweenness()[0],5);
-	EXPECT_EQ(gb.groupMaxBetweenness()[1],0);
-	EXPECT_EQ(gb.groupMaxBetweenness()[2],4);
-	EXPECT_EQ(gb.groupMaxBetweenness()[3],6);
-	EXPECT_EQ(gb.groupMaxBetweenness()[4],3);
-	
-
-
-
-
-
-}
 
 
 
