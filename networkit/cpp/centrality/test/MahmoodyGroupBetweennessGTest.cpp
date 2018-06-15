ddd1275f48a930a7ec4133254a97c21f56b6236c
MarvinPogoda <30391525+MarvinPogoda@users.noreply.github.com>
2018-06-15 03:31:28 +0200

    GTEST5

--- networkit/cpp/centrality/test/MahmoodyGroupBetweennessGTest.cpp
+++ networkit/cpp/centrality/test/MahmoodyGroupBetweennessGTest.cpp
@@ -30,7 +30,7 @@ TEST_F(MahmoodyGroupBetweennessGTest, testMahmoodyGroupBetweennessSmallGraph1) {
 	g.addEdge(5, 7);
 	g.addEdge(0,5);
 
-	MahmoodyGroupBetweenness gb(g,2,0.1);
+	MahmoodyGroupBetweenness gb(g,2,0.01);
 	gb.run();
 	
 
@@ -47,7 +47,7 @@ TEST_F(MahmoodyGroupBetweennessGTest, testMahmoodyGroupBetweennessSmallGraph1) {
 
 TEST_F(MahmoodyGroupBetweennessGTest, testMahmoodyGroupBetweennessSmallGraph2) {
 
-	Aux::Random::setSeed(2,false);
+	Aux::Random::setSeed(3,false);
 	Graph g(9, false,false);
 	g.addEdge(0,2);
 	g.addEdge(0,3);
@@ -71,7 +71,7 @@ TEST_F(MahmoodyGroupBetweennessGTest, testMahmoodyGroupBetweennessSmallGraph2) {
 
 
 
-	MahmoodyGroupBetweenness gb(g,2,0.1);
+	MahmoodyGroupBetweenness gb(g,2,0.01);
 	gb.run();
 
 	EXPECT_EQ(gb.groupMaxBetweenness()[0],4);
@@ -93,27 +93,23 @@ TEST_F(MahmoodyGroupBetweennessGTest, testMahmoodyGroupBetweennessSmallGraph3) {
 
 	Aux::Random::setSeed(13,false);
 	Graph g(10, false,false);
-	g.addEdge(0,1);
-	g.addEdge(0,2);
 	g.addEdge(1,2);
-	g.addEdge(1,8);
 	g.addEdge(1,3);
-	g.addEdge(2,6);
-	g.addEdge(2,5);
-	g.addEdge(3,4);
-	g.addEdge(3,8);
-	g.addEdge(4,7);
-	g.addEdge(4,5);
-	g.addEdge(5,7);
+	g.addEdge(1,4);
+	g.addEdge(1,5);
 	g.addEdge(5,6);
-	g.addEdge(6,8);
+	g.addEdge(5,7);
+	g.addEdge(5,8);
+	g.addEdge(5,9);
+	g.addEdge(0,5);
 
 
-	MahmoodyGroupBetweenness gb(g,2,0.1);
+
+	MahmoodyGroupBetweenness gb(g,2,0.01);
 	gb.run();
 
 	EXPECT_EQ(gb.groupMaxBetweenness()[0],5);
-	EXPECT_EQ(gb.groupMaxBetweenness()[1],3);
+	EXPECT_EQ(gb.groupMaxBetweenness()[1],1);
 
 
 	
