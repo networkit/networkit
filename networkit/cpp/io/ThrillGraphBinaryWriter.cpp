a7ff6f55733400afb172d0a24669715cb7fea8d2
Michael Hamann <michael.hamann@kit.edu>
2018-06-19 11:20:46 +0200

    ThrillGraphBinaryWriter: remove expanded loop, write little endian

--- networkit/cpp/io/ThrillGraphBinaryWriter.cpp
+++ networkit/cpp/io/ThrillGraphBinaryWriter.cpp
@@ -1,94 +1,7 @@
 #include "ThrillGraphBinaryWriter.h"
 
-namespace {
-    //! Append a varint to the writer.
-template <typename stream>
-stream& PutVarint(stream& s, uint64_t v) {
-	if (v < 128) {
-		s << uint8_t(v);
-	}
-	else if (v < 128 * 128) {
-		s << uint8_t(((v >> 00) & 0x7F) | 0x80);
-		s << uint8_t((v >> 07) & 0x7F);
-	}
-	else if (v < 128 * 128 * 128) {
-		s << uint8_t(((v >> 00) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 07) & 0x7F) | 0x80);
-		s << uint8_t((v >> 14) & 0x7F);
-	}
-	else if (v < 128 * 128 * 128 * 128) {
-		s << uint8_t(((v >> 00) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 07) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 14) & 0x7F) | 0x80);
-		s << uint8_t((v >> 21) & 0x7F);
-	}
-	else if (v < 128llu * 128 * 128 * 128 * 128) {
-		s << uint8_t(((v >> 00) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 07) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 14) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 21) & 0x7F) | 0x80);
-		s << uint8_t((v >> 28) & 0x7F);
-	}
-	else if (v < 128llu * 128 * 128 * 128 * 128 * 128) {
-		s << uint8_t(((v >> 00) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 07) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 14) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 21) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 28) & 0x7F) | 0x80);
-		s << uint8_t((v >> 35) & 0x7F);
-	}
-	else if (v < 128llu * 128 * 128 * 128 * 128 * 128 * 128) {
-		s << uint8_t(((v >> 00) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 07) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 14) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 21) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 28) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 35) & 0x7F) | 0x80);
-		s << uint8_t((v >> 42) & 0x7F);
-	}
-	else if (v < 128llu * 128 * 128 * 128 * 128 * 128 * 128 * 128) {
-		s << uint8_t(((v >> 00) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 07) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 14) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 21) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 28) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 35) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 42) & 0x7F) | 0x80);
-		s << uint8_t((v >> 49) & 0x7F);
-	}
-	else if (v < 128llu * 128 * 128 * 128 * 128 * 128 * 128 * 128 * 128) {
-		s << uint8_t(((v >> 00) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 07) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 14) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 21) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 28) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 35) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 42) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 49) & 0x7F) | 0x80);
-		s << uint8_t((v >> 56) & 0x7F);
-	}
-	else {
-		s << uint8_t(((v >> 00) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 07) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 14) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 21) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 28) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 35) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 42) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 49) & 0x7F) | 0x80);
-		s << uint8_t(((v >> 56) & 0x7F) | 0x80);
-		s << uint8_t((v >> 63) & 0x7F);
-	}
-
-	return s;
-};
-
-
-}
-
-
 void NetworKit::ThrillGraphBinaryWriter::write( const NetworKit::Graph &G, const std::string &path ) {
-	if (G.numberOfNodes() > std::numeric_limits<uint32_t>::max()) {
+	if (G.upperNodeIdBound() > std::numeric_limits<uint32_t>::max()) {
 		throw std::runtime_error("Thrill binary graphs only support graphs with up to 2^32-1 nodes.");
 	}
 	
@@ -103,11 +16,25 @@ void NetworKit::ThrillGraphBinaryWriter::write( const NetworKit::Graph &G, const
 					}
 			});
 
-			PutVarint(out_stream, neighbors.size());
-			
+			size_t deg = neighbors.size();
+
+			// Write variable length int for the degree
+			if(!deg) {
+				out_stream << uint8_t(0);
+			}
+
+			while(deg) {
+				auto u = deg & 0x7F;
+				deg >>= 7;
+				out_stream << uint8_t(u | (deg ? 0x80 : 0));
+			}
+
 			for (uint32_t v : neighbors) {
-				static_assert(std::is_same<uint32_t, decltype(v)>::value, "Node type is not uint32 anymore, adjust code!");
-				out_stream.write(reinterpret_cast<const char*>(&v), 4);
+				// write neighbor as little endian
+				for (size_t i = 0; i < sizeof(uint32_t); ++i) {
+					out_stream << uint8_t(v);
+					v >>= 7;
+				}
 			}
 	});
 
