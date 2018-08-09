0144cbf2a78abb45152a2b9fff762c37c0ab10fa
Manuel Penschuck <networkit@manuel.jetzt>
2018-08-09 16:36:33 +0200

    Do not use pragma omp atomic

--- networkit/cpp/auxiliary/test/AuxRandomBenchmark.cpp
+++ networkit/cpp/auxiliary/test/AuxRandomBenchmark.cpp
@@ -22,6 +22,22 @@ static double measure(F f, const size_t iterations = 50000000) {
 	return (1.0e6 * ms / iterations);
 }
 
+template<typename F>
+static double measureParallel(F f) {
+	// TODO: replace with google benchmark infrastructure
+	std::atomic<uint64_t> atime{0}; // this is a very dirty hack, but atomic float-points are not fully support by standard
+	std::atomic<int> num_threads;
+
+	#pragma omp parallel
+	{
+		const double local_time = f();
+		num_threads.store(omp_get_num_threads());
+		atime.fetch_add(static_cast<uint64_t>(1e6 * local_time), std::memory_order_relaxed);
+	}
+
+	return 1e-6 * atime / num_threads;
+}
+
 TEST_F(AuxRandomBenchmark, benchmarkInteger) {
 	uint64_t tmp = 0;
 	auto atime = measure([&] {
@@ -46,41 +62,32 @@ TEST_F(AuxRandomBenchmark, benchmarkLocalDistrInteger) {
 }
 
 TEST_F(AuxRandomBenchmark, benchmarkIntegerParallel) {
-	double atime{0};
-
-	#pragma omp parallel
-	{
+	auto atime = measureParallel([] {
 		uint64_t tmp = 0;
 		auto local_time = measure([&] {
 			tmp += Aux::Random::integer();
-		}) / omp_get_num_threads();
+		});
 		volatile auto dummy = tmp;
 
-		#pragma omp atomic
-		atime += local_time;
-	}
+		return local_time;
+	});
 
 	std::cout << "Average time of operation: " << atime << "ns\n";
 }
 
 TEST_F(AuxRandomBenchmark, benchmarkLocalDistrIntegerParallel) {
-	double atime{0};
-
-	#pragma omp parallel
-	{
-		uint64_t tmp = 0;
+	auto atime = measureParallel([] {
 		auto &prng = Aux::Random::getURNG();
 		std::uniform_int_distribution<uint64_t> distr;
 
+		uint64_t tmp = 0;
 		auto local_time = measure([&] {
 			tmp += distr(prng);
-		}) / omp_get_num_threads();
-
+		});
 		volatile auto dummy = tmp;
 
-		#pragma omp atomic
-		atime += local_time;
-	}
+		return local_time;
+	});
 
 	std::cout << "Average time of operation: " << atime << "ns\n";
 }
@@ -110,41 +117,32 @@ TEST_F(AuxRandomBenchmark, benchmarkLocalDistrProb) {
 }
 
 TEST_F(AuxRandomBenchmark, benchmarkProbParallel) {
-	double atime{0};
-
-	#pragma omp parallel
-	{
+	auto atime = measureParallel([] {
 		double tmp = 0.0;
-		auto local_time = measure([&] {
+		auto local_time =  measure([&] {
 			tmp += Aux::Random::probability();
-		}) / omp_get_num_threads();
+		});
 		volatile auto dummy = tmp;
 
-		#pragma omp atomic
-		atime += local_time;
-	}
+		return local_time;
+	});
 
 	std::cout << "Average time of operation: " << atime << "ns\n";
 }
 
 TEST_F(AuxRandomBenchmark, benchmarkLocalDistrProbParallel) {
-	double atime{0};
-
-	#pragma omp parallel
-	{
-		double tmp = 0.0;
+	auto atime = measureParallel([] {
 		auto &prng = Aux::Random::getURNG();
 		std::uniform_real_distribution<double> distr{0.0, 1.0};
 
+		double tmp = 0.0;
 		auto local_time = measure([&] {
 			tmp += distr(prng);
-		}) / omp_get_num_threads();
-
+		});
 		volatile auto dummy = tmp;
 
-		#pragma omp atomic
-		atime += local_time;
-	}
+		return local_time;
+	});
 
 	std::cout << "Average time of operation: " << atime << "ns\n";
 }
