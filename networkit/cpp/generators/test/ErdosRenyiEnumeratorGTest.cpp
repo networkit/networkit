ea4e5a501fa13f8689db91551b73ffc1d519057d
Manuel Penschuck <networkit@manuel.jetzt>
2018-08-23 14:43:48 +0200

    Improve testing of ErdosRenyiEnumerator

--- networkit/cpp/generators/test/ErdosRenyiEnumeratorGTest.cpp
+++ networkit/cpp/generators/test/ErdosRenyiEnumeratorGTest.cpp
@@ -1,4 +1,5 @@
 #include "ErdosRenyiEnumeratorGTest.h"
+#include <numeric>
 #include "../ErdosRenyiEnumerator.h"
 
 #include "../../auxiliary/Random.h"
@@ -7,7 +8,7 @@ namespace NetworKit {
 
 template <bool Parallel, bool FixedPoint>
 static void testEre(const bool directed, const node n, const double prob) {
-	Aux::Random::setSeed(static_cast<size_t>((123456 + directed * 23425) * prob * n), false);
+	Aux::Random::setSeed(static_cast<size_t>((123456 + directed * 23425 + 234*Parallel + 34*FixedPoint) * prob * n), false);
 
 	std::vector<unsigned> edge_counts(n * n, 0);
 	std::vector<unsigned> edge_exists(n * n, 0); // std::vector<bool> is not thread safe
@@ -17,7 +18,7 @@ static void testEre(const bool directed, const node n, const double prob) {
 	};
 
 	// after this many rounds each edge should be hit at least once
-	const int rounds = 10 * (1/prob) * std::log(1/prob);
+	const auto rounds = (0.0 < prob && prob < 1.0) ? static_cast<int>(20.0 / prob) : 1;
 
 	for(int i=0; i<rounds; i++) {
 		ErdosRenyiEnumerator<FixedPoint> ere(n, prob, directed);
@@ -55,16 +56,22 @@ static void testEre(const bool directed, const node n, const double prob) {
 		ASSERT_EQ(num_edges, num_edges_gen);
 
 		// Check that result is somewhat balanced along threads
-		if (Parallel) {
-			EXPECT_EQ(omp_get_max_threads(), active_threads);
+		if (Parallel && prob > 0) {
+			EXPECT_EQ(omp_get_max_threads(), static_cast<int>(active_threads));
 			for (auto count : num_edges_thread)
 				ASSERT_LE(count, 2 * num_edges / active_threads);
 		}
 
-		// count that we produced a number of edges, near the expected number
-		// we could apply much sharper bounds here if we allow for rare errors
-		ASSERT_NEAR(num_edges, ere.expectedNumberOfEdges(), 0.6 * ere.expectedNumberOfEdges());
+		if (prob == 0.0 || prob == 1.0) {
+			ASSERT_EQ(num_edges, ere.expectedNumberOfEdges());
+		} else {
+			// count that we produced a number of edges, near the expected number
+			// we could apply much sharper bounds here if we allow for rare errors
+			ASSERT_NEAR(num_edges, ere.expectedNumberOfEdges(), 0.6 * ere.expectedNumberOfEdges());
+		}
 	}
+
+	if (prob == 0.0) return;
 
 	for(node u = 1; u != n; u++) {
 		const node upper = directed ? n : u;
@@ -72,8 +79,10 @@ static void testEre(const bool directed, const node n, const double prob) {
 			// Again, much sharper bounds are possible if we do not
 			// rely on the implicit union bound here.
 			auto count = edge_counts[edgeindex(u,v,n)];
-			EXPECT_GE(count, 1) << "edge(" << u << ", " << v << "), rounds" << rounds;
-			ASSERT_LE(count, log(rounds) * rounds * prob) << "edge(" << u << ", " << v << "), rounds" << rounds;
+			EXPECT_GE(count, 1u) << "edge(" << u << ", " << v << "), rounds=" << rounds;
+			if (prob < 1.0) {
+				ASSERT_LE(count, log(rounds) * rounds * prob) << "edge(" << u << ", " << v << "), rounds=" << rounds;
+			}
 		}
 	}
 }
@@ -97,8 +106,12 @@ TEST_P(ErdosRenyiEnumeratorGTest, TestFixedPointParallel) {
 
 INSTANTIATE_TEST_CASE_P(ErdosRenyiEnumeratorGTest, ErdosRenyiEnumeratorGTest,
 						::testing::Values(
+  						 std::make_tuple(false, 100, 0.0),  std::make_tuple(true, 100, 0.0),
 						 std::make_tuple(false, 100, 0.1),  std::make_tuple(true, 100, 0.1),
-						 std::make_tuple(false, 200, 0.01), std::make_tuple(true,  200, 0.01)
-						));
+						 std::make_tuple(false, 100, 0.5),  std::make_tuple(true, 100, 0.5),
+						 std::make_tuple(false, 100, 0.7),  std::make_tuple(true, 100, 0.7),
+						 std::make_tuple(false, 100, 1.0),  std::make_tuple(true, 100, 1.0),
+						 std::make_tuple(false, 200, 0.01), std::make_tuple(true, 200, 0.01)
+						),);
 
 } // ! namespace NetworKit
