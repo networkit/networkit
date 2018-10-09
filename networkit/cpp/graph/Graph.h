f096e901f296a2e293463c6e32f910172fcd3882
Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>
2018-10-09 14:33:56 +0200

    Documented removeEdgesFromIsolatedSet method in the Graph class
    Signed-off-by: Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>

--- networkit/cpp/graph/Graph.h
+++ networkit/cpp/graph/Graph.h
@@ -645,6 +645,13 @@ public:
 	 */
 	void removeEdge(node u, node v);
 
+	/**
+	 * Efficiently removes all the edges adjacent to a set of nodes that is not
+	 * connected to the rest of the graph. This is meant to optimize the Kadabra
+	 * algorithm.
+	 * @param nodesInSet vector of nodes that form a connected component that is
+	 * isolated from the rest of the graph.
+	 */
 	void removeEdgesFromIsolatedSet(const std::vector<node> &nodesInSet);
 
 	/**
