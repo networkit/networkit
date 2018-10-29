2094c3212da2690cac86a9db0246113bbd8d1e33
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2018-10-29 16:56:49 +0100

    sphinx: Enable Napoleon extension
    This extension makes Sphinx actually parse our Docstrings
    correctly. Arguments and return values are not rendered properly.

--- Doc/doc/conf.py
+++ Doc/doc/conf.py
@@ -33,6 +33,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.ifconfig',
               'sphinx.ext.mathjax',
               'sphinx.ext.autosummary',
+              'sphinx.ext.napoleon',
               ]
 
 # Generate the API documentation when building
