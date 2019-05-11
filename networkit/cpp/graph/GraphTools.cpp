1a787addcbe042fd6e2ec1faf3e0ff4db321bb58
Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>
2019-05-12 01:38:19 +0200

    New method: extractLargestConnectedComponent
    Signed-off-by: Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>

--- networkit/cpp/graph/GraphTools.cpp
+++ networkit/cpp/graph/GraphTools.cpp
@@ -1,7 +1,10 @@
-#include <networkit/graph/GraphTools.hpp>
-#include <unordered_map>
-#include <networkit/graph/Graph.hpp>
+#include <algorithm>
 #include <random>
+#include <unordered_map>
+
+#include <networkit/components/ConnectedComponents.hpp>
+#include <networkit/graph/GraphTools.hpp>
+#include <networkit/graph/Graph.hpp>
 
 namespace NetworKit {
 
@@ -24,7 +27,6 @@ std::unordered_map<node,node> getContinuousNodeIds(const Graph& graph) {
 	graph.forNodes(addToMap);
 	return nodeIdMap;
 }
-
 std::unordered_map<node,node> getRandomContinuousNodeIds(const Graph& graph) {
 	std::unordered_map<node,node> nodeIdMap;
 	std::vector<node> nodes;
@@ -73,6 +75,47 @@ Graph restoreGraph(const std::vector<node>& invertedIdMap, const Graph& G) {
 	return Goriginal;
 }
 
+Graph extractLargestConnectedComponent(const Graph &G, bool compactGraph) {
+	if (!G.numberOfNodes())
+		return G;
+
+	ConnectedComponents cc(G);
+	cc.run();
+
+	auto compSizes = cc.getComponentSizes();
+	if (compSizes.size() == 1)
+			return G;
+
+	auto largestCC = std::max_element(
+		compSizes.begin(), compSizes.end(),
+		[](const std::pair<index, count> &x, const std::pair<index, count> &y) {
+			return x.second < y.second;
+		});
+
+	std::unordered_map<node, node> continuousNodeIds;
+	index nextId = 0;
+	G.forNodes([&](node u) {
+		if (cc.componentOfNode(u) == largestCC->first)
+			continuousNodeIds[u] = nextId++;
+	});
+
+	if (compactGraph) {
+		Graph S = getRemappedGraph(G, largestCC->second,
+			[&](node u) { return continuousNodeIds[u]; },
+			[&](node u) { return cc.componentOfNode(u) != largestCC->first; });
+		return S;
+	} else {
+		Graph S(G);
+		auto components = cc.getComponents();
+		for (count i = 0; i < components.size(); ++i) {
+			if (i != largestCC->first) {
+				for (node u : components[i])
+					S.removeNode(u);
+			}
+		}
+		return S;
+	}
+}
+
 } // namespace GraphTools
 } // namespace NetworKit
-
