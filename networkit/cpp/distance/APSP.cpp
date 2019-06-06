1c2395867e4d616d0bf112e4d50f1d3800d63747
Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>
2019-06-06 10:08:33 +0200

    Clang format
    Signed-off-by: Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>

--- networkit/cpp/distance/APSP.cpp
+++ networkit/cpp/distance/APSP.cpp
@@ -5,36 +5,36 @@
  *      Author: Arie Slobbe
  */
 
-#include <networkit/distance/APSP.hpp>
 #include <networkit/auxiliary/Log.hpp>
-#include <networkit/distance/Dijkstra.hpp>
+#include <networkit/distance/APSP.hpp>
 #include <networkit/distance/BFS.hpp>
+#include <networkit/distance/Dijkstra.hpp>
 
 namespace NetworKit {
 
-APSP::APSP(const Graph& G) : Algorithm(), G(G) {}
+APSP::APSP(const Graph &G) : Algorithm(), G(G) {}
 
 void APSP::run() {
-	std::vector<edgeweight> distanceVector(G.upperNodeIdBound(), 0.0);
-	distances.resize(G.upperNodeIdBound(), distanceVector);
-	if (G.isWeighted()) {
-		G.parallelForNodes([&](node u){
-			Dijkstra dijk(G, u);
-			dijk.run();
-			distances[u] = dijk.getDistances();
-		});
-	} else {
-		G.parallelForNodes([&](node u){
-			BFS bfs(G, u);
-			bfs.run();
-			distances[u] = bfs.getDistances();
-		});
-	}
-	hasRun = true;
+    std::vector<edgeweight> distanceVector(G.upperNodeIdBound(), 0.0);
+    distances.resize(G.upperNodeIdBound(), distanceVector);
+    if (G.isWeighted()) {
+        G.parallelForNodes([&](node u) {
+            Dijkstra dijk(G, u);
+            dijk.run();
+            distances[u] = dijk.getDistances();
+        });
+    } else {
+        G.parallelForNodes([&](node u) {
+            BFS bfs(G, u);
+            bfs.run();
+            distances[u] = bfs.getDistances();
+        });
+    }
+    hasRun = true;
 }
 
 std::string APSP::toString() const {
-	return "All-Pairs Shortest Path Algorithm";
+    return "All-Pairs Shortest Path Algorithm";
 }
 
 } /* namespace NetworKit */
