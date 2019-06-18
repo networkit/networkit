10a75e2fb4c0860fafd71f5ba033f7aa24079a9c
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2019-06-18 18:58:19 +0200

    Modernize UnitTest-X
    - Use only necessary include
    - Remove obsolete Preprocessor Flags
    - Remove obsolete Parameters
    - Be more verbose

--- networkit/cpp/Unittests-X.cpp
+++ networkit/cpp/Unittests-X.cpp
@@ -1,81 +1,58 @@
-//============================================================================
-// Name        : Unittests-X.cpp
-// Author      : Christian Staudt (christian.staudt@kit.edu),
-//		 Henning Meyerhenke (henning.meyerhenke@kit.edu)
-// Version     :
-// Copyright   : 2012, Christian Staudt, Henning Meyerhenke
-// Description : Calling unit tests and benchmarks
-//============================================================================
+/*
+ * Unittests-X.cpp
+ *
+ *  Created on: 2012
+ *      Author: Christian Staudt <christian.staudt@kit.edu>,
+ *              Henning Meyerhenke <henning.meyerhenke@kit.edu>,
+ *              Manuel Penschuck <networkit@manuel.jetzt>
+ */
+
 
 // includes
+#include <algorithm>
 #include <iostream>
 #include <utility>
-//#include <cfenv>	// floating point exceptions
-#include <stdexcept>
 
 // GoogleTest
-#ifndef NOGTEST
 #include <gtest/gtest.h>
-#endif
 
 // OpenMP
 #include <omp.h>
 
-// necessary for some reasons?
-#include <networkit/Globals.hpp>
-#include <networkit/ext/optionparser.hpp>
 #include <networkit/auxiliary/Log.hpp>
-#include <networkit/graph/Graph.hpp>
 #include <networkit/auxiliary/Parallelism.hpp>
-
-
-using namespace NetworKit;
-
-
-// *** Option Parser Configuration ***//
+#include <networkit/ext/optionparser.hpp>
 
 class Arg: public OptionParser::Arg {
+    static OptionParser::ArgStatus Required(const OptionParser::Option& option, bool msg)
+    {
+      if (option.arg != 0)
+        return OptionParser::ARG_OK;
 
-static OptionParser::ArgStatus Required(const OptionParser::Option& option, bool msg)
-{
-  if (option.arg != 0)
-    return OptionParser::ARG_OK;
-
-  if (msg) {
-	  std::cout << "Option '" << option << "' requires an argument" << std::endl;
-  }
-  return OptionParser::ARG_ILLEGAL;
-}
-
+      if (msg) {
+          std::cout << "Option '" << option << "' requires an argument" << std::endl;
+      }
+      return OptionParser::ARG_ILLEGAL;
+    }
 };
 
-// TODO: clean up obsolete parameters
-enum  optionIndex { UNKNOWN, HELP, LOGLEVEL, THREADS, TESTS, RUNNABLE, DEBUG, BENCHMARKS, FILTER };
-const OptionParser::Descriptor usage[] =
-{
-{UNKNOWN,	0,	"" ,	"",				OptionParser::Arg::None,	"Options:" },
-{HELP,		0,	"h",	"help",			OptionParser::Arg::None,	"  --help  \t Print usage and exit." },
-{LOGLEVEL,	0,	"",		"loglevel",		OptionParser::Arg::Required,"  --loglevel=<LEVEL>  \t set the log level" },
-{THREADS,	0,	"",		"threads",		OptionParser::Arg::Required,"  --threads=<NUM>  \t set the maximum number of threads" },
-{TESTS,		0,	"t",	"tests",		OptionParser::Arg::None,	"  --tests \t Run unit tests"},
-{RUNNABLE,	0,	"r",	"run",			OptionParser::Arg::None,	"  --run \t Run unit tests which don't use assertions"},
-{DEBUG,		0,	"d",	"debug",		OptionParser::Arg::None,	"  --debug \t Run tests to debug some algorithms"},
-{BENCHMARKS,0,	"b",	"benchmarks",	OptionParser::Arg::None,	"  --benchmarks \t Run benchmarks"},
-{FILTER,	0,	"f",	"gtest_filter",	OptionParser::Arg::Required,"  --gtest_filter=<FILTER_PATTERN> \t Run tests that match the filter pattern" },
-{UNKNOWN,	0,	"",		"",				OptionParser::Arg::None,	"\nExamples:\n TODO" },
-{0,			0,	0,		0,				0,							0}
+enum  optionIndex { UNKNOWN, HELP, LOGLEVEL, THREADS, TESTS, RUNNABLE, DEBUG, BENCHMARKS };
+const OptionParser::Descriptor usage[] = {
+    {UNKNOWN,	0,	"" ,	"",				OptionParser::Arg::None,	"Options:" },
+    {HELP,		0,	"h",	"help",			OptionParser::Arg::None,	"  --help  \t Print usage and exit." },
+    {LOGLEVEL,	0,	"",		"loglevel",		OptionParser::Arg::Required,"  --loglevel=<LEVEL>  \t set the log level" },
+    {THREADS,	0,	"",		"threads",		OptionParser::Arg::Required,"  --threads=<NUM>  \t set the maximum number of threads" },
+    {TESTS,		0,	"t",	"tests",		OptionParser::Arg::None,	"  --tests \t Run unit tests"},
+    {RUNNABLE,	0,	"r",	"run",			OptionParser::Arg::None,	"  --run \t Run unit tests which don't use assertions"},
+    {DEBUG,		0,	"d",	"debug",		OptionParser::Arg::None,	"  --debug \t Run tests to debug some algorithms"},
+    {BENCHMARKS,0,	"b",	"benchmarks",	OptionParser::Arg::None,	"  --benchmarks \t Run benchmarks"},
+    {UNKNOWN,	0,	"",		"",				OptionParser::Arg::None,	"" },
+    {0,			0,	0,		0,				0,							0}
 };
 
 
 int main(int argc, char **argv) {
-	std::cout << "*** NetworKit Unit Tests *** " << std::endl;
-
-	// ENABLE FLOATING POINT EXCEPTIONS (needs GNU extension, apparently only available on Linux)
-#ifdef _GNU_SOURCE
-	// feenableexcept(FE_ALL_EXCEPT);
-#endif
-
-	std::string program_name(argc ? argv[0] : "");
+	std::cout << "*** NetworKit Unit Tests ***\n";
 
 	// PARSE OPTIONS
 	const auto parse_argc = std::max(0, argc - 1);
@@ -85,18 +62,18 @@ int main(int argc, char **argv) {
 	OptionParser::Parser parse(usage, parse_argc, parse_argv, options.data(), buffer.data());
 
 	if (parse.error())
-	 return 1;
+		return 1;
 
 	if (options[HELP]) {
-	 OptionParser::printUsage(std::cout, usage);
-	 return 0;
+		OptionParser::printUsage(std::cout, usage);
+		return 0;
 	}
 
 	for (OptionParser::Option* opt = options[UNKNOWN]; opt; opt = opt->next())
-	 std::cout << "Unknown option: " << opt->name << "\n";
+	    std::cout << "Unknown option: " << opt->name << "\n";
 
 	for (int i = 0; i < parse.nonOptionsCount(); ++i)
-	 std::cout << "Non-option #" << i << ": " << parse.nonOption(i) << "\n";
+	    std::cout << "Non-option #" << i << ": " << parse.nonOption(i) << "\n";
 
 	// CONFIGURE LOGGING
 #ifndef NOLOGGING
@@ -113,11 +90,8 @@ int main(int argc, char **argv) {
 	}
 #endif
 
-
 	// CONFIGURE PARALLELISM
-#ifdef _OPENMP
 	omp_set_nested(1); // enable nested parallelism
-#endif
 
 	if (options[THREADS]) {
 		// set number of threads
@@ -125,30 +99,23 @@ int main(int argc, char **argv) {
 		Aux::setNumberOfThreads(nThreads);
 	}
 
-	// get program name (currently only for unix)
-	auto pos = program_name.find_last_of("/");
-	program_name = program_name.substr(pos+1,program_name.length()-1);
-
-#ifndef NOGTEST
-	if (options[TESTS]) {
-		::testing::GTEST_FLAG(filter) = "*Test.test*";
-	} else if (options[DEBUG]) {
-		::testing::GTEST_FLAG(filter) = "*Test.debug*";
-	} else if (options[BENCHMARKS]) {
-		if (program_name != "NetworKit-Tests-O") {
-			std::cout << "Hint: Performance tests should be run in optimized mode" << std::endl;
-		}
-		::testing::GTEST_FLAG(filter) = "*Benchmark*";
-	} else if (options[FILTER]) {
-		::testing::GTEST_FLAG(filter) = options[FILTER].arg;
-	}	else if (options[RUNNABLE]) {
-		::testing::GTEST_FLAG(filter) = "*Test.run*";
-	}
-	::testing::GTEST_FLAG(shuffle) = 1;
 	::testing::InitGoogleTest(&argc, argv);
+	auto set_filter = [&] (const char* filter) {
+	    std::cout << "Set --gtest_filter='" << filter << "'\n";
+        ::testing::GTEST_FLAG(filter) = filter;
+	};
+
+	if (options[TESTS]) {
+        set_filter("*Test.test*");
+	} else if (options[DEBUG]) {
+        set_filter("*Test.debug*");
+	} else if (options[BENCHMARKS]) {
+        set_filter("*Benchmark*");
+	}	else if (options[RUNNABLE]) {
+        set_filter("*Test.run*");
+	}
+
 	INFO("=== starting unit tests ===");
+
 	return RUN_ALL_TESTS();
-#else
-	   throw std::runtime_error("unit tests are excluded from build by the NOGTEST preprocessor directive");
-#endif
 }
