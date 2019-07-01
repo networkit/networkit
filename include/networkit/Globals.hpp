02cf99167eac6b1e49ce91808d5b9a90b1d09c1c
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2019-07-01 18:34:42 +0200

    Move PI into NetworKit namespace and modernize Globals.hpp

--- include/networkit/Globals.hpp
+++ include/networkit/Globals.hpp
@@ -14,34 +14,29 @@
 
 #include <networkit/ext/ttmath/ttmath.hpp>
 
-
 namespace NetworKit {
-	/** Typedefs **/
-	typedef uint64_t index; // more expressive name for an index into an array
-#ifdef _MSC_VER	
-	typedef int64_t omp_index;
-#else
-	typedef index omp_index;
-#endif
-	typedef uint64_t count; // more expressive name for an integer quantity
-	typedef ttmath::Big<TTMATH_BITS(64),TTMATH_BITS(64)> bigfloat;	// big floating point number
-	typedef index node; // node indices are 0-based
-	typedef double edgeweight; // edge weight type
-	typedef index edgeid;	// edge id
+    using index = uint64_t; ///< more expressive name for an index into an array
 
-	/** Constants **/
-	constexpr index none = std::numeric_limits<index>::max(); // value for not existing nodes/edges
-	constexpr edgeweight defaultEdgeWeight = 1.0;
-	constexpr edgeweight nullWeight = 0.0;
+    /// Should be used in OpenMP parallel for-loops and is associated with unsigned semantics.
+    /// On MSVC it falls back to being signed, as MSVC does not support unsigned parallel fors.
+#ifdef _MSC_VER
+    using omp_index = int64_t;
+#else
+    using omp_index = index;
+#endif
+
+    using count      = uint64_t; ///< more expressive name for an integer quantity
+    using node       = index;    ///< node indices are 0-based
+    using edgeweight = double;   ///< edge weight type
+    using edgeid     = index;	 ///< edge id
+    using bigfloat = ttmath::Big<TTMATH_BITS(64),TTMATH_BITS(64)>;	///< big floating point number
+
+    constexpr index none = std::numeric_limits<index>::max(); ///< value for not existing nodes/edges
+    constexpr edgeweight defaultEdgeWeight = 1.0;
+    constexpr edgeweight nullWeight = 0.0;
+
+    constexpr double PI = 3.141592653589793238462643383279502884197169399375105820974944592307816406286;
+
 }
-
-#ifdef __INTEL_COMPILER
-constexpr double PI = 3.141592653589793238462643383279502884197169399375105820974944592307816406286;
-#else
-const double PI = 2.0*std::acos(0);
-#endif
-
-// CODE STYLE GUIDELINES: Do not rely on global variables for algorithm parametrization.
-
 
 #endif /* GLOBALS_H_ */
