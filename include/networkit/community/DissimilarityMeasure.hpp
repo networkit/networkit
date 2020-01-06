d6722311bdfcdf36e4daac7fa042ce04235635d5
Charmaine Ndolo <Charmaine.Ndolo@hu-berlin.de>
2020-01-06 15:42:17 +0100

    Refactor DissimilarityMeasure

--- include/networkit/community/DissimilarityMeasure.hpp
+++ include/networkit/community/DissimilarityMeasure.hpp
@@ -1,8 +1,8 @@
 /*
- * DissimilarityMeasure.h
+ * DissimilarityMeasure.hpp
  *
  *  Created on: 19.01.2013
- *      Author: Christian Staudt (christian.staudt@kit.edu)
+ *      Author: Christian Staudt
  */
 
 #ifndef NETWORKIT_COMMUNITY_DISSIMILARITY_MEASURE_HPP_
--- networkit/cpp/community/DissimilarityMeasure.cpp
+++ networkit/cpp/community/DissimilarityMeasure.cpp
@@ -2,7 +2,7 @@
  * DissimilarityMeasure.cpp
  *
  *  Created on: 19.01.2013
- *      Author: Christian Staudt (christian.staudt@kit.edu)
+ *      Author: Christian Staudt
  */
 
 #include <networkit/community/DissimilarityMeasure.hpp>
