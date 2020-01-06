9a67538d3da9e4db75d882f2a2103587fd5db27a
Charmaine Ndolo <Charmaine.Ndolo@hu-berlin.de>
2020-01-06 16:08:35 +0100

    Refactor NodeStructuralRandMeasure

--- networkit/cpp/community/NodeStructuralRandMeasure.cpp
+++ networkit/cpp/community/NodeStructuralRandMeasure.cpp
@@ -2,7 +2,7 @@
  * RandMeasure.cpp
  *
  *  Created on: 19.01.2013
- *      Author: Christian Staudt (christian.staudt@kit.edu)
+ *      Author: Christian Staudt
  */
 
 #include <networkit/community/NodeStructuralRandMeasure.hpp>
