634d4de72698df52f979a9c137b78678defadb61
Charmaine Ndolo <Charmaine.Ndolo@hu-berlin.de>
2020-01-06 15:43:46 +0100

    Refactor EdgeCut

--- include/networkit/community/EdgeCut.hpp
+++ include/networkit/community/EdgeCut.hpp
@@ -1,5 +1,5 @@
 /*
- * EdgeCut.h
+ * EdgeCut.hpp
  *
  *  Created on: Jun 20, 2013
  *      Author: Henning
@@ -15,9 +15,9 @@ namespace NetworKit {
 /**
  * @ingroup community
  */
-class EdgeCut: public QualityMeasure {
+class EdgeCut final : public QualityMeasure {
 public:
-    virtual double getQuality(const Partition& zeta, const Graph& G);
+    double getQuality(const Partition& zeta, const Graph& G) override;
 };
 
 } /* namespace NetworKit */
