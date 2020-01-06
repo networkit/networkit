d3d38b0ccd538c78c89b26be65f07eb18c17918a
Charmaine Ndolo <Charmaine.Ndolo@hu-berlin.de>
2020-01-06 16:12:44 +0100

    Refactor SampledGraphStructuralRandMeasure

--- networkit/cpp/community/SampledGraphStructuralRandMeasure.cpp
+++ networkit/cpp/community/SampledGraphStructuralRandMeasure.cpp
@@ -5,10 +5,9 @@
  *      Author: cls
  */
 
-#include <networkit/community/SampledGraphStructuralRandMeasure.hpp>
-
 #include <networkit/auxiliary/Random.hpp>
 #include <networkit/auxiliary/Log.hpp>
+#include <networkit/community/SampledGraphStructuralRandMeasure.hpp>
 
 namespace NetworKit {
 
