fb0cdeec9b558e6600b6f427fae7559b72c19b2e
Charmaine Ndolo <Charmaine.Ndolo@hu-berlin.de>
2020-01-06 15:48:15 +0100

    Refactor JaccardMeasure

--- networkit/cpp/community/JaccardMeasure.cpp
+++ networkit/cpp/community/JaccardMeasure.cpp
@@ -2,7 +2,7 @@
  * JaccardMeasure.cpp
  *
  *  Created on: 19.01.2013
- *      Author: Christian Staudt (christian.staudt@kit.edu)
+ *      Author: Christian Staudt
  */
 
 #include <networkit/community/JaccardMeasure.hpp>
