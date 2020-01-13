fd53941a68fc5ed29f69d1b9de806b8869e1f797
Charmaine Ndolo <Charmaine.Ndolo@hu-berlin.de>
2020-01-13 10:21:39 +0100

    Refactor PrefixJaccardScore

--- include/networkit/edgescores/PrefixJaccardScore.hpp
+++ include/networkit/edgescores/PrefixJaccardScore.hpp
@@ -6,17 +6,17 @@
 namespace NetworKit {
 
 template <typename AttributeT>
-class PrefixJaccardScore : public EdgeScore<double> {
+class PrefixJaccardScore final : public EdgeScore<double> {
 
 public:
     PrefixJaccardScore(const Graph& G, const std::vector<AttributeT>& attribute);
-    virtual double score(edgeid eid) override;
-    virtual double score(node u, node v) override;
-    virtual void run() override;
+    double score(edgeid eid) override;
+    double score(node u, node v) override;
+    void run() override;
 
 
 private:
-    const std::vector<AttributeT>& inAttribute;
+    const std::vector<AttributeT>* inAttribute;
 
 };
 
