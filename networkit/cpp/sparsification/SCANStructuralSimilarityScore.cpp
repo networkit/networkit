ab442d6b406eeb8df83566aa816d992cdaeebc76
Charmaine Ndolo <Charmaine.Ndolo@hu-berlin.de>
2020-01-13 11:32:20 +0100

    Refactor SCANStructuralSimilarityScore

--- networkit/cpp/sparsification/SCANStructuralSimilarityScore.cpp
+++ networkit/cpp/sparsification/SCANStructuralSimilarityScore.cpp
@@ -1,14 +1,14 @@
 #include <networkit/sparsification/SCANStructuralSimilarityScore.hpp>
 
-NetworKit::SCANStructuralSimilarityScore::SCANStructuralSimilarityScore(const NetworKit::Graph &G, const std::vector< NetworKit::count > &triangles) : EdgeScore<double>(G), triangles(triangles) { }
+NetworKit::SCANStructuralSimilarityScore::SCANStructuralSimilarityScore(const NetworKit::Graph &G, const std::vector< NetworKit::count > &triangles) : EdgeScore<double>(G), triangles(&triangles) { }
 
 void NetworKit::SCANStructuralSimilarityScore::run() {
-    std::vector<double> workScores(G.upperEdgeIdBound());
+    std::vector<double> workScores(G->upperEdgeIdBound());
 
-    if (!G.hasEdgeIds()) throw std::runtime_error("Error, edges must be indexed");
+    if (!G->hasEdgeIds()) throw std::runtime_error("Error, edges must be indexed");
 
-    G.parallelForEdges([&](node u, node v, edgeid eid) {
-        workScores[eid] = (triangles[eid] + 1) * 1.0 / std::sqrt((G.degree(u) + 1)*(G.degree(v) + 1));
+    G->parallelForEdges([&](node u, node v, edgeid eid) {
+        workScores[eid] = ((*triangles)[eid] + 1) * 1.0 / std::sqrt((G->degree(u) + 1)*(G->degree(v) + 1));
     });
 
     scoreData = std::move(workScores);
