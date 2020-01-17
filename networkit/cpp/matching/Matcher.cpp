75eb5ad7e8af127ae7053550f40c5012f81b0342
Charmaine Ndolo <Charmaine.Ndolo@hu-berlin.de>
2020-01-17 15:50:23 +0100

    Refactor Matcher

--- networkit/cpp/matching/Matcher.cpp
+++ networkit/cpp/matching/Matcher.cpp
@@ -2,19 +2,19 @@
  * Matcher.cpp
  *
  *  Created on: 30.10.2012
- *      Author: Christian Staudt (christian.staudt@kit.edu)
+ *      Author: Christian Staudt
  */
 
 #include <networkit/matching/Matcher.hpp>
 
 namespace NetworKit {
 
-Matcher::Matcher(const Graph& G): G(G), M(G.upperNodeIdBound()), edgeScoresAsWeights(false) {
+Matcher::Matcher(const Graph& G): G(&G), M(G.upperNodeIdBound()), edgeScoresAsWeights(false) {
 
 }
 
 
-Matcher::Matcher(const Graph& G, const std::vector<double>& edgeScores): G(G), M(G.upperNodeIdBound()), edgeScoresAsWeights(true), edgeScores(edgeScores) {
+Matcher::Matcher(const Graph& G, const std::vector<double>& edgeScores): G(&G), M(G.upperNodeIdBound()), edgeScoresAsWeights(true), edgeScores(edgeScores) {
     if (!G.hasEdgeIds()) throw std::invalid_argument("index edges of input graph first");
 }
 
