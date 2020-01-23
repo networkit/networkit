397abd976fc6ef41f9c7541a9c0fbc82646920a3
Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>
2020-01-23 18:43:56 +0100

    CommonNeighborsIndex: remove source file

--- include/networkit/linkprediction/CommonNeighborsIndex.hpp
+++ include/networkit/linkprediction/CommonNeighborsIndex.hpp
@@ -9,6 +9,7 @@
 #define NETWORKIT_LINKPREDICTION_COMMON_NEIGHBORS_INDEX_HPP_
 
 #include <networkit/linkprediction/LinkPredictor.hpp>
+#include <networkit/linkprediction/NeighborhoodUtility.hpp>
 
 namespace NetworKit {
 
@@ -19,14 +20,15 @@ namespace NetworKit {
  * neighbors of a node-pair in a given graph.
  */
 class CommonNeighborsIndex final : public LinkPredictor {
-private:
   /**
    * Returns the number of common neighbors of the given nodes @a u and @a v.
    * @param u First node
    * @param v Second node
    * @return the number of common neighbors of @a u and @a v
    */
-  double runImpl(node u, node v) override;
+  double runImpl(node u, node v) override {
+      return NeighborhoodUtility::getCommonNeighbors(*G, u, v).size();
+  }
 
 public:
   using LinkPredictor::LinkPredictor;
