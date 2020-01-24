1df914e5e4b5393bf4a21161d37b04a4fbf53b1a
Charmaine Ndolo <Charmaine.Ndolo@hu-berlin.de>
2020-01-24 11:47:57 +0100

    Refactor QuadtreePolarEuclid

--- include/networkit/generators/quadtree/QuadtreePolarEuclid.hpp
+++ include/networkit/generators/quadtree/QuadtreePolarEuclid.hpp
@@ -1,8 +1,8 @@
 /*
- * Quadtree.h
+ * Quadtree.hpp
  *
  *  Created on: 21.05.2014
- *      Author: Moritz v. Looz (moritz.looz-corswarem@kit.edu)
+ *      Author: Moritz v. Looz
  */
 
 #ifndef NETWORKIT_GENERATORS_QUADTREE_QUADTREE_POLAR_EUCLID_HPP_
@@ -18,7 +18,7 @@
 namespace NetworKit {
 
 template <class T>
-class QuadtreePolarEuclid {
+class QuadtreePolarEuclid final {
     friend class QuadTreePolarEuclidGTest;
 public:
     QuadtreePolarEuclid() {
