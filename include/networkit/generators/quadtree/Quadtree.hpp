68aa49a2932bfd2421059735cc5f8c2a4b2427ba
Charmaine Ndolo <Charmaine.Ndolo@hu-berlin.de>
2020-01-24 11:47:21 +0100

    Refacto Quadtree

--- include/networkit/generators/quadtree/Quadtree.hpp
+++ include/networkit/generators/quadtree/Quadtree.hpp
@@ -1,26 +1,26 @@
 /*
- * Quadtree.h
+ * Quadtree.hpp
  *
  *  Created on: 21.05.2014
- *      Author: Moritz v. Looz (moritz.looz-corswarem@kit.edu)
+ *      Author: Moritz v. Looz
  */
 
 #ifndef NETWORKIT_GENERATORS_QUADTREE_QUADTREE_HPP_
 #define NETWORKIT_GENERATORS_QUADTREE_QUADTREE_HPP_
 
-#include <vector>
-#include <memory>
 #include <cmath>
+#include <memory>
 #include <omp.h>
+#include <vector>
 #include <functional>
+#include <networkit/auxiliary/Parallel.hpp>
 #include <networkit/generators/quadtree/QuadNode.hpp>
 #include <networkit/geometric/HyperbolicSpace.hpp>
-#include <networkit/auxiliary/Parallel.hpp>
 
 namespace NetworKit {
 
 template <class T, bool poincare=true>
-class Quadtree {
+class Quadtree final {
     friend class QuadTreeGTest;
 public:
     Quadtree() {
@@ -108,7 +108,6 @@ public:
         Point2DWithIndex<double> center = HyperbolicSpace::polarToCartesian(cc_phi, r_e);
         double minR = r_e - radius;
         double maxR = r_e + radius;
-        //assert(maxR < 1);//this looks fishy
         if (maxR > 1) maxR = 1;
         if (minR < 0) {
             maxR = std::max(abs(minR), maxR);
@@ -146,7 +145,6 @@ public:
             if (denizen >= size()) {
                 DEBUG("Content ", denizen, " found in quadtree of size ", size(), ", as one of ", circleDenizens.size(), " neighbours.");
             }
-            assert(denizen < size());//TODO: remove this after debugging, in general the quadtree should handle arbitrary contents
         }
 
         //we have sort(deg(v)) here! This is not good, but does not make the asymptotical complexity of O(deg(v) log n) worse.
@@ -163,7 +161,6 @@ public:
 
         for (T denizen : circleDenizens) {
             if (denizen >= size()) DEBUG("Content ", denizen, " found in quadtree of size ", size(), ", as one of ", circleDenizens.size(), " neighbours, after sorting");
-            assert(denizen < size());//TODO: remove this after debugging, in general the quadtree should handle arbitrary contents
         }
     }
 
