eada21e1f0ba122d9e23d12f86849c22b68280f1
Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>
2020-01-25 12:03:25 +0100

    NeighborhoodDistanceIndex: move implementation to header

--- include/networkit/linkprediction/NeighborhoodDistanceIndex.hpp
+++ include/networkit/linkprediction/NeighborhoodDistanceIndex.hpp
@@ -8,8 +8,8 @@
 #ifndef NETWORKIT_LINKPREDICTION_NEIGHBORHOOD_DISTANCE_INDEX_HPP_
 #define NETWORKIT_LINKPREDICTION_NEIGHBORHOOD_DISTANCE_INDEX_HPP_
 
-#include <algorithm>
 #include <math.h>
+
 #include <networkit/linkprediction/LinkPredictor.hpp>
 
 namespace NetworKit {
@@ -21,18 +21,22 @@ namespace NetworKit {
  * overlap of their neighborhoods.
  */
 class NeighborhoodDistanceIndex final : public LinkPredictor {
-private:
   /**
    * Returns the Neighborhood Distance index for the given node-pair (@a u, @a v).
    * @param u First node
    * @param v Second node
    * @return the Neighborhood Distance index for the given node-pair (@a u, @a v)
    */
-  double runImpl(node u, node v) override;
+  double runImpl(node u, node v) override {
+    count uNeighborhood = G->degree(u);
+    count vNeighborhood = G->degree(v);
+    count intersection = NeighborhoodUtility::getCommonNeighbors(*G, u, v).size();
+    return ((double)intersection) / (sqrt(uNeighborhood * vNeighborhood));
+  }
 
 public:
   using LinkPredictor::LinkPredictor;
-  
+
 };
 
 } /* namespace NetworKit */
