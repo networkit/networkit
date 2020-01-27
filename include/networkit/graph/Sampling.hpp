b45736d5af8e1ea48b787ad0d719a39d244776ce
Charmaine Ndolo <Charmaine.Ndolo@hu-berlin.de>
2020-01-27 10:25:06 +0100

    Refactor Sampling

--- include/networkit/graph/Sampling.hpp
+++ include/networkit/graph/Sampling.hpp
@@ -1,5 +1,5 @@
 /*
- * Sampling.h
+ * Sampling.hpp
  *
  *  Created on: 17.02.2014
  *      Author: cls
@@ -9,6 +9,7 @@
 #define NETWORKIT_GRAPH_SAMPLING_HPP_
 
 #include <networkit/graph/Graph.hpp>
+#include <networkit/graph/GraphTools.hpp>
 
 namespace NetworKit {
 
@@ -19,11 +20,17 @@ class Sampling {
 
 public:
 
-    static node randomNode(const Graph& G);
+    static node randomNode(const Graph& G) {
+        return GraphTools::randomNode(G);
+    }
 
     static std::pair<node, node> randomEdge(const Graph& G);
+        return GraphTools::randomEdge(G);
+    }
 
     static node randomNeighbor(const Graph& G, node u);
+        return GraphTools::randomNeighbor(G, u);
+    }
 
 };
 
