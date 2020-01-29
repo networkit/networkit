b0540510330ca21880bf55e42e804c218056e7ea
Charmaine Ndolo <Charmaine.Ndolo@hu-berlin.de>
2020-01-29 14:30:30 +0100

    Redactor GraphReader

--- include/networkit/io/GraphReader.hpp
+++ include/networkit/io/GraphReader.hpp
@@ -1,25 +1,17 @@
 /*
- * GraphReader.h
+ * GraphReader.hpp
  *
  *  Created on: 17.01.2013
- *      Author: Christian Staudt (christian.staudt@kit.edu)
+ *      Author: Christian Staudt
  */
 
 #ifndef NETWORKIT_IO_GRAPH_READER_HPP_
 #define NETWORKIT_IO_GRAPH_READER_HPP_
 
-#include <networkit/graph/Graph.hpp>
 #include <networkit/auxiliary/StringTools.hpp>
-
+#include <networkit/graph/Graph.hpp>
 
 namespace NetworKit {
-
-/*
-* Enum for defining handling of graph formats with multiple same edges.
-*/
-
-
-
 /**
  * @ingroup io
  * Abstract base class for graph readers.
@@ -27,11 +19,12 @@ namespace NetworKit {
 class GraphReader {
 public:
 
+    // Enum for defining handling of graph formats with multiple same edges.
     enum MultipleEdgesHandling
     {
          DISCARD_EDGES, //Reads and selects the first edge which occurs and discards all following
          SUM_WEIGHTS_UP, //If an edge occurs again, the weight of it is added to the existing edge
-         KEEP_MINIUM_WEIGHT //The edge with the lowest weight is kept
+         KEEP_MINIMUM_WEIGHT //The edge with the lowest weight is kept
     };
 
     virtual ~GraphReader() = default;
@@ -39,7 +32,7 @@ public:
     /**
      * Given the path of an input file, read the graph contained.
      *
-     * @param[in]	path	input file path
+     * @param[in]  path  input file path
      */
     virtual Graph read(const std::string& path) = 0;
 };
