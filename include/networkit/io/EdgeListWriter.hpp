3c9fdb081e7f685228298580bf52e4f56569a33d
Charmaine Ndolo <Charmaine.Ndolo@hu-berlin.de>
2020-01-31 10:37:53 +0100

    Refactor EdgeListWriter

--- include/networkit/io/EdgeListWriter.hpp
+++ include/networkit/io/EdgeListWriter.hpp
@@ -26,22 +26,22 @@ public:
     EdgeListWriter() = default; //nullary constructor for Python shell
 
     /**
-     * @param[in]	separator	character used to separate nodes in an edge line
-     * @param[in]	firstNode	index of the first node in the file
-     * @param[in]	bothDirections	for undirected graphs: if every edge shall be written in both directions (default: false)
+     * @param[in]  separator  character used to separate nodes in an edge line
+     * @param[in]  firstNode  index of the first node in the file
+     * @param[in]  bothDirections  for undirected graphs: if every edge shall be written in both directions (default: false)
      */
     EdgeListWriter(char separator, node firstNode, bool bothDirections = false);
 
     /**
      * Write the graph to a file.
-     * @param[in]	G		the graph
-     * @param[in]	path	the output file path
+     * @param[in]  G    the graph
+     * @param[in]  path  the output file path
      */
     void write(const Graph &G, const std::string &path) override;
 
-protected:
+private:
 
-    char separator; 	//!< character separating nodes in an edge line
+    char separator;   //!< character separating nodes in an edge line
     node firstNode;
     bool bothDirections;
 };
