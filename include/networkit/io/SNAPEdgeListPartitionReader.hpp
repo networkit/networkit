1362890deaae0d3f10b6fc3ecfc0684d2394d877
Charmaine Ndolo <Charmaine.Ndolo@hu-berlin.de>
2020-01-31 15:13:58 +0100

    Refactor SNAPEdgeListPartitionReader

--- include/networkit/io/SNAPEdgeListPartitionReader.hpp
+++ include/networkit/io/SNAPEdgeListPartitionReader.hpp
@@ -1,5 +1,5 @@
 /*
- * SNAPEdgeListPartitionReader.h
+ * SNAPEdgeListPartitionReader.hpp
  *
  *  Created on: Jun 20, 2013
  *      Author: forigem
@@ -8,13 +8,13 @@
 #ifndef NETWORKIT_IO_SNAP_EDGE_LIST_PARTITION_READER_HPP_
 #define NETWORKIT_IO_SNAP_EDGE_LIST_PARTITION_READER_HPP_
 
-#include <set>
-#include <vector>
 #include <fstream>
+#include <set>
 #include <unordered_map>
+#include <vector>
 
-#include <networkit/graph/Graph.hpp>
 #include <networkit/auxiliary/StringTools.hpp>
+#include <networkit/graph/Graph.hpp>
 #include <networkit/structures/Cover.hpp>
 
 namespace NetworKit {
@@ -23,11 +23,9 @@ namespace NetworKit {
  * @ingroup io
  * Reads the clustering files from the SNAP collection.
  */
-class SNAPEdgeListPartitionReader {
+class SNAPEdgeListPartitionReader final {
 public:
-    virtual Cover read(std::string path, std::unordered_map<node,node>& mapNodeIds, Graph& G);
-
-//	virtual Partition readWithInfo(std::string path, count nNodes);
+    Cover read(std::string path, std::unordered_map<node,node>& mapNodeIds, Graph& G);
 
 };
 
--- networkit/cpp/io/SNAPEdgeListPartitionReader.cpp
+++ networkit/cpp/io/SNAPEdgeListPartitionReader.cpp
@@ -25,32 +25,23 @@ Cover SNAPEdgeListPartitionReader::read(std::string path, std::unordered_map<nod
         throw std::runtime_error("unable to read from file");
     }
     
-//	std::string previousLine;
     node current;
     
     std::string commentPrefix = "#";
     
-//	count firstNode = 0;
-//	char separator = '\t';
     std::set<node> uniqueIDs;
     count totalCounter = 0;
 
     Cover communities(G.upperNodeIdBound());
 
-    //DEBUG("separator: " , separator);
-    //DEBUG("first node: " , firstNode);
 
     // first find out the maximum node id
-    //DEBUG("first pass");
     count i = 0;
     while (file.good()) {
         ++i;
         std::getline(file, line);
-        // TRACE("read line: " , line);
         if (line.compare(0, commentPrefix.length(), commentPrefix) == 0) {
-            // TRACE("ignoring comment: " , line);
             } else if (line.length() == 0) {
-                // TRACE("ignoring empty line");
         } else {
             std::stringstream linestream(line);
             while (linestream >> current) {
@@ -100,51 +91,5 @@ Cover SNAPEdgeListPartitionReader::read(std::string path, std::unordered_map<nod
     communities.setUpperBound(i);
     return communities;
 }
-
-#if 0
-Partition SNAPEdgeListPartitionReader::readWithInfo(std::string path, count nNodes) {
-    std::ifstream file(path);
-
-    // check if file readable
-    if (!file) {
-        throw std::runtime_error("invalid clustering file");
-    }
-
-    Partition communities(nNodes);
-    std::string line;
-    //	std::stringstream linestream;
-    count lines = 1;
-    count nValues = 0;
-    std::unordered_map<node,count> ids;
-    node current;
-    while (std::getline(file,line)) {
-        std::stringstream linestream(line);
-        while (linestream >> current) {
-            nValues++;
-            if (!ids.insert(std::make_pair(current,0)).second) {
-                ids[current]++;
-            }
-            //DEBUG("read nodeID: ", current);
-            if ( current < nNodes ) {
-                communities[current] = lines;
-            } else {
-                DEBUG("found invalid node ID: ",current);
-            }
-        }
-        lines++;
-    }
-    count sum = 0;
-    for(auto x: ids) {
-        if (x.second > 1) {
-            DEBUG(x.first," has been found ",x.second, " times");
-            sum += x.second;
-        }
-    }
-    WARN("found ",ids.size()," values and ",sum," total occurrences");
-
-    communities.setUpperBound(lines);
-    return communities;
-}
-#endif
 
 } /* namespace NetworKit */
