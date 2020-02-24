2dbf0f38bb98b46828cc750d9f57639987555666
Charmaine Ndolo <Charmaine.Ndolo@hu-berlin.de>
2020-02-24 09:38:52 +0100

    Fix issue #505

--- networkit/cpp/generators/WattsStrogatzGenerator.cpp
+++ networkit/cpp/generators/WattsStrogatzGenerator.cpp
@@ -5,27 +5,46 @@
 *      Author: Simon Bischof
 */
 
+// networkit-format
+
+#include <limits>
+
 #include <networkit/auxiliary/Random.hpp>
+#include <networkit/auxiliary/Log.hpp>
 #include <networkit/generators/RegularRingLatticeGenerator.hpp>
 #include <networkit/generators/WattsStrogatzGenerator.hpp>
 
 namespace NetworKit {
 
 WattsStrogatzGenerator::WattsStrogatzGenerator(count nNodes, count nNeighbors, double p)
-    : nNodes(nNodes), nNeighbors(nNeighbors), p(p) {}
+    : nNodes(nNodes), nNeighbors(nNeighbors), p(p) {
+		if (nNeighbors*2 >= nNodes-1) {
+			throw std::runtime_error("nNeighbors*2 cannot be equal to nNodes-1.");
+		} else if (nNeighbors >= nNodes / 2 - 1) {
+            nNeighbors = nNodes / 2 - 1;
+        }
+	}
 
 Graph WattsStrogatzGenerator::generate() {
     //generate regular ring lattice as initial graph
     RegularRingLatticeGenerator R(nNodes, nNeighbors);
     Graph G = R.generate();
+    std::uniform_int_distribution<node> dist(0, nNodes - 1);
+    std::mt19937_64 generator(Aux::Random::integer(0, std::numeric_limits<count>::max()));
+    std::uniform_real_distribution<double> prob{0.0, std::nexttoward(1.0, 2.0)};
 
     //list all valid end nodes for rewiring an edge
-    auto validEndNode = [&](node u, node v) {
-        return ! G.hasEdge(u, v) && u != v;
+    auto validEndNode = [&G](node u, node v) {
+        return u != v && !G.hasEdge(u, v);
     };
 
-    //rewire according to Watts and Strogatz model
-    for (node u = 0; u < nNodes; u++) {
+    std::vector<node> prevNeighbors;
+    prevNeighbors.reserve(GraphTools::maxDegree(G));
+
+    // rewire according to Watts and Strogatz model
+    for (const auto u : G.nodeRange()) {
+        if (G.degree(u) == (nNodes - 1))
+            continue;
         /* save edges before rewiring incident edges for a node
          * because they may get changed through rewiring
          */
@@ -36,19 +55,19 @@ Graph WattsStrogatzGenerator::generate() {
             }
         });
         for (node v : prevNeighbors) {
-            if (Aux::Random::probability() < p) {
+            if (prob(generator) < p) {
                 bool found = false;
-                node w;
+                node w = none;
                 while(!found) {
-                    w = Aux::Random::integer(nNodes-1);
+                    w = dist(generator);
                     if (validEndNode(u,w)) {
-                        found = true;
+                       found = true;
                     }
                 }
                 G.removeEdge(u, v);
                 G.addEdge(u, w);
             }
-        }
+		}
     }
 
     return G;
