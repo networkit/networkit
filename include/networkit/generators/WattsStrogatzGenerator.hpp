a515ef0bf1018cfa3ec94c8cdf63444dfa4a221a
Charmaine Ndolo <Charmaine.Ndolo@hu-berlin.de>
2020-02-26 16:56:22 +0100

    Subscribe to networkit-format

--- include/networkit/generators/WattsStrogatzGenerator.hpp
+++ include/networkit/generators/WattsStrogatzGenerator.hpp
@@ -1,17 +1,16 @@
 /*
-* WattsStrogatzGenerator.hpp
-*
-*  Created on: 09.07.2014
-*      Author: Simon Bischof
-*/
-
+ * WattsStrogatzGenerator.hpp
+ *
+ *  Created on: 09.07.2014
+ *      Author: Simon Bischof
+ */
+// networkit-format
 #ifndef NETWORKIT_GENERATORS_WATTS_STROGATZ_GENERATOR_HPP_
 #define NETWORKIT_GENERATORS_WATTS_STROGATZ_GENERATOR_HPP_
 
 #include <networkit/generators/StaticGraphGenerator.hpp>
 
 namespace NetworKit {
-
 
 /**
  * @ingroup generators
@@ -20,17 +19,17 @@ class WattsStrogatzGenerator final : public StaticGraphGenerator {
 
 public:
     /**
-    * Constructs a graph according to the Watts and Strogatz model
-    * (https://en.wikipedia.org/wiki/Watts_and_Strogatz_model),
-    * which produces graphs with high clustering and low average path length.
-    *
-    * First, a regular ring lattice is generated.
-    * Then some edges are rewired randomly.
-    *
-    * @param nNodes number of nodes in target graph
-    * @param nNeighbors number of neighbors on each side of a node
-    * @param p rewiring probability
-    */
+     * Constructs a graph according to the Watts and Strogatz model
+     * (https://en.wikipedia.org/wiki/Watts_and_Strogatz_model),
+     * which produces graphs with high clustering and low average path length.
+     *
+     * First, a regular ring lattice is generated.
+     * Then some edges are rewired randomly.
+     *
+     * @param nNodes number of nodes in target graph
+     * @param nNeighbors number of neighbors on each side of a node
+     * @param p rewiring probability
+     */
     WattsStrogatzGenerator(count nNodes, count nNeighbors, double p);
 
     Graph generate() override;
--- networkit/cpp/generators/WattsStrogatzGenerator.cpp
+++ networkit/cpp/generators/WattsStrogatzGenerator.cpp
@@ -1,16 +1,15 @@
 /*
-* WattsStrogatzGenerator.cpp
-*
-*  Created on: 09.07.2014
-*      Author: Simon Bischof
-*/
+ * WattsStrogatzGenerator.cpp
+ *
+ *  Created on: 09.07.2014
+ *      Author: Simon Bischof
+ */
 
 // networkit-format
 
 #include <limits>
 
 #include <networkit/auxiliary/Random.hpp>
-#include <networkit/auxiliary/Log.hpp>
 #include <networkit/generators/RegularRingLatticeGenerator.hpp>
 #include <networkit/generators/WattsStrogatzGenerator.hpp>
 
@@ -18,25 +17,23 @@ namespace NetworKit {
 
 WattsStrogatzGenerator::WattsStrogatzGenerator(count nNodes, count nNeighbors, double p)
     : nNodes(nNodes), nNeighbors(nNeighbors), p(p) {
-		if (nNeighbors*2 >= nNodes-1) {
-			throw std::runtime_error("nNeighbors*2 cannot be equal to nNodes-1.");
-		} else if (nNeighbors >= nNodes / 2 - 1) {
-            nNeighbors = nNodes / 2 - 1;
-        }
-	}
+    if (nNeighbors * 2 >= nNodes - 1) {
+        throw std::runtime_error("nNeighbors*2 cannot be equal to nNodes-1.");
+    } else if (nNeighbors >= nNodes / 2 - 1) {
+        nNeighbors = nNodes / 2 - 1;
+    }
+}
 
 Graph WattsStrogatzGenerator::generate() {
-    //generate regular ring lattice as initial graph
+    // generate regular ring lattice as initial graph
     RegularRingLatticeGenerator R(nNodes, nNeighbors);
     Graph G = R.generate();
     std::uniform_int_distribution<node> dist(0, nNodes - 1);
     std::mt19937_64 generator(Aux::Random::integer(0, std::numeric_limits<count>::max()));
     std::uniform_real_distribution<double> prob{0.0, std::nexttoward(1.0, 2.0)};
 
-    //list all valid end nodes for rewiring an edge
-    auto validEndNode = [&G](node u, node v) {
-        return u != v && !G.hasEdge(u, v);
-    };
+    // list all valid end nodes for rewiring an edge
+    auto validEndNode = [&G](node u, node v) { return u != v && !G.hasEdge(u, v); };
 
     std::vector<node> prevNeighbors;
     prevNeighbors.reserve(GraphTools::maxDegree(G));
@@ -58,16 +55,16 @@ Graph WattsStrogatzGenerator::generate() {
             if (prob(generator) < p) {
                 bool found = false;
                 node w = none;
-                while(!found) {
+                while (!found) {
                     w = dist(generator);
-                    if (validEndNode(u,w)) {
-                       found = true;
+                    if (validEndNode(u, w)) {
+                        found = true;
                     }
                 }
                 G.removeEdge(u, v);
                 G.addEdge(u, w);
             }
-		}
+        }
     }
 
     return G;
