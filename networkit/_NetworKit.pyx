944a5a07fd7bf040afaa67ef9e0889c97c9cfe00
Charmaine Ndolo <Charmaine.Ndolo@hu-berlin.de>
2020-05-01 21:11:19 +0200

    Move gini to stats module

--- networkit/_NetworKit.pyx
+++ networkit/_NetworKit.pyx
@@ -466,17 +466,3 @@ def sort2(sample):
 	sort(result.begin(),result.end())
 	return result
 
-# stats
-
-def gini(values):
-	"""
-	Computes the Gini coefficient for the distribution given as a list of values.
-	"""
-	sorted_list = sorted(values)
-	height, area = 0, 0
-	for value in sorted_list:
-		height += value
-		area += height - value / 2.
-	fair_area = height * len(values) / 2
-	return (fair_area - area) / fair_area
-
--- networkit/stats.py
+++ networkit/stats.py
@@ -1,3 +1,16 @@
 """ Missing statistics functions"""
 
-from _NetworKit import gini
+# stats
+
+def gini(values):
+	"""
+	Computes the Gini coefficient for the distribution given as a list of values.
+	"""
+	sorted_list = sorted(values)
+	height, area = 0, 0
+	for value in sorted_list:
+		height += value
+		area += height - value / 2.
+	fair_area = height * len(values) / 2
+	return (fair_area - area) / fair_area
+
