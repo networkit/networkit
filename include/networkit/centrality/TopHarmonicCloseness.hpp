3551aa99603d7a051b575f33cbc6e65903c45c37
Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>
2020-09-12 18:48:09 +0200

    Refactor TopHarmonicCloseness.
    This commit resolves the following TopHarmonicCloseness issues
    - Wrong ranking for NBbound
    - Missing tests for NBbound
    - Inefficient priority queue (resolved by using a heap)
    
    Further, TopHarmonicCloseness (NBcut variation) now also supports
    weighed graphs.

--- include/networkit/centrality/TopHarmonicCloseness.hpp
+++ include/networkit/centrality/TopHarmonicCloseness.hpp
@@ -1,5 +1,5 @@
 /*
- * TopHarmonicCloseness.h
+ * TopHarmonicCloseness.hpp
  *
  * Created on: 25.02.2018
  *		Authors: nemes, Eugenio Angriman
@@ -8,160 +8,145 @@
 #ifndef NETWORKIT_CENTRALITY_TOP_HARMONIC_CLOSENESS_HPP_
 #define NETWORKIT_CENTRALITY_TOP_HARMONIC_CLOSENESS_HPP_
 
+// networkit-format
+
+#include <memory>
+
 #include <networkit/base/Algorithm.hpp>
-#include <networkit/components/ConnectedComponents.hpp>
+#include <networkit/components/WeaklyConnectedComponents.hpp>
 #include <networkit/graph/Graph.hpp>
-#include <networkit/structures/Partition.hpp>
+
+#include <tlx/container/d_ary_addressable_int_heap.hpp>
 
 namespace NetworKit {
 
 /**
  * @ingroup centrality
  */
-class TopHarmonicCloseness : public Algorithm {
+class TopHarmonicCloseness final : public Algorithm {
 public:
-  /**
-   * Finds the top k nodes with highest harmonic closeness centrality faster
-   * than computing it for all nodes. The implementation is based on "Computing
-   * Top-k Centrality Faster in Unweighted Graphs", Bergamini et al., ALENEX16.
-   * The algorithms are based on two heuristics. We recommend to use
-   * useBFSbound = false for complex networks (or networks with small diameter)
-   * and useBFSbound = true for street networks (or networks with large
-   * diameters). Notice that the worst case running time of the algorithm is
-   * O(nm), where n is the number of nodes and m is the number of edges.
-   * However, for most real-world networks the empirical running time is O(m).
-   *
-   * @param G An unweighted graph.
-   * @param k Number of nodes with highest harmonic closeness that have to be
-   * found. For example, k = 10, the top 10 nodes with highest harmonic
-   * closeness will be computed.
-   * @param useBFSbound If true, BFSbound is re-computed at each iteration. If
-   * false, BFScut is used
-   */
-  TopHarmonicCloseness(const Graph &G, count k = 1, bool useBFSbound = false);
+    /**
+     * Finds the top k nodes with highest harmonic closeness centrality faster
+     * than computing it for all nodes. The implementation is based on "Computing
+     * Top-k Centrality Faster in Unweighted Graphs", Bergamini et al., ALENEX16.
+     * The algorithms are based on two heuristics. We recommend to use
+     * useNBbound = false for complex networks (or networks with small diameter)
+     * and useNBbound = true for street networks (or networks with large
+     * diameters). Notice that the worst case running time of the algorithm is
+     * O(nm), where n is the number of nodes and m is the number of edges.
+     * However, for most real-world networks the empirical running time is O(m).
+     *
+     * @param G An unweighted graph.
+     * @param k Number of nodes with highest harmonic closeness that have to be
+     * found. For example, k = 10, the top 10 nodes with highest harmonic
+     * closeness will be computed.
+     * @param useNBbound If true, BFSbound is re-computed at each iteration. If
+     * false, BFScut is used
+     */
+    explicit TopHarmonicCloseness(const Graph &G, count k = 1, bool useNBbound = false);
 
-  /**
-   * Computes top-k harmonic closeness on the graph passed in the constructor.
-   */
-  void run() override;
+    ~TopHarmonicCloseness() override;
 
-  /**
-   * Returns a list with the k nodes with highest harmonic closeness.
-   * WARNING: closeness centrality of some nodes below the top-k could be equal
-   * to the k-th closeness, we call them trail. Set the parameter includeTrail
-   * to true to also include those nodes but consider that the resulting vector
-   * could be longer than k.
-   *
-   * @param includeTrail Whether or not to include trail nodes.
-   * @return The list of the top-k nodes.
-   */
-  std::vector<node> topkNodesList(bool includeTrail = false);
+    /**
+     * Computes top-k harmonic closeness on the graph passed in the constructor.
+     */
+    void run() override;
 
-  /**
-   * Returns a list with the scores of the k nodes with highest harmonic
-   * closeness.
-   * WARNING: closeness centrality of some nodes below the top-k could
-   * be equal to the k-th closeness, we call them trail. Set the parameter
-   * includeTrail to true to also include those centrality values but consider
-   * that the resulting vector could be longer than k.
-   *
-   * @param includeTrail Whether or not to include trail centrality value.
-   * @return The closeness centralities of the k most central nodes.
-   */
-  std::vector<edgeweight> topkScoresList(bool includeTrail = false);
+    /**
+     * Returns a list with the k nodes with highest harmonic closeness.
+     * WARNING: closeness centrality of some nodes below the top-k could be equal
+     * to the k-th closeness, we call them trail. Set the parameter includeTrail
+     * to true to also include those nodes but consider that the resulting vector
+     * could be longer than k.
+     *
+     * @param includeTrail Whether or not to include trail nodes.
+     * @return The list of the top-k nodes.
+     */
+    std::vector<node> topkNodesList(bool includeTrail = false);
 
-protected:
-  /**
-   * Runs a pruned BFS from the node @a u. The search is aborted once the
-   * closeness centrality of @a u must be smaller than @a x.
-   *
-   * @param v The start node.
-   * @param x The harmonic closeness centrality of the k-th most central node.
-   * @param n The number of nodes in the graph.
-   * @param r The number of reachable nodes (or an upper bound in directed
-   * graphs) for each node.
-   * @param visited The vector containing the visited status.
-   * @param distances The vector containing the distances.
-   * @param pred The vector containing the predecessor of each node.
-   * @param visitedEdges The number of visited edges.
-   * @return
-   */
-  std::pair<edgeweight, bool> BFScut(node v, edgeweight x, count n, count r,
-                                     std::vector<uint8_t> &visited,
-                                     std::vector<count> &distances,
-                                     std::vector<node> &pred,
-                                     count &visitedEdges);
+    /**
+     * Returns a list with the scores of the k nodes with highest harmonic
+     * closeness.
+     * WARNING: closeness centrality of some nodes below the top-k could
+     * be equal to the k-th closeness, we call them trail. Set the parameter
+     * includeTrail to true to also include those centrality values but consider
+     * that the resulting vector could be longer than k.
+     *
+     * @param includeTrail Whether or not to include trail centrality value.
+     * @return The closeness centralities of the k most central nodes.
+     */
+    std::vector<edgeweight> topkScoresList(bool includeTrail = false);
 
-  /**
-   * Computes the exact harmonic closeness centrality of the node @a x and
-   * stores upper bounds for all other nodes in @a S2.
-   *
-   * @param x The start node.
-   * @param S2 The upper bounds for all other nodes.
-   * @param visEdges The number of visited edges.
-   */
-  void BFSbound(node x, std::vector<double> &S2, count *visEdges);
+private:
+    const Graph *G;
+    const count k;
+    const bool useNBbound;
 
-  /**
-   * Computes the number of reachable nodes or an upper bound in directed
-   * graphs.
-   */
-  void computeReachableNodes();
+    std::vector<double> hCloseness;
+    std::vector<count> reachableNodes;
+    std::vector<std::vector<uint8_t>> visitedGlobal;
+    std::vector<uint8_t> tsGlobal;
 
-  /**
-   * Computes an upper bound for the number of reachable nodes for each node
-   * in a directed graph.
-   */
-  void computeReachableNodesDirected();
+    std::vector<node> topKNodes;
+    std::vector<double> topKScores;
 
-  /**
-   * Computes the number of reachable nodes in an undirected graph.
-   */
-  void computeReachableNodesUndirected();
+    // For NBbound
+    std::vector<count> reachU, reachL;
+    std::vector<std::vector<count>> numberOfNodesAtLevelGlobal;
+    std::vector<std::vector<node>> nodesAtCurrentLevelGlobal;
+    std::vector<std::vector<std::vector<node>>> nodesAtLevelGlobal;
+    std::vector<double> levelImprovement;
+    std::unique_ptr<WeaklyConnectedComponents> wccPtr;
+    void updateTimestamp() {
+        auto &visited = visitedGlobal[omp_get_thread_num()];
+        auto &ts = tsGlobal[omp_get_thread_num()];
+        if (ts++ == std::numeric_limits<uint8_t>::max()) {
+            ts = 1;
+            std::fill(visited.begin(), visited.end(), 0);
+        }
+    }
 
-  void init();
+    template <class Type>
+    struct Greater {
+        Greater(const std::vector<Type> &vec) : vec(&vec) {}
+        bool operator()(node u, node v) const noexcept { return (*vec)[u] > (*vec)[v]; }
+        const std::vector<Type> *vec;
+    };
 
-  Graph G;
-  count k;
-  count n;
-  count trail = 0;
-  double minCloseness = std::numeric_limits<double>::max();
-  count nMinCloseness = 0;
-  bool useBFSbound;
-  std::vector<node> topk;
-  std::vector<edgeweight> topkScores;
-  std::vector<edgeweight> allScores;
-  std::vector<uint8_t> isExact;
-  std::vector<uint8_t> isValid;
-  std::vector<edgeweight> cutOff;
-  std::vector<uint8_t> exactCutOff;
-  Partition component;
-  std::vector<count> r;
-  std::vector<count> rOld;
+    template <class Type>
+    struct Less {
+        Less(const std::vector<Type> &vec) : vec(&vec) {}
+        bool operator()(node u, node v) const noexcept { return (*vec)[u] < (*vec)[v]; }
+        const std::vector<Type> *vec;
+    };
+
+    tlx::d_ary_addressable_int_heap<node, 2, Greater<double>> prioQ;
+    tlx::d_ary_addressable_int_heap<node, 2, Less<double>> topKNodesPQ;
+    std::vector<node> trail;
+
+	// For weighted graphs
+	std::vector<tlx::d_ary_addressable_int_heap<node, 2, Less<edgeweight>>> dijkstraHeaps;
+	std::vector<std::vector<edgeweight>> distanceGlobal;
+	edgeweight minEdgeWeight;
+
+    omp_lock_t lock;
+
+    void runNBcut();
+    void runNBbound();
+    bool bfscutUnweighted(node source, double kthCloseness);
+    bool bfscutWeighted(node source, double kthCloseness);
+    void bfsbound(node source);
+    void computeReachableNodes();
+    void computeReachableNodesBounds();
+    void computeNeighborhoodBasedBound();
+    void updateTopkPQ(node u);
+
+    double initialBoundNBcutUnweighted(node u) const noexcept;
+    double initialBoundNBcutWeighted(node u) const noexcept;
+
+    template <class Type>
+    std::vector<Type> resizedCopy(const std::vector<Type> &vec) const noexcept;
 };
-
-inline std::vector<node>
-TopHarmonicCloseness::topkNodesList(bool includeTrail) {
-  assureFinished();
-  if (!includeTrail) {
-    auto begin = topk.begin();
-    std::vector<node> topkNoTrail(begin, begin + k);
-    return topkNoTrail;
-  }
-
-  return topk;
-}
-
-inline std::vector<edgeweight>
-TopHarmonicCloseness::topkScoresList(bool includeTrail) {
-  assureFinished();
-  if (!includeTrail) {
-    auto begin = topkScores.begin();
-    std::vector<double> topkScoresNoTrail(begin, begin + k);
-    return topkScoresNoTrail;
-  }
-  return topkScores;
-}
 
 } // namespace NetworKit
 #endif // NETWORKIT_CENTRALITY_TOP_HARMONIC_CLOSENESS_HPP_
