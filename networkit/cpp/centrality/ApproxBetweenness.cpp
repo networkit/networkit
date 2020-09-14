dba868bceec545c1e8d4762192afdaf6897cd8aa
Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>
2020-09-14 17:25:14 +0200

    ApproxBetweenness atomic aggregation.
    Remove scorePerThread vector and aggregate scoreData using an omp atomic
    operation as suggested in #532.

--- networkit/cpp/centrality/ApproxBetweenness.cpp
+++ networkit/cpp/centrality/ApproxBetweenness.cpp
@@ -47,69 +47,54 @@ void ApproxBetweenness::run() {
     r = ceil((universalConstant / (epsilon * epsilon)) * (floor(log2(vd - 2)) + 1 - log(delta)));
 
     INFO("taking ", r, " path samples");
-    // parallelization:
-    count maxThreads = omp_get_max_threads();
-    DEBUG("max threads: ", maxThreads);
-    std::vector<std::vector<double> > scorePerThread(maxThreads, std::vector<double>(G.upperNodeIdBound()));
-    DEBUG("score per thread size: ", scorePerThread.size());
     handler.assureRunning();
-    #pragma omp parallel for
-    for (omp_index i = 1; i <= static_cast<omp_index>(r); i++) {
-        count thread = omp_get_thread_num();
-        DEBUG("sample ", i);
-        // if (i >= 1000) throw std::runtime_error("too many iterations");
-        // DEBUG
-        // sample random node pair
-        node u, v;
-        u = GraphTools::randomNode(G);
-        do {
-            v = GraphTools::randomNode(G);
-        } while (v == u);
+    #pragma omp parallel
+    {
+        auto ssspPtr = G.isWeighted() ? std::unique_ptr<SSSP>(new Dijkstra(G, 0, true, false))
+                                      : std::unique_ptr<SSSP>(new BFS(G, 0, true, false));
 
-        // runs faster for unweighted graphs
-        std::unique_ptr<SSSP> sssp;
-        if (G.isWeighted()) {
-            sssp.reset(new Dijkstra(G, u, true, false, v));
-        } else {
-            sssp.reset(new BFS(G, u, true, false, v));
-        }
-        DEBUG("running shortest path algorithm for node ", u);
-        if (!handler.isRunning()) continue;
-        sssp->run();
-        if (!handler.isRunning()) continue;
-        if (sssp->numberOfPaths(v) > 0) { // at least one path between {u, v} exists
-            DEBUG("updating estimate for path ", u, " <-> ", v);
-            // random path sampling and estimation update
-            // node s = v;
-            node t = v;
-            while (t != u)  {
-                // sample z in P_u(t) with probability sigma_uz / sigma_us
-                std::vector<std::pair<node, double> > choices;
-                for (node z : sssp->getPredecessors(t)) {
-                    bigfloat tmp = sssp->numberOfPaths(z) / sssp->numberOfPaths(t);
-                    double weight;
-                    tmp.ToDouble(weight);
-                    choices.emplace_back(z, weight); 	// sigma_uz / sigma_us
+#pragma omp for
+        for (omp_index i = 1; i <= static_cast<omp_index>(r); i++) {
+            DEBUG("sample ", i);
+            // sample random node pair
+            node u, v;
+            u = GraphTools::randomNode(G);
+            do {
+                v = GraphTools::randomNode(G);
+            } while (v == u);
+
+            auto &sssp = *ssspPtr;
+            sssp.setSource(u);
+            sssp.setTarget(v);
+
+            DEBUG("running shortest path algorithm for node ", u);
+            if (!handler.isRunning()) continue;
+            sssp.run();
+            if (!handler.isRunning()) continue;
+            if (sssp.numberOfPaths(v) > 0) { // at least one path between {u, v} exists
+                DEBUG("updating estimate for path ", u, " <-> ", v);
+                // random path sampling and estimation update
+                node t = v;
+                while (t != u)  {
+                    // sample z in P_u(t) with probability sigma_uz / sigma_us
+                    std::vector<std::pair<node, double> > choices;
+                    for (node z : sssp.getPredecessors(t)) {
+                        bigfloat tmp = sssp.numberOfPaths(z) / sssp.numberOfPaths(t);
+                        double weight;
+                        tmp.ToDouble(weight);
+                        choices.emplace_back(z, weight); 	// sigma_uz / sigma_us
+                    }
+                    node z = Aux::Random::weightedChoice(choices);
+                    assert (z <= G.upperNodeIdBound());
+                    if (z != u)
+#pragma omp atomic
+                        scoreData[z] += 1. / static_cast<double>(r);
+                    t = z;
                 }
-                node z = Aux::Random::weightedChoice(choices);
-                assert (z <= G.upperNodeIdBound());
-                if (z != u) {
-                    scorePerThread[thread][z] += 1 / (double) r;
-                }
-                // s = t;
-                t = z;
             }
         }
     }
     handler.assureRunning();
-
-    INFO("adding thread-local scores");
-    // add up all thread-local values
-    for (auto &local : scorePerThread) {
-        G.parallelForNodes([&](node v){
-            scoreData[v] += local[v];
-        });
-    }
 
     hasRun = true;
 }
