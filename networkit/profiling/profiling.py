b36589dc602b972d4ee7ad50dd7abf7dc0e3b260
Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>
2020-10-28 16:08:30 +0100

    Profiling: comment broken features, fix EstimateBetweenness

--- networkit/profiling/profiling.py
+++ networkit/profiling/profiling.py
@@ -641,10 +641,10 @@ class Profile:
 									value = self.__correlations[category][keyA][keyB]
 								except:
 									value = self.__correlations[category][keyB][keyA]
-								if outputType == "HTML":
-									result += "<div class=\"Thumbnail_ScatterPlot\" data-title=\"" + keyB + "\" data-title-second=\"" + keyA + "\"><img src=\"data:image/svg+xml;utf8," + value["image"] + "\" /></div>"
-								elif outputType == "LaTeX":
-									result += "\n\\includegraphics[width=0.5\\textwidth]{{\"" + value["image"] + "\"}.pdf}"
+#								if outputType == "HTML":
+#									result += "<div class=\"Thumbnail_ScatterPlot\" data-title=\"" + keyB + "\" data-title-second=\"" + keyA + "\"><img src=\"data:image/svg+xml;utf8," + value["image"] + "\" /></div>"
+#								elif outputType == "LaTeX":
+#									result += "\n\\includegraphics[width=0.5\\textwidth]{{\"" + value["image"] + "\"}.pdf}"
 				return result
 			results[category]["Correlations"]["ScatterPlots"] += funcScatterPlot(category)
 
