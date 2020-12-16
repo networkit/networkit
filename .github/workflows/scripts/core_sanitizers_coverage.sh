07140d45bf72134dae00024e71395f3399031bbe
Fabian Brandt-Tumescheit <fabian.brandt-tumescheit@informatik.hu-berlin.de>
2020-12-16 10:39:51 +0100

    Fixes publishing of doc- and coverage-build. Temp. removes gcc-10 tests for macOS 11.0

--- .github/workflows/scripts/core_sanitizers_coverage.sh
+++ .github/workflows/scripts/core_sanitizers_coverage.sh
@@ -5,7 +5,8 @@ pip3 install --upgrade pip
 pip3 install setuptools cpp-coveralls
 
 mkdir build && cd "$_"
-cmake -GNinja -DNETWORKIT_BUILD_TESTS=ON -DCMAKE_BUILD_TYPE=Debug -DNETWORKIT_WITH_SANITIZERS=leak -DNETWORKIT_COVERAGE=ON ..
-ninja
+export CPU_COUNT=$(python3 $GITHUB_WORKSPACE/.github/workflows/scripts/get_core_count.py)
+cmake -DNETWORKIT_BUILD_TESTS=ON -DCMAKE_BUILD_TYPE=Debug -DNETWORKIT_WITH_SANITIZERS=leak -DNETWORKIT_COVERAGE=ON ..
+make -j$CPU_COUNT
 
 ctest -V
