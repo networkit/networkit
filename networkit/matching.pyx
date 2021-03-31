cf7ea624aea31a51f4f9b4944dad8a3add639106
peterlqa <peterlqa@hu-berlin.de>
2021-03-31 13:36:06 +0200

    Docs update for Matching.

--- networkit/matching.pyx
+++ networkit/matching.pyx
@@ -20,39 +20,144 @@ cdef class Matching:
 		return self
 
 	def match(self, node u, node v):
+		"""
+		Set two nodes u and v as each others matching partners.
+		
+		Parameters:
+		-----------
+		u : node
+		v : node
+		"""
 		self._this.match(u,v)
 
 	def unmatch(self, node u,  node v):
+		"""
+		Reset the two nodes u and v to unmatched.
+		
+		Parameters:
+		-----------
+		u : node
+		v : node
+		"""
 		self._this.unmatch(u, v)
 
 	def isMatched(self, node u):
+		"""
+		Check if node u is matched.
+		
+		Parameters:
+		-----------
+		u : node
+		
+		Returns:
+		-----------
+		bool
+			true if u has a matching partner.
+		"""
 		return self._this.isMatched(u)
 
 	def areMatched(self, node u, node v):
+		"""
+		Check if the nodes u and v are matched to each other.
+		
+		Parameters:
+		-----------
+		u : node
+		v : node
+		
+		Returns:
+		-----------
+		bool
+			true if node u and v are matched to each other.
+		"""
 		return self._this.areMatched(u,v)
 
 	def isProper(self, Graph G):
+		"""
+		Check whether this is a proper matching in the graph, i.e. there is an edge between
+		each pair and if the matching partner of v is u then the matching partner of u is v.
+		
+		Parameters:
+		-----------
+		G : networkit.Graph
+			Graph to be checked.
+		Returns:
+		-----------
+		bool
+			true if this is a proper matching.
+		"""
 		return self._this.isProper(G._this)
 
 	def size(self, Graph G):
+		"""
+		Get the number of edges in this matching.
+		
+		Parameters:
+		-----------
+		G : networkit.Graph
+		
+		Returns:
+		-----------
+		int
+			total number of edges in the matching.
+		"""
 		return self._this.size(G._this)
 
 	def mate(self, node v):
+		"""
+		Get the matched neighbor of v if it exists, otherwise +inf.
+		
+		Parameters:
+		-----------
+		v : node
+		
+		Returns:
+		-----------
+		int
+			matching partner of v if it exists, otherwise +inf.
+		"""
 		return self._this.mate(v)
 
 	def weight(self, Graph G):
+		"""
+		Get total weight of edges in this matching.
+		
+		Parameters:
+		-----------
+		G : networkit.Graph
+			The corresponding graph.
+		
+		Returns:
+		-----------
+		float
+			Total weight of edges in this matching.
+		"""
 		return self._this.weight(G._this)
 
 	def toPartition(self, Graph G):
+		"""
+		Convert matching to a Partition object where matched nodes belong to the same subset
+		and unmatched nodes belong to a singleton subset.
+		
+		Parameters:
+		-----------
+		G : networkit.Graph
+			The corresponding graph.
+			
+		Returns:
+		-----------
+		networkit.Partition
+			
+		"""
 		return Partition().setThis(self._this.toPartition(G._this))
 
 	def getVector(self):
-		""" Get the vector storing the data
+		""" Get the vector storing the data.
 
 		Returns:
 		--------
-		vector
-			Vector indexed by node id to node id of mate or none if unmatched
+		list
+			Vector indexed by node id containing the node id of mate or networkit.none if unmatched
 		"""
 		return self._this.getVector()
 
@@ -71,11 +176,12 @@ cdef class Matcher(Algorithm):
 			raise RuntimeError("Instantiation of abstract base class")
 
 	def getMatching(self):
-		"""  Returns the matching.
+		""" 
+		Returns the matching.
 
 		Returns:
 		--------
-		Matching
+		networkit.Matching
 		"""
 		if self._this == NULL:
 			raise RuntimeError("Error, object not properly initialized")
@@ -92,10 +198,17 @@ cdef class PathGrowingMatcher(Matcher):
 	"""
 	Path growing matching algorithm as described by  Hougardy and Drake.
 	Computes an approximate maximum weight matching with guarantee 1/2.
+	
+	Parameters:
+	-----------
+	G : networkit.Graph
+		Graph to apply matching.
+	edgeScores : list, optional
+		list with edgeScores.
 	"""
 	def __cinit__(self, Graph G not None, edgeScores=None):
 		self.G = G
-		if edgeScores:
+		if edgeScores is not None:
 			self._this = new _PathGrowingMatcher(G._this, edgeScores)
 		else:
 			self._this = new _PathGrowingMatcher(G._this)
