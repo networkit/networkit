88bbacd76ce728decf9f1071b0202178334b38e7
Michael Hamann <michael.hamann@kit.edu>
2021-09-09 11:38:55 +0200

    LFM: Add Python test case

--- networkit/test/test_community.py
+++ networkit/test/test_community.py
@@ -28,5 +28,15 @@ class Test_CommunityDetection(unittest.TestCase):
 		hierarchy = community.CutClustering.getClusterHierarchy(jazz)
 		self.assertEqual(3, len(hierarchy))
 
+
+	def test_LFM(self):
+		local = scd.LFMLocal(self.G)
+		lfm = community.LFM(self.G, local)
+		lfm.run()
+		cover = lfm.getCover()
+		for u in self.G.iterNodes():
+			self.assertGreaterEqual(len(cover.subsetsOf(u)), 1)
+
+
 if __name__ == "__main__":
 	unittest.main()
