c3f0d8a8d0dbf8f56d21112ba8c4ea4aa7fdba78
Jonas Charfreitag <jcharfreitag@gmail.com>
2023-02-22 11:52:05 +0100

    first version of fixed directed edmondskarp

--- networkit/cpp/flow/EdmondsKarp.cpp
+++ networkit/cpp/flow/EdmondsKarp.cpp
@@ -18,7 +18,8 @@ namespace NetworKit {
 EdmondsKarp::EdmondsKarp(const Graph &graph, node source, node sink)
     : graph(&graph), source(source), sink(sink) {}
 
-edgeweight EdmondsKarp::BFS(std::vector<edgeweight> &residFlow, std::vector<node> &pred) const {
+edgeweight EdmondsKarp::BFS(const std::vector<edgeweight> &residFlow,
+                            std::vector<node> &pred) const {
     std::fill(pred.begin(), pred.end(), none);
     pred.resize(graph->upperNodeIdBound(), none);
     std::vector<edgeweight> gain(graph->upperNodeIdBound(), 0);
@@ -55,16 +56,121 @@ edgeweight EdmondsKarp::BFS(std::vector<edgeweight> &residFlow, std::vector<node
     return 0.0;
 }
 
+edgeweight EdmondsKarp::directedBFS(const std::vector<count> &reverseEdges,
+                                    std::vector<node> &pred) const {
+    std::fill(pred.begin(), pred.end(), none);
+    pred.resize(graph->upperNodeIdBound(), none);
+    std::vector<edgeweight> gain(graph->upperNodeIdBound(), std::numeric_limits<edgeweight>::max());
+
+    std::queue<node> Q;
+    Q.push(source);
+    pred[source] = source;
+    gain[source] = std::numeric_limits<edgeweight>::max();;
+
+    while (!Q.empty()) {
+        node u = Q.front();
+        Q.pop();
+        bool sinkReached = false;
+
+        graph->forNeighborsOf(u, [&](node, node v, edgeweight weight, edgeid eid) {
+            if (!sinkReached && pred[v] == none && flow[eid] < weight) {
+                pred[v] = u;
+                if (v != sink && !sinkReached) {
+                    Q.push(v);
+                } else {
+                    sinkReached = true;
+                }
+
+                auto residCapa =
+                    weight - flow[eid] + (reverseEdges[eid] == none ? 0 : flow[reverseEdges[eid]]);
+                gain[v] = std::min(gain[u], residCapa);
+            }
+        });
+
+        if (sinkReached) {
+            return gain[sink];
+        }
+
+        graph->forInNeighborsOf(u, [&](node, node v, edgeweight weight, edgeid eid) {
+            if (!sinkReached && (reverseEdges[eid] == none) && (flow[eid] > 0)
+                && (pred[v] == none)) {
+                pred[v] = u;
+                gain[v] = std::min(gain[u], flow[eid]);
+
+                if (v != sink && !sinkReached) {
+                    Q.push(v);
+                } else {
+                    sinkReached = true;
+                }
+            }
+        });
+
+        if (sinkReached) {
+            return gain[sink];
+        }
+    }
+
+    return 0.0;
+}
+
 void EdmondsKarp::run() {
     if (!graph->hasEdgeIds()) {
         throw std::runtime_error("edges have not been indexed - call indexEdges first");
     }
     flow.clear();
     flow.resize(graph->upperEdgeIdBound(), 0.0);
+    flowValue = 0;
 
+    if (graph->isDirected()) {
+        runDirected();
+    } else {
+        runUndirected();
+    }
+
+    hasRun = true;
+}
+
+void EdmondsKarp::runDirected() {
+    std::vector<count> reverseEdges(graph->upperEdgeIdBound(), none);
+    graph->parallelForEdges([&](node u, node v, edgeid eid) {
+        reverseEdges[eid] = graph->hasEdge(v, u) ? graph->edgeId(v, u) : none;
+    });
+    std::vector<node> pred;
+    edgeweight gain = directedBFS(reverseEdges, pred);
+
+    while (gain > 0) {
+        flowValue += gain;
+        node v = sink;
+        while (v != source) {
+            node u = pred[v];
+
+            if (graph->hasEdge(u, v)) {
+                edgeid eid = graph->edgeId(u, v);
+                unsigned long reverseId = reverseEdges[eid];
+                if (reverseId != none) {
+                    auto reverseFlow = flow[reverseId];
+                    assert(!((reverseFlow > 0) && (flow[eid] > 0)));
+                    flow[reverseId] = std::max(reverseFlow - gain, 0.);
+                    flow[eid] += gain - reverseFlow;
+                } else {
+                    flow[eid] += gain;
+                    assert(flow[eid] <= graph->weight(u, v));
+                }
+            } else {
+                edgeid eid = graph->edgeId(v, u);
+                flow[eid] -= gain;
+                assert(flow[eid] >= 0);
+            }
+
+            v = u;
+        }
+        gain = directedBFS(reverseEdges, pred);
+    }
+}
+
+void EdmondsKarp::runUndirected() {
     std::vector<edgeweight> residFlow(graph->upperEdgeIdBound(), 0.0);
 
-    flowValue = 0;
     do {
         std::vector<node> pred;
         edgeweight gain = BFS(residFlow, pred);
@@ -89,8 +195,6 @@ void EdmondsKarp::run() {
 
     graph->parallelForEdges(
         [&](node, node, edgeid eid) { flow[eid] = std::max(flow[eid], residFlow[eid]); });
-
-    hasRun = true;
 }
 
 edgeweight EdmondsKarp::getMaxFlow() const {
