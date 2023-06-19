b69996ff9854ac8e1cd0bc1d717fd2275cda6e2d
petholza <angus.petholz@web.de>
2023-06-19 14:31:57 +0200

    python test: adds tests in test_matching.py

--- networkit/test/test_matching.py
+++ networkit/test/test_matching.py
@@ -28,6 +28,18 @@ class TestMatching(unittest.TestCase):
 		runAlgo(self.g)
 		runAlgo(self.gw)
 
+	def testPathGrowingMatcherEdgeScores(self):
+		G = nk.Graph(5)
+		G.addEdge(0,1)
+		G.addEdge(2,1)
+		G.addEdge(3,1)
+		G.addEdge(2,4)
+		G.indexEdges()
+		edgeScores = [0.5, 1.5, 1.0, 2.0]
+		pgmes = nk.matching.PathGrowingMatcher(G, edgeScores)
+		pgmes.run()
+		self.assertEqual(len(pgmes.getMatching().getVector()), 5)
+	
 	def testSuitorMatcher(self):
 
 		def doTest(g):
@@ -44,6 +56,29 @@ class TestMatching(unittest.TestCase):
 
 		doTest(self.g)
 		doTest(self.gw)
+
+	def testMatching(self):
+		#test all functions of abstract Matching class 
+		m1 = nk.matching.Matching(4)
+		G = nk.Graph(4)
+		G.addEdge(0,1) # G: 0 -- 1
+		G.addEdge(0,2) #	|
+		G.addEdge(2,3) #	2 -- 3
+		m1.match(0,1)
+		m1.match(2,3)
+		self.assertTrue(m1.isProper(G))
+		self.assertTrue(m1.areMatched(0,1))
+		self.assertTrue(m1.isMatched(0))
+		self.assertTrue(m1.isMatched(1))
+		self.assertEqual(m1.size(G), 2)
+		self.assertEqual(m1.mate(1), 0)
+		self.assertEqual(m1.weight(G), 2.0)
+		G2=nk.Graph(2)#size of matching
+		Par=m1.toPartition(G2)
+		self.assertEqual(len(Par), 2)
+		self.assertListEqual(m1.getVector(), [1, 0, 3, 2])
+		m1.unmatch(0,1)
+		self.assertFalse(m1.areMatched(0,1))	
 
 if __name__ == "__main__":
 	unittest.main()
