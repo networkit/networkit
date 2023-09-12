454bce5de50c673a109853efde1c552fe6e1f373
Frieda Gerharz <frieda.gerharz@informatik.hu-berlin.de>
2023-09-12 16:29:19 +0200

    change suitor/proposed structure

--- networkit/cpp/matching/BSuitorMatcher.cpp
+++ networkit/cpp/matching/BSuitorMatcher.cpp
@@ -1,10 +1,10 @@
 #include <networkit/matching/BSuitorMatcher.hpp>
 
 #include <algorithm>
+#include <iomanip>
 #include <stdexcept>
 
 namespace NetworKit {
-
 BSuitorMatcher::BSuitorMatcher(const Graph &G, const std::vector<count> &b) : BMatcher(G, b), b(b) {
     if (G.numberOfSelfLoops() > 0)
         throw std::runtime_error("This algorithm does not support graphs with self-loops.");
@@ -25,48 +25,47 @@ BSuitorMatcher::BSuitorMatcher(const Graph &G, const std::string &path)
 
 void BSuitorMatcher::findSuitors(node u) {
     for (count i = 0; i < b.at(u); i++) {
-        auto x = findPreferred(u);
-        if (x != none)
-            makeSuitor(u, x);
+        auto [x, w] = findPreferred(u);
+        if (x != none) {
+            makeSuitor(u, w, x);
+        }
     }
 }
 
-node BSuitorMatcher::findPreferred(node y) {
-    node x = none;
-    edgeweight heaviest = 0;
+Node BSuitorMatcher::findPreferred(node y) {
+    Node best = Node{none, 0};
 
     auto hasProposedTo = [&](node n) -> bool {
-        return (std::find(proposed[y].begin(), proposed[y].end(), n) != proposed[y].end());
+        return std::any_of(T.at(y)->list.begin(), T.at(y)->list.end(),
+                           [n](const Node &p) { return p.id == n; });
     };
 
     for (auto v : G->weightNeighborRange(y)) {
-        if (!hasProposedTo(v.first)) {
-            const edgeweight weight = v.second;
-            if (weight > heaviest || (weight == heaviest && v.first < x)) {
-                if (weight > G->weight(v.first, suitors[v.first].back())
-                    || (weight == G->weight(v.first, suitors[v.first].back())
-                        && y < suitors[v.first].back())) {
-                    x = v.first;
-                    heaviest = weight;
+        const Node n = Node(v.first, v.second);
+        if (!hasProposedTo(n.id)) {
+            if (n.weight > best.weight || (n.weight == best.weight && n.id < best.id)) {
+                const auto n_suitor_weight = S.at(n.id)->min.weight;
+
+                if (n.weight > n_suitor_weight
+                    || (n.weight == n_suitor_weight && y < S.at(n.id)->min.id)) {
+                    best = n;
                 }
             }
         }
     }
-    return x;
+    return best;
 }
 
-void BSuitorMatcher::makeSuitor(node u, node x) {
-    auto y = suitors[x].back();
-    sortInsert(suitors[x], x, u);
-    auto i = findFirstFreeIndex(proposed[u]);
-    proposed[u][i] = x;
+void BSuitorMatcher::makeSuitor(node u, edgeweight w, node x) {
+    auto y = S.at(x)->popMinIfFull();
+    S.at(x)->insert(Node(u, w));
+    T.at(u)->insert(Node(x, w));
 
     if (y != none) {
-        sortRemove(proposed[y], x);
-        auto z = findPreferred(y);
-
+        T.at(y)->remove(Node(x, w));
+        auto [z, w] = findPreferred(y);
         if (z != none) {
-            makeSuitor(y, z);
+            makeSuitor(y, w, z);
         }
     }
 }
@@ -103,60 +102,63 @@ std::vector<count> BSuitorMatcher::readBValuesFromFile(count size, const std::st
     return b;
 }
 
-void BSuitorMatcher::sortInsert(std::vector<node> &nodes, node u, node v) {
-    auto i = std::find_if(nodes.begin(), nodes.end(), [&](const node &y) {
-        return (G->weight(u, v) > G->weight(u, y))
-               || ((G->weight(u, v) == G->weight(u, y)) && (v < y));
-    });
-    nodes.insert(i, v);
-    nodes.pop_back();
-}
-
-void BSuitorMatcher::sortRemove(std::vector<node> &nodes, const node u) {
-    nodes.erase(std::remove(nodes.begin(), nodes.end(), u), nodes.end());
-    nodes.push_back(none);
-}
-
-index BSuitorMatcher::findFirstFreeIndex(const std::vector<node> &nodes) const {
-    return findIndexOf(nodes, none);
-}
-
-index BSuitorMatcher::findIndexOf(const std::vector<node> &nodes, node x) const {
-    auto i = std::find(nodes.begin(), nodes.end(), x);
-    return i == nodes.end() ? none : i - nodes.begin();
-}
-
-void BSuitorMatcher::checkSymmetry() const {
+bool BSuitorMatcher::isSymmetrical() const {
+    bool sym;
     auto areMatchedSymmetrical = [&](node u, node v) -> void {
-        [[maybe_unused]] auto i_1 =
-            std::find(suitors[u].begin(), suitors[u].end(), v) != suitors[u].end();
-        [[maybe_unused]] auto i_2 =
-            std::find(suitors[v].begin(), suitors[v].end(), u) != suitors[v].end();
-        assert(i_1 == i_2);
+        auto i_1 = std::find_if(S.at(u)->list.begin(), S.at(u)->list.end(),
+                                [v](const Node &p) { return p.id == v; })
+                   != S.at(u)->list.end();
+        auto i_2 = std::find_if(S.at(v)->list.begin(), S.at(v)->list.end(),
+                                [u](const Node &p) { return p.id == u; })
+                   != S.at(v)->list.end();
+        sym = (i_1 == i_2);
     };
 
     G->forNodes([&](node u) { G->forNodes([&](node v) { areMatchedSymmetrical(u, v); }); });
+    return sym;
+}
+
+edgeweight BSuitorMatcher::getWeight() const {
+    edgeweight w = 0.0;
+
+#pragma omp parallel for reduction(+ : w)
+    for (auto s : S) {
+        w += s->weight;
+    }
+    return w;
 }
 
 void BSuitorMatcher::run() {
     const auto n = G->upperNodeIdBound();
+    S.reserve(n);
+    T.reserve(n);
     for (index i = 0; i < n; i++) {
-        auto v = std::vector<node>(b.at(i), none);
-        suitors.emplace_back(v);
-        proposed.emplace_back(v);
+        Suitors *s = new Suitors(b.at(i));
+        Proposed *p = new Proposed(b.at(i));
+        S.emplace_back(s);
+        T.emplace_back(p);
     }
-
     G->forNodes([&](node u) { findSuitors(u); });
-    checkSymmetry();
 
     // TODO make parallel
-    G->forNodes([&suitors = suitors, &M = M](node u) {
-        for (auto p : suitors[u]) {
-            if (p != none && u < p) { // Ensure we match a pair of nodes only once
-                M.match(u, p);
+    G->forNodes([&S = S, &M = M](node u) {
+        for (auto v : S.at(u)->list) {
+            if (v.id != none && u < v.id) { // Ensure we match a pair of nodes only once
+                M.match(u, v.id);
             }
         }
     });
+
+    // for (auto s : S) {
+    //     s->sort();
+    // }
+    // assert(isSymmetrical());
+
+    for (auto s : S)
+        delete s;
+    for (auto t : T)
+        delete t;
+
     hasRun = true;
 }
 } // namespace NetworKit
