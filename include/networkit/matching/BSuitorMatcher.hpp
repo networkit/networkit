3d45e1e046bbb10cdfb03cc3c4aa26a1987e769b
Frieda Gerharz <frieda.gerharz@informatik.hu-berlin.de>
2023-09-12 16:29:19 +0200

    change suitor/proposed structure

--- include/networkit/matching/BSuitorMatcher.hpp
+++ include/networkit/matching/BSuitorMatcher.hpp
@@ -1,10 +1,101 @@
 #ifndef NETWORKIT_MATCHING_B_SUITOR_MATCHER_HPP_
 #define NETWORKIT_MATCHING_B_SUITOR_MATCHER_HPP_
 
+#include <algorithm>
 #include <networkit/graph/Graph.hpp>
 #include <networkit/matching/BMatcher.hpp>
 
 namespace NetworKit {
+
+struct Node {
+    node id;
+    edgeweight weight;
+
+    Node() = default;
+    Node(node n, edgeweight w) : id(n), weight(w) {}
+
+    bool operator==(const Node &other) const { return id == other.id && weight == other.weight; }
+    bool operator!=(const Node &other) const { return id != other.id || weight != other.weight; }
+};
+
+struct Data {
+    std::vector<Node> list;
+    Node min; // (none, 0) if list still has free capacity
+    edgeweight weight;
+    count size;
+    count max_size;
+
+    Data() = default;
+
+    Data(count b) {
+        list.reserve(b);
+        min = Node(none, 0);
+        weight = 0;
+        size = 0;
+        max_size = b;
+    }
+
+    node popMinIfFull() {
+        if (size < max_size) {
+            return none;
+        } else {
+            auto ret = min.id;
+            remove(min);
+            return ret;
+        }
+    }
+
+    void insert(const Node &v) {
+        assert(size < max_size);
+
+        list.emplace_back(v);
+        size += 1;
+        weight += v.weight;
+        if (size == max_size && !list.empty()) {
+            min = *std::min_element(list.begin(), list.end(), [](const Node &a, const Node &b) {
+                if (a.weight == b.weight) {
+                    return a.id > b.id;
+                }
+                return a.weight < b.weight;
+            });
+        }
+    }
+
+    void remove(const Node &u) {
+        list.erase(
+            std::remove_if(list.begin(), list.end(), [u](const Node &p) { return p.id == u.id; }),
+            list.end());
+        if (u.id == min.id) {
+            min = Node(none, 0);
+        }
+        size -= 1;
+        weight -= u.weight;
+    }
+
+    void sort() {
+        std::sort(list.begin(), list.end(), [](const Node &u, const Node &v) {
+            return (u.weight > v.weight || (u.weight == v.weight) && u.id < v.id);
+        });
+    }
+};
+
+struct Suitors : public Data {
+    Suitors(count b) : Data(b) {}
+
+    edgeweight getWeight() {
+        edgeweight w = 0.0;
+
+#pragma omp parallel for reduction(+ : w)
+        for (auto e : list) {
+            w += e.weight;
+        }
+        return w;
+    }
+};
+
+struct Proposed : public Data {
+    Proposed(count b) : Data(b) {}
+};
 
 /**
  * @ingroup matching
@@ -59,43 +150,32 @@ public:
      * smaller than w(u, v) to break ties.
      *
      * @param y
-     * @return node
+     * @return Node
      */
-    node findPreferred(node y);
+    Node findPreferred(node y);
 
     /**
      * Makes @a x a suitor of @a u and recursively calls itself for previous worse
      * suitors of @a u that got replaced with their new best match.
      *
      * @param u
+     * @param w
      * @param x
      */
-    void makeSuitor(node u, node x);
+    void makeSuitor(node u, edgeweight w, node x);
 
     /**
-     * Inserts @a v into the sorted vector @a nodes[u] based on node weights. If there are
-     * ties, the nodes are placed in lexicographic order. The size of @a nodes remains because the
-     * last element gets removed.
+     * Checks the symmetry of pairs of nodes. It must hold that v is in suitors(u) iff u is
+     * in suitors(v).
      *
-     * @param nodes
-     * @param u
-     * @param v
      */
-    void sortInsert(std::vector<node> &nodes, node u, node v);
+    bool isSymmetrical() const;
 
-    /**
-     * Removes @a u from @a nodes while maintaining its size. Subsequent nodes are shifted
-     * and @c none is added to the end of @a nodes.
-     *
-     * @param nodes
-     * @param u
-     * @param v
-     */
-    void sortRemove(std::vector<node> &nodes, node u);
+    edgeweight getWeight() const;
 
 private:
-    std::vector<std::vector<node>> suitors;
-    std::vector<std::vector<node>> proposed;
+    std::vector<Suitors *> S;
+    std::vector<Proposed *> T;
     const std::vector<count> b;
 
     /**
@@ -106,32 +186,6 @@ private:
      * @return std::vector<count>
      */
     std::vector<count> readBValuesFromFile(count size, const std::string &path) const;
-
-    /**
-     * Finds the index of the first @c none value in the list of @a nodes if present, otherwise @c
-     * none.
-     *
-     * @param nodes
-     * @param u
-     * @return index
-     */
-    index findFirstFreeIndex(const std::vector<node> &nodes) const;
-
-    /**
-     * Finds the index of node @a x in the list of @a nodes if present, otherwise @c none.
-     *
-     * @param nodes
-     * @param x
-     * @return index
-     */
-    index findIndexOf(const std::vector<node> &nodes, node x) const;
-
-    /**
-     * Checks the symmetry of pairs of nodes. It must hold that v is in suitors(u) iff u is
-     * in suitors(v).
-     *
-     */
-    void checkSymmetry() const;
 };
 } // namespace NetworKit
 
