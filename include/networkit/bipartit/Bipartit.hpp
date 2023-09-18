3f7e26a374aa83ef6110b7a6e13db379448f8d83
michaelk <s6mikaib@uni-bonn.de>
2023-09-18 10:28:38 +0200

    Added documentation comments to Bipartit

--- include/networkit/bipartit/Bipartit.hpp
+++ include/networkit/bipartit/Bipartit.hpp
@@ -16,14 +16,37 @@ namespace NetworKit {
 
 class Bipartit : public Algorithm {
 public:
+    /**
+     * Creates Bipartit class for graph @G
+     *
+     * @param G the graph
+     */
     Bipartit(const Graph &G);
 
+    /**
+     * Calculates if graph @G is bipartit. If yes generates bipartition, if no finds odd cycle
+     */
     void run() override;
 
+    /**
+     * Check if the graph is bipartit
+     *
+     * @return true iff the graph is bipartit
+     */
     bool isBipartit();
 
+    /**
+     * Get a bipartition iff the graph is bipartit
+     *
+     * @return a bipartition iff the graph is bipartit, throws error otherwise
+     */
     const Partition &getPartition();
 
+    /**
+     * Get an odd cycle iff one exists in the graph
+     *
+     * @return on odd cycle iff the graph has one, throws error otherwise
+     */
     const std::vector<node> &getOddCircle();
 
 protected:
