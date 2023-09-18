43f7767931bde8f66d8d25428884590df7c489d8
michaelk <s6mikaib@uni-bonn.de>
2023-09-18 10:12:25 +0200

    Added larger tests and algorithm now provides odd circle for non-bipartit graphs

--- networkit/cpp/bipartit/test/Bipartit_GTest.cpp
+++ networkit/cpp/bipartit/test/Bipartit_GTest.cpp
@@ -7,6 +7,8 @@
 
 #include <gtest/gtest.h>
 
+#include <random>
+
 #include "networkit/bipartit/Bipartit.hpp"
 #include "networkit/graph/Graph.hpp"
 #include "networkit/generators/ErdosRenyiGenerator.hpp"
@@ -15,7 +17,7 @@ namespace NetworKit {
 
 class BipartitGTest : public testing::Test {};
 
-bool bipartitPartition(const Graph &G, const Partition &partition) {
+bool isBipartitPartition(const Graph &G, const Partition &partition) {
     for (node v : G.nodeRange()) {
         if (partition[v] == none)
             return false;
@@ -30,6 +32,21 @@ bool bipartitPartition(const Graph &G, const Partition &partition) {
                 return false;
         }
     }
+
+    return true;
+}
+
+bool isOddCircle(const Graph &G, const std::vector<node> &path) {
+    if (path.size() % 2 == 0)
+        return false;
+    for (count i = 0; i < path.size(); i++)
+        for (count j = i+1; j < path.size(); j++)
+            if (path[i] == path[j])
+                throw std::runtime_error("Not a path");
+
+    for (count i = 0; i < path.size(); i++)
+        if (not G.hasEdge(path[i], path[(i + 1) % path.size()]))
+            return false;
 
     return true;
 }
@@ -51,7 +68,9 @@ TEST_F(BipartitGTest, testBipartitTinyBipartit) {
 
     EXPECT_TRUE(bipartit.isBipartit());
 
-    EXPECT_TRUE(bipartitPartition(G, bipartit.getPartition()));
+    EXPECT_TRUE(isBipartitPartition(G, bipartit.getPartition()));
+
+    EXPECT_ANY_THROW(bipartit.getOddCircle());
 }
 
 TEST_F(BipartitGTest, testBipartitTinyOddCircle) {
@@ -71,6 +90,8 @@ TEST_F(BipartitGTest, testBipartitTinyOddCircle) {
     EXPECT_FALSE(bipartit.isBipartit());
 
     EXPECT_ANY_THROW(bipartit.getPartition());
+
+    EXPECT_TRUE(isOddCircle(G, bipartit.getOddCircle()));
 }
 
 TEST_F(BipartitGTest, testBipartitLargeRandom) {
@@ -81,8 +102,48 @@ TEST_F(BipartitGTest, testBipartitLargeRandom) {
     bipartit.run();
 
     if (bipartit.isBipartit()) {
-        EXPECT_TRUE(bipartitPartition(G, bipartit.getPartition()));
+        EXPECT_TRUE(isBipartitPartition(G, bipartit.getPartition()));
+
+        EXPECT_ANY_THROW(bipartit.getOddCircle());
     }
+    else {
+        EXPECT_ANY_THROW(bipartit.getPartition());
+
+        EXPECT_TRUE(isOddCircle(G, bipartit.getOddCircle()));
+    }
+}
+
+//Since a random ErdosRenyi-Graph almost certainly has an odd circle we also do a test with an explicitly bipartit graph
+TEST_F(BipartitGTest, testBipartitLargeRandomBipartit) {
+    node n = 200;
+
+    std::mt19937 rng(42);
+    std::uniform_int_distribution<node> dis(1000000);
+
+    std::vector<node> nodes(n);
+    for (count i = 0; i < n; i++)
+        nodes[i] = i;
+
+    for (count i = 0; i < n; i++)
+        std::swap(nodes[i], nodes[i + (dis(rng) % (n - i))]);
+
+    Graph G(n);
+
+    for (count i = 0; i < 5*n; i++) {
+        node v = nodes[dis(rng) % (n/2)], w = nodes[(n/2) + (dis(rng) % (n/2))];
+
+        if (not G.hasEdge(v, w))
+            G.addEdge(v, w);
+    }
+
+    Bipartit bipartit(G);
+    bipartit.run();
+
+    EXPECT_TRUE(bipartit.isBipartit());
+
+    EXPECT_TRUE(isBipartitPartition(G, bipartit.getPartition()));
+
+    EXPECT_ANY_THROW(bipartit.getOddCircle());
 }
 
 } // NetworKit
