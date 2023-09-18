b83bed09b89fdaf0bc04d7e01b6ed34cd714c1d3
michaelk <s6mikaib@uni-bonn.de>
2023-09-18 09:06:48 +0200

    Added algorithm to check for if a graph is bipartit

---
+++ include/networkit/bipartit/Bipartit.hpp
@@ -0,0 +1,36 @@
+/*
+ * Bipartit.hpp
+ *
+ * Created on: 18.09.2023
+ *     Author: Michael Kaibel
+ */
+
+#ifndef NETWORKIT_BIPARTIT_BIPARTIT_HPP
+#define NETWORKIT_BIPARTIT_BIPARTIT_HPP
+
+#include <networkit/graph/Graph.hpp>
+#include <networkit/base/Algorithm.hpp>
+#include <networkit/structures/Partition.hpp>
+
+namespace NetworKit {
+
+class Bipartit : public Algorithm {
+    Bipartit(const Graph &G);
+
+    void run() override;
+
+    bool isBipartit();
+
+    const Partition &getPartition();
+
+protected:
+    const Graph *G;
+
+    Partition partition;
+
+    bool bipartit = false;
+};
+
+} // NetworKit
+
+#endif // NETWORKIT_BIPARTIT_BIPARTIT_HPP
