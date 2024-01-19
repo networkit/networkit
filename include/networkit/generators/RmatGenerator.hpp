f5585caf898df1a7c49826420c2146ceeb909268
petholza <angus.petholz@web.de>
2024-01-19 12:49:27 +0100

    cpp: improves RmatGenerator and tests

--- include/networkit/generators/RmatGenerator.hpp
+++ include/networkit/generators/RmatGenerator.hpp
@@ -24,10 +24,19 @@ namespace NetworKit {
 class RmatGenerator final : public StaticGraphGenerator {
     count scale; ///< n = 2^scale
     count edgeFactor;
-    double a, b, c; ///< probabilities
     double defaultEdgeWeight;
     bool weighted;
     count reduceNodes;
+    bool discardSelfLoops;
+
+    // Data for the alias table:
+    std::vector<std::pair<uint32_t, uint32_t>> bits;
+    std::vector<uint8_t> numberOfBits;
+    std::vector<uint32_t> coinFlipProbability;
+    std::vector<uint32_t> coinFlipReplacement;
+    uint32_t mask;
+    std::pair<uint32_t, uint32_t> curBits{0, 0};
+    uint32_t remainingBits = 0;
 
 public:
     /**
@@ -39,14 +48,20 @@ public:
      * @param[in] d Probability for quadrant lower right
      * @param[in] weighted  result graph weighted?
      * @param[in] reduceNodes  number of random nodes to delete to achieve a given node count
+     * @param[in] discardSelfLoops ignore self loops
      */
     RmatGenerator(count scale, count edgeFactor, double a, double b, double c, double d,
-                  bool weighted = false, count reduceNodes = 0);
+                  bool weighted = false, count reduceNodes = 0, bool discardSelfLoops = true);
 
     /**
      * @return Graph to be generated according to parameters specified in constructor.
      */
     Graph generate() override;
+
+private:
+    void sample(std::mt19937_64 &urng);
+
+    std::pair<node, node> sampleEdge(std::mt19937_64 &urng, uint8_t bits);
 };
 
 } /* namespace NetworKit */
--- networkit/cpp/generators/RmatGenerator.cpp
+++ networkit/cpp/generators/RmatGenerator.cpp
@@ -3,6 +3,9 @@
  *
  *  Created on: 18.03.2014
  *      Author: Henning, cls
+ *
+ * Uses the algorithm described by Hübschle-Schneider and Sanders in
+ * "Linear Work Generation of R-MAT Graphs" https://arxiv.org/abs/1905.03525
  */
 
 #include <networkit/auxiliary/Log.hpp>
@@ -12,10 +15,66 @@
 
 namespace NetworKit {
 
+struct Entry {
+    uint32_t i;
+    uint32_t j;
+    uint32_t numberOfBits;
+    double probability;
+};
+
+bool priorityQueueCompare(const Entry &a, const Entry &b) {
+    if (a.numberOfBits == 32 || b.numberOfBits == 32) {
+        return a.numberOfBits > b.numberOfBits;
+    }
+    return a.probability < b.probability;
+}
+
+// std::priority_queue doesn't support iterating over its elements.
+struct PriorityQueue {
+    std::vector<Entry> &internal;
+
+    PriorityQueue(std::vector<Entry> &vec) : internal(vec) {}
+
+    void push(Entry &&entry) {
+        internal.push_back(entry);
+        std::push_heap(internal.begin(), internal.end(), priorityQueueCompare);
+    }
+
+    Entry pop() {
+        std::pop_heap(internal.begin(), internal.end(), priorityQueueCompare);
+        Entry result = internal.back();
+        internal.pop_back();
+        return result;
+    }
+};
+
+void generateEntries(std::vector<Entry> &entryList, count size, double a, double b, double c,
+                     double d) {
+    entryList.reserve(size);
+    PriorityQueue priorityQueue(entryList);
+    priorityQueue.push(Entry{0, 0, 1, a});
+    priorityQueue.push(Entry{0, 1, 1, b});
+    priorityQueue.push(Entry{1, 0, 1, c});
+    priorityQueue.push(Entry{1, 1, 1, d});
+    while (entryList.size() <= size - 3) {
+        // Take the entry with the highest probability and split it up.
+        // This increases the average number of bits compared to a uniform distribution.
+        Entry old = priorityQueue.pop();
+        priorityQueue.push(
+            Entry{old.i << 1 | 0, old.j << 1 | 0, old.numberOfBits + 1, a * old.probability});
+        priorityQueue.push(
+            Entry{old.i << 1 | 0, old.j << 1 | 1, old.numberOfBits + 1, b * old.probability});
+        priorityQueue.push(
+            Entry{old.i << 1 | 1, old.j << 1 | 0, old.numberOfBits + 1, c * old.probability});
+        priorityQueue.push(
+            Entry{old.i << 1 | 1, old.j << 1 | 1, old.numberOfBits + 1, d * old.probability});
+    }
+}
+
 RmatGenerator::RmatGenerator(count scale, count edgeFactor, double a, double b, double c, double d,
-                             bool weighted, count reduceNodes)
-    : scale(scale), edgeFactor(edgeFactor), a(a), b(b), c(c), weighted(weighted),
-      reduceNodes(reduceNodes) {
+                             bool weighted, count reduceNodes, bool discardSelfLoops)
+    : scale(scale), edgeFactor(edgeFactor), weighted(weighted), reduceNodes(reduceNodes),
+      discardSelfLoops(discardSelfLoops) {
     if (scale > 63)
         throw std::runtime_error("Cannot generate more than 2^63 nodes");
     double sum = a + b + c + d;
@@ -23,6 +82,88 @@ RmatGenerator::RmatGenerator(count scale, count edgeFactor, double a, double b, 
     if (!Aux::NumericTools::equal(sum, 1.0, 0.0001))
         throw std::runtime_error("Probabilities in Rmat have to sum to 1.");
     defaultEdgeWeight = 1.0;
+
+    count size = 1 << std::min((count)12, (count)std::round(scale * 5.0 / 8.0) + 1);
+
+    assert((size & (size - 1)) == 0); // The size must be a power of two.
+    std::vector<Entry> entryList;
+    generateEntries(entryList, size, a, b, c, d);
+    while (entryList.size() < size) { // Fill the remaining entries with 0-probability entries.
+        entryList.push_back(Entry{0, 0, 0, 0});
+    }
+
+    // Construct the alias table:
+    mask = size - 1;
+    bits.resize(size);
+    numberOfBits.resize(size);
+    coinFlipProbability.resize(size);
+    coinFlipReplacement.resize(size);
+    for (count i = 0; i < size; i++) {
+        bits[i] = std::make_pair(entryList[i].i, entryList[i].j);
+        numberOfBits[i] = entryList[i].numberOfBits;
+        coinFlipProbability[i] = 0;
+        coinFlipReplacement[i] = 0;
+    }
+    double baseProbability = 1.0 / (double)size;
+    count lastOverfullIndex = 0;
+    count lastUnderfullIndex = 0;
+    while (true) {
+        while (entryList[lastUnderfullIndex].probability >= baseProbability) {
+            lastUnderfullIndex++;
+            if (lastUnderfullIndex == size)
+                return;
+        }
+        int curUnderfullIndex = lastUnderfullIndex;
+        while (true) {
+            while (entryList[lastOverfullIndex].probability <= baseProbability) {
+                lastOverfullIndex++;
+                if (lastOverfullIndex == size)
+                    return;
+            }
+            double delta = baseProbability - entryList[curUnderfullIndex].probability;
+            entryList[curUnderfullIndex].probability = baseProbability;
+            entryList[lastOverfullIndex].probability -= delta;
+            coinFlipReplacement[curUnderfullIndex] = lastOverfullIndex;
+            coinFlipProbability[curUnderfullIndex] =
+                (uint32_t)(delta / baseProbability * std::numeric_limits<uint32_t>::max());
+            if (entryList[lastOverfullIndex].probability < baseProbability
+                && lastOverfullIndex < lastUnderfullIndex) {
+                curUnderfullIndex = lastOverfullIndex;
+            } else
+                break;
+        }
+    }
+}
+
+void RmatGenerator::sample(std::mt19937_64 &urng) {
+    std::uniform_int_distribution<uint64_t> dist;
+    uint64_t randomNumber = dist(urng);
+    uint32_t index = randomNumber & mask;
+    uint32_t coinFlip = randomNumber >> 32;
+    if (coinFlip <= coinFlipProbability[index]) {
+        index = coinFlipReplacement[index];
+    }
+    curBits = bits[index];
+    remainingBits = numberOfBits[index];
+}
+
+std::pair<node, node> RmatGenerator::sampleEdge(std::mt19937_64 &urng, uint8_t bits) {
+    std::pair<node, node> result{0, 0};
+    while (true) {
+        if (remainingBits >= bits) {
+            remainingBits -= bits;
+            result.first = result.first << bits | curBits.first >> remainingBits;
+            result.second = result.second << bits | curBits.second >> remainingBits;
+            curBits.first &= (1 << remainingBits) - 1;
+            curBits.second &= (1 << remainingBits) - 1;
+            return result;
+        }
+        result.first = result.first << remainingBits | curBits.first;
+        result.second = result.second << remainingBits | curBits.second;
+        bits -= remainingBits;
+
+        sample(urng);
+    }
 }
 
 Graph RmatGenerator::generate() {
@@ -34,36 +175,11 @@ Graph RmatGenerator::generate() {
     count numEdges = n * edgeFactor * n * 1.0 / static_cast<double>(n - reduceNodes);
     count wantedEdges = (n - reduceNodes) * edgeFactor;
     Graph G(n - reduceNodes, weighted);
-    double ab = a + b;
-    double abc = ab + c;
+    // Reset the internal state of the alias table:
+    curBits = {0, 0};
+    remainingBits = 0;
 
-    auto quadrant([&]() {
-        double r = Aux::Random::probability();
-        TRACE("r: ", r);
-
-        if (r <= a) {
-            return 0;
-        } else if (r <= ab) {
-            return 1;
-        } else if (r <= abc) {
-            return 2;
-        } else
-            return 3;
-    });
-
-    auto drawEdge([&]() {
-        node u = 0;
-        node v = 0;
-        for (index i = 0; i < scale; ++i) {
-            count q = quadrant();
-            u = u << 1;
-            v = v << 1;
-            u = u | (q >> 1);
-            v = v | (q & 1);
-        }
-
-        return std::make_pair(u, v);
-    });
+    auto drawEdge([&]() { return sampleEdge(Aux::Random::getURNG(), (uint8_t)scale); });
 
     if (reduceNodes > 0) {
         std::vector<node> nodemap(n, 0);
@@ -89,6 +205,8 @@ Graph RmatGenerator::generate() {
                 std::tie(u, v) = drawEdge();
                 u = nodemap[u];
                 v = nodemap[v];
+                if (discardSelfLoops && u == v)
+                    continue;
                 if (u != none && v != none) {
                     G.increaseWeight(u, v, defaultEdgeWeight);
                 }
@@ -98,7 +216,9 @@ Graph RmatGenerator::generate() {
                 std::tie(u, v) = drawEdge();
                 u = nodemap[u];
                 v = nodemap[v];
-                if (u != none && v != none && u != v && !G.hasEdge(u, v)) {
+                if (discardSelfLoops && u == v)
+                    continue;
+                if (u != none && v != none && !G.hasEdge(u, v)) {
                     G.addEdge(u, v);
                 }
             }
@@ -107,11 +227,15 @@ Graph RmatGenerator::generate() {
         if (weighted) {
             for (index e = 0; e < numEdges; ++e) {
                 std::pair<node, node> drawnEdge = drawEdge();
+                if (discardSelfLoops && drawnEdge.first == drawnEdge.second)
+                    continue;
                 G.increaseWeight(drawnEdge.first, drawnEdge.second, defaultEdgeWeight);
             }
         } else {
             while (G.numberOfEdges() < wantedEdges) {
                 std::pair<node, node> drawnEdge = drawEdge();
+                if (discardSelfLoops && drawnEdge.first == drawnEdge.second)
+                    continue;
                 if (!G.hasEdge(drawnEdge.first, drawnEdge.second)) {
                     G.addEdge(drawnEdge.first, drawnEdge.second);
                 }
