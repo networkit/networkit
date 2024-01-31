7849b14d1cb24eaf18db17040f976a0cb100dbca
petholza <angus.petholz@web.de>
2024-01-31 12:54:20 +0100

    introduces parallel BA generator

--- include/networkit/generators/BarabasiAlbertGenerator.hpp
+++ include/networkit/generators/BarabasiAlbertGenerator.hpp
@@ -18,24 +18,31 @@ namespace NetworKit {
  */
 class BarabasiAlbertGenerator final : public StaticGraphGenerator {
     Graph initGraph;
-    count k{0};    //!< Attachments made per node
-    count nMax{0}; //!< The maximal number of nodes attached
-    count n0{0};   //!< The number of initial connected nodes
-    bool batagelj; //!< Specifies whether to use batagelj's method or the original one
+    count k{0};      //!< Attachments made per node
+    count nMax{0};   //!< The maximal number of nodes attached
+    count n0{0};     //!< The number of initial connected nodes
+    bool sequential; //!< Specifies whether to compute sequentially using batagelj's method or in
+                     //!< parallel (if the number of threads allow for it).
+
+    Graph generateParallel();
+    Graph generateBatagelj();
 
 public:
     BarabasiAlbertGenerator() = default;
 
     /**
-     * This generator implements the preferential attachment model as introduced by Barabasi and
-     * Albert[1]. The original algorithm is very slow and thus, the much faster method from Batagelj
-     * and Brandes[2] is implemented and the current default. The original method is no longer
-     * supported and the \p batagelj parameter will be removed in future releases.
-     * [1] Barabasi, Albert: Emergence of Scaling in Random Networks
-     *   http://arxiv.org/pdf/cond-mat/9910332.pdf
-     * [2] ALG 5 of Batagelj, Brandes: Efficient Generation of Large Random Networks
-     *   https://kops.uni-konstanz.de/bitstream/handle/123456789/5799/random.pdf?sequence=1
-     * Running time is O(n+m)
+     * This generator uses the preferential attachment model as introduced by Barabasi and
+     * Albert[1], implemented in the much faster method from Batagelj and Brandes[2] per default
+     * where the running time is O(n+m). Furthermore there is a parallel version from Sanders and
+     * Schulz[3] implemented. This implementation can be selected by setting sequential=false.
+     * Empirically, the parallel version shows better runtimes (when executed multi-threaded).
+     *
+     * [1] Barabasi, Albert: [Emergence of Scaling in Random Networks]
+     *   (http://arxiv.org/pdf/cond-mat/9910332.pdf)
+     * [2] ALG 5 of Batagelj, Brandes: [Efficient Generation of Large Random Networks]
+     *   (https://kops.uni-konstanz.de/bitstream/handle/123456789/5799/random.pdf?sequence=1)
+     * [3] Peter Sanders, Christian Schulz: [Scalable generation of scale-free graphs]
+     *   (https://www.sciencedirect.com/science/article/pii/S0020019016300102)
      *
      * The generator will emit a simple graph, where all
      * new nodes are initially connected to k random neighbors.
@@ -43,32 +50,35 @@ public:
      * @param k     Number of attachments per node
      * @param nMax  Number of nodes in the graph
      * @param n0    Number of connected nodes to begin with
-     * @param batagelj Specifies whether to use Batagelj and Brandes's method (much faster)
-     *              rather than the naive one; default: true
+     * @param sequential Specifies whether to use Batagelj and Brandes's method (sequential) or a
+     * parallel variant. Default: true.
      */
-    BarabasiAlbertGenerator(count k, count nMax, count n0 = 0, bool batagelj = true);
+    BarabasiAlbertGenerator(count k, count nMax, count n0 = 0, bool sequential = true);
 
     /**
-     * This generator implements the preferential attachment model as introduced by Barabasi and
-     * Albert[1]. The original algorithm is very slow and thus, the much faster method from Batagelj
-     * and Brandes[2] is implemented and the current default. The original method is no longer
-     * supported and the \p batagelj parameter will be removed in future releases.
-     * [1] Barabasi, Albert: Emergence of Scaling in Random Networks
-     *   http://arxiv.org/pdf/cond-mat/9910332.pdf
-     * [2] ALG 5 of Batagelj, Brandes: Efficient Generation of Large Random Networks
-     *   https://kops.uni-konstanz.de/bitstream/handle/123456789/5799/random.pdf?sequence=1
-     * Running time is O(n+m)
+     * This generator uses the preferential attachment model as introduced by Barabasi and
+     * Albert[1], implemented in the much faster method from Batagelj and Brandes[2] per default
+     * where the running time is O(n+m). Furthermore there is a parallel version from Sanders and
+     * Schulz[3] implemented. This implementation can be selected by setting sequential=false.
+     * Empirically, the parallel version shows better runtimes (when executed multi-threaded).
      *
-     * The generator will emit a simple graph (given that the seed graph is simple), where all
+     * [1] Barabasi, Albert: [Emergence of Scaling in Random Networks]
+     *   (http://arxiv.org/pdf/cond-mat/9910332.pdf)
+     * [2] ALG 5 of Batagelj, Brandes: [Efficient Generation of Large Random Networks]
+     *   (https://kops.uni-konstanz.de/bitstream/handle/123456789/5799/random.pdf?sequence=1)
+     * [3] Peter Sanders, Christian Schulz: [Scalable generation of scale-free graphs]
+     *   (https://www.sciencedirect.com/science/article/pii/S0020019016300102)
+     *
+     * The generator will emit a simple graph, where all
      * new nodes are initially connected to k random neighbors.
      *
      * @param k     Number of attachments per node
      * @param nMax  Number of nodes in the graph
      * @param initGraph   The initial graph to start from
-     * @param batagelj Specifies whether to use Batagelj and Brandes's method (much faster)
-     *              rather than the naive one; default: true
+     * @param sequential Specifies whether to use Batagelj and Brandes's method (sequential) or a
+     * parallel variant. Default: true.
      */
-    BarabasiAlbertGenerator(count k, count nMax, const Graph &initGraph, bool batagelj = true);
+    BarabasiAlbertGenerator(count k, count nMax, const Graph &initGraph, bool sequential = true);
 
     Graph generate() override;
 };
