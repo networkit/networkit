6e44249bcd6174cd596690c78e961702526060aa
petholza <angus.petholz@web.de>
2024-02-07 09:15:50 +0100

    fix: LouvainMapGTest

--- networkit/cpp/community/test/LouvainMapEquationGTest.cpp
+++ networkit/cpp/community/test/LouvainMapEquationGTest.cpp
@@ -8,8 +8,8 @@
 #include <gtest/gtest.h>
 
 #include <networkit/community/LouvainMapEquation.hpp>
-#include <networkit/generators/ClusteredRandomGraphGenerator.hpp>
-#include <networkit/io/METISGraphReader.hpp>
+#include <networkit/generators/BarabasiAlbertGenerator.hpp>
+#include <networkit/graph/GraphTools.hpp>
 
 namespace NetworKit {
 
@@ -48,11 +48,41 @@ TEST_F(MapEquationGTest, testLocalMoveSmall) {
 
 TEST_F(MapEquationGTest, testLocalMove) {
     Aux::Random::setSeed(2342556, false);
-    ClusteredRandomGraphGenerator generator(100, 4, 0.5, 0.05);
-    Graph G = generator.generate();
-    Partition groundTruth = generator.getCommunities();
 
-    LouvainMapEquation algo(G, true, 32, "synchronous");
+    count n = 300;
+    BarabasiAlbertGenerator generator(50, n);
+    Graph G = generator.generate();
+
+    // Generates an std::unordered_set<node> with the sequence of consecutive integers in [start,
+    // end).
+    auto generateConsecutiveNodes = [](node start, node end) -> std::unordered_set<node> {
+        std::vector<node> nodes(end - start);
+        std::iota(nodes.begin(), nodes.end(), start);
+        return {nodes.begin(), nodes.end()};
+    };
+
+    Graph G1 = GraphTools::subgraphFromNodes(G, generateConsecutiveNodes(0, 100));
+    const Graph G2 = GraphTools::subgraphFromNodes(G, generateConsecutiveNodes(100, 200));
+    const Graph G3 = GraphTools::subgraphFromNodes(G, generateConsecutiveNodes(200, 300));
+
+    // generate 3 independent clusterings (=ground truth)
+    Partition groundTruth(n);
+    groundTruth.setUpperBound(3);
+    for (node i = 0; i < n; ++i) {
+        if (i < 100) {
+            groundTruth.addToSubset(0, i);
+        } else if (100 <= i && i < 200) {
+            groundTruth.addToSubset(1, i);
+        } else {
+            groundTruth.addToSubset(2, i);
+        }
+    }
+
+    // G1 becomes clustered version of G
+    GraphTools::merge(G1, G2);
+    GraphTools::merge(G1, G3);
+
+    LouvainMapEquation algo(G1, true, 32, "synchronous");
     algo.run();
     auto partition = algo.getPartition();
 
