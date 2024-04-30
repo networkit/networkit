212f02f2be42a192aeb178af6aeb9dbecc8c6847
Lukas Berner <Lukas.Berner@hu-berlin.de>
2024-04-30 16:07:13 +0200

    set default values for LamgSolverStatus

--- include/networkit/numerics/LAMG/SolverLamg.hpp
+++ include/networkit/numerics/LAMG/SolverLamg.hpp
@@ -33,9 +33,9 @@ struct LAMGSolverStatus {
     count numPostSmoothIters = 2; // number of post smoothing iterations
 
     // out
-    count numIters;                      // number of iterations needed during solve phase
-    double residual;                     // absolute final residual
-    bool converged;                      // flag of conversion status
+    count numIters = 0; // number of iterations needed during solve phase
+    double residual = std::numeric_limits<double>::infinity(); // absolute final residual
+    bool converged = false;                                    // flag of conversion status
     std::vector<double> residualHistory; // history of absolute residuals
 };
 
