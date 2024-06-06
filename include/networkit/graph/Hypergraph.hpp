b2d3fa8d1fae5c224f711efc2a1e1b6ac8896280
fabratu <fabbrandt@gmail.com>
2024-06-06 12:14:27 +0200

    Add implementation for basic node/edge manipulation functions.

--- include/networkit/graph/Hypergraph.hpp
+++ include/networkit/graph/Hypergraph.hpp
@@ -8,6 +8,9 @@
 #ifndef NETWORKIT_GRAPH_HYPERGRAPH_HPP_
 #define NETWORKIT_GRAPH_HYPERGRAPH_HPP_
 
+#include <set>
+#include <vector>
+
 #include <networkit/Globals.hpp>
 #include <networkit/graph/Attributes.hpp>
 #include <networkit/graph/EdgeIterators.hpp>
@@ -38,7 +41,7 @@ class Hypergraph final {
     //!< true if the hypergraph is weighted, false otherwise
     bool weighted;
     //!< true if the hypergraph is directed, false otherwise
-    bool directed;
+    // bool directed;
 
     // Node related data
 
@@ -46,12 +49,10 @@ class Hypergraph final {
     std::vector<bool> nodeExists;
 
     //!< list of node weights
-    std::vector<std::vector<nodeweight>> nodeWeights;
+    std::vector<nodeweight> nodeWeights;
 
-    //!< list of edge ids, which a node is incident to as Tail. For undirected
-    // hypergraphs this is the same as the list of head incidence.
-    std::vector<std::vector<edgeid>> nodeTailIncidence;
-    std::vector<std::vector<edgeid>> nodeHeadIncidence;
+    //!< list of edge ids, which a node is incident to.
+    std::vector<std::set<edgeid>> nodeIncidence;
 
     // Edge related data
 
@@ -59,12 +60,10 @@ class Hypergraph final {
     std::vector<bool> edgeExists;
 
     //!< list of edge weights
-    std::vector<std::vector<edgeweight>> edgeWeights;
+    std::vector<edgeweight> edgeWeights;
 
-    //!< list of node ids, which are part of an edge as tail. For undirected
-    // hypergraphs this is the same as the list of head incidence.
-    std::vector<std::vector<edgeid>> edgeTailIncidence;
-    std::vector<std::vector<edgeid>> edgeHeadIncidence;
+    //!< list of node ids, which are part of a certain hyperedge.
+    std::vector<std::set<edgeid>> edgeIncidence;
 
     AttributeMap<PerNode, Hypergraph> nodeAttributeMap;
     AttributeMap<PerEdge, Hypergraph> edgeAttributeMap;
@@ -89,12 +88,6 @@ public:
      * than 1.0.
      */
     bool isWeighted() const noexcept { return weighted; }
-
-    /**
-     * Return @c true if this hypergraph supports directed edges.
-     * @return @c true if this hypergraph supports directed edges.
-     */
-    bool isDirected() const noexcept { return directed; }
 
     /**
      * Return <code>true</code> if hypergraph contains no nodes.
@@ -125,6 +118,73 @@ public:
      * @return An upper bound for the node ids.
      */
     index upperEdgeIdBound() const noexcept { return omega; }
+
+    /**
+     * Add a new node to the hypergraph and return it.
+     * @return The new node.
+     */
+    node addNode();
+
+    /**
+     * Connect a node @a u to a list of hyperedges @a edges. If no node is given, a new
+     * node is added to the hypergraph and directly connected  to a list of hyperedges
+     * @a edges.
+     * @return The new node.
+     */
+    node addNodeTo(std::vector<edgeid> edges, node u = none);
+
+    /**
+     * Remove a node @a v from the hypergraph. This removes it from every hyperedge.
+     * @param v Node.
+     */
+    void removeNode(node v);
+
+    /**
+     * Remove a node @a v from a certain edge in  the hypergraph.
+     * @param v Node.
+     * @param eid Edge.
+     */
+    void removeNodeFrom(node v, edgeid eid);
+
+    /* EDGE MODIFIERS */
+
+    /**
+     * Add a new edge to the hypergraph and return it.
+     * @return The new edge or none, if the creation was unsuccessful
+     */
+    edgeid addEdge();
+
+    /**
+     * TODO: Add documentation string.
+     * @param nodes Nodes to add to this edge. If a node is non-existing, this node is added if @a
+     * addMissing is set to true.
+     * @param ew Optional edge weight.
+     * @param addMissing Adds unknown nodes if set to true. Note, that this increases the running
+     * time due to additional checks.
+     * @return The new edge or none, if the creation was unsuccessful
+     */
+    edgeid addEdge(std::vector<node> nodes, edgeweight ew = defaultEdgeWeight,
+                   bool addMissing = false);
+
+    bool removeEdge(...);
+
+    /**
+     * Return node weight of node @a u. Returns 0 if node does not
+     * exist.
+     *
+     * @param u Node id
+     * @return Node weight of @a u or 0 if edge does not exist.
+     */
+    nodeweight weight(node u) const;
+
+    /**
+     * Set the weight of a node. If the node does not exist,
+     * it will be inserted.
+     *
+     * @param[in]	u	node id
+     * @param[in]	weight	node weight
+     */
+    void setWeight(node u, nodeweight nw);
 };
 
 } // namespace NetworKit
