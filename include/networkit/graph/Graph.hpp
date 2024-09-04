61d16f7c31a856b82e4d6366e0f60e3bb3a913a1
Lukas Berner <Lukas.Berner@hu-berlin.de>
2024-09-04 13:34:52 +0200

    fix clang-format

--- include/networkit/graph/Graph.hpp
+++ include/networkit/graph/Graph.hpp
@@ -429,8 +429,8 @@ private:
                                   typename Aux::FunctionTraits<F>::template arg<2>::type>::value
                   && std::is_same<edgeid, typename Aux::FunctionTraits<F>::template arg<3>::type>::
                       value>::type * = (void *)0>
-    auto edgeLambda(F &f, node u, node v, edgeweight ew,
-                    edgeid id) const -> decltype(f(u, v, ew, id)) {
+    auto edgeLambda(F &f, node u, node v, edgeweight ew, edgeid id) const
+        -> decltype(f(u, v, ew, id)) {
         return f(u, v, ew, id);
     }
 
@@ -462,8 +462,8 @@ private:
                   (Aux::FunctionTraits<F>::arity >= 2)
                   && std::is_same<edgeweight, typename Aux::FunctionTraits<F>::template arg<
                                                   2>::type>::value>::type * = (void *)0>
-    auto edgeLambda(F &f, node u, node v, edgeweight ew,
-                    edgeid /*id*/) const -> decltype(f(u, v, ew)) {
+    auto edgeLambda(F &f, node u, node v, edgeweight ew, edgeid /*id*/) const
+        -> decltype(f(u, v, ew)) {
         return f(u, v, ew);
     }
 
@@ -476,8 +476,8 @@ private:
                            (Aux::FunctionTraits<F>::arity >= 1)
                            && std::is_same<node, typename Aux::FunctionTraits<F>::template arg<
                                                      1>::type>::value>::type * = (void *)0>
-    auto edgeLambda(F &f, node u, node v, edgeweight /*ew*/,
-                    edgeid /*id*/) const -> decltype(f(u, v)) {
+    auto edgeLambda(F &f, node u, node v, edgeweight /*ew*/, edgeid /*id*/) const
+        -> decltype(f(u, v)) {
         return f(u, v);
     }
 
@@ -613,7 +613,7 @@ public:
     public:
         NodeRange(const Graph &G) : G(&G) {}
 
-        NodeRange() : G(nullptr) {};
+        NodeRange() : G(nullptr){};
 
         ~NodeRange() = default;
 
@@ -838,7 +838,7 @@ public:
     public:
         EdgeRange(const Graph &G) : G(&G) {}
 
-        EdgeRange() : G(nullptr) {};
+        EdgeRange() : G(nullptr){};
 
         ~EdgeRange() = default;
 
@@ -863,7 +863,7 @@ public:
     public:
         EdgeWeightRange(const Graph &G) : G(&G) {}
 
-        EdgeWeightRange() : G(nullptr) {};
+        EdgeWeightRange() : G(nullptr){};
 
         ~EdgeWeightRange() = default;
 
@@ -1028,7 +1028,7 @@ public:
     public:
         NeighborRange(const Graph &G, node u) : G(&G), u(u) { assert(G.hasNode(u)); };
 
-        NeighborRange() : G(nullptr) {};
+        NeighborRange() : G(nullptr){};
 
         NeighborIterator begin() const {
             assert(G);
@@ -1060,7 +1060,7 @@ public:
     public:
         NeighborWeightRange(const Graph &G, node u) : G(&G), u(u) { assert(G.hasNode(u)); };
 
-        NeighborWeightRange() : G(nullptr) {};
+        NeighborWeightRange() : G(nullptr){};
 
         NeighborWeightIterator begin() const {
             assert(G);
