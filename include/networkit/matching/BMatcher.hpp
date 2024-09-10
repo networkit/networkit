f92ca8c0e247b9c20b7e6401a5c948f04ee5a7e6
fabratu <brandtfa@hu-berlin.de>
2024-09-10 10:34:41 +0200

    Add missing doc for BMatcher.hpp

--- include/networkit/matching/BMatcher.hpp
+++ include/networkit/matching/BMatcher.hpp
@@ -32,6 +32,11 @@ public:
      */
     void run() override = 0;
 
+    /**
+     * Gets the b-matching for the stored graph. 
+     * Note: The b-matching is only valid, if the corresponding algorithm
+     * has taken care of populating it properly.
+     */
     BMatching getBMatching() const;
 };
 
