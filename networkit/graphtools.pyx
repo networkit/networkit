db1e9cfd505c1272e28f8de1744bb1cf51b0138c
Jure Pustoslemšek <jure.poglednik@gmail.com>
2024-09-15 15:10:20 +0200

    TopologicalSort: fixed cython code to make it compile

--- networkit/graphtools.pyx
+++ networkit/graphtools.pyx
@@ -40,7 +40,7 @@ cdef extern from "<networkit/graph/GraphTools.hpp>" namespace "NetworKit::GraphT
 	unordered_map[node,node] getRandomContinuousNodeIds(_Graph G) except + nogil
 	void sortEdgesByWeight(_Graph G, bool_t) except + nogil
 	vector[node] topologicalSort(_Graph G) except + nogil
-	vector[node] topologicalSort(_Graph G, unordered_map[node, node]) except + nogil
+	vector[node] topologicalSort(_Graph G, unordered_map[node, node], bool_t) except + nogil
 	node augmentGraph(_Graph G) except + nogil
 	pair[_Graph, node] createAugmentedGraph(_Graph G) except + nogil
 	void randomizeWeights(_Graph G) except + nogil
@@ -630,9 +630,9 @@ cdef class GraphTools:
 		sortEdgesByWeight(G._this, decreasing)
 
 	@staticmethod
-	def topologicalSort(Graph G, dict[node, node] nodeIdMap = None):
+	def topologicalSort(Graph G, dict[node, node] nodeIdMap = None, bool_t checkMapping = False):
 		"""
-		topologicalSort(G, nodeIdMap = None)
+		topologicalSort(G, nodeIdMap = None, checkMapping = False)
 
 		Given a directed graph G, the topology sort algorithm creates one valid topology order of nodes.
 		Undirected graphs are not accepted as input, since a topology sort is a linear ordering of vertices 
@@ -644,10 +644,16 @@ cdef class GraphTools:
 		G : networkit.Graph
 			The directed input graph.
 		nodeIdMap : dict(int, int), optional
-					Optional continuous node id mapping
+					Optional continuous node id mapping.
+		checkMapping : bool, optional
+					   Flag to determine if the node id mapping should be checked that it 
+					   is continuous. This check takes O(|V|) time and space.
 		"""
+		cdef unordered_map[node,node] cNodeIdMap
 		if nodeIdMap is not None:
-			return topologicalSort(G._this, nodeIdMap)
+			for node, mapped in nodeIdMap.items():
+				cNodeIdMap[node] = mapped
+			return topologicalSort(G._this, cNodeIdMap, checkMapping)
 		return topologicalSort(G._this)
 
 	@staticmethod
