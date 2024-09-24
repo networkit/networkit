4f04b25bc2aa3bed45d5485e9663124766a89fdf
fabratu <brandtfa@hu-berlin.de>
2024-09-24 16:58:07 +0200

    Rename some variables.

--- networkit/cpp/matching/BMatcher.cpp
+++ networkit/cpp/matching/BMatcher.cpp
@@ -3,11 +3,11 @@
 namespace NetworKit {
 
 BMatcher::BMatcher(const Graph &G, const std::vector<count> &b)
-    : G(&G), M(b, G.upperNodeIdBound()) {}
+    : G(&G), bMatch(b, G.upperNodeIdBound()) {}
 
 BMatching BMatcher::getBMatching() const {
     assureFinished();
-    return M;
+    return bMatch;
 }
 
 } /* namespace NetworKit */
