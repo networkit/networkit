1e70350c49f45ad52f2c0e74f90b0f7883fdebca
Lukas Berner <Lukas.Berner@hu-berlin.de>
2024-10-18 12:14:19 +0200

    add RBMatrix reader and RBGraph reader

---
+++ include/networkit/io/RBGraphReader.hpp
@@ -0,0 +1,31 @@
+#ifndef NETWORKIT_IO_RB_GRAPH_READER_HPP_
+#define NETWORKIT_IO_RB_GRAPH_READER_HPP_
+
+#include <networkit/io/GraphReader.hpp>
+
+namespace NetworKit {
+
+/**
+ * @ingroup io
+ *
+ * Reader for the RB matrix file format documented in
+ * https://networkrepository.com/mtx-matrix-market-format.html
+ *
+ * Does not allow complex fields.
+ *
+ */
+class RBGraphReader final : public GraphReader {
+public:
+    RBGraphReader() = default;
+
+    /**
+     * Takes a file path as parameter and returns a graph.
+     *
+     * @param path
+     * @return Graph
+     */
+    Graph read(std::string_view path) override;
+};
+
+} /* namespace NetworKit */
+#endif // NETWORKIT_IO_MTX_GRAPH_READER_HPP_
---
+++ include/networkit/io/RBMatrixReader.hpp
@@ -0,0 +1,48 @@
+/*
+ * RBMatrixReader.hpp
+ *
+ *  Created on: 25.07.2014
+ *      Author: dhoske
+ */
+
+#ifndef NETWORKIT_IO_RB_MATRIX_READER_HPP_
+#define NETWORKIT_IO_RB_MATRIX_READER_HPP_
+
+#include <networkit/io/MatrixReader.hpp>
+#include <networkit/io/RBGraphReader.hpp>
+
+namespace NetworKit {
+
+/**
+ * @ingroup io
+ * Reader for the Rutherford Boeing (RB) matrix file format as described in
+ * http://sparse-files.engr.tamu.edu/files/DOC/rb.pdf.
+ *
+ * @note currently only supports compressed column format for real, integer, or pattern data types.
+ */
+class RBMatrixReader final : public MatrixReader {
+public:
+    RBMatrixReader() = default;
+
+    CSRMatrix read(std::string_view path) override;
+
+    /** Reads the matrix in @a in. */
+    CSRMatrix read(std::istream &in);
+
+private:
+    friend RBGraphReader;
+    // data format is CSC
+    std::vector<index> pointers;
+    std::vector<index> rowindex;
+    std::vector<double> values;
+
+    count nMatrixRows;
+    count nMatrixCols;
+    count nMatrixVals;
+    bool symmetric = false;
+    bool patternOnly = false;
+    void readToVectors(std::istream &in);
+};
+
+} // namespace NetworKit
+#endif // NETWORKIT_IO_MATRIX_MARKET_READER_HPP_
