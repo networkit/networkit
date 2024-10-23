88395d333f085b012c7b148a179a90a515ddf08b
Jones <2944106+c-bebop@users.noreply.github.com>
2024-10-23 17:14:44 +0200

    Use omp parallel for schedule(guided) if possible.

--- networkit/cpp/graph/DHBGraph.cpp
+++ networkit/cpp/graph/DHBGraph.cpp
@@ -288,7 +288,7 @@ bool DHBGraph::addEdges(std::vector<WeightedEdge>&& weighted_edges, bool do_upda
             std::vector<WeightedEdge> weighted_edges_v_to_u;
             weighted_edges_v_to_u.resize(weighted_edges.size());
 
-#pragma omp parallel for
+#pragma omp parallel for schedule(guided)
             for (count i = 0; i < weighted_edges_v_to_u.size(); ++i) {
                 weighted_edges_v_to_u[i].u = weighted_edges[i].v;
                 weighted_edges_v_to_u[i].v = weighted_edges[i].u;
