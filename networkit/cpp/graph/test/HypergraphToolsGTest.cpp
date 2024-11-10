fb947273bb14e52647f30fe92f0b5b0918241889
fabratu <brandtfa@hu-berlin.de>
2024-11-10 17:31:03 +0100

    Add tests for new HypergraphTools functions.

--- networkit/cpp/graph/test/HypergraphToolsGTest.cpp
+++ networkit/cpp/graph/test/HypergraphToolsGTest.cpp
@@ -108,4 +108,89 @@ TEST_P(HypergraphToolsGTest, testRandomNodes) {
     EXPECT_EQ((std::unordered_set<node>{someSample.begin(), someSample.end()}).size(), sampleSome);
 }
 
+TEST_P(HypergraphToolsGTest, testGetIntersection) {
+    Hypergraph hGraph(4, 0, weighted());
+    hGraph.addEdge({0, 1});
+    hGraph.addEdge({1, 2, 3});
+    hGraph.addEdge({2, 3});
+
+    std::unordered_set<node> intersection0_1{1};
+    std::unordered_set<node> intersection1_2{2, 3};
+    std::unordered_set<node> intersection2_0{};
+
+    EXPECT_EQ(HypergraphTools::getIntersection(hGraph, 0, 1), intersection0_1);
+    EXPECT_EQ(HypergraphTools::getIntersection(hGraph, 1, 2), intersection1_2);
+    EXPECT_EQ(HypergraphTools::getIntersection(hGraph, 2, 0), intersection2_0);
+}
+
+TEST_P(HypergraphToolsGTest, testGetIntersectionSize) {
+    Hypergraph hGraph(4, 0, weighted());
+    hGraph.addEdge({0, 1});
+    hGraph.addEdge({1, 2, 3});
+    hGraph.addEdge({2, 3});
+
+    EXPECT_EQ(HypergraphTools::getIntersectionSize(hGraph, 0, 1), 1);
+    EXPECT_EQ(HypergraphTools::getIntersectionSize(hGraph, 1, 2), 2);
+    EXPECT_EQ(HypergraphTools::getIntersectionSize(hGraph, 2, 0), 0);
+}
+
+TEST_P(HypergraphToolsGTest, testCliqueExpansion) {
+    Hypergraph hGraph(4, 0, weighted());
+    hGraph.addEdge({0, 1});
+    hGraph.addEdge({1, 2, 3});
+    hGraph.addEdge({0, 3});
+
+    Graph cGraph = HypergraphTools::cliqueExpansion(hGraph);
+
+    EXPECT_EQ(cGraph.numberOfNodes(), 4);
+    EXPECT_EQ(cGraph.numberOfEdges(), 5);
+    ASSERT_TRUE(cGraph.hasEdge(0, 1));
+    ASSERT_TRUE(cGraph.hasEdge(1, 2));
+    ASSERT_TRUE(cGraph.hasEdge(1, 3));
+    ASSERT_TRUE(cGraph.hasEdge(2, 3));
+    ASSERT_TRUE(cGraph.hasEdge(0, 3));
+}
+
+TEST_P(HypergraphToolsGTest, testLineExpansion) {
+    Hypergraph hGraph(4, 0, weighted());
+    hGraph.addEdge({0, 1});
+    hGraph.addEdge({1, 2, 3});
+    hGraph.addEdge({0, 3});
+
+    Graph lGraph = HypergraphTools::lineExpansion(hGraph);
+
+    EXPECT_EQ(lGraph.numberOfNodes(), 7);
+    EXPECT_EQ(lGraph.numberOfEdges(), 8);
+
+    auto attrNodeRef = lGraph.nodeAttributes().get<node>("node");
+    auto attrEdgeRef = lGraph.nodeAttributes().get<edgeid>("edgeid");
+
+    lGraph.forNodes([&](node u) { ASSERT_TRUE(hGraph.hasNode(attrNodeRef[u], attrEdgeRef[u])); });
+}
+
+TEST_P(HypergraphToolsGTest, testLineGraph) {
+    Hypergraph hGraph(4, 0, weighted());
+    hGraph.addEdge({0, 1});
+    hGraph.addEdge({1, 2, 3});
+    hGraph.addEdge({2, 3});
+
+    Graph unweightedLineGraph = HypergraphTools::lineGraph(hGraph, false);
+    EXPECT_TRUE(unweightedLineGraph.hasEdge(0, 1));
+    EXPECT_TRUE(unweightedLineGraph.hasEdge(1, 2));
+    EXPECT_FALSE(unweightedLineGraph.hasEdge(2, 0));
+}
+
+TEST_P(HypergraphToolsGTest, testWeightedLineGraph) {
+    Hypergraph hGraph(4, 0, weighted());
+    hGraph.addEdge({0, 1});
+    hGraph.addEdge({1, 2, 3});
+    hGraph.addEdge({2, 3});
+
+    Graph weightedLineGraph = HypergraphTools::lineGraph(hGraph, true);
+    EXPECT_TRUE(weightedLineGraph.hasEdge(1, 2));
+    EXPECT_DOUBLE_EQ(weightedLineGraph.weight(1, 2), 0.5);
+    EXPECT_FALSE(weightedLineGraph.hasEdge(2, 0));
+    EXPECT_DOUBLE_EQ(weightedLineGraph.weight(2, 0), 0.0);
+}
+
 } // namespace NetworKit
