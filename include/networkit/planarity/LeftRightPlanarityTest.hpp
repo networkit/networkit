05e2cb7e700917248561cc8b0d6d6612372afb87
Andreas Scharf <andreas.b.scharf@gmail.com>
2025-01-03 17:54:25 +0100

    Start with needed data structures.

--- include/networkit/planarity/LeftRightPlanarityTest.hpp
+++ include/networkit/planarity/LeftRightPlanarityTest.hpp
@@ -4,5 +4,37 @@
 
 #ifndef LEFT_RIGHT_PLANARITY_TEST_HPP
 #define LEFT_RIGHT_PLANARITY_TEST_HPP
+#include <networkit/base/Algorithm.hpp>
+#include <networkit/graph/Graph.hpp>
 
+namespace NetworKit {
+
+class LeftRightPlanarityTest final : public Algorithm {
+
+public:
+    LeftRightPlanarityTest(const Graph &graph): graph_(&graph){}
+    void run() override;
+	bool is_planar() const
+    {
+        return is_planar_;
+    }
+
+private:
+    const Graph *graph_;
+	bool is_planar_{};
+    void dfsOrientation();
+    void dfsTesting();
+    bool applyConstraints();
+    void removeBackEdges();
+    std::vector<index> height;
+    std::vector<count> lowPt;
+    std::vector<count> lowPt2;
+	std::unordered_map<Edge, Edge> lowPtEdge;
+    std::unordered_map<Edge, count> nestingDepth;
+    std::unordered_map<index, Edge> parentEdges;
+    std::unordered_set<Edge> visitedEdges;
+    Graph dfsGraph;
+
+};
+}
 #endif //LEFT_RIGHT_PLANARITY_TEST_HPP
--- networkit/cpp/planarity/LeftRightPlanarityTest.cpp
+++ networkit/cpp/planarity/LeftRightPlanarityTest.cpp
@@ -2,3 +2,9 @@
 // Created by andreas on 03.01.25.
 //
 #include <networkit/planarity/LeftRightPlanarityTest.hpp>
+
+namespace NetworKit {
+
+
+
+}
