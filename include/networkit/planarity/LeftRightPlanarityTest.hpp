9720eda8e0c07669441b8d3c503ef7a94fe1cf9e
Andreas Scharf <andreas.b.scharf@gmail.com>
2025-01-03 19:53:25 +0100

    Add definitions for Interval and ConflicPair. Define stack of conflict pairs.

--- include/networkit/planarity/LeftRightPlanarityTest.hpp
+++ include/networkit/planarity/LeftRightPlanarityTest.hpp
@@ -9,6 +9,61 @@
 
 namespace NetworKit {
 
+const Edge LRNoneEdge{none, none};
+
+struct Interval
+{
+    Edge low{LRNoneEdge}; // Represents the lower bound of the interval
+    Edge high{LRNoneEdge}; // Represents the upper bound of the interval
+
+    // Default constructor
+    Interval() = default;
+
+    // Constructor with specific low and high values
+    Interval(const Edge& low, const Edge& high)
+        : low(low), high(high)
+    {
+    }
+
+    bool is_empty() const
+    {
+        return low == LRNoneEdge && high == LRNoneEdge;
+    }
+};
+
+inline bool operator==(const Interval& lhs, const Interval& rhs)
+{
+    return lhs.low == rhs.low && lhs.high == rhs.high;
+}
+
+
+struct ConflictPair
+{
+    Interval left{}; // Left interval of edges
+    Interval right{}; // Right interval of edges
+
+    ConflictPair() = default;
+
+    // Constructor with initial intervals
+    ConflictPair(const Interval& left, const Interval& right)
+        : left(left), right(right)
+    {
+    }
+
+    void swap()
+    {
+        std::swap(left, right);
+    }
+};
+
+const ConflictPair NoneConflictPair{Interval{}, Interval{}};
+
+inline  bool operator==(const ConflictPair& lhs, const ConflictPair& rhs)
+{
+    return lhs.left == rhs.left && lhs.right == rhs.right;
+}
+
+
 class LeftRightPlanarityTest final : public Algorithm {
 
 public:
@@ -27,12 +82,14 @@ private:
     bool applyConstraints();
     void removeBackEdges();
     std::vector<index> height;
-    std::vector<count> lowPt;
-    std::vector<count> lowPt2;
-	std::unordered_map<Edge, Edge> lowPtEdge;
+    std::vector<count> lowPoint;
+    std::vector<count> secondLowestPoint;
+	std::unordered_map<Edge, Edge> lowPointEdge;
     std::unordered_map<Edge, count> nestingDepth;
     std::unordered_map<index, Edge> parentEdges;
     std::unordered_set<Edge> visitedEdges;
+    std::stack<ConflictPair> stack;
+    std::unordered_map<Edge, ConflictPair> stackBottom;
     Graph dfsGraph;
 
 };
