3da52205639946f21c5cf796842841e4517c1554
Andreas Scharf <andreas.b.scharf@gmail.com>
2025-02-09 11:31:42 +0100

    Add implementation.

--- networkit/cpp/bipartite/BFSBipartiteCheck.cpp
+++ networkit/cpp/bipartite/BFSBipartiteCheck.cpp
@@ -4,4 +4,36 @@
  *  Authors: Andreas Scharf (andreas.b.scharf@gmail.com)
  *
  */
+#include <queue>
 #include <networkit/bipartite/BFSBipartiteCheck.hpp>
+namespace NetworKit {
+
+void BFSBipartiteCheck::run() {
+    constexpr signed char uncolored{-1};
+    constexpr signed char black{};
+    constexpr signed char red{1};
+    std::vector<signed char> colors(graph->numberOfNodes(), uncolored);
+    std::queue<node> q;
+    isGraphBiPartite = true;
+    for (node u = 0; u < graph->numberOfNodes(); u++) {
+        if (colors[u] == uncolored) {
+            colors[u] = black;
+            q.push(u);
+            do {
+                const node currentNode = q.front();
+                q.pop();
+                for (const auto neighbor : graph->neighborRange(currentNode)) {
+                    if (colors[neighbor] == uncolored) {
+                        colors[neighbor] = red - colors[currentNode];
+                        q.push(neighbor);
+                    } else if (colors[neighbor] == colors[currentNode]) {
+                        isGraphBiPartite = false;
+                        return;
+                    }
+                }
+            } while (!q.empty());
+        }
+    }
+}
+
+} // namespace NetworKit
