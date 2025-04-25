2777f650eb4a43ba2be72a7e3f586e6c3dd8606d
Andreas Scharf <andreas.b.scharf@gmail.com>
2025-04-25 17:26:34 +0200

    Add test case to close coverage gap.

--- networkit/cpp/graph/test/SpanningGTest.cpp
+++ networkit/cpp/graph/test/SpanningGTest.cpp
@@ -263,4 +263,22 @@ TEST_F(SpanningGTest, testPrimMinimumSpanningForestIsMSFNonUnitWeights) {
     EXPECT_EQ(msf.getTotalWeight(), 6);
 }
 
+TEST_F(SpanningGTest, testPrimMinimumSpanningForestIsMSFUnweighted) {
+    Graph g(6);
+    g.addEdge(0, 1);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(3, 4);
+    g.addEdge(4, 5);
+    g.addEdge(5, 3);
+    g.indexEdges();
+
+    PrimMSF msf(g);
+    msf.run();
+    Graph T = msf.getForest();
+
+    isValidForest(g, T);
+    EXPECT_EQ(msf.getTotalWeight(), 4);
+}
+
 } /* namespace NetworKit */
