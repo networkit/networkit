5aa43f52929e757667b5575220155680ec0c7bd7
fabratu <brandtfa@hu-berlin.de>
2025-05-21 17:12:58 +0200

    Add initial version of parallel hashmap + tests.

---
+++ include/networkit/auxiliary/AtomicUtils.hpp
@@ -0,0 +1,51 @@
+#ifndef NETWORKIT_AUXILIARY_ATOMIC_UTILS_HPP_
+#define NETWORKIT_AUXILIARY_ATOMIC_UTILS_HPP_
+
+#include <atomic>
+
+namespace Aux {
+inline uint32_t unset_bit_atomically(std::atomic_uint32_t &bitmask, size_t position) {
+    uint32_t current_bitmask = bitmask.load();
+    uint32_t desired_bitmask = current_bitmask & ~(1u << position);
+
+    while (!bitmask.compare_exchange_weak(current_bitmask, desired_bitmask)) {
+        current_bitmask = bitmask.load();
+        desired_bitmask = current_bitmask & ~(1u << position);
+    }
+
+    return desired_bitmask;
+};
+
+inline uint32_t set_bit_atomically(std::atomic_uint32_t &bitmask, size_t position) {
+    uint32_t current_bitmask = bitmask.load();
+    uint32_t desired_bitmask = current_bitmask | (1u << position);
+
+    while (!bitmask.compare_exchange_weak(current_bitmask, desired_bitmask)) {
+        current_bitmask = bitmask.load();
+        desired_bitmask = current_bitmask | (1u << position);
+    }
+
+    return desired_bitmask;
+}
+
+template <typename T>
+void swap_atomics_non_atomically(std::atomic<T> &a, std::atomic<T> &b) {
+    T const a_temp = a.load();
+    a.store(b);
+    b.store(a_temp);
+};
+
+template <typename T>
+T increment_atomically(std::atomic<T> &value, T increment = T(1)) {
+    T current_value = value.load();
+    T incremented_value = current_value + increment;
+
+    while (!value.compare_exchange_weak(current_value, incremented_value)) {
+        incremented_value = current_value + increment;
+    }
+
+    return incremented_value;
+}
+} // namespace Aux
+
+#endif // NETWORKIT_AUXILIARY_ATOMIC_UTILS_HPP_
---
+++ networkit/cpp/auxiliary/ParallelHashMap.cpp
@@ -0,0 +1,405 @@
+#include <omp.h>
+
+#include <algorithm>
+#include <atomic>
+#include <cassert>
+#include <cmath>
+#include <cstdint>
+#include <cuchar>
+#include <iterator>
+#include <thread>
+
+#include <networkit/auxiliary/ParallelHashMap.hpp>
+
+namespace Aux {
+
+namespace {
+// From 5.3.1 of "Concurrent Hash Tables: Fast and general(?)!" from Maier et
+// al. (2016): h_c(x) := floor(h(x) * (c/U)) where c = capacity and U = key
+// space = 64 (bit) in our case. Since capacity and U are both power of 2 we can
+// change the formula to use bit-shifting.
+uint64_t scaling(uint64_t const h, size_t const capacity) {
+    return h >> uint64_t(ParallelHashMap::ht_key_space - std::log2(capacity));
+}
+
+// Use this if you already precomputed log2(capacity).
+uint64_t scaling_log(uint64_t const h, uint64_t const log_2_capacity) {
+    return h >> (ParallelHashMap::ht_key_space - log_2_capacity);
+}
+
+size_t index(uint64_t const key, uint64_t const offset, size_t const capacity) {
+    uint64_t const h_c = scaling(key, capacity);
+    return fast_mod(h_c + offset, capacity);
+}
+
+bool ht_filled(size_t const occupancy, size_t const capacity) {
+    uint64_t const alpha_elements_count = capacity >> 1;
+    return occupancy > alpha_elements_count;
+}
+
+ParallelHashMap::HTAtomic128 *grow_hashtable(std::unique_ptr<ParallelHashMap::HTAtomic128> &source,
+                                             std::unique_ptr<ParallelHashMap::HTAtomic128> &target,
+                                             std::atomic_bool &request_growth, int const p_count,
+                                             int const p_id) {
+    request_growth.store(true);
+
+#pragma omp single
+    { target = std::make_unique<ParallelHashMap::HTAtomic128>(source->capacity() << 1); }
+    // omp implicit barrier
+
+    source->roam(*target, p_count, p_id);
+#pragma omp barrier
+
+#pragma omp single
+    {
+        request_growth.store(false);
+        source = std::move(target);
+    }
+    // omp implicit barrier
+
+    return source.get();
+}
+
+} // namespace
+
+using HTAtomic128 = ParallelHashMap::HTAtomic128;
+using HTSyncData = ParallelHashMap::HTSyncData;
+using HTHandle = ParallelHashMap::HTHandle;
+
+HTAtomic128::HTAtomic128() : m_cells(m_capacity, Cell{}) {}
+
+HTAtomic128::HTAtomic128(size_t const capacity)
+    : m_cells(capacity, Cell{}), m_capacity(capacity), m_log_capacity(std::log2(capacity)) {}
+
+HTAtomic128::HTAtomic128(HTAtomic128 const &other)
+    : m_cells(other.m_cells), m_capacity(other.m_capacity), m_log_capacity(other.m_log_capacity),
+      m_global_occupancy(other.m_global_occupancy.load()) {}
+
+HTAtomic128::HTAtomic128(HTAtomic128 &&other) : HTAtomic128() {
+    swap(*this, other);
+}
+
+HTAtomic128 &HTAtomic128::operator=(HTAtomic128 other) {
+    swap(*this, other);
+    return *this;
+}
+
+bool HTAtomic128::insert(uint64_t const key, uint64_t const value) {
+    Cell const desired = make_cell(key, value);
+    Cell expected = invalid_cell();
+    uint64_t const hashed_key = hash64(key);
+    size_t idx = scaling_log(hashed_key, m_log_capacity);
+
+    while (true) {
+        idx = fast_mod(idx, m_capacity);
+        assert(idx < m_capacity);
+
+        Cell &cell = m_cells[idx];
+
+#pragma omp atomic read
+        expected.key = cell.key;
+
+        if (desired.key == expected.key) {
+            return false;
+        }
+
+#pragma omp atomic read
+        expected.value = cell.value;
+
+        // Data Race check on invalid and desired
+        if (expected.key == ParallelHashMap::ht_invalid_key) {
+            if (dwcas(cell, expected, desired)) {
+                return true;
+            }
+            // DWCAS did not work.. thus reaching this line due to a race on
+            // the cells index with another thread when trying to write. We
+            // simply don't increment the idx variable which will try again
+            // with the same idx and proceed with linear probing.
+        } else {
+            idx++;
+        }
+    }
+}
+
+uint64_t HTAtomic128::find(uint64_t const key) const {
+    Cell actual = invalid_cell();
+    uint64_t const hashed_key = hash64(key);
+    size_t idx = scaling_log(hashed_key, m_log_capacity);
+
+    while (true) {
+        idx = fast_mod(idx, m_capacity);
+        assert(idx < m_capacity);
+
+        Cell const &cell = m_cells[idx];
+
+#pragma omp atomic read
+        actual.key = cell.key;
+
+#pragma omp atomic read
+        actual.value = cell.value;
+
+        if (actual.key == key) {
+            return actual.value;
+        }
+
+        if (actual.key == ParallelHashMap::ht_invalid_key) {
+            return ParallelHashMap::ht_invalid_value;
+        }
+
+        idx++;
+    }
+}
+
+bool HTAtomic128::update(uint64_t const key, uint64_t const value) {
+    Cell const desired = make_cell(key, value);
+    Cell expected = invalid_cell();
+    uint64_t const hashed_key = hash64(key);
+    size_t idx = scaling_log(hashed_key, m_log_capacity);
+
+    while (true) {
+        idx = fast_mod(idx, m_capacity);
+        assert(idx < m_capacity);
+
+        Cell &cell = m_cells[idx];
+
+#pragma omp atomic read
+        expected.key = cell.key;
+
+#pragma omp atomic read
+        expected.value = cell.value;
+
+        if (expected.key == desired.key) {
+            // If DWCAS fails we simply try again for the same cell.
+            if (dwcas(cell, expected, desired)) {
+                return true;
+            }
+        } else if (expected.key == ParallelHashMap::ht_invalid_key) {
+            return false;
+        } else {
+            idx++;
+        }
+    }
+}
+
+HTAtomic128::Iterator::Iterator(Cells const &cells)
+    : m_cells_it(std::begin(cells)), m_cells_end(std::end(cells)) {
+    while (m_cells_it != m_cells_end && m_cells_it->key == ParallelHashMap::ht_invalid_key) {
+        m_cells_it++;
+    }
+}
+
+HTAtomic128::Iterator::pointer HTAtomic128::Iterator::operator->() {
+    return &*m_cells_it;
+}
+
+HTAtomic128::Iterator &HTAtomic128::Iterator::operator++() {
+    next();
+    return *this;
+}
+
+HTAtomic128::Iterator HTAtomic128::Iterator::operator++(int) {
+    Iterator tmp = *this;
+    next();
+    return tmp;
+}
+
+void HTAtomic128::Iterator::invalidate() {
+    m_cells_it = m_cells_end;
+}
+
+void HTAtomic128::Iterator::next() {
+    if (m_cells_it == m_cells_end) {
+        return;
+    }
+
+    do {
+        m_cells_it++;
+    } while (m_cells_it != m_cells_end && m_cells_it->key == ParallelHashMap::ht_invalid_key);
+}
+
+HTAtomic128::Iterator HTAtomic128::end() const {
+    HTAtomic128::Iterator end_it{m_cells};
+    end_it.invalidate();
+    return end_it;
+}
+
+size_t HTAtomic128::increment_global_occupancy(size_t increment) {
+    return Aux::increment_atomically(m_global_occupancy, increment);
+}
+
+size_t HTAtomic128::global_occupancy() const {
+    return m_global_occupancy.load();
+}
+
+size_t HTAtomic128::capacity() const {
+    return m_capacity;
+}
+
+bool HTAtomic128::filled() const {
+    return ht_filled(m_global_occupancy.load(), m_capacity);
+}
+
+void HTAtomic128::roam(HTAtomic128 &target, uint32_t const p_count, uint32_t const p_id) {
+    auto range_to_move = cluster_range(p_count, p_id);
+    move_cells(range_to_move.first, range_to_move.second, target);
+}
+
+void HTAtomic128::move_cells(HTAtomic128::Cells::const_iterator begin,
+                             HTAtomic128::Cells::const_iterator end, HTAtomic128 &target) {
+    size_t local_occupancy = 0;
+    for (HTAtomic128::Cells::const_iterator c = begin; c != end; ++c) {
+        if (c->key != ParallelHashMap::ht_invalid_key) {
+            bool inserted = target.insert(c->key, c->value);
+            assert(inserted);
+            local_occupancy += size_t(inserted);
+        }
+    }
+    target.increment_global_occupancy(local_occupancy);
+}
+
+std::pair<ParallelHashMap::HTAtomic128::Cells::const_iterator,
+          ParallelHashMap::HTAtomic128::Cells::const_iterator>
+HTAtomic128::cluster_range(uint32_t const p_count, uint32_t const p_id) {
+    auto cells_begin = m_cells.begin();
+    auto cells_end = m_cells.end();
+
+    if (cells_begin == cells_end) {
+        return std::make_pair(cells_end, cells_end);
+    }
+
+    HTAtomic128::Cells::difference_type const cell_count = std::distance(cells_begin, cells_end);
+    if (cell_count < (p_count * p_count)) {
+        if (p_id == 0) {
+            return std::make_pair(cells_begin, cells_end);
+        }
+
+        return std::make_pair(cells_end, cells_end);
+    }
+
+    size_t const cells_per_thread = cell_count / p_count;
+
+    auto p_cells_end = cells_begin;
+    if (p_id == (p_count - 1)) {
+        p_cells_end = cells_end;
+    } else {
+        size_t const max_advance = std::min<size_t>((p_id + 1) * cells_per_thread, cell_count);
+        std::advance(p_cells_end, max_advance);
+
+        while (p_cells_end != cells_end && p_cells_end->key != ParallelHashMap::ht_invalid_key) {
+            ++p_cells_end;
+        }
+    }
+
+    auto p_cells_begin = cells_begin;
+    if (p_id > 0) {
+        size_t const max_advance =
+            std::min<size_t>(p_id * cells_per_thread, std::distance(p_cells_begin, p_cells_end));
+        std::advance(p_cells_begin, max_advance);
+
+        while (p_cells_begin != p_cells_end
+               && p_cells_begin->key != ParallelHashMap::ht_invalid_key) {
+            ++p_cells_begin;
+        }
+    }
+
+    return std::make_pair(p_cells_begin, p_cells_end);
+}
+
+HTSyncData::HTSyncData(std::unique_ptr<HTAtomic128> &_source, std::unique_ptr<HTAtomic128> &_target,
+                       std::atomic_uint32_t &_busy, std::atomic_bool &_request_growth, int _p_count,
+                       int _p_id, uint32_t _insert_threshold)
+    : source(_source), target(_target), busy(_busy), request_growth(_request_growth),
+      p_count(_p_count), p_id(_p_id), insert_threshold(_insert_threshold) {}
+
+HTHandle::HTHandle(HTSyncData sync_data) : m_ht(sync_data.source.get()), m_sync_data(sync_data) {
+    set_bit_atomically(m_sync_data.busy, m_sync_data.p_id);
+}
+
+HTHandle::~HTHandle() {
+    size_t const occupancy = m_ht->increment_global_occupancy(m_sync_data.insert_counter);
+
+    if (ht_filled(occupancy, m_ht->capacity())) {
+        m_ht = grow_hashtable(m_sync_data.source, m_sync_data.target, m_sync_data.request_growth,
+                              m_sync_data.p_count, m_sync_data.p_id);
+    }
+
+    Aux::unset_bit_atomically(m_sync_data.busy, m_sync_data.p_id);
+
+    while (m_sync_data.busy.load() != 0u) {
+        if (m_sync_data.request_growth.load()) {
+            auto disposable_ht =
+                grow_hashtable(m_sync_data.source, m_sync_data.target, m_sync_data.request_growth,
+                               m_sync_data.p_count, m_sync_data.p_id);
+        }
+    }
+}
+
+bool HTHandle::insert(uint64_t const key, uint64_t const value) {
+    bool const success = m_ht->insert(key, value);
+    m_sync_data.insert_counter += uint32_t(success);
+
+    if (m_sync_data.insert_threshold == m_sync_data.insert_counter) {
+        size_t const occupancy = m_ht->increment_global_occupancy(m_sync_data.insert_counter);
+
+        if (ht_filled(occupancy, m_ht->capacity())) {
+            m_ht =
+                grow_hashtable(m_sync_data.source, m_sync_data.target, m_sync_data.request_growth,
+                               m_sync_data.p_count, m_sync_data.p_id);
+        }
+
+        m_sync_data.insert_counter = 0;
+    }
+
+    return success;
+}
+
+uint64_t HTHandle::find(uint64_t const key) const {
+    return m_ht->find(key);
+}
+
+bool HTHandle::update(uint64_t const key, uint64_t const value) const {
+    return m_ht->update(key, value);
+}
+
+HTAtomic128 const &HTHandle::hashtable() {
+    return *m_ht;
+}
+
+ParallelHashMap::ParallelHashMap() : ParallelHashMap(ParallelHashMap::ht_begin_capacity) {}
+
+ParallelHashMap::ParallelHashMap(size_t const begin_capacity) {
+    m_source = std::make_unique<HTAtomic128>(begin_capacity);
+}
+
+ParallelHashMap::ParallelHashMap(ParallelHashMap const &other) {
+    m_source = std::make_unique<HTAtomic128>(*other.m_source.get());
+}
+
+ParallelHashMap::ParallelHashMap(ParallelHashMap &&other) : ParallelHashMap() {
+    swap(*this, other);
+}
+
+std::unique_ptr<HTHandle> ParallelHashMap::make_handle() {
+    Aux::HTSyncData sync_data{m_source,
+                              m_target,
+                              m_busy_bitset,
+                              m_request_growth,
+                              omp_get_num_threads(),
+                              omp_get_thread_num(),
+                              random_thread_range()};
+
+    auto handle = std::make_unique<HTHandle>(sync_data);
+
+    return handle;
+}
+
+HTAtomic128 const *const ParallelHashMap::current_table() const {
+    return m_source.get();
+}
+
+uint32_t ParallelHashMap::random_thread_range() {
+    std::uniform_int_distribution<uint32_t> m_dist(1, omp_get_num_threads());
+    return m_dist(m_generator);
+}
+
+} // namespace Aux
