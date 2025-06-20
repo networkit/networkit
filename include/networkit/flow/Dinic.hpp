b005bed7b018812635d6b7b4bc31a25226e45740
Andreas Scharf <andreas.b.scharf@gmail.com>
2025-06-20 20:08:42 +0200

    Add files.

---
+++ include/networkit/flow/Dinic.hpp
@@ -0,0 +1,11 @@
+/*  Dinic.hpp
+*
+ *	Created on: 20.06.2025
+ *  Authors: Andreas Scharf (andreas.b.scharf@gmail.com)
+ *
+ */
+
+#ifndef DINIC_HPP
+#define DINIC_HPP
+
+#endif //DINIC_HPP
---
+++ networkit/cpp/flow/test/DinicGTest.cpp
@@ -0,0 +1,6 @@
+/*  DinicGTest.cpp
+*
+ *	Created on: 20.06.2025
+ *  Authors: Andreas Scharf (andreas.b.scharf@gmail.com)
+ *
+ */
