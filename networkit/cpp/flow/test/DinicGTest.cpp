33e90ddb59ee066dcb0b6fbdd351344b47f59876
Andreas Scharf <andreas.b.scharf@gmail.com>
2025-07-07 17:25:50 +0200

    Start debugging.

--- networkit/cpp/flow/test/DinicGTest.cpp
+++ networkit/cpp/flow/test/DinicGTest.cpp
@@ -52,5 +52,28 @@ TEST_F(DinicGTest, testConstructorThrowsForSameSourceAndTarget) {
     }
 }
 
+TEST_F(DinicGTest, testSimpleGraph) {
+    Graph graph(2, true, true);
+    try {
+        Dinic test(graph, /* source */ 0, /*target */ 0);
+        FAIL() << "Expected std::runtime_error";
+    } catch (const std::runtime_error &e) {
+        EXPECT_STREQ(e.what(), "Dinic algorithm requires `source` and `target` node to be different!");
+    } catch (...) {
+        FAIL() << "Expected std::runtime_error but got a different exception.";
+    }
+}
+
+TEST_F(DinicGTest, testMaxFlow) {
+    Graph graph(4, true, true);
+    graph.addEdge(0, 1, 0.3);
+    graph.addEdge(1, 2, 0.6);
+    graph.addEdge(2, 0, 0.9);
+    graph.addEdge(2, 3, 0.7);
+    constexpr double expectedFlow{0.3};
+    Dinic test(graph,  0,  3);
+    test.run();
+    EXPECT_DOUBLE_EQ(expectedFlow, test.getMaxFlow());
+}
 
 } // namespace NetworKit
