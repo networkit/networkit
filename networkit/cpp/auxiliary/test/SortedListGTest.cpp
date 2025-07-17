b877df365ea883528b679dc6d140e1d69ea30fb1
Andreas Scharf <andreas.b.scharf@gmail.com>
2025-07-17 11:07:48 +0200

    Code cleanup.

--- networkit/cpp/auxiliary/test/SortedListGTest.cpp
+++ networkit/cpp/auxiliary/test/SortedListGTest.cpp
@@ -1,5 +1,5 @@
 /*
-* SortedListGTest.cpp
+ * SortedListGTest.cpp
  *
  *  Created on: 17.07.2025
  *      Author: Andreas Scharf (andreas.b.scharf@gmail.com)
@@ -9,89 +9,89 @@
 #include <networkit/auxiliary/SortedList.hpp>
 namespace Networkit {
 
-TEST(SortedListTest, testBasicInsertionSortedOrder) {
-    Aux::SortedList sl(3, 10);
+class SortedListGTest : public testing::Test {};
 
-    sl.insert(1, 5.0);
-    sl.insert(2, 7.0);
-    sl.insert(3, 6.0);
+TEST(SortedListGTest, testBasicInsertionSortedOrder) {
+    Aux::SortedList list(3, 10);
 
-    EXPECT_EQ(sl.getSize(), 3u);
-    EXPECT_EQ(sl.getElement(0), 2u);
-    EXPECT_EQ(sl.getValue(0), 7.0);
-    EXPECT_EQ(sl.getElement(1), 3u);
-    EXPECT_EQ(sl.getValue(1), 6.0);
-    EXPECT_EQ(sl.getElement(2), 1u);
-    EXPECT_EQ(sl.getValue(2), 5.0);
+    list.insert(1, 5.0);
+    list.insert(2, 7.0);
+    list.insert(3, 6.0);
+
+    EXPECT_EQ(list.getSize(), 3u);
+    EXPECT_EQ(list.getElement(0), 2u);
+    EXPECT_EQ(list.getValue(0), 7.0);
+    EXPECT_EQ(list.getElement(1), 3u);
+    EXPECT_EQ(list.getValue(1), 6.0);
+    EXPECT_EQ(list.getElement(2), 1u);
+    EXPECT_EQ(list.getValue(2), 5.0);
 }
 
-TEST(SortedListTest, UpdateExistingElementHigherValue) {
-    Aux::SortedList sl(3, 10);
+TEST(SortedListGTest, testInsertExistingElementWithHigherValue) {
+    Aux::SortedList list(3, 10);
 
-    sl.insert(4, 3.0);
-    sl.insert(5, 4.0);
-    sl.insert(6, 5.0);
+    list.insert(4, 3.0);
+    list.insert(5, 4.0);
+    list.insert(6, 5.0);
 
-    sl.insert(4, 6.0);
+    list.insert(4, 6.0);
 
-    EXPECT_EQ(sl.getElement(0), 4u);
-    EXPECT_EQ(sl.getValue(0), 6.0);
-    EXPECT_EQ(sl.getSize(), 3u);
+    EXPECT_EQ(list.getSize(), 3u);
+
+    EXPECT_EQ(list.getElement(0), 4u);
+    EXPECT_EQ(list.getValue(0), 6.0);
+    EXPECT_EQ(list.getElement(1), 6u);
+    EXPECT_EQ(list.getValue(1), 5.0);
+    EXPECT_EQ(list.getElement(2), 5u);
+    EXPECT_EQ(list.getValue(2), 4.0);
 }
 
-TEST(SortedListTest, ExceedingCapacityOnlyKeepsTopK) {
-    Aux::SortedList sl(3, 10);
+TEST(SortedListGTest, testExceedingCapacityKeepsTopK) {
+    Aux::SortedList list(3, 10);
 
-    sl.insert(1, 1.0);
-    sl.insert(2, 2.0);
-    sl.insert(3, 3.0);
-    EXPECT_EQ(sl.getElement(2), 1u);
-    EXPECT_EQ(sl.getSize(), 3u);
+    list.insert(1, 1.0);
+    list.insert(2, 2.0);
+    list.insert(3, 3.0);
+    EXPECT_EQ(list.getElement(2), 1u);
+    EXPECT_EQ(list.getSize(), 3u);
 
-    sl.insert(4, 4.0);  // Should evict (1, 1.0)
-    EXPECT_EQ(sl.getSize(), 3u);
-    EXPECT_NE(sl.getElement(2), 1u);
-    EXPECT_EQ(sl.getElement(2), 2u);
+    list.insert(4, 4.0); // Should evict (1, 1.0)
+    EXPECT_EQ(list.getSize(), 3u);
+    EXPECT_NE(list.getElement(2), 1u);
+    EXPECT_EQ(list.getElement(2), 2u);
 }
 
-TEST(SortedListTest, DuplicateInsertSameValueIgnoredPosition) {
-    Aux::SortedList sl(3, 10);
+TEST(SortedListGTest, testDuplicateInsertSameElement) {
+    Aux::SortedList list(3, 10);
 
-    sl.insert(1, 5.0);
-    sl.insert(2, 6.0);
-    sl.insert(1, 7.0);
+    list.insert(1, 5.0);
+    list.insert(2, 6.0);
+    list.insert(1, 7.0);
 
-    EXPECT_EQ(sl.getSize(), 3u);
-    EXPECT_EQ(sl.getElement(0), 1u);
-    EXPECT_EQ(sl.getValue(0), 7.0);
-    EXPECT_EQ(sl.getElement(1), 2u);
-    EXPECT_EQ(sl.getValue(1), 6.0);
-    EXPECT_EQ(sl.getElement(2), 1u);
-    EXPECT_EQ(sl.getValue(2), 5.0);
+    EXPECT_EQ(list.getSize(), 3u);
+    EXPECT_EQ(list.getElement(0), 1u);
+    EXPECT_EQ(list.getValue(0), 7.0);
+    EXPECT_EQ(list.getElement(1), 2u);
+    EXPECT_EQ(list.getValue(1), 6.0);
+    EXPECT_EQ(list.getElement(2), 1u);
+    EXPECT_EQ(list.getValue(2), 5.0);
 }
 
-TEST(SortedListTest, ClearEmptiesAll) {
-    Aux::SortedList sl(3, 10);
-    sl.insert(1, 10.0);
-    sl.insert(2, 9.0);
-    sl.clear();
+TEST(SortedListGTest, testClear) {
+    Aux::SortedList list(3, 10);
+    list.insert(1, 10.0);
+    list.insert(2, 9.0);
+    list.clear();
 
-    EXPECT_EQ(sl.getSize(), 0u);
-    sl.insert(3, 8.0);
-    EXPECT_EQ(sl.getSize(), 1u);
-    EXPECT_EQ(sl.getElement(0), 3u);
+    EXPECT_EQ(list.getSize(), 0u);
+    list.insert(3, 8.0);
+    EXPECT_EQ(list.getSize(), 1u);
+    EXPECT_EQ(list.getElement(0), 3u);
+    EXPECT_EQ(list.getValue(0), 8.0);
 }
 
-TEST(SortedListTest, InsertingOutOfRangeKeyThrows) {
+TEST(SortedListGTest, testInsertingOutOfRangeKeyThrows) {
     EXPECT_THROW(Aux::SortedList(5, 3), std::runtime_error);
 }
 
-TEST(SortedListTest, InsertingNonMonotonicValueIgnored) {
-    Aux::SortedList sl(3, 10);
-
-    sl.insert(2, 5.0);
-    sl.insert(2, 3.0);
-    EXPECT_EQ(sl.getElement(0), 2u);
-    EXPECT_EQ(sl.getValue(0), 5.0);
-}
-}
+} // namespace Networkit
