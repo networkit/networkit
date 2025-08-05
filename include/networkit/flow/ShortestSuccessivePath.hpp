24835d81e1e49b2609e3e21c7a4be19bd20004e3
Andreas Scharf <andreas.b.scharf@gmail.com>
2025-08-05 12:46:05 +0200

    Add constructor.

--- include/networkit/flow/ShortestSuccessivePath.hpp
+++ include/networkit/flow/ShortestSuccessivePath.hpp
@@ -8,11 +8,30 @@
 #ifndef SHORTESTSUCCESSIVEPATH_H
 #define SHORTESTSUCCESSIVEPATH_H
 
+#include <networkit/base/Algorithm.hpp>
+#include <networkit/graph/Graph.hpp>
+
 namespace NetworKit {
-class MinFlowShortestSuccessivePath  {
+class MinFlowShortestSuccessivePath : public Algorithm {
 
+   public:
+        /**
+         * @param G            underlying graph (must be directed + weighted)
+         * @param capacityAttr name of the edge attribute holding capacity values
+         * @param supplyAttr   name of the node attribute holding supply values
+         *
+         * @throws std::runtime_error if G is not directed or not weighted,
+         *         or if either of the named attributes is missing.
+         */
+        MinFlowShortestSuccessivePath(const Graph &G,
+                                      const std::string &capacityName,
+                                      const std::string &supplyName);
 
-
+        void run() override;
+    private:
+        const Graph &graph;
+        std::string capacityAttributeName;
+        std::string supplyAttributeName;
 };
 
 }
--- networkit/cpp/flow/ShortestSuccessivePath.cpp
+++ networkit/cpp/flow/ShortestSuccessivePath.cpp
@@ -6,3 +6,41 @@
  */
 
 #include <networkit/flow/ShortestSuccessivePath.hpp>
+namespace NetworKit {
+MinFlowShortestSuccessivePath::MinFlowShortestSuccessivePath(
+    const Graph &G,
+    const std::string &capacityName,
+    const std::string &supplyName)
+    : graph(G), capacityAttributeName(capacityName), supplyAttributeName(supplyName) {
+
+    if (!G.isDirected()) {
+        throw std::runtime_error(
+            "MinFlowShortestSuccessivePath: Graph must be directed");
+    }
+
+    if (!G.isWeighted()) {
+        throw std::runtime_error(
+            "MinFlowShortestSuccessivePath: Graph must be weighted.");
+    }
+
+    try {
+        // this internally calls AttributeMap::find and will throw if the attribute does not exist
+       (void)graph.edgeAttributes().find(capacityName);
+    } catch (const std::runtime_error &e) {
+        // catch the generic “No such attribute” and give our own
+        throw std::runtime_error(
+            "MinFlowShortestSuccessivePath: Provided edge attribute '" + capacityName + "' not found");
+    }
+
+    try {
+        // this internally calls AttributeMap::find and will throw if the attribute does not exist
+        (void)graph.nodeAttributes().find(supplyName);
+    } catch (const std::runtime_error &e) {
+        // catch the generic “No such attribute” and give our own
+        throw std::runtime_error(
+            "MinFlowShortestSuccessivePath: Provided node attribute '" + supplyName + "' not found");
+    }
+}
+
+
+}
