f7089dbc83ad22461a8655d7c97ae0a4be5710c1
Andreas Scharf <andreas.b.scharf@gmail.com>
2025-08-05 12:13:41 +0200

    Add files.

---
+++ include/networkit/flow/ShortestSuccessivePath.hpp
@@ -0,0 +1,19 @@
+/*  ShortestSuccessivePath.hpp
+*
+ *	Created on: 05.08.2025
+ *  Authors: Andreas Scharf (andreas.b.scharf@gmail.com)
+ *
+ */
+
+#ifndef SHORTESTSUCCESSIVEPATH_H
+#define SHORTESTSUCCESSIVEPATH_H
+
+namespace NetworKit {
+class MinFlowShortestSuccessivePath  {
+
+
+
+};
+
+}
+#endif //SHORTESTSUCCESSIVEPATH_H
---
+++ networkit/cpp/flow/ShortestSuccessivePath.cpp
@@ -0,0 +1,8 @@
+/*  ShortestSuccessivePath.hpp
+*
+ *	Created on: 05.08.2025
+ *  Authors: Andreas Scharf (andreas.b.scharf@gmail.com)
+ *
+ */
+
+#include <networkit/flow/ShortestSuccessivePath.hpp>
