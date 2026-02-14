f40c2acc1e31cba33cf54fbb60a3029280eb95e0
Andreas Scharf <andreas.b.scharf@gmail.com>
2026-02-14 13:01:57 +0100

    Fix format.

--- networkit/cpp/io/NetworkitBinaryReader.cpp
+++ networkit/cpp/io/NetworkitBinaryReader.cpp
@@ -51,8 +51,7 @@ Graph NetworkitBinaryReader::readData(const T &source) {
             version = 2;
         } else if (!memcmp("nkbg003", header.magic, 8)) {
             version = 3;
-        }
-        else if (!memcmp("nkbg004", header.magic, 8)) {
+        } else if (!memcmp("nkbg004", header.magic, 8)) {
             version = 4;
         } else {
             throw std::runtime_error("Reader expected another magic value");
