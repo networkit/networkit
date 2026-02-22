4a974ad256bc49c89347d5f64f50cad692226c4c
Andreas Scharf <andreas.b.scharf@gmail.com>
2026-02-22 21:48:08 +0100

    Add test files.

---
+++ networkit/cpp/simulation/test/EpidemicSimulationSEIRTest.cpp
@@ -0,0 +1,13 @@
+/*  EpidemicSimulationSEIRTest.cpp
+ *
+ *  Created on: 22.02.2026
+ *  Authors: Andreas Scharf (andreas.b.scharf@gmail.com)
+ *
+ */
+
+#include <networkit/simulation/EpidemicSimulationSEIR.hpp>
+
+namespace NetworKit {
+
+
+}
