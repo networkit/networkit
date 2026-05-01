ece684e5e70e32ae8a0461951ea327d60a00ef73
Andreas Scharf <andreas.b.scharf@gmail.com>
2026-05-01 12:40:30 +0200

    WIP.

--- networkit/cpp/edgescores/SimRankScore.cpp
+++ networkit/cpp/edgescores/SimRankScore.cpp
@@ -13,7 +13,7 @@ SimRankScore::SimRankScore(const Graph &G, double similarityPropagationFactor, c
     : EdgeScore<double>(G), similarityPropagationFactor(similarityPropagationFactor), maxIterations(maxIterations), tolerance(tolerance), iterations(0)
 {
     if (similarityPropagationFactor < 0.0 || similarityPropagationFactor > 1.0) {
-        throw std::invalid_argument("damping must be in the range [0,1]");
+        throw std::invalid_argument("similarityPropagationFactor must be in the range [0,1]");
     }
 
     if (maxIterations == 0) {
@@ -26,6 +26,31 @@ SimRankScore::SimRankScore(const Graph &G, double similarityPropagationFactor, c
 }
 
 void SimRankScore::run() {
+    if (!G->hasEdgeIds()) {
+        throw std::runtime_error("Graph must have edge IDs for SimRank calculation");
+    }
+
+    const index n = G->upperNodeIdBound();
+
+    if (n != 0 && n > std::numeric_limits<index>::max() / n) {
+        throw std::overflow_error("SimRankScore matrix size overflows");
+    }
+
+    const index matrixSize = n * n;
+
+    auto matrixIndex = [n](node u, node v) -> index {
+        return u * n + v;
+    };
+
+    std::vector<double> oldScore(matrixSize, 0.0);
+    std::vector<double> newScore(matrixSize, 0.0);
+
+    G->forEdges([&](node u) {
+       oldScore[matrixIndex(u, u)] = 1.0;
+    });
+    iterations = 0;
+
+
     hasFinished();
 }
 
