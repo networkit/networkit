eb659851a830daa4a6c7f822aa2c617417a0ff81
Andreas Scharf <andreas.b.scharf@gmail.com>
2026-05-08 16:19:03 +0200

    Add python API for SimRank.

--- networkit/test/test_sparsification.py
+++ networkit/test/test_sparsification.py
@@ -41,5 +41,95 @@ class TestSparsification(unittest.TestCase):
 			self.assertGreaterEqual(ratio, minExpectedRatio)
 			self.assertLessEqual(ratio, maxExpectedRatio)
 
+	def testSimRankScoreRejectsInvalidSimilarityPropagationFactor(self):
+		G = Graph(3, False, False)
+
+		with self.assertRaises(ValueError):
+			sparsification.SimRankScore(G, 1.1, 100, 1e-4)
+
+		with self.assertRaises(ValueError):
+			sparsification.SimRankScore(G, -0.1, 100, 1e-4)
+
+	def testSimRankScoreRejectsZeroMaxIterations(self):
+		G = Graph(3, False, False)
+
+		with self.assertRaises(ValueError):
+			sparsification.SimRankScore(G, 0.9, 0, 1e-4)
+
+	def testSimRankScoreRejectsNegativeTolerance(self):
+		G = Graph(3, False, False)
+
+		with self.assertRaises(ValueError):
+			sparsification.SimRankScore(G, 0.9, 100, -1e-4)
+
+	def testSimRankScoreRunRequiresIndexedEdges(self):
+		G = Graph(2, False, False)
+		G.addEdge(0, 1)
+
+		simrank = sparsification.SimRankScore(G)
+
+		with self.assertRaises(RuntimeError):
+			simrank.run()
+
+	def testSimRankScoreScoresUnavailableBeforeRun(self):
+		G = Graph(2, False, False)
+		G.addEdge(0, 1)
+		G.indexEdges()
+
+		simrank = sparsification.SimRankScore(G)
+
+		with self.assertRaises(RuntimeError):
+			simrank.scores()
+
+		with self.assertRaises(RuntimeError):
+			simrank.score(0)
+
+		with self.assertRaises(RuntimeError):
+			simrank.score(0, 1)
+
+	def testSimRankScoreRunAcceptsEmptyGraph(self):
+		G = Graph(0, False, False)
+		G.indexEdges()
+
+		simrank = sparsification.SimRankScore(G)
+		simrank.run()
+
+		self.assertEqual(simrank.scores(), [])
+
+	def testSimRankScoreProducesExpectedTriangleScores(self):
+		G = Graph(3, False, False)
+		G.addEdge(0, 1)
+		G.addEdge(1, 2)
+		G.addEdge(0, 2)
+		G.indexEdges()
+
+		simrank = sparsification.SimRankScore(G, 0.5, 100, 1e-12)
+		simrank.run()
+
+		scores = simrank.scores()
+
+		self.assertEqual(len(scores), G.upperEdgeIdBound())
+
+		for eid in range(G.upperEdgeIdBound()):
+			self.assertAlmostEqual(simrank.score(eid), scores[eid])
+
+		expected = 0.2
+
+		for u, v in [(0, 1), (1, 2), (0, 2)]:
+			eid = G.edgeId(u, v)
+			self.assertAlmostEqual(simrank.score(u, v), expected, delta=1e-10)
+			self.assertAlmostEqual(simrank.score(eid), expected, delta=1e-10)
+	def testSimRankScoreDirectedGraphUsesInNeighbors(self):
+		G = Graph(3, False, True)
+		G.addEdge(0, 1)
+		G.addEdge(0, 2)
+		G.addEdge(1, 2)
+		G.indexEdges()
+
+		simrank = sparsification.SimRankScore(G, 0.8, 1, 0.0)
+		simrank.run()
+
+		self.assertAlmostEqual(simrank.score(G.edgeId(1, 2)), 0.4, delta=1e-12)
+
 if __name__ == "__main__":
 	unittest.main()
