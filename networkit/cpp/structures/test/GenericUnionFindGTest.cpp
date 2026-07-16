82b420b6c07f1994ce26199d2274bd09c129cefc
Andreas Scharf <andreas.b.scharf@gmail.com>
2026-07-16 10:44:56 +0200

    Adapt tests for GenericUnionFind.

---
+++ networkit/cpp/structures/test/GenericUnionFindGTest.cpp
@@ -0,0 +1,121 @@
+/*
+ * GenericUnionFindGTest.cpp
+ *
+ *  Created on: 04.12.2013
+ *      Author: Maximilian Vogel (uocvf@student.kit.edu)
+ */
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <concepts>
+#include <cstdint>
+
+#include <networkit/structures/GenericUnionFind.hpp>
+
+namespace NetworKit {
+namespace {
+
+using ::testing::Eq;
+using ::testing::Ne;
+
+template <typename IndexType>
+class GenericUnionFindGTest : public testing::Test {};
+
+TYPED_TEST_SUITE_P(GenericUnionFindGTest);
+
+TYPED_TEST_P(GenericUnionFindGTest, testAllToSingletons) {
+    GenericUnionFind<TypeParam> unionFind(TypeParam{10});
+    unionFind.allToSingletons();
+
+    for (TypeParam i = 0; i < TypeParam{10}; ++i) {
+        EXPECT_THAT(unionFind.find(i),
+                    Ne(unionFind.find(static_cast<TypeParam>((i + TypeParam{1}) % TypeParam{10}))));
+    }
+}
+
+TYPED_TEST_P(GenericUnionFindGTest, testMergeSimple) {
+    GenericUnionFind<TypeParam> unionFind(TypeParam{10});
+
+    unionFind.merge(TypeParam{3}, TypeParam{8});
+
+    EXPECT_THAT(unionFind.find(TypeParam{8}), Eq(unionFind.find(TypeParam{3})));
+}
+
+TYPED_TEST_P(GenericUnionFindGTest, testMergeSubsets) {
+    GenericUnionFind<TypeParam> unionFind(TypeParam{10});
+    unionFind.allToSingletons();
+
+    unionFind.merge(TypeParam{0}, TypeParam{9});
+    unionFind.merge(TypeParam{1}, TypeParam{8});
+    unionFind.merge(TypeParam{2}, TypeParam{7});
+    unionFind.merge(TypeParam{0}, TypeParam{1});
+    unionFind.merge(TypeParam{1}, TypeParam{2});
+
+    EXPECT_THAT(unionFind.find(TypeParam{9}), Eq(unionFind.find(TypeParam{7})));
+}
+
+TYPED_TEST_P(GenericUnionFindGTest, testMergeCircular) {
+    GenericUnionFind<TypeParam> unionFind(TypeParam{16});
+
+    unionFind.merge(TypeParam{0}, TypeParam{4});
+    unionFind.merge(TypeParam{1}, TypeParam{5});
+    unionFind.merge(TypeParam{2}, TypeParam{6});
+    unionFind.merge(TypeParam{3}, TypeParam{7});
+    unionFind.merge(TypeParam{8}, TypeParam{12});
+    unionFind.merge(TypeParam{9}, TypeParam{13});
+    unionFind.merge(TypeParam{10}, TypeParam{14});
+    unionFind.merge(TypeParam{11}, TypeParam{15});
+
+    unionFind.merge(TypeParam{0}, TypeParam{8});
+    unionFind.merge(TypeParam{1}, TypeParam{9});
+    unionFind.merge(TypeParam{2}, TypeParam{10});
+    unionFind.merge(TypeParam{3}, TypeParam{11});
+    unionFind.merge(TypeParam{4}, TypeParam{12});
+    unionFind.merge(TypeParam{5}, TypeParam{13});
+    unionFind.merge(TypeParam{6}, TypeParam{14});
+    unionFind.merge(TypeParam{7}, TypeParam{15});
+
+    for (TypeParam i = 0; i < TypeParam{15}; ++i) {
+        EXPECT_THAT(unionFind.find(i),
+                    Eq(unionFind.find(static_cast<TypeParam>((i + TypeParam{4}) % TypeParam{16}))));
+        EXPECT_THAT(unionFind.find(i),
+                    Eq(unionFind.find(static_cast<TypeParam>((i + TypeParam{8}) % TypeParam{16}))));
+        EXPECT_THAT(unionFind.find(i), Eq(unionFind.find(static_cast<TypeParam>((i + TypeParam{12})
+                                                                                % TypeParam{16}))));
+        EXPECT_THAT(unionFind.find(i),
+                    Ne(unionFind.find(static_cast<TypeParam>((i + TypeParam{1}) % TypeParam{16}))));
+        EXPECT_THAT(unionFind.find(i),
+                    Ne(unionFind.find(static_cast<TypeParam>((i + TypeParam{2}) % TypeParam{16}))));
+        EXPECT_THAT(unionFind.find(i),
+                    Ne(unionFind.find(static_cast<TypeParam>((i + TypeParam{3}) % TypeParam{16}))));
+    }
+}
+
+TYPED_TEST_P(GenericUnionFindGTest, testToPartitionUsesTemplateIndexType) {
+    GenericUnionFind<TypeParam> unionFind(TypeParam{6});
+    unionFind.merge(TypeParam{0}, TypeParam{5});
+    unionFind.merge(TypeParam{1}, TypeParam{4});
+    unionFind.merge(TypeParam{0}, TypeParam{1});
+
+    const auto partition = unionFind.toPartition();
+
+    EXPECT_THAT(partition.inSameSubset(TypeParam{0}, TypeParam{4}), Eq(true));
+    EXPECT_THAT(partition.inSameSubset(TypeParam{5}, TypeParam{1}), Eq(true));
+    EXPECT_THAT(partition.inSameSubset(TypeParam{0}, TypeParam{2}), Eq(false));
+}
+
+REGISTER_TYPED_TEST_SUITE_P(GenericUnionFindGTest, testAllToSingletons, testMergeSimple,
+                            testMergeSubsets, testMergeCircular,
+                            testToPartitionUsesTemplateIndexType);
+
+using GenericUnionFindTestTypes =
+    ::testing::Types<index, std::uint32_t, std::uint16_t, std::int64_t, std::int32_t, std::int16_t>;
+
+INSTANTIATE_TYPED_TEST_SUITE_P(TestGenericUnionFind, GenericUnionFindGTest,
+                               GenericUnionFindTestTypes, );
+
+static_assert(std::same_as<UnionFind, GenericUnionFind<index>>);
+
+} // namespace
+} /* namespace NetworKit */
