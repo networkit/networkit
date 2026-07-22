35488c6eee6f44bc7eedf264bd7218e972bd055b
Andreas Scharf <andreas.b.scharf@gmail.com>
2026-07-22 15:51:27 +0200

    Remove deprecation message.

--- include/networkit/graph/Graph.hpp
+++ include/networkit/graph/Graph.hpp
@@ -1,9 +1,6 @@
 #ifndef NETWORKIT_GRAPH_GRAPH_HPP_
 #define NETWORKIT_GRAPH_GRAPH_HPP_
 
-#pragma message( \
-    "warning: Graph.hpp is deprecated. Include a graph implementation like AdjListGraph.hpp instead.")
-
 #include <networkit/graph/AdjListGraph.hpp>
 
 #endif // NETWORKIT_GRAPH_GRAPH_HPP_
